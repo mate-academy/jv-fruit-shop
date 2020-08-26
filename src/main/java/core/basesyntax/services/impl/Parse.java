f2c50b0d06ebddc61b032ef1d494b4290e5450c5
Laszlo Petechuk <frostpv@gmail.com>
2020-08-26 14:30:38 +0300

    Добавил предварительную реализацию метода, который парсит файл

--- src/main/java/core/basesyntax/services/impl/Parse.java
+++ src/main/java/core/basesyntax/services/impl/Parse.java
@@ -5,20 +5,28 @@ import core.basesyntax.services.FruitParse;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Parse implements FruitParse {
     @Override
     public List<List<String>> readFile(String path) {
+        List<List<String>> result = new ArrayList<>();
+        String line;
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            while ((line = bufferedReader.readLine()) != null) {
+                List<String> oneLine = Arrays.asList(line.split(","));
+                result.add(oneLine);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-        //@ TODO: 26.08.2020  здесь нужно распарсить файл в лист листов
-        return null;
+        return result;
     }
 
 
