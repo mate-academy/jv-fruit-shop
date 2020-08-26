62b55a4667b99aa2b896dd5d11c3cac589df6471
Laszlo Petechuk <frostpv@gmail.com>
2020-08-26 15:57:50 +0300

    Создал два интерфайса покупка и возврат

--- src/main/java/core/basesyntax/FruitShop.java
+++ src/main/java/core/basesyntax/FruitShop.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import core.basesyntax.model.Fruit;
 import core.basesyntax.services.FruitParse;
 import core.basesyntax.services.impl.Parse;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class FruitShop {
     private String path;
@@ -10,11 +14,19 @@ public class FruitShop {
         this.path = path;
     }
 
-    public void start(){
-        Storage  storage = new Storage();
+    public void start() {
+        Storage storage = new Storage();
         FruitParse fruitParse = new Parse();
 
+        for (List row : fruitParse.readFile(path)) {
+            if (row.get(0).equals("s")||row.get(0).equals("r")) {
+                System.out.println(row.get(0));
+                storage.getFruits().add(new Fruit(row.get(1).toString(), Integer.parseInt(row.get(2).toString()), LocalDate.parse(row.get(3).toString())));
+            }
 
-        System.out.println(fruitParse.readFile(path));
+
+        }
+
+        System.out.println(storage.getFruits());
     }
 }
---
+++ src/main/java/core/basesyntax/services/SupplyInterface.java
@@ -0,0 +1,4 @@
+package core.basesyntax.services;
+
+public interface SupplyInterface {
+}
