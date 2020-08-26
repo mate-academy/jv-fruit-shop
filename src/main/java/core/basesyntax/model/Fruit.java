dc30ec87fbf371dcc44a248f7a42bd7b6d49daa6
Laszlo Petechuk <frostpv@gmail.com>
2020-08-26 13:14:38 +0300

    Создал модель фрукта - сгенерировал методы иклс и хешкод, так как они мне могут понадобиться в дальнейшем. - Сгенерировал метод toString

---
+++ src/main/java/core/basesyntax/model/Fruit.java
@@ -0,0 +1,68 @@
+package core.basesyntax.model;
+
+import java.time.LocalDate;
+
+public class Fruit {
+    private String type;
+    private int stock_balance;
+    private LocalDate date;
+
+    public Fruit(String type, int stock_balance, LocalDate date) {
+        this.type = type;
+        this.stock_balance = stock_balance;
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getStock_balance() {
+        return stock_balance;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setStock_balance(int stock_balance) {
+        this.stock_balance = stock_balance;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Fruit fruit = (Fruit) o;
+
+        if (stock_balance != fruit.stock_balance) return false;
+        if (type != null ? !type.equals(fruit.type) : fruit.type != null) return false;
+        return date != null ? date.equals(fruit.date) : fruit.date == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type != null ? type.hashCode() : 0;
+        result = 31 * result + stock_balance;
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "type='" + type + '\'' +
+                ", stock_balance=" + stock_balance +
+                ", date=" + date +
+                '}';
+    }
+}
