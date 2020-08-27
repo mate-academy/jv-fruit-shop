bafa4132499e12915f13bf2bc61fe053a79436e7
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-08-27 15:33:11 +0300

    Add main

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -1,43 +1,42 @@
-//package app;
-//
-//import app.service.FileReadService;
-//import app.service.FileWriterService;
-//import app.service.Operation;
-//import app.service.impl.FileReadServiceImplements;
-//import app.service.impl.FileWriterServiceImplements;
-//import app.service.impl.OperationBuy;
-//import app.service.impl.OperationReturn;
-//import app.service.impl.OperationSupply;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//public class MainApp {
-//    public static final String FILE_PATH_READ = "src/test/resources/testOpen.csv";
-//    public static final String FILE_PATH_WRITE = "src/main/java/resources/result.csv";
-//
-//    public static void main(String[] args) {
-//        getStart();
-//    }
-//
-//    public static void fillMapOfOperators(Map<String, Operation> fruitOperations) {
-//        fruitOperations.put("s",new OperationSupply());
-//        fruitOperations.put("b", new OperationBuy());
-//        fruitOperations.put("r", new OperationReturn());
-//    }
-//
-//    public static void getStart() {
-//        Map<String, Operation> fruitOperations = new HashMap<>();
-//        fillMapOfOperators(fruitOperations);
-//        FruitOperationStrategy fruitOperationStrategy
-//        = new FruitOperationStrategy(fruitOperations);
-//        FileReadService fileReadService = new FileReadServiceImplements();
-//        List<List<String>> allData = fileReadService.readFile(FILE_PATH_READ);
-//        for (List<String> line : allData) {
-//            Operation operation = fruitOperationStrategy.getOperation(line);
-//            operation.doOperation(line);
-//        }
-//        FileWriterService fileWriterServiceImplements = new FileWriterServiceImplements();
-//        fileWriterServiceImplements.writeData(FruitStorage.fruits, FILE_PATH_WRITE);
-//    }
-//}
+package app;
+
+import app.service.FileReadService;
+import app.service.FileWriterService;
+import app.service.Operation;
+import app.service.impl.FileReadServiceImplements;
+import app.service.impl.FileWriterServiceImplements;
+import app.service.impl.OperationBuy;
+import app.service.impl.OperationReturn;
+import app.service.impl.OperationSupply;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainApp {
+    public static final String FILE_PATH_READ = "src/test/resources/testOpen.csv";
+    public static final String FILE_PATH_WRITE = "src/main/java/resources/result.csv";
+
+    public static void main(String[] args) {
+        getStart();
+    }
+
+    public static void fillMapOfOperators(Map<String, Operation> fruitOperations) {
+        fruitOperations.put("s",new OperationSupply());
+        fruitOperations.put("b", new OperationBuy());
+        fruitOperations.put("r", new OperationReturn());
+    }
+
+    public static void getStart() {
+        Map<String, Operation> fruitOperations = new HashMap<>();
+        fillMapOfOperators(fruitOperations);
+        FruitOperationStrategy fruitOperationStrategy = new FruitOperationStrategy(fruitOperations);
+        FileReadService fileReadService = new FileReadServiceImplements();
+        List<List<String>> allData = fileReadService.readFile(FILE_PATH_READ);
+        for (List<String> line : allData) {
+            Operation operation = fruitOperationStrategy.getOperation(line);
+            operation.doOperation(line);
+        }
+        FileWriterService fileWriterServiceImplements = new FileWriterServiceImplements();
+        fileWriterServiceImplements.writeData(FruitStorage.fruits, FILE_PATH_WRITE);
+    }
+}
