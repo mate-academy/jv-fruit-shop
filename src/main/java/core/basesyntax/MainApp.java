c8c216d89034062dee8dc6c81148b48307364e3b
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-08-27 13:52:16 +0300

    deleted few tests to check trevis

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -8,6 +8,19 @@ import java.util.Scanner;
  */
 public class MainApp {
     public static void main(String[] args) throws IOException {
+        StorageUpdater newUpdate = new StorageUpdaterImpl();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, input path to the file filled with new data: ");
+        String filePath = scanner.nextLine();
+        System.out.println("Please, input path for the report file based on new data: ");
+        String outPutFilePath = scanner.nextLine();
+
+        LocalFileReader reader = new LocalFileReader(filePath);
+        newUpdate.parseData(reader.readFromFile());
+        WriteToFile writer = new WriteToFile(outPutFilePath);
+        writer.csvFileWriter();
+
         System.out.println("All data has been successfully processed. File was written");
     }
 }
