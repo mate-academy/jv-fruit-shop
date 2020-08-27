e52cb238d363b5b8d5c396f519e98fcbd4e24d7c
Laszlo Petechuk <frostpv@gmail.com>
2020-08-27 22:20:07 +0300

    - Добавил смоук тесты

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -5,13 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Storage {
-    private List<Fruit> fruits = new ArrayList<>();
+    private final List<Fruit> fruits = new ArrayList<>();
 
     public List<Fruit> getFruits() {
         return fruits;
-    }
-
-    public void setFruits(List<Fruit> fruits) {
-        this.fruits = fruits;
     }
 }
--- src/test/java/core/basesyntax/FruitShopTest.java
+++ src/test/java/core/basesyntax/FruitShopTest.java
@@ -1,28 +1,62 @@
 package core.basesyntax;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FruitShopTest {
+    private static final String NORMAL_FILE = "src/test/resources/file.csv";
     private static final String WRONG_DATE_FILE = "src/test/resources/wrongDateFile.csv";
-    private static final String WRONG_STOCK = "src/test/resources/wrongStockFile.csv";
+    private static final String WRONG_STOCK_FILE = "src/test/resources/wrongStockFile.csv";
+    private static final String EMPTY_FILE = "src/test/resources/empty.csv";
+    private static final String STOCK = "src/main/resources/stock.csv";
+    private static final String STOCK_NORMAL = "src/test/resources/normal.csv";
 
     @Test
-    public void normalFileTest(){
+    public void normalFileTest() {
+        FruitShop fruitShop = new FruitShop(NORMAL_FILE);
+        try {
+            fruitShop.start();
+            BufferedReader shopQuantityFile = new BufferedReader(new FileReader(STOCK));
+            BufferedReader preparedFile = new BufferedReader(new FileReader(STOCK_NORMAL));
+            Assert.assertEquals("Файлы не равны", shopQuantityFile.read(), preparedFile.read());
+        } catch (IOException e) {
+            throw new RuntimeException("Нет доступа к тестовым файлам");
+        }
 
     }
 
-    @Test (expected = RuntimeException.class)
-    public void fileWhichWrongDate(){
+    @Test(expected = RuntimeException.class)
+    public void fileWhichWrongDate() {
         FruitShop fruitShop = new FruitShop(WRONG_DATE_FILE);
         fruitShop.start();
     }
 
-    @Test (expected = RuntimeException.class)
-    public void fileWhichWrongStock(){
-        FruitShop fruitShop = new FruitShop(WRONG_STOCK);
+    @Test(expected = RuntimeException.class)
+    public void fileWhichWrongStock() {
+        FruitShop fruitShop = new FruitShop(WRONG_STOCK_FILE);
         fruitShop.start();
     }
 
+    @Test(expected = RuntimeException.class)
+    public void fileIsNotFoundTest() {
+        FruitShop fruitShop = new FruitShop("bla-bla.xx.f");
+        fruitShop.start();
+    }
+
+    @Test
+    public void fileIsEmptyTest() {
+        FruitShop fruitShop = new FruitShop(EMPTY_FILE);
+        try {
+            fruitShop.start();
+            BufferedReader shopQuantityFile = new BufferedReader(new FileReader(STOCK));
+            BufferedReader preparedFile = new BufferedReader(new FileReader(STOCK_NORMAL));
+            Assert.assertEquals(shopQuantityFile.read(), preparedFile.read());
+        } catch (IOException e) {
+            throw new RuntimeException("Нет доступа к тестовым файлам");
+        }
+    }
 }
