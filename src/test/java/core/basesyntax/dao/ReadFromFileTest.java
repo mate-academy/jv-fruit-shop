f439e83606064128eb9a2ad13f382ba8d26beb1f
Nokafan <volodymy.stepura@gmail.com>
2020-08-27 20:06:51 +0300

    Added ReadFromFileTest

--- src/test/java/core/basesyntax/dao/ReadFromFileTest.java
+++ src/test/java/core/basesyntax/dao/ReadFromFileTest.java
@@ -4,31 +4,42 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadFromFileTest {
-    private String fileDest = "src\\test\\resources\\ReadFromFile.csv";
-    private static final String TEST_STRING = "1 2 3 4 5 6 a b c D F G ! # $ % ^ ^";
+    private static final String FILE_DEST_WRONG = "src/test/resources/WrongPath.csv";
+    private static final String FILE_DEST_TEST_1 = "src/test/resources/Test_1.csv";
+    private static final String FILE_DEST_TEST_2 = "src/test/resources/Test_2.csv";
+    private static final String FIRST_STRING = "banana";
+    private static final String SECOND_STRING = "90";
+    private static final List<String[]> list = new ArrayList<>();
 
     @Before
     public void setUp() {
-        WriteToFile write = new WriteToFile();
-        write.writeToFile(fileDest);
+        String[] testStringArray = {FIRST_STRING, SECOND_STRING};
+        list.add(testStringArray);
+    }
 
+    @Test(expected = RuntimeException.class)
+    public void fileReaderCreateExeption() {
+        ReadFromFile newReader = new ReadFromFile();
+        newReader.readFromFile(FILE_DEST_WRONG);
+    }
+
+    @Test
+    public void fileExist() {
+        ReadFromFile newReader = new ReadFromFile();
+        newReader.readFromFile(FILE_DEST_TEST_1);
+        Assert.assertTrue(Files.exists(Paths.get(FILE_DEST_TEST_1)));
     }
 
     @Test
     public void readFromFileOk() {
-        Path path = Paths.get(fileDest);
-        String read = null;
-        try {
-            read = Files.readAllLines(path).get(0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Assert.assertEquals(TEST_STRING, read);
+        ReadFromFile newReader = new ReadFromFile();
+        List<String[]> testListReader = newReader.readFromFile(FILE_DEST_TEST_2);
+        Assert.assertArrayEquals(list.get(0), testListReader.get(0));
     }
 }
