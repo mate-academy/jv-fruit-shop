2932ce7cd2a18e170511674abf08445eee82ff62
Dm_Grt <grytsishin@gmail.com>
2020-08-27 11:06:15 +0300

    added few tests

--- src/test/java/core/basesyntax/operations/PerformStrategyTest.java
+++ src/test/java/core/basesyntax/operations/PerformStrategyTest.java
@@ -9,6 +9,9 @@ public class PerformStrategyTest {
     private static final String B_TYPE = "b";
     private static final String R_TYPE = "r";
     private static final String WRONG_TYPE = "a";
+    private static final String WRONG_TYPE_EMPTY = "";
+    private static final String WRONG_TYPE_NULL = null;
+    private static final String WRONG_TYPE_SOME = "abc";
 
     @Test
     public void getSTypeStrategy() {
@@ -35,6 +38,16 @@ public class PerformStrategyTest {
 
     @Test(expected = NoSuchOperation.class)
     public void getEmptyStrategy() {
-        PerformStrategy.getStrategy("");
+        PerformStrategy.getStrategy(WRONG_TYPE_EMPTY);
+    }
+
+    @Test(expected = NoSuchOperation.class)
+    public void getWrongTypeNullStrategy() {
+        PerformStrategy.getStrategy(WRONG_TYPE_NULL);
+    }
+
+    @Test(expected = NoSuchOperation.class)
+    public void getWrongTypeStrategySomeChars() {
+        PerformStrategy.getStrategy(WRONG_TYPE_SOME);
     }
 }
