a1811bf55ca83af35a7c2adacd58efac4ddef37e
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-28 16:07:12 +0300

    removed Hello World

--- src/main/java/core/storageParser/FileServiceImpl.java
+++ src/main/java/core/storageParser/FileServiceImpl.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,26 +15,31 @@ public class FileServiceImpl implements FileService {
     private String fileName;
     private String extension;
     private String topLine;
+    private String fullPath;
 
     public FileServiceImpl() {
-        this.path = "";
+        this.path = "src\\test\\resources\\";
         this.extension = ".csv";
         this.fileName = "FileOut";
         this.topLine = "fruit,quantity";
+        this.fullPath = path + fileName + extension;
     }
 
     public FileServiceImpl(String path, String extension, String fileName) {
         this.path = path;
         this.extension = extension;
         this.fileName = fileName;
+        this.fullPath = path + fileName + extension;
     }
 
     public void setExtension(String extension) {
         this.extension = extension;
+        refreshPath();
     }
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+        refreshPath();
     }
 
     public String getPath() {
@@ -40,12 +48,13 @@ public class FileServiceImpl implements FileService {
 
     public void setPath(String path) {
         this.path = path;
+        refreshPath();
     }
 
     public List<String> readFile() {
         List<String> lines = new ArrayList<>();
         String line = "";
-        try (BufferedReader br = new BufferedReader(new FileReader(path+fileName+extension))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fullPath))) {
             while ((line = br.readLine()) != null) {
                 lines.add(line);
             }
@@ -58,14 +67,25 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public boolean writeFile(List<String> text) {
-        try (FileWriter fw = new FileWriter(path+fileName+extension)) {
+        Path path = Paths.get(fullPath);
+        try {
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (FileWriter fw = new FileWriter(path + fileName + extension)) {
             fw.write(topLine);
-            for(String line:text){
+            for (String line : text) {
                 fw.write(line + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return true;
+    }
+
+    private void refreshPath() {
+        fullPath = path + fileName + extension;
     }
 }
