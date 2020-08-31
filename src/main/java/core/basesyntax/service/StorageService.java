6b2220908c03a4b3be5cc2d829e11a4985ced869
Jah <moskovienkor@gmail.com>
2020-08-31 16:24:10 +0300

    hw20: - fix after review;       - StorageService modified;

--- src/main/java/core/basesyntax/service/StorageService.java
+++ src/main/java/core/basesyntax/service/StorageService.java
@@ -2,36 +2,26 @@ package core.basesyntax.service;
 
 import core.basesyntax.model.FruitBox;
 import core.basesyntax.model.Storage;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class StorageService<T> {
-    private static final String BANANA_FRUIT_TYPE = "banana";
-    private List<String> fruits = new ArrayList<>();
+    private StringBuilder fruitsString = new StringBuilder();
+    private Map<String, Integer> storageContent = new LinkedHashMap<>();
 
     public String getStorage() {
-        fruits.add(BANANA_FRUIT_TYPE);
-        Map<String, Integer> storageContent = new LinkedHashMap<>();
-        mark: for (FruitBox fruitBox : Storage.storage) {
-            switch (fruitBox.getName()) {
-                case BANANA_FRUIT_TYPE:
-                    if (storageContent.containsKey(BANANA_FRUIT_TYPE)) {
-                        storageContent.replace(BANANA_FRUIT_TYPE,
-                                storageContent.get(BANANA_FRUIT_TYPE) + fruitBox.getAmount());
-                    }
-                    storageContent.put(BANANA_FRUIT_TYPE, fruitBox.getAmount());
-                    continue mark;
-                default:
-                    throw new RuntimeException("Unknown fruit");
+        for (FruitBox fruitBox : Storage.storage) {
+            if (storageContent.containsKey(fruitBox.getName())) {
+                storageContent.replace(fruitBox.getName(),
+                        storageContent.get(fruitBox.getName()) + fruitBox.getAmount());
+            } else {
+                storageContent.put(fruitBox.getName(), fruitBox.getAmount());
             }
         }
-        StringBuilder fruitsString = new StringBuilder();
-        for (int i = 0; i < storageContent.size(); i++) {
-            fruitsString.append(fruits.get(i))
+        for (String key : storageContent.keySet()) {
+            fruitsString.append(key)
                     .append(",")
-                    .append(storageContent.get(fruits.get(i)))
+                    .append(storageContent.get(key))
                     .append("\n");
         }
         return fruitsString.toString();
