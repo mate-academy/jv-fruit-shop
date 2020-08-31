a230d576ac134f8fd8438b7037de6ce418e95b84
vladd <vladdyachkin11@gmail.com>
2020-08-31 08:55:46 +0300

    delete class FruitStorageService

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import core.basesyntax.fileservice.FileReader;
 import core.basesyntax.fileservice.FileWriter;
 import core.basesyntax.fileservice.TransactionParser;
-import core.basesyntax.fruitservice.FruitStorageService;
+import core.basesyntax.fruitservice.FruitStorage;
 import core.basesyntax.operationstrategy.OperationStrategy;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +13,11 @@ public class Main {
         FileReader readFile = new FileReader();
         TransactionParser parser = new TransactionParser();
         FileWriter writer = new FileWriter();
-        FruitStorageService fruitStorageService = new FruitStorageService();
+        FruitStorage fruitStorage = new FruitStorage();
         OperationStrategy operationStrategy = new OperationStrategy();
         Map<String, Integer> fruitOutput = new HashMap<>();
-        fruitStorageService.addAllToStorage(parser.parseDate(readFile.read("input.csv")));
-        operationStrategy.operation(fruitOutput, fruitStorageService.getStorage());
+        fruitStorage.getStorage().addAll(parser.parseDate(readFile.read("input.csv")));
+        operationStrategy.operation(fruitOutput, fruitStorage.getStorage());
         writer.writeToFile(fruitOutput, "output.csv");
     }
 }
