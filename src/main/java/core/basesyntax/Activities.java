b428b24a1b1edd5aa5db70be875ba1174463aa53
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-30 14:33:12 +0200

    added Warehouse class. added base method

--- src/main/java/core/basesyntax/Activities.java
+++ src/main/java/core/basesyntax/Activities.java
@@ -1,4 +1,8 @@
 package core.basesyntax;
 
 public enum Activities {
+    B,
+    S,
+    P,
+    R;
 }
---
+++ src/main/java/core/basesyntax/Product.java
@@ -0,0 +1,6 @@
+package core.basesyntax;
+
+public enum Product {
+    BANANA,
+    APPLE;
+}
