9e11bc77f36d1db2e9139e4b8fe7b81b23d6bef6
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-31 22:02:11 +0200

    deleted interface StoreService

--- src/main/java/core/basesyntax/service/StoreService.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service;
-
-import core.basesyntax.model.Fruit;
-import java.util.List;
-
-public interface StoreService {
-    List<Fruit> setFruits(FruitService fruitService);
-}
