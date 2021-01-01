9c2ac3b666f4be59c740da18ab768033821abe7a
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-01-01 16:29:20 +0200

    constants was added

--- src/main/java/core/basesyntax/report/ReportFormatterImpl.java
+++ src/main/java/core/basesyntax/report/ReportFormatterImpl.java
@@ -11,6 +11,10 @@ import java.util.List;
 import java.util.Map;
 
 public class ReportFormatterImpl implements ReportFormatter {
+    private static final String SPLITTER = ",";
+    private static final int OPERATION = 0;
+    private static final int FRUIT = 1;
+    private static final int AMOUNT = 2;
     private final Fruits fruits;
     private final OperationStrategy operationStrategy;
 
@@ -23,12 +27,12 @@ public class ReportFormatterImpl implements ReportFormatter {
     public String createReport(DataReader dataReader) {
         List<String> list = dataReader.readData();
         for (String row: list) {
-            String[] data = row.split(",");
-            if (Integer.parseInt(data[2]) < 0 || !Operations.contains(data[0])) {
+            String[] data = row.split(SPLITTER);
+            if (Integer.parseInt(data[AMOUNT]) < 0 || !Operations.contains(data[OPERATION])) {
                 throw new RuntimeException("Incorrect data");
             }
-            operationStrategy.get(Operations.valueOf(data[0].toUpperCase()))
-                    .doOperation(fruits, data[1], Integer.parseInt(data[2]));
+            operationStrategy.get(Operations.valueOf(data[OPERATION].toUpperCase()))
+                    .doOperation(fruits, data[FRUIT], Integer.parseInt(data[AMOUNT]));
 
         }
         StringBuilder report = new StringBuilder("fruit,quantity");
