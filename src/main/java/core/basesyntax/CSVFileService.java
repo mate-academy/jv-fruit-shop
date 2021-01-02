aa63a99d6cce0fda2b9452c368e29fdbdf829994
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-02 14:43:44 +0200

    add FileService class with methods readFromFile and writeToFile

---
+++ src/main/java/core/basesyntax/CSVFileService.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.List;
+
+public class CSVFileService implements FileService {
+    @Override
+    public List<String[]> readFromFile(String filePath) {
+        return null;
+    }
+
+    @Override
+    public void writeToFile(String filePath) {
+
+    }
+}
