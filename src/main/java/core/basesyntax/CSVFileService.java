b6540cfc1334d5bfcfc592bca8787b8b279ab345
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-02 15:30:03 +0200

    add dependency in the pom.xml for opencsv

--- src/main/java/core/basesyntax/CSVFileService.java
+++ src/main/java/core/basesyntax/CSVFileService.java
@@ -1,11 +1,25 @@
 package core.basesyntax;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CSVFileService implements FileService {
     @Override
     public List<String[]> readFromFile(String filePath) {
-
+        List<String[]> list;
+        try (CSVReader csvReader = new CSVReader(new FileReader(filePath))) {
+            list = csvReader.readAll();
+        } catch (IOException | CsvException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+        return list;
     }
 
     @Override
