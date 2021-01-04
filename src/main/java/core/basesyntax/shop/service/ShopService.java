7aeecc2f700043ebe2473fbe0b0b83f499a9bbf1
Vadim <Vedroidua@gmail.com>
2021-01-04 16:04:40 +0200

    Added ShopService

---
+++ src/main/java/core/basesyntax/shop/service/ShopService.java
@@ -0,0 +1,11 @@
+package core.basesyntax.shop.service;
+
+import core.basesyntax.shop.model.Fruit;
+import core.basesyntax.shop.model.TransactionDto;
+import java.util.List;
+import java.util.Map;
+
+public interface ShopService {
+    void applyOperationOnFruitsDto(List<TransactionDto> transactionDtos);
+    Map<Fruit, Integer> getFruitReport();
+}
