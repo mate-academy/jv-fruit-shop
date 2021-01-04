d1cd24d5ece598cfc385e2da1147f0accac55d5a
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-04 22:32:20 +0200

    minor changes in methods couse of db

--- src/main/java/core/basesyntax/dao/WarehouseImpl.java
+++ src/main/java/core/basesyntax/dao/WarehouseImpl.java
@@ -1,41 +1,39 @@
 package core.basesyntax.dao;
 
-import java.util.HashMap;
+import core.basesyntax.db.DataBase;
 import java.util.Map;
 
 public class WarehouseImpl implements Warehouse {
-    private final Map<String, Integer> listItems = new HashMap<>();
 
     @Override
     public void addItem(String fruit, int amount) {
-        if (listItems.containsKey(fruit)) {
-            listItems.replace(fruit, (amount + listItems.get(fruit)));
+        if (DataBase.getListItems().containsKey(fruit)) {
+            DataBase.getListItems().replace(fruit, (amount + DataBase.getListItems().get(fruit)));
         } else {
-            listItems.put(fruit, amount);
+            DataBase.getListItems().put(fruit, amount);
         }
     }
 
     @Override
     public void replace(String fruit, int amount) {
-        if (listItems.get(fruit) == null) {
-            listItems.put(fruit, amount);
+        if (DataBase.getListItems().get(fruit) == null) {
+            DataBase.getListItems().put(fruit, amount);
         } else {
-            listItems.replace(fruit, (amount));
+            DataBase.getListItems().replace(fruit, (amount));
         }
     }
 
-    @Override
     public Map<String, Integer> getListItems() {
-        return listItems;
+        return DataBase.getListItems();
     }
 
     @Override
     public int getAmountOfItem(String fruit) {
-        return listItems.getOrDefault(fruit, 0);
+        return DataBase.getListItems().getOrDefault(fruit, 0);
     }
 
     @Override
     public void getItemFrom(String fruit, int amount) {
-        listItems.replace(fruit, (listItems.get(fruit) - amount));
+        DataBase.getListItems().replace(fruit, (DataBase.getListItems().get(fruit) - amount));
     }
 }
