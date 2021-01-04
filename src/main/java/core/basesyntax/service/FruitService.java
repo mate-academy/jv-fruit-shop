120660799b227739ee4ac85aead2a8f5a96ed724
ivanluchkin <id.luchkin@gmail.com>
2021-01-04 15:02:01 +0200

    Fixed operation not being executed

--- src/main/java/core/basesyntax/service/FruitService.java
+++ src/main/java/core/basesyntax/service/FruitService.java
@@ -1,11 +1,9 @@
 package core.basesyntax.service;
 
 import core.basesyntax.model.AbstractOperation;
-import core.basesyntax.model.Operation;
 import core.basesyntax.model.OperationFactory;
 import core.basesyntax.model.OperationSet;
 import core.basesyntax.model.entities.Fruit;
-import core.basesyntax.model.entities.Product;
 import core.basesyntax.service.io.DataImporter;
 import core.basesyntax.service.io.ReportWriter;
 
@@ -23,9 +21,10 @@ public class FruitService extends AbstractService<Fruit> {
         Validator<Fruit> validator = new Validator<>(warehouse);
         for (String[] row : data) {
             AbstractOperation<Fruit> operation = operationFactory.get(OperationSet.valueOf(row[0].toUpperCase()));
-            Product fruit = new Fruit(row[1]);
+            Fruit fruit = new Fruit(row[1]);
             Integer amount = Integer.valueOf(row[2]);
             validator.validateOperation(operation, fruit, amount);
+            operation.execute(fruit, amount);
         }
     }
 
