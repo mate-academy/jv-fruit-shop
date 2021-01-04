dc74da6b3a2f728638ea82a6abcc4e8774af00e6
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-04 12:56:15 +0200

    more refactoring

--- src/test/java/core/basesyntax/shopimpl/storage/StorageDaoTest.java
+++ src/test/java/core/basesyntax/shopimpl/storage/StorageDaoTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class StorageDaoTest {
     private FruitShopDao testFruitShopDao;
     
