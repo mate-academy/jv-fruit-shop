25c472a758dda166fa98a99339ca489eda920c99
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-01-05 17:35:20 +0200

    ff

--- src/main/java/core/basesyntax/service/Filereader.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax.service;
-
-import core.basesyntax.model.TransactionDto;
-
-import java.util.List;
-
-public interface Filereader {
-    List<TransactionDto> readFile(String fileName);
-}
