5b648b1a561d70e39bb747c0768fc30a5bc4ddc9
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-01-05 10:58:35 +0200

    add files to database

---
+++ src/main/java/core/basesyntax/strategy/AdditionStrategy.java
@@ -0,0 +1,17 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.TransactionDto;
+
+public class AdditionStrategy implements OperationStrategy{
+    @Override
+    public void apply(TransactionDto transactionDto) {
+        if (!Storage.fruits.containsKey(transactionDto.getFruit())) {
+            Storage.fruits.put(transactionDto.getFruit(), transactionDto.getQuantity());
+        } else {
+            Integer oldValue = Storage.fruits.get(transactionDto.getFruit());
+            Storage.fruits.put(transactionDto.getFruit(), oldValue + transactionDto.getQuantity());
+        }
+    }
+}
