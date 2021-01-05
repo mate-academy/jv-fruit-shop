d9d5da05f184006ad0366caf35130e00cfc5ff1d
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-01-05 11:46:05 +0200

    our storage with map

---
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -0,0 +1,9 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.Fruit;
+
+import java.util.*;
+
+public class Storage {
+    public static Map<Fruit, Integer> fruits = new HashMap<>();
+}
