ca0c5736c75453ea08f3fc0d44ed2936daba111d
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-01-05 10:46:03 +0200

    tests for validator was added

---
+++ src/test/java/core/basesyntax/workwithfiles/impl/DataValidatorImplTest.java
@@ -0,0 +1,29 @@
+package core.basesyntax.workwithfiles.impl;
+
+import static org.junit.Assert.assertTrue;
+
+import core.basesyntax.workwithfiles.DataValidator;
+import java.util.List;
+import org.junit.Test;
+
+public class DataValidatorImplTest {
+    @Test
+    public void testCorrectInput() {
+        List<String> list1 = List.of("P,Apple,22","S,Banana,10");
+        List<String> list2 = List.of("r,tomato,56","b,Banana,70");
+        DataValidator validator = new DataValidatorImpl();
+        assertTrue(validator.validate(list1));
+        assertTrue(validator.validate(list2));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testIncorrectInput() {
+        List<String> list1 = List.of(",Apple,22","S,Banana,10");
+        List<String> list2 = List.of("r,tomato,56","b,Banana,-70");
+        List<String> list3 = List.of("F,tomato,56","b,Banana,70");
+        DataValidator validator = new DataValidatorImpl();
+        assertTrue(validator.validate(list1));
+        assertTrue(validator.validate(list2));
+        assertTrue(validator.validate(list3));
+    }
+}
