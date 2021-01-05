7748166336ea954350c4292005fe4d5b802f9b9e
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-01-05 17:32:36 +0200

    FruitService Interface Impl

---
+++ src/main/java/core/basesyntax/service/impl/FruitServiceImpl.java
@@ -0,0 +1,32 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.Operation;
+import core.basesyntax.model.TransactionDto;
+import core.basesyntax.service.FruitService;
+import core.basesyntax.strategy.OperationStrategy;
+
+import java.util.List;
+import java.util.Map;
+
+public class FruitServiceImpl implements FruitService {
+    private final Map<Operation, OperationStrategy> operationStrategyMap;
+
+    public FruitServiceImpl(Map<Operation, OperationStrategy> operationStrategyMap) {
+        this.operationStrategyMap = operationStrategyMap;
+    }
+
+    @Override
+    public void applyOperationoOnFruitsDto(List<TransactionDto> transactionDtos) {
+        for (TransactionDto dto: transactionDtos) {
+            Operation operation = dto.getOperation();
+            operationStrategyMap.get(operation).apply(dto);
+        }
+    }
+
+    @Override
+    public Map<Fruit, Integer> getFruitsReport() {
+        return Storage.getFruits();
+    }
+}
