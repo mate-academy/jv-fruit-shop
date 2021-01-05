48a974211bc22893e59264e482d624b8da5952a6
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2021-01-05 14:42:15 +0200

    added constants in CsvFileWriter class

--- src/main/java/core/basesyntax/service/impl/CsvFileWriterImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriterImpl.java
@@ -6,19 +6,24 @@ import java.io.PrintWriter;
 import java.util.Map;
 
 public class CsvFileWriterImpl implements FileWriter {
+    public static String FRUIT = "fruit";
+    public static String QUANTITY = "quantity";
+    public static String COMMA = ",";
+    public static String LINE_SEPARATOR = "\n";
+
     @Override
     public void writeDataInFile(Map<String, Long> report, String fileName) {
         try (PrintWriter writer = new PrintWriter(fileName)) {
             StringBuilder sb = new StringBuilder();
-            sb.append("fruit");
-            sb.append(",");
-            sb.append("quantity");
-            sb.append("\n");
+            sb.append(FRUIT);
+            sb.append(COMMA);
+            sb.append(QUANTITY);
+            sb.append(LINE_SEPARATOR);
             for (Map.Entry<String, Long> entry : report.entrySet()) {
                 sb.append(entry.getKey())
-                        .append(",")
+                        .append(COMMA)
                         .append(entry.getValue())
-                        .append("\n");
+                        .append(LINE_SEPARATOR);
             }
             writer.write(sb.toString());
         } catch (FileNotFoundException e) {
