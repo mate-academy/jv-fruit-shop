41a840aaf96a37a93dbfb9de0335df8e5d9ad146
Resci <vIsUAl123>
2021-01-06 17:31:41 +0200

    Introduce const for titles

--- src/main/java/core/basesyntax/service/impl/CsvFileWriter.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriter.java
@@ -6,11 +6,12 @@ import java.io.Writer;
 import java.util.Map;
 
 public class CsvFileWriter implements FileWriter {
+    private static final String COLUMN_TITLES = "fruit,amount";
 
     @Override
     public void writeReportToFile(Map<String, Long> report, String pathToFile) {
         try (Writer writer = new java.io.FileWriter(pathToFile)) {
-            writer.append("fruit,amount").append(System.lineSeparator());
+            writer.append(COLUMN_TITLES).append(System.lineSeparator());
             for (Map.Entry<String, Long> entry : report.entrySet()) {
                 writer.append(entry.getKey())
                         .append(',')
