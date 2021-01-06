064795334ddff9b8188e50fe8d07e7ba74d1e23a
AndreiYazadzhi <belator251000@gmail.com>
2021-01-06 13:10:53 +0200

    Variables fix

--- src/main/java/core/basesyntax/service/impl/ReportCreatorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportCreatorImpl.java
@@ -13,6 +13,7 @@ public class ReportCreatorImpl implements ReportCreator {
     private static final int OPERATION = 0;
     private static final int FRUIT = 1;
     private static final int AMOUNT = 2;
+    private static final int FIELDS_QUANTITY = 3;
     private final Storage storage;
     private final OperationStrategy operationStrategy;
 
@@ -26,7 +27,7 @@ public class ReportCreatorImpl implements ReportCreator {
         List<String> data = file.read();
         for (String row: data) {
             String[] lineData = row.split(SPLITTER);
-            if (lineData.length < 3
+            if (lineData.length < FIELDS_QUANTITY
                     || Integer.parseInt(lineData[AMOUNT]) < 0
                     || !Operations.contains(lineData[OPERATION])) {
                 throw new RuntimeException("Incorrect data");
@@ -38,7 +39,7 @@ public class ReportCreatorImpl implements ReportCreator {
         StringBuilder report = new StringBuilder("fruit,quantity");
         for (Map.Entry<Fruit, Integer> entry: storage.getStorage().entrySet()) {
             report.append(System.lineSeparator())
-                    .append(entry.getKey().getName()).append(",").append(entry.getValue());
+                    .append(entry.getKey().getName()).append(SPLITTER).append(entry.getValue());
         }
         return report.toString();
     }
