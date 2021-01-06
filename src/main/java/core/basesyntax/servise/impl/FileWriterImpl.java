909daee5d8b5a79774f7ac4fae83e61a48fdb5d5
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2021-01-06 16:30:35 +0200

    Fixed: added constant

--- src/main/java/core/basesyntax/servise/impl/FileWriterImpl.java
+++ src/main/java/core/basesyntax/servise/impl/FileWriterImpl.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FileWriterImpl implements FileWriter {
+    private static final String HEADER = "fruit, quantity";
 
     @Override
     public void createReport(Map<Fruit, Integer> map, String filePath) {
@@ -19,7 +20,7 @@ public class FileWriterImpl implements FileWriter {
                 .map(e -> e.getKey() + "," + e.getValue())
                 .collect(Collectors.toList());
         try {
-            Files.writeString(file, "fruit, quantity" + System.lineSeparator());
+            Files.writeString(file, HEADER + System.lineSeparator());
             Files.write(file, report, StandardOpenOption.APPEND);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
