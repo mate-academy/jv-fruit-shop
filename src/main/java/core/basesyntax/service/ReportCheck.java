01f174f699315d0afaf991f77e7c64f18d0e85e1
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-06 15:20:30 +0200

    changed methods readFromReport

--- src/main/java/core/basesyntax/service/ReportCheck.java
+++ src/main/java/core/basesyntax/service/ReportCheck.java
@@ -1,10 +1,8 @@
 package core.basesyntax.service;
 
-import core.basesyntax.strategy.OperationStrategy;
-import java.util.Map;
-
 public interface ReportCheck {
-    void readFromReport(String filName, Map<Procedure, OperationStrategy> operationStrategyMap);
+
+    void readFromReport(String filName);
 
     String writeToReport();
 }
