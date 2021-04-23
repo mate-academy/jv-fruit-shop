943ea4b85dec5299bf0bf3df99910a3273bd7078
Nikber5 <nikber198@gmail.com>
2021-04-23 22:22:12 +0300

    Removed objects creation from FruitShopServiceImpl to main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,8 +8,12 @@ import core.basesyntax.operations.Operations;
 import core.basesyntax.operations.PurchaseOperation;
 import core.basesyntax.operations.ReturnOperation;
 import core.basesyntax.operations.SupplyOperation;
+import core.basesyntax.service.CsvReaderServiceImpl;
+import core.basesyntax.service.CsvWriterServiceImpl;
 import core.basesyntax.service.FruitShopService;
 import core.basesyntax.service.FruitShopServiceImpl;
+import core.basesyntax.service.ReaderService;
+import core.basesyntax.service.WriterService;
 import core.basesyntax.strategy.OperationsStrategy;
 import core.basesyntax.strategy.OperationsStrategyImpl;
 import java.util.HashMap;
@@ -28,9 +32,11 @@ public class Main {
 
         OperationsStrategy operationsStrategy = new OperationsStrategyImpl(operationMap);
         ProductDao productDao = new ProductDaoImpl();
+        ReaderService readerService = new CsvReaderServiceImpl();
+        WriterService writerService = new CsvWriterServiceImpl();
 
         FruitShopService fruitShopService = new FruitShopServiceImpl(productDao,
-                operationsStrategy);
+                operationsStrategy, readerService, writerService);
         fruitShopService.createReport(FILE_PATH_FROM, FILE_PATH_TO);
     }
 }
--- src/main/java/core/basesyntax/service/FruitShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/FruitShopServiceImpl.java
@@ -7,22 +7,25 @@ import java.util.List;
 public class FruitShopServiceImpl implements FruitShopService {
     private ProductDao productDao;
     private OperationsStrategy operationsStrategy;
+    private ReaderService readerService;
+    private WriterService writerService;
 
-    public FruitShopServiceImpl(ProductDao productDao, OperationsStrategy operationsStrategy) {
+    public FruitShopServiceImpl(ProductDao productDao, OperationsStrategy operationsStrategy,
+                                ReaderService readerService, WriterService writerService) {
         this.productDao = productDao;
         this.operationsStrategy = operationsStrategy;
+        this.readerService = readerService;
+        this.writerService = writerService;
     }
 
     @Override
     public void createReport(String filePathFrom, String filePathTo) {
-        ReaderService readerService = new CsvReaderServiceImpl();
         List<String> data = readerService.readFromFile(filePathFrom);
 
         ProductsService productsService = new ProductsServiceImpl(operationsStrategy, productDao);
         productsService.addToStorage(data);
         List<String> report = productsService.getFromStorage();
 
-        WriterService writerService = new CsvWriterServiceImpl();
         writerService.writeToFile(report, filePathTo);
     }
 }
