71d316b9b2bcea95ec9086c92fe9d31ff63126e2
Dalai <semenov.dalay@gmail.com>
2021-04-27 21:30:04 +0300

    removed magic number

--- src/main/java/core/basesyntax/validator/length/ValidateLineLengthImpl.java
+++ src/main/java/core/basesyntax/validator/length/ValidateLineLengthImpl.java
@@ -1,10 +1,11 @@
 package core.basesyntax.validator.length;
 
 public class ValidateLineLengthImpl implements ValidateLineLength {
+    private static final int CORRECT_LENGTH = 3;
     @Override
     public void isLengthCorrect(String[] line, int lineNumber) {
         try {
-            if (line.length != 3) {
+            if (line.length != CORRECT_LENGTH) {
                 throw new LineHasNotThreeWords();
             }
         } catch (LineHasNotThreeWords e) {
