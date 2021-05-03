62e8db67cda50bbbbbb1b82ebf8fb9c9fbfb14b0
Дмитро <dima4994@gmail.com>
2021-05-03 22:33:25 +0300

    fixed all bugs by Iryna-U part 2!

--- src/main/java/core/basesyntax/storage/StrategySubtraction.java
+++ src/main/java/core/basesyntax/storage/StrategySubtraction.java
@@ -8,11 +8,11 @@ public class StrategySubtraction implements Strategy {
     @Override
     public int changeBalance(FruitRecordDto fruitRecordDto) {
         Fruit newFruit = new Fruit(fruitRecordDto.getFruit());
-        Integer currentAmount = Storage.storageOfFruits.get(newFruit);
-        if (currentAmount == null) {
+        if (!Storage.storageOfFruits.containsKey(newFruit)) {
             throw new InvalidValueOfAmountException("Amount of fruit \"" + fruitRecordDto.getFruit()
                     + "\" is not exist!  Please check your file and try again.");
         }
+        Integer currentAmount = Storage.storageOfFruits.get(newFruit);
         int newAmount = currentAmount - fruitRecordDto.getAmountOfFruit();
         if (newAmount < 0) {
             throw new InvalidValueOfAmountException("Amount can't be less then 0!"
