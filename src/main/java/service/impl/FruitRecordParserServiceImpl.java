7c925f5ab0b5dc0a468f730ad4553660ebeccda6
IhorRome <ihor.rome@gmail.com>
2021-05-04 18:42:36 +0300

    patch 1.1.4

--- src/main/java/service/impl/FruitRecordParserServiceImpl.java
+++ src/main/java/service/impl/FruitRecordParserServiceImpl.java
@@ -1,5 +1,6 @@
 package service.impl;
 
+import exception.InvalidInputException;
 import java.util.ArrayList;
 import java.util.List;
 import model.FruitRecordDto;
@@ -8,6 +9,7 @@ import service.validator.DataValidator;
 
 public class FruitRecordParserServiceImpl implements FruitRecordParserService {
     public static final String COMMA = ",";
+    public static final int LIST_TITLE_INDEX = 0;
     public static final int OPERATION_INDEX = 0;
     public static final int FRUIT_INDEX = 1;
     public static final int QUANTITY_INDEX = 2;
@@ -15,9 +17,10 @@ public class FruitRecordParserServiceImpl implements FruitRecordParserService {
     @Override
     public List<FruitRecordDto> getRecord(List<String> stringsFromFile) {
         List<FruitRecordDto> outputList = new ArrayList<>();
+        stringsFromFile.remove(LIST_TITLE_INDEX);
         for (String string : stringsFromFile) {
             if (!new DataValidator().test(string)) {
-                continue;
+                throw new InvalidInputException("Invalid line in input file: " + string);
             }
             String[] stringElements = string.split(COMMA);
             outputList.add(new FruitRecordDto(stringElements[OPERATION_INDEX],
