e8add84b042dd7cba9498fc847339db1edd67623
the5tranger <mishaantonyuk00@gmail.com>
2021-05-05 14:27:09 +0300

    Relatively working shop v1.2

--- src/main/java/core/basesyntax/fileservice/CsvWriter.java
+++ src/main/java/core/basesyntax/fileservice/CsvWriter.java
@@ -1,23 +1,18 @@
 package core.basesyntax.fileservice;
 
+import core.basesyntax.reportcreator.CsvReporter;
+import core.basesyntax.reportcreator.ReportCreator;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class CsvWriter implements Writer {
-    private static final String CSV_HEADER = "fruit,quantity";
-
     @Override
     public void writeToFile(String fileName, List<String> data) {
-        try (FileWriter writer = new FileWriter("report.csv", false)) {
-
-            writer.write(CSV_HEADER);
-            writer.append('\n');
-            for (String word : data) {
-                writer.append(word);
-                writer.append("\n");
-            }
-
+        try (FileWriter writer = new FileWriter(fileName, false)) {
+            ReportCreator csvReporter = new CsvReporter();
+            writer.write(csvReporter.createReport(data));
             writer.flush();
         } catch (IOException ex) {
             throw new RuntimeException("Can`t write to file!");
--- src/main/java/core/basesyntax/strategy/OperationStrategy.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategy.java
@@ -5,29 +5,23 @@ import core.basesyntax.operations.AddOperation;
 import core.basesyntax.operations.BalanceOperation;
 import core.basesyntax.operations.Operation;
 import core.basesyntax.operations.PurchaseOperation;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OperationStrategy {
+    private static  final Map<String, Operation> operationMap = new HashMap<>();
+
     public void doAction(List<ProductDto> productDtoList) {
-        Operation purchaseOperation = new PurchaseOperation();
-        Operation addOperation = new AddOperation();
-        Operation balanceOperation = new BalanceOperation();
+        operationMap.put("b", new BalanceOperation());
+        operationMap.put("r", new AddOperation());
+        operationMap.put("s", new AddOperation());
+        operationMap.put("p", new PurchaseOperation());
 
         for (ProductDto productDto : productDtoList) {
-            switch (productDto.getOperationType()) {
-                case "b":
-                    balanceOperation.apply(productDto);
-                    break;
-                case "r":
-                case "s":
-                    addOperation.apply(productDto);
-                    break;
-                case "p":
-                    purchaseOperation.apply(productDto);
-                    break;
-                default:
-                    throw new RuntimeException("Invalid operation");
-            }
+            Operation operation = operationMap.get(productDto.getOperationType());
+            operation.apply(productDto);
         }
     }
 }
