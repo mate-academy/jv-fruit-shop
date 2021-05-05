d51fc2751848ce36bd23ea3db4b9e18dd184efb5
Дмитро <dima4994@gmail.com>
2021-05-05 15:41:23 +0300

    There are fixed all comments mention by Max-Pochepets! + a few minors fixes

--- src/main/java/core/basesyntax/servise/ReportGenerator.java
+++ src/main/java/core/basesyntax/servise/ReportGenerator.java
@@ -6,9 +6,11 @@ import java.util.Map;
 
 public class ReportGenerator {
     private static final String COMA = ",";
+    private static final String NAME_OF_COLUMNS = "fruit,quantity";
 
     public String readData() {
         StringBuilder line = new StringBuilder();
+        line.append(NAME_OF_COLUMNS).append(System.lineSeparator());
         for (Map.Entry<Fruit, Integer> entry : Storage.getAll().entrySet()) {
             line.append(entry.getKey().getTypeOfFruit())
                     .append(COMA).append(entry.getValue())
--- src/main/java/core/basesyntax/servise/ReportWriterImpl.java
+++ src/main/java/core/basesyntax/servise/ReportWriterImpl.java
@@ -7,16 +7,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class ReportWriterImpl implements ReportWriter {
-    private static final String NAME_OF_COLUMNS = "fruit,quantity";
-
     @Override
     public void writeReport(String fileName) {
         File file = new File(fileName);
         try (BufferedWriter bufferedWriter =
                      new BufferedWriter(new FileWriter(file, true))) {
-            bufferedWriter.write(NAME_OF_COLUMNS
-                    + System.lineSeparator()
-                    + new ReportGenerator().readData());
+            bufferedWriter.write(new ReportGenerator().readData());
         } catch (IOException e) {
             throw new RuntimeException("Can't found the file", e);
         }
