d6ed44be58d7e17d507a00e9cd1732a7568ef5b0
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-05-05 16:18:37 +0300

    removed test dependency from pom.xml

---
+++ src/main/test/core/basesyntax/dao/FruitDaoImplTest.java
@@ -0,0 +1,16 @@
+package core.basesyntax.dao;
+
+import org.junit.jupiter.api.Test;
+
+class FruitDaoImplTest {
+
+    @Test
+    void saveToDb_OK() {
+
+    }
+
+    @Test
+    void getQuantityFromDb_OK() {
+
+    }
+}
