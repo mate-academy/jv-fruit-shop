4cde4762323788b6269d3de86aafb4f6e68c9970
Tetiana Chupika <mtv1301@gmail.com>
2021-06-13 14:30:41 +0300

    refactored

--- src/main/java/core/basesyntax/ValidateDao.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax;
-
-import java.util.List;
-
-public interface ValidateDao {
-    List<String> validateData(String fileName);
-}
--- src/main/java/core/basesyntax/exeptions/WrongQuantityException.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.exeptions;
-
-public class WrongQuantityException extends Exception {
-    public WrongQuantityException(String message) {
-        super(message);
-    }
-}
