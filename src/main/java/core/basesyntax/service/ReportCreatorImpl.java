737afb29005f8875f37b46e965c246969f47b348
Tamara-Paskalova <forptv@gmail.com>
2021-07-05 23:21:21 +0300

    created classes SubtractionOperatorHandler, AdditionOperationHandler, BalanceOperationHandler, service package, model package, dto package, storage package, strategy package

--- src/main/java/core/basesyntax/service/ReportCreatorImpl.java
+++ src/main/java/core/basesyntax/service/ReportCreatorImpl.java
@@ -1,15 +1,19 @@
 package core.basesyntax.service;
 
 import core.basesyntax.storage.Storage;
-import java.util.stream.Collectors;
 
 public class ReportCreatorImpl implements ReportCreator {
     private static final String REPORT_STARTING = "fruit,quantity" + System.lineSeparator();
+    private static final String COMMA = ",";
 
     @Override
     public String createReport() {
-        return Storage.storage.entrySet().stream()
-                .map(s -> s.getKey().getName() + "," + s.getValue())
-                .collect(Collectors.joining(System.lineSeparator(), REPORT_STARTING, ""));
+        StringBuilder builder = new StringBuilder().append(REPORT_STARTING)
+                .append(System.lineSeparator());
+        Storage.storage.forEach((key, value) -> builder.append(key.getName())
+                .append(COMMA)
+                .append(value)
+                .append(System.lineSeparator()));
+        return builder.toString();
     }
 }
