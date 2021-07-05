984d6d52daac56eefb94a18ad8ef82c8e13675d7
Benito <zara.benito17@gmail.com>
2021-07-05 21:04:55 +0300

    completed fruit-shop-task;

--- src/main/java/core/basesyntax/service/Parser.java
+++ src/main/java/core/basesyntax/service/Parser.java
@@ -2,8 +2,6 @@ package core.basesyntax.service;
 
 import core.basesyntax.dto.Transaction;
 
-import java.util.List;
-
 public interface Parser {
     Transaction parse(String line);
 }
--- src/main/java/core/basesyntax/service/ReportImpl.java
+++ src/main/java/core/basesyntax/service/ReportImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
-
 import java.util.Map;
 
 public class ReportImpl implements Report {
