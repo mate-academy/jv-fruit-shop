093f87dcdeb9f8ea448964164c54354aa1da3580
PashaTerletskyi <p.terletsky@gmail.com>
2021-07-06 14:28:33 +0300

    Changed Validator to regex logic

--- src/main/java/app/service/impl/LineValidatorImpl.java
+++ src/main/java/app/service/impl/LineValidatorImpl.java
@@ -3,22 +3,10 @@ package app.service.impl;
 import app.service.LineValidator;
 
 public class LineValidatorImpl implements LineValidator {
-    private static final String SEPARATOR = ",";
-    private static final int WORDS_COUNT = 3;
-    private static final int QUANTITY = 2;
+    private static final String VALIDATION_REGEX = "[bspr],\\w+,\\d+";
 
     @Override
     public boolean isValid(String line) {
-        if (line.length() > 0) {
-            String[] data = line.split(SEPARATOR);
-            if (data.length == WORDS_COUNT) {
-                try {
-                    return Integer.parseInt(data[QUANTITY]) > 0;
-                } catch (NumberFormatException e) {
-                    return false;
-                }
-            }
-        }
-        return false;
+        return line.matches(VALIDATION_REGEX);
     }
 }
