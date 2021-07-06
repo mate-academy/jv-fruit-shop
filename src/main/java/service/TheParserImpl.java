0aa00c1f49abfa6fd6a373450f947b7c67bdebdf
sSlonIk <kryachok@gmail.com>
2021-07-06 08:53:31 +0300

    fixed some code with constant

--- src/main/java/service/TheParserImpl.java
+++ src/main/java/service/TheParserImpl.java
@@ -4,10 +4,6 @@ import dto.Transaction;
 
 public class TheParserImpl implements TheParser {
     private static final String COMMA = ",";
-    private static final String B_CHAR = "b";
-    private static final String P_CHAR = "p";
-    private static final String R_CHAR = "r";
-    private static final String S_CHAR = "s";
     private static final int OPERATION_INDEX = 0;
     private static final int FRUIT_INDEX = 1;
     private static final int QUANTITY_INDEX = 2;
