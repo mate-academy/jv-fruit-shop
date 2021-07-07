e6aa23a0b9a7701d478ebb892e875e6e53a3c3bb
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-07-07 03:08:20 +0300

    Update Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,10 +4,11 @@ import java.nio.file.Path;
 import reporter.FruitShopAutoReporterImpl;
 
 public class Main {
+    private static final Path CSV_DATA_SOURCE = Path.of("./src/main/resources/reportData.txt");
+    private static final Path CSV_DATA_OUTPUT = Path.of("./src/main/resources/FruitShopReport.txt");
+
     public static void main(String[] args) {
-        Path from = Path.of("./src/main/resources/reportData.txt");
-        Path to = Path.of("./src/main/resources/FruitShopReport.txt");
         FruitShopAutoReporterImpl report = new FruitShopAutoReporterImpl();
-        report.makeReportFromTo(from, to);
+        report.makeReportFromTo(CSV_DATA_SOURCE, CSV_DATA_OUTPUT);
     }
 }
