d64434be3aa6b06336f863c30aa2d007012aa3b2
lavrivskyi <lavrivskyi@gmail.com>
2021-09-14 23:01:06 +0300

    Data base class

---
+++ src/main/java/core/basesyntax/db/DataBase.java
@@ -0,0 +1,9 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.FruitRecord;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase {
+    public static final List<FruitRecord> db = new ArrayList<>();
+}
