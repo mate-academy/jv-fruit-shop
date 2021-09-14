b889d369d04f3f39f9fb1989101ebb98fffc0d81
lavrivskyi <lavrivskyi@gmail.com>
2021-09-14 22:57:27 +0300

    Report writer Writes final report to file

---
+++ src/main/java/core/basesyntax/service/files/ReportWriter.java
@@ -0,0 +1,5 @@
+package core.basesyntax.service.files;
+
+public interface ReportWriter {
+    void writeReportToFile(String fileName);
+}
---
+++ src/main/java/core/basesyntax/service/files/ReportWriterImpl.java
@@ -0,0 +1,30 @@
+package core.basesyntax.service.files;
+
+import core.basesyntax.dao.FruitsDao;
+import core.basesyntax.dao.FruitsDaoImpl;
+import core.basesyntax.model.FruitRecord;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class ReportWriterImpl implements ReportWriter {
+    private static final String REPORT_HEADER = "fruitRecord,quantity";
+    private static final String DATA_DIVIDER = ",";
+
+    @Override
+    public void writeReportToFile(String filename) {
+        FruitsDao fruitDao = new FruitsDaoImpl();
+        List<FruitRecord> fruitsDbRecords = fruitDao.getRecords();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))) {
+            bw.write(REPORT_HEADER + System.lineSeparator());
+            for (FruitRecord record : fruitsDbRecords) {
+                String fruitName = record.getName();
+                int fruitsAmount = record.getAmount();
+                bw.write(fruitName + DATA_DIVIDER + fruitsAmount + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write report to file, " + e);
+        }
+    }
+}
