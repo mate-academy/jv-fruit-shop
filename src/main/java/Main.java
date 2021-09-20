786411d3d7cc03a7df22f6e70f28fe12686f5823
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-09-20 16:31:05 +0300

    release working version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import service.FileWriterService;
 import service.InputDataValidator;
 import service.Parse;
 import service.impl.FileReaderServiceImpl;
+import service.impl.FileWriterServiceImpl;
 import service.impl.InputDataValidatorImpl;
 import service.impl.ParseImpl;
 
@@ -8,6 +10,8 @@ import java.util.List;
 
 public class Main {
     private static final String INPUT_FILE_NAME = "src/main/res/input.csv";
+    private static final String OUTPUT_FILE_NAME = "src/main/res/report.csv";
+
     public static void main(String[] args) {
         FileReaderServiceImpl fileReaderService = new FileReaderServiceImpl();
         List<String> strings = fileReaderService.readFile(INPUT_FILE_NAME);
@@ -18,6 +22,7 @@ public class Main {
         Parse parse = new ParseImpl();
         parse.parseList(strings);
 
-
+        FileWriterService fileWriterService = new FileWriterServiceImpl();
+        fileWriterService.writeToFile(OUTPUT_FILE_NAME);
     }
 }
---
+++ src/main/java/service/impl/FileWriterServiceImpl.java
@@ -0,0 +1,29 @@
+package service.impl;
+
+import db.Storage;
+import service.FileWriterService;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterServiceImpl implements FileWriterService {
+    private static final String REPORT_HEAD = "fruit,quantity";
+
+    @Override
+    public void writeToFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder(REPORT_HEAD);
+        Storage.storage.forEach((key, value) -> stringBuilder.append(System.lineSeparator())
+                .append(key)
+                .append(",")
+                .append(value));
+        String s = stringBuilder.toString();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(s);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file " + fileName, e);
+        }
+    }
+
+}
