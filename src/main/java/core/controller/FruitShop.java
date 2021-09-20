c50da1138535685d842e8c51d2303461c51aaeab
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-09-20 12:05:33 +0300

    edited ReportGeneraorImpl class to have opportunity to process differrent fruits

--- src/main/java/core/controller/FruitShop.java
+++ src/main/java/core/controller/FruitShop.java
@@ -9,26 +9,29 @@ import core.service.report.ReportGenerator;
 import core.service.report.ReportGeneratorImpl;
 import core.service.writer.ReportWriter;
 import core.service.writer.ReportWriterImpl;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class FruitShop {
-    private Fruit banana;
-    private Fruit apple;
-    private Set<Fruit> fruits;
+    public static final Fruit BANANA;
+    public static final Fruit APPLE;
+    public static final Map<String, Fruit> FRUITS;
+
+    static {
+        BANANA = new Fruit(MyConstants.BANANA);
+        APPLE = new Fruit(MyConstants.APPLE);
+        FRUITS = new HashMap<>();
+        FRUITS.put(BANANA.getName(), BANANA);
+        FRUITS.put(APPLE.getName(), APPLE);
+    }
+
     private ReaderService readerService;
     private ReportGenerator reportGenerator;
     private ReportWriter reportWriter;
     private Map<String, OperationHandler> operationHandlersMap;
 
     public FruitShop(Map<String, OperationHandler> operationHandlersMap) {
-        banana = new Fruit(MyConstants.BANANA);
-        apple = new Fruit(MyConstants.APPLE);
-        fruits = new HashSet<>();
-        fruits.add(banana);
-        fruits.add(apple);
         readerService = new ReaderServiceImpl();
         reportWriter = new ReportWriterImpl();
         this.operationHandlersMap = operationHandlersMap;
--- src/main/java/core/service/report/ReportGeneratorImpl.java
+++ src/main/java/core/service/report/ReportGeneratorImpl.java
@@ -1,12 +1,12 @@
 package core.service.report;
 
+import core.controller.FruitShop;
 import core.model.Fruit;
 import core.model.MyConstants;
 import core.service.oparation.OperationHandler;
 import core.service.oparation.OperationStrategy;
 import core.service.oparation.OperationStrategyImpl;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,25 +20,21 @@ public class ReportGeneratorImpl implements ReportGenerator {
 
     @Override
     public List<String> createReport(List<String> strings) {
-        Fruit banana = new Fruit("banana");
-        Fruit apple = new Fruit("apple");
-        Map<String, Fruit> fruitMap = new HashMap<>();
-        fruitMap.put(MyConstants.BANANA, banana);
-        fruitMap.put(MyConstants.APPLE, apple);
         for (String string: strings) {
             String[] tmpStrings = string.split(MyConstants.COMA_SEPARATOR);
             OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlersMap);
             OperationHandler operationHandler =
                     operationStrategy.get(tmpStrings[INDEX_OPERATION_FOR_OPERATION_STRATAGY]);
-            operationHandler.doOperation(fruitMap, tmpStrings);
+            operationHandler.doOperation(FruitShop.FRUITS, tmpStrings);
         }
         List<String> report = new ArrayList<>();
-        String bananaToString = banana.getName()
-                + MyConstants.COMA_SEPARATOR + banana.getQuantity();
-        String appleToString = apple.getName()
-                + MyConstants.COMA_SEPARATOR + apple.getQuantity();
-        report.add(bananaToString);
-        report.add(appleToString);
+        Map<String, Fruit> fruits = FruitShop.FRUITS;
+        for (Map.Entry<String, Fruit> stringFruitEntry : fruits.entrySet()) {
+            report.add(stringFruitEntry.getValue().getName()
+                    + MyConstants.COMA_SEPARATOR
+                    + stringFruitEntry.getValue().getQuantity());
+        }
         return report;
     }
+
 }
