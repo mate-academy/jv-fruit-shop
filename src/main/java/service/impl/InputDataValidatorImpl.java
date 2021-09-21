b8957ec105e2b42fbbda3c14e9ad5fdfd8b1f039
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-09-21 12:49:17 +0300

    performance improvement

--- src/main/java/service/impl/InputDataValidatorImpl.java
+++ src/main/java/service/impl/InputDataValidatorImpl.java
@@ -1,15 +1,17 @@
 package service.impl;
 
 import java.util.List;
+import java.util.regex.Pattern;
+
 import service.InputDataValidator;
 
 public class InputDataValidatorImpl implements InputDataValidator {
-    private static final String VALID_FORMAT = "[sprb],[a-zA-Z]+,[0-9]+";
+    private static final Pattern VALID_RECORD = Pattern.compile("[sprb],[a-zA-Z]+,[0-9]+");
 
     @Override
     public void chekDate(List<String> str) {
         for (int i = 1; i < str.size(); i++) {
-            if (!str.get(i).matches(VALID_FORMAT)) {
+            if (!VALID_RECORD.matcher(str.get(i)).matches()) {
                 throw new RuntimeException("Input data is wrong!");
             }
         }
