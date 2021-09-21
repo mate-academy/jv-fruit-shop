e85c7eeb8ece2d85d60ed8148c1b2a1968d83e98
P.Zbarazkyy <mtu09@ukr.net>
2021-09-21 14:21:03 +0300

    Fixed the output of the getType() method in the TypeHandlerReturn() class

--- src/main/java/service/type/service/TypeHandlerReturn.java
+++ src/main/java/service/type/service/TypeHandlerReturn.java
@@ -3,6 +3,6 @@ package service.type.service;
 public class TypeHandlerReturn implements TypeHandler {
     @Override
     public int getType(int amount) {
-        return amount * -1;
+        return amount;
     }
 }
