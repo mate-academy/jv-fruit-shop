6de7350b490ba6cfbf28098448d56238788a43ee
dogovor <dogovor@lan.com.ua>
2021-09-22 12:03:15 +0300

    add Validator

--- src/main/java/core/basesyntax/servises/Mapper.java
+++ src/main/java/core/basesyntax/servises/Mapper.java
@@ -9,21 +9,14 @@ public class Mapper {
     private static final int NAME = 1;
     private static final int QUANTITY = 2;
     private static final String SEPARATOR = ",";
+    private Validator validator = new ValidatorImpl();
 
     public List<TransferDto> apply(List<String> list) {
         List<TransferDto> data = new ArrayList<>();
         for (int i = 1; i < list.size(); i++) {
             String[] rows = list.get(i).split(SEPARATOR);
-            if (rows.length != 3) {
-                throw new RuntimeException("Data is incorrect");
-            }
-            if (!"bprs".contains(rows[TYPE])) {
-                throw new RuntimeException("OperationType isn't valid");
-            }
-            if ((Integer.parseInt(rows[QUANTITY]) < 0)) {
-                throw new RuntimeException("Quantity is incorrect");
-            }
-            TransferDto dto = new TransferDto(OperationType.fromString(rows[0]),
+            validator.validate(rows);
+            TransferDto dto = new TransferDto(OperationType.fromString(rows[TYPE]),
                         rows[NAME], Integer.parseInt(rows[QUANTITY]));
             data.add(dto);
         }
