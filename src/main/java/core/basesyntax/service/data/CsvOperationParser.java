bb30fc2fedfb25a05f08bb8a73e8a20276cbba44
taras-oleksiuk <taras.oleksiuk@gmail.com>
2021-09-22 16:39:46 +0300

    Second solution. Fixed fileNotFound exception and add function: remove first line of CSV.

--- src/main/java/core/basesyntax/service/data/CsvOperationParser.java
+++ src/main/java/core/basesyntax/service/data/CsvOperationParser.java
@@ -9,11 +9,14 @@ public class CsvOperationParser implements DataParser<Operation, String> {
     private static final int FRUIT_INDEX = 1;
     private static final int AMOUNT_INDEX = 2;
     private static final String DATA_SPLITTER = ",";
+    public static final int FIRST_LINE_OF_CSV = 0;
 
     @Override
     public List<Operation> formatData(List<String> data) {
         DataValidator dataValidator = new DataValidatorImpl();
         List<Operation> operations = new ArrayList<>();
+        data.remove(FIRST_LINE_OF_CSV);
+
         for (String proper : data) {
             String[] properArray = proper.split(DATA_SPLITTER);
             dataValidator.validate(properArray);
