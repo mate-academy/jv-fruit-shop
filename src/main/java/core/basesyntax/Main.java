c9dbb803275cd6eb77e5b124e57cc2888ef296eb
ASelivonchyk <aselivonchyk@gmail.com>
2021-11-10 08:50:13 +0200

    some fixes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,18 +1,12 @@
 package core.basesyntax;
 
-import core.basesyntax.dao.ResourcesDao;
-import core.basesyntax.dao.ResourcesDaoImpl;
 import core.basesyntax.service.ReportService;
 import core.basesyntax.service.ReportServiceImpl;
 import core.basesyntax.service.StorageService;
 import core.basesyntax.service.StorageServiceImpl;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ResourcesDao resourcesDao = new ResourcesDaoImpl();
-        List<String> dataFromFile = resourcesDao
-                .readFromFile("src/main/java/core/basesyntax/resources/input");
         StorageService storageService = new StorageServiceImpl();
         storageService
                 .updateProductsAmountInStorage("src/main/java/core/basesyntax/resources/input");
