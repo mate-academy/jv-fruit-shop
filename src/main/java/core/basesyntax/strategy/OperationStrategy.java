59d481870a8e788ba434a3cbddd649019cbd83e6
Konstantin <www.kosta.kon@gmail.com>
2021-11-23 16:27:36 +0200

    Import order was fixed.

--- src/main/java/core/basesyntax/strategy/OperationStrategy.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategy.java
@@ -3,15 +3,14 @@ package core.basesyntax.strategy;
 import core.basesyntax.services.OperationHandler;
 import core.basesyntax.services.impl.AddOperationHandler;
 import core.basesyntax.services.impl.PurchaseOperationHandler;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class OperationStrategy {
-    private static final String BALANCE_OPERATION= "b";
-    private static final String SUPPLY_OPERATION= "s";
-    private static final String PURCHASE_OPERATION= "p";
-    private static final String RETURN_OPERATION= "r";
+    private static final String BALANCE_OPERATION = "b";
+    private static final String SUPPLY_OPERATION = "s";
+    private static final String PURCHASE_OPERATION = "p";
+    private static final String RETURN_OPERATION = "r";
     private final Map<String, OperationHandler> operationMap = new HashMap<>();
 
     public OperationStrategy() {
