00a02a880dafcf73324fbb40826da5a20c0399e6
Konstantin <www.kosta.kon@gmail.com>
2021-11-23 16:24:33 +0200

    Constants for operations were added.

--- src/main/java/core/basesyntax/strategy/OperationStrategy.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategy.java
@@ -8,15 +8,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OperationStrategy {
+    private static final String BALANCE_OPERATION= "b";
+    private static final String SUPPLY_OPERATION= "s";
+    private static final String PURCHASE_OPERATION= "p";
+    private static final String RETURN_OPERATION= "r";
     private final Map<String, OperationHandler> operationMap = new HashMap<>();
 
     public OperationStrategy() {
         OperationHandler addOperationHandler = new AddOperationHandler();
         OperationHandler purchaseOperationHandler = new PurchaseOperationHandler();
-        operationMap.put("b", addOperationHandler);
-        operationMap.put("s", addOperationHandler);
-        operationMap.put("p", purchaseOperationHandler);
-        operationMap.put("r", addOperationHandler);
+        operationMap.put(BALANCE_OPERATION, addOperationHandler);
+        operationMap.put(SUPPLY_OPERATION, addOperationHandler);
+        operationMap.put(PURCHASE_OPERATION, purchaseOperationHandler);
+        operationMap.put(RETURN_OPERATION, addOperationHandler);
     }
 
     public OperationHandler getOperationHandler(String operation) {
