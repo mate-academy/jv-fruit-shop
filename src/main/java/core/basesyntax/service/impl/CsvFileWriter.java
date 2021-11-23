8d6e73f91d440538c57fe84293ae15a48c84b2e1
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-11-23 16:03:38 +0200

    Used try with resources

--- src/main/java/core/basesyntax/service/impl/CsvFileWriter.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriter.java
@@ -8,11 +8,10 @@ public class CsvFileWriter implements FileWriter {
     @Override
     public boolean write(String path, String report) {
         File file = new File(path);
-        try {
+        try (java.io.FileWriter fileWriter = new java.io.FileWriter(file);) {
             file.createNewFile();
-            java.io.FileWriter fileWriter = new java.io.FileWriter(file);
             fileWriter.write(report);
-            fileWriter.close();
+
         } catch (IOException e) {
             throw new RuntimeException("Can't create report file, " + e);
         }
