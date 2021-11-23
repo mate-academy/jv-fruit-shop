381257036361ffa0ff3571239d661bfb21adf4f1
Natalia Vidzivashets <nata.vidzivashets@gmail.com>
2021-11-23 22:05:48 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/strategy/AddOperationHandler.java
+++ src/main/java/core/basesyntax/strategy/AddOperationHandler.java
@@ -8,7 +8,7 @@ public class AddOperationHandler implements OperationHandler {
     @Override
     public void apply(TransactionDto transactionDto) {
         Fruit fruit = new Fruit(transactionDto.getFruitName());
-        int oldQuantity = Storage.fruits.get(fruit);
+        int oldQuantity = Storage.fruits.get(fruit) == null ? 0 : Storage.fruits.get(fruit);
         int quantity = transactionDto.getQuantity() + oldQuantity;
         Storage.fruits.put(fruit, quantity);
     }
--- src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
@@ -8,7 +8,7 @@ public class PurchaseOperationHandler implements OperationHandler {
     @Override
     public void apply(TransactionDto transactionDto) {
         Fruit fruit = new Fruit(transactionDto.getFruitName());
-        int oldQuantity = Storage.fruits.get(fruit);
+        int oldQuantity = Storage.fruits.get(fruit) == null ? 0 : Storage.fruits.get(fruit);
         int quantity = oldQuantity - transactionDto.getQuantity();
         if (quantity < 0) {
             throw new PurchaseOperationHandlerException("Not enough fruits to buy");
