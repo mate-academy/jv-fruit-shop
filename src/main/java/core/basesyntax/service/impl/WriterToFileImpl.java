e6b502b4ffdc4fd245badb2082936777bddd2508
Panchenko Volodymyr <volodymyr.panchenko147@gmail.com>
2021-11-23 23:02:23 +0200

    Change writrer for correct output

--- src/main/java/core/basesyntax/service/impl/WriterToFileImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriterToFileImpl.java
@@ -4,6 +4,7 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.service.WriterToFile;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
@@ -11,14 +12,28 @@ import java.util.Map;
 public class WriterToFileImpl implements WriterToFile {
     @Override
     public void writeDataToFile(String pathToFile) {
+        File file = new File(pathToFile);
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(file, true));
+            bufferedWriter.write("fruit,quantity\n");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write text to file", e);
+        }
         for (Map.Entry<Fruit, Integer> fruit : Storage.getDataBase().entrySet()) {
-            try (BufferedWriter bufferedWriter =
-                         new BufferedWriter((new FileWriter(pathToFile,true)))) {
-                bufferedWriter.write(fruit.toString());
+            try {
+                bufferedWriter.write(fruit.getKey().getFruitName() + ',' + fruit.getValue());
                 bufferedWriter.write(System.lineSeparator());
+                bufferedWriter.flush();
             } catch (IOException e) {
                 throw new RuntimeException("Can't write data to file", e);
             }
         }
+        try {
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't close file", e);
+        }
     }
 }
+
