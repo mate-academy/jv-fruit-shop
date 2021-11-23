e0946cbb24c23f17e6ed3ace64cfda444be04cde
Konstantin <www.kosta.kon@gmail.com>
2021-11-23 18:47:17 +0200

    Constants in ReportMaker were removed.

--- src/main/java/core/basesyntax/services/impl/ReportMakerImpl.java
+++ src/main/java/core/basesyntax/services/impl/ReportMakerImpl.java
@@ -5,15 +5,12 @@ import core.basesyntax.services.ReportMaker;
 import java.util.Map;
 
 public class ReportMakerImpl implements ReportMaker {
-    private static final String REPORT_TABLE_COLUMNS = "fruit,quantity";
-    private static final String SEPARATOR = ",";
-
     @Override
     public String createReport() {
-        StringBuilder result = new StringBuilder(REPORT_TABLE_COLUMNS);
+        StringBuilder result = new StringBuilder("fruit,quantity");
         for (Map.Entry<String, Integer> entry : Storage.storage.entrySet()) {
             result.append(System.lineSeparator()).append(entry.getKey())
-                    .append(SEPARATOR).append(entry.getValue());
+                    .append(",").append(entry.getValue());
         }
         return result.toString();
     }
