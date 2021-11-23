bfc43814275c746444ea0fde02422e3dca888497
Anton Volskyi <antonvolskyi1234@gmail.com>
2021-11-23 09:55:20 +0200

    changet hendlers

--- src/main/java/core/basesyntax/service/handler/impl/FruitPurchase.java
+++ src/main/java/core/basesyntax/service/handler/impl/FruitPurchase.java
@@ -6,6 +6,8 @@ import core.basesyntax.service.handler.WorkWithFruits;
 public class FruitPurchase implements WorkWithFruits {
     @Override
     public void workWithFruitInStorage(int fruitNumber, String fruitName, FruitDao fruitDao) {
+        fruitDao.get(fruitName).orElseThrow(()
+                -> new RuntimeException("You really thought I had this fruit?"));
         int oldCountFruit = fruitDao.getCurrentCountFruitsInStorage(fruitName);
         if (oldCountFruit < fruitNumber) {
             throw new RuntimeException("Not enough fruits in storage");
--- src/main/java/core/basesyntax/service/handler/impl/FruitReturn.java
+++ src/main/java/core/basesyntax/service/handler/impl/FruitReturn.java
@@ -7,6 +7,8 @@ public class FruitReturn implements WorkWithFruits {
 
     @Override
     public void workWithFruitInStorage(int fruitNumber, String fruitName, FruitDao fruitDao) {
+        fruitDao.get(fruitName).orElseThrow(()
+                -> new RuntimeException("You can`t return that fruit!"));
         int oldCountFruit = fruitDao.getCurrentCountFruitsInStorage(fruitName);
         fruitDao.changeFruitCountInStorage(oldCountFruit + fruitNumber, fruitName);
     }
