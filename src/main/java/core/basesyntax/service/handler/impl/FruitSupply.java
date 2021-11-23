c3386b6ca87a1646a9ea9e20cf5faef59b8f5e9d
Anton Volskyi <antonvolskyi1234@gmail.com>
2021-11-23 15:25:22 +0200

    mvn cp success

--- src/main/java/core/basesyntax/service/handler/impl/FruitSupply.java
+++ src/main/java/core/basesyntax/service/handler/impl/FruitSupply.java
@@ -7,7 +7,8 @@ import core.basesyntax.service.handler.WorkWithFruits;
 public class FruitSupply implements WorkWithFruits {
     @Override
     public Fruit workWithFruitInStorage(int fruitNumber, String fruitName, FruitDao fruitDao) {
-        Fruit currentFruit = fruitDao.get(fruitName).orElseGet(() -> fruitDao.add(new Fruit(fruitName, 0)));
+        Fruit currentFruit = fruitDao.get(fruitName).orElseGet(()
+                -> fruitDao.add(new Fruit(fruitName, 0)));
         int oldCountFruit = fruitDao.getFruitsCount(fruitName);
         fruitDao.updateFruitCount(oldCountFruit + fruitNumber, fruitName);
         return currentFruit;
