8e8de774dbe2095d23949d4761cbbf40055abb48
Dany Nesterov <nesterov1200997@gmail.com>
2021-11-23 21:11:29 +0200

    constants were added

--- src/main/java/core/basesyntax/services/impl/ParserImpl.java
+++ src/main/java/core/basesyntax/services/impl/ParserImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class ParserImpl implements Parser<TransactionDto> {
     private static final String INPUT_TITLE = "type,fruit,quantity";
+    private static final String COMA_SEPARATOR = ",";
     private static final int INDEX_OF_OPERATION = 0;
     private static final int INDEX_OF_PRODUCT = 1;
     private static final int INDEX_OF_QUANTITY = 2;
@@ -23,7 +24,7 @@ public class ParserImpl implements Parser<TransactionDto> {
         List<TransactionDto> transactions = new ArrayList<>();
         line.stream()
                 .filter(e -> !e.equals(INPUT_TITLE))
-                .map(e -> e.split(","))
+                .map(e -> e.split(COMA_SEPARATOR))
                 .forEach(e -> transactions.add(new TransactionDto(e[INDEX_OF_OPERATION],
                         e[INDEX_OF_PRODUCT], Integer.parseInt(e[INDEX_OF_QUANTITY]))));
         return transactions;
