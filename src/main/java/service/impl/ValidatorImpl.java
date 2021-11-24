10cf6805542942042109a4393d1a78e31bb64b23
Oleksii-kho <oleksiikhoriev@gmail.com>
2021-11-24 08:33:43 +0200

    ValidatorImpl updated

--- src/main/java/service/impl/ValidatorImpl.java
+++ src/main/java/service/impl/ValidatorImpl.java
@@ -3,10 +3,19 @@ package service.impl;
 import service.Validator;
 
 public class ValidatorImpl implements Validator {
-    private static final String LINE_MATCHER_REGEX = "[bspr],\\w+,\\d+";
+    private static final String LINE_MATCHER_REGEX = "[bspr],[a-zA-Z]+,[0-9]+";
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int FRUIT_NAME_INDEX = 1;
+    private static final int FRUIT_AMOUNT_INDEX = 2;
 
     @Override
     public boolean validate(String line) {
-        return line.matches(LINE_MATCHER_REGEX);
+        if (line.isEmpty()) {
+            throw new RuntimeException("Empty line in inputFile");
+        }
+        if (line.matches(LINE_MATCHER_REGEX)) {
+            return true;
+        }
+        throw new RuntimeException("invalid value in inputFile");
     }
 }
