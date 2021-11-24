685238d099db69fa298e0ef470a95476cc0e4e33
Artik <Artik25942@gmail.com>
2021-11-24 16:45:28 +0200

    all empty lines deleted

--- src/main/java/service/InputDataValidator.java
+++ src/main/java/service/InputDataValidator.java
@@ -3,6 +3,5 @@ package service;
 import java.util.List;
 
 public interface InputDataValidator {
-
     boolean validate(List<String> input);
 }
--- src/main/java/strategy/OperationStrategy.java
+++ src/main/java/strategy/OperationStrategy.java
@@ -4,5 +4,5 @@ import core.basesyntax.model.ParseLine;
 import service.OperationService;
 
 public interface OperationStrategy {
-    public OperationService getOperationService(ParseLine line);
+    OperationService getOperationService(ParseLine line);
 }
