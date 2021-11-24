6d46c71a6785e510fbe56f011094e58b9c26c4f0
Artik <Artik25942@gmail.com>
2021-11-24 14:33:52 +0200

    after sr

--- src/main/java/service/implement/LineParserImp.java
+++ src/main/java/service/implement/LineParserImp.java
@@ -5,13 +5,17 @@ import java.util.List;
 import service.LineParser;
 
 public class LineParserImp implements LineParser {
+    private static final int OPERATION_INDEX = 0;
+    private static final int FRUIT_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
 
     @Override
     public List<ParseLine> lineParcer(List<String> input) {
         inputValidator.validate(input);
         for (String string: input) {
             String[] values = string.split(",");
-            parseLineList.add(new ParseLine(values[0], values[1], Integer.parseInt(values[2])));
+            parseLineList.add(new ParseLine(values[OPERATION_INDEX],
+                    values[FRUIT_INDEX], Integer.parseInt(values[QUANTITY_INDEX])));
         }
         return parseLineList;
     }
