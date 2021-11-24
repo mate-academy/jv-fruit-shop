0929da06a94c92b9a24e8f2b43a0566db6548aef
Natalia Vidzivashets <nata.vidzivashets@gmail.com>
2021-11-24 09:38:47 +0200

    added a check for the first line in the file

--- src/main/java/core/basesyntax/service/impl/ParserServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ParserServiceImpl.java
@@ -10,6 +10,7 @@ public class ParserServiceImpl implements ParserService<TransactionDto> {
     private static final int OPERATION_INDEX = 0;
     private static final int NAME_OF_FRUIT_INDEX = 1;
     private static final int QUANTITY_OF_FRUITS_INDEX = 2;
+    private static final String FIRST_LINE_IN_FILE = "type,fruit,quantity";
 
     private ValidatorService<String> validatorService;
 
@@ -20,7 +21,9 @@ public class ParserServiceImpl implements ParserService<TransactionDto> {
     @Override
     public List<TransactionDto> parse(List<String> list) {
         List<TransactionDto> transactionDtoList = new ArrayList<>();
-        list.remove(0);
+        if (list.get(0).equals(FIRST_LINE_IN_FILE)) {
+            list.remove(0);
+        }
         for (String line : list) {
             validatorService.validate(line);
             String[] information = line.split(",");
