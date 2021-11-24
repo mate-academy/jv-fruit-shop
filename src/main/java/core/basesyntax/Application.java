5f1922d73d4e81f3c7156d4f7ac3fa7461fbb199
Dima Barshay <barshayerum8@gmail.com>
2021-11-24 13:21:38 +0200

    added files' paths

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -17,6 +17,9 @@ import java.util.List;
 import java.util.Map;
 
 public class Application {
+    private static final String INPUT_FILE_PATH = "src/main/resources/input.csv";
+    private static final String OUTPUT_FILE_PATH = "src/main/resources/output.csv";
+
     public static void main(String[] args) {
         Map<String, OperationHandler> operationHandlerMap = new HashMap<>();
         operationHandlerMap.put("s", new AddOperationHandler());
@@ -25,7 +28,7 @@ public class Application {
         operationHandlerMap.put("b", new BalanceOperationHandler());
 
         ReaderService reader = new ReaderServiceImpl();
-        List<String> lines = reader.readFromFile("");
+        List<String> lines = reader.readFromFile(INPUT_FILE_PATH);
 
         ParserService<TransactionDto> parserService =
                 new ParserServiceImpl(new ValidatorServiceImpl());
@@ -35,6 +38,6 @@ public class Application {
                 .forEach(t -> operationHandlerMap.get(t.getOperation()).apply(t));
 
         String report = new ReportServiceImpl().formReport();
-        new WriterServiceImpl().writeToFile("", report);
+        new WriterServiceImpl().writeToFile(OUTPUT_FILE_PATH, report);
     }
 }
