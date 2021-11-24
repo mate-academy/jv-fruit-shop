cb78dbcf7cd52b36d956c24c80883f856b08b88a
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-24 09:52:13 +0200

    Application with report generator was created

---
+++ src/main/java/core/basesyntax/strategy/BalanceOperationHandler.java
@@ -0,0 +1,13 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.TransactionDto;
+
+public class BalanceOperationHandler implements OperationHandler{
+    @Override
+    public void apply(TransactionDto transactionDto) {
+        Fruit fruit = new Fruit(transactionDto.getFruit());
+        Storage.fruitStorage.put(fruit, transactionDto.getAmount());
+    }
+}
