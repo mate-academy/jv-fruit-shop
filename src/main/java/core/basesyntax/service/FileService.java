649e7ce4f853307efb389ada7d9a9a1d2cf70070
Serhii Pedchenko <djmolot@gmail.com>
2022-01-10 12:09:51 +0200

    removed redundant comments in FileService.java

--- src/main/java/core/basesyntax/service/FileService.java
+++ src/main/java/core/basesyntax/service/FileService.java
@@ -3,13 +3,10 @@ package core.basesyntax.service;
 import java.io.File;
 import java.util.List;
 
-public interface FileService { //просто записує і зчитує з файлу будь-яку інфу
-    //FileService має використовуватись всередині твого StoreService
+public interface FileService {
 
-    //read data from csv file
     List<String> readDataFromFile(File file);
 
-    //write report to new csv file
     void writeDataToFile(List<String> data, File file);
 
     File createNewFile(String fileFullPath);
