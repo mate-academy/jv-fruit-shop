42289dfdb20b35dec0d895d6ea43dc940536035e
Maksym Pokras <mxpokras@gmail.com>
2022-01-16 15:25:27 +0200

    db package created with FruitsData and TransactionsData

---
+++ src/main/java/core/basesyntax/db/FruitsData.java
@@ -0,0 +1,11 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.Fruit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FruitsData {
+    public static Map<String, Integer> fruitMap = new HashMap<>();
+
+}
