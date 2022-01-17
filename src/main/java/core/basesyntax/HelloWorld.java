2845eca5d97a01a7e5ab8f9346c567c0c5b51b82
Maksym Pokras <mxpokras@gmail.com>
2022-01-17 11:07:12 +0200

    TransactionServiceImpl modified to choose handler from Map

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,70 @@
 package core.basesyntax;
 
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.dao.TransactionDao;
+import core.basesyntax.dao.impl.FruitDaoImpl;
+import core.basesyntax.dao.impl.TransactionDaoImpl;
+import core.basesyntax.db.FruitsData;
+import core.basesyntax.db.TransactionsData;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.Transaction;
+import core.basesyntax.model.TransactionType;
+import core.basesyntax.service.FruitsConsumer;
+import core.basesyntax.service.TransactionService;
+import core.basesyntax.service.TransactionsSupplier;
+import core.basesyntax.service.impl.FruitsConsumerCsv;
+import core.basesyntax.service.impl.TransactionServiceImpl;
+import core.basesyntax.service.impl.TransactionsSupplierCsv;
+import core.basesyntax.strategy.BalanceHandler;
+import core.basesyntax.strategy.PurchaseHandler;
+import core.basesyntax.strategy.ReturnHandler;
+import core.basesyntax.strategy.SupplyHandler;
+import core.basesyntax.strategy.TransactionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    private final static String journalPath = "src/main/resources/daily_journal.csv";
+    private final static String reportPath = "src/main/resources/daily_report.csv";
+
+    public static void main(String[] args) {
+        FruitDao fruitDao = new FruitDaoImpl();
+        TransactionDao transactionDao = new TransactionDaoImpl();
+        loadTransactions(journalPath);
+        TransactionService transactionService = new TransactionServiceImpl(fruitDao, strategyMap());
+        for(Transaction transaction : transactionDao.get()) {
+            transactionService.accept(transaction);
+        }
+        saveReport(reportPath);
+    }
+
+    private static Map<TransactionType, TransactionHandler> strategyMap() {
+        Map<TransactionType, TransactionHandler> mapStrategy = new HashMap<>();
+        mapStrategy.put(TransactionType.BALANCE, new BalanceHandler());
+        mapStrategy.put(TransactionType.PURCHASE, new PurchaseHandler());
+        mapStrategy.put(TransactionType.RETURN, new ReturnHandler());
+        mapStrategy.put(TransactionType.SUPPLY, new SupplyHandler());
+        return mapStrategy;
+    }
+
+    private static void loadTransactions(String filePath) {
+        TransactionsSupplier supplier = new TransactionsSupplierCsv(filePath);
+        TransactionsData.transactions.addAll(supplier.get());
+    }
+
+    private static void saveReport(String filePath) {
+        FruitsConsumer consumer = new FruitsConsumerCsv(filePath);
+        List<Fruit> fruits = FruitsData.fruitMap.entrySet()
+                .stream()
+                .map(e -> new Fruit(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
+        consumer.accept(fruits);
+    }
 
 }
--- src/main/java/core/basesyntax/service/impl/TransactionServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/TransactionServiceImpl.java
@@ -3,24 +3,26 @@ package core.basesyntax.service.impl;
 import core.basesyntax.dao.FruitDao;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.model.Transaction;
-import core.basesyntax.service.FruitsConsumer;
+import core.basesyntax.model.TransactionType;
 import core.basesyntax.service.TransactionService;
 import core.basesyntax.strategy.TransactionHandler;
-import core.basesyntax.strategy.TransactionHandlerProvider;
+
+import java.util.Map;
 
 public class TransactionServiceImpl implements TransactionService {
     private final FruitDao fruitDao;
-    private final TransactionHandlerProvider provider = new TransactionHandlerProvider();
+    private final Map<TransactionType, TransactionHandler> strategyMap;
 
-    public TransactionServiceImpl(FruitDao fruitDao) {
+    public TransactionServiceImpl(FruitDao fruitDao, Map<TransactionType, TransactionHandler> strategyMap) {
         this.fruitDao = fruitDao;
+        this.strategyMap = strategyMap;
     }
 
     @Override
     public void accept(Transaction transaction) {
         Fruit fruit = fruitDao.get(transaction.getFruitName())
                 .orElse(new Fruit(transaction.getFruitName()));
-        TransactionHandler handler = provider.getHandler(transaction.getType());
+        TransactionHandler handler = strategyMap.get(transaction.getType());
         handler.perform(fruit, transaction);
         fruitDao.put(fruit);
     }
