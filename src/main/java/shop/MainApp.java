381aa336a126295761e1cde53bb3e13bd0273777
Yurii Tomashevskyi <niteris92@gmail.com>
2022-02-08 18:23:18 +0200

    added Storage, Fruit, FruitTransaction & MainApp classes

---
+++ src/main/java/shop/MainApp.java
@@ -0,0 +1,7 @@
+package shop;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/shop/db/Storage.java
@@ -0,0 +1,17 @@
+package shop.db;
+
+import shop.model.Fruit;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storage {
+    private final Map<Fruit, Integer> storageOfFruits;
+
+    public Storage() {
+        this.storageOfFruits = new HashMap<>();
+    }
+
+    public Map<Fruit, Integer> getStorageOfFruits() {
+        return storageOfFruits;
+    }
+}
