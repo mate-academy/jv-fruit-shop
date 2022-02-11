5bd99f426b5f10c610862d8de1d4f510fb87a09d
Andriy_Poluhovic <chimbar@ukr.net>
2022-02-11 12:01:55 +0200

    Created Storage with TransactionService

---
+++ src/main/java/fruitshop/database/TransactionService.java
@@ -0,0 +1,22 @@
+package fruitshop.database;
+
+import fruitshop.model.FruitTransaction;
+import fruitshop.model.SaveData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionService implements SaveData {
+
+    @Override
+    public List<FruitTransaction> transactionData(List<String> readFile) {
+        List<FruitTransaction> transactionDao = new ArrayList<>();
+        String[] temp;
+        readFile.remove(0);
+        for (String line : readFile) {
+            temp = line.split(",");
+            transactionDao.add(FruitTransaction.of(temp[0], temp[1], Integer.parseInt(temp[2])));
+        }
+        return transactionDao;
+    }
+}
