10565e7c5b1801cc75d5a1e5b25768dd0293a1f9
Andriy_Poluhovic <chimbar@ukr.net>
2022-02-18 10:06:03 +0200

    Rename transactionDao to transaction

--- src/main/java/fruitshop/database/TransactionServiceImpl.java
+++ src/main/java/fruitshop/database/TransactionServiceImpl.java
@@ -9,13 +9,13 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public List<FruitTransaction> transactionData(List<String> readFile) {
-        List<FruitTransaction> transactionDao = new ArrayList<>();
+        List<FruitTransaction> transaction = new ArrayList<>();
         String[] temp;
         readFile.remove(0);
         for (String line : readFile) {
             temp = line.split(",");
-            transactionDao.add(FruitTransaction.of(temp[0],temp[1],Integer.parseInt(temp[2])));
+            transaction.add(FruitTransaction.of(temp[0],temp[1],Integer.parseInt(temp[2])));
         }
-        return transactionDao;
+        return transaction;
     }
 }
