49d1cba1324abe4331a59a3e0badd1e7cdb53ed2
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-02-21 17:54:55 +0200

    Removed empty line

--- src/main/java/core/basesyntax/service/operations/BalanceOperation.java
+++ src/main/java/core/basesyntax/service/operations/BalanceOperation.java
@@ -4,7 +4,6 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
 
 public class BalanceOperation implements FruitOperation {
-
     @Override
     public void operation(String[] data) {
         Fruit temporaryFruit = new Fruit(data[NAME_INDEX]);
