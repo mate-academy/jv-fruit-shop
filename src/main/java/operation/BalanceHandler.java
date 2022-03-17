7499bd0adbffb0be6fddc8b9571decd8bfd77fb3
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-03-17 17:13:38 +0200

    refactoring

--- src/main/java/operation/BalanceHandler.java
+++ src/main/java/operation/BalanceHandler.java
@@ -11,6 +11,15 @@ public class BalanceHandler implements Operation {
         if (fruitShopDao.getValue(fruit) == null) {
             fruitShopDao.save(fruit);
         }
+        if (fruit.getQuantity() == 0
+                || fruit.getFruit().isEmpty()
+                || fruit.getOperation() == null
+                || fruit.getFruit() == null) {
+            throw new NullPointerException("This line cannot be empty");
+        }
+        if (fruit.getQuantity() < 0) {
+            throw new RuntimeException("The amount cannot be negative");
+        }
         fruit.setQuantity(fruitShopDao.getValue(fruit) + fruit.getQuantity());
         fruitShopDao.save(fruit);
     }
