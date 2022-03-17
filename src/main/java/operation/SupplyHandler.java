6da89056f9fee6958d96bae653e8511e3e132438
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-03-17 17:14:31 +0200

    refactoring

--- src/main/java/operation/SupplyHandler.java
+++ src/main/java/operation/SupplyHandler.java
@@ -15,10 +15,10 @@ public class SupplyHandler implements Operation {
                 || fruit.getFruit().isEmpty()
                 || fruit.getOperation() == null
                 || fruit.getFruit() == null) {
-            throw new NullPointerException("This line cannot be empty");
+            throw new NullPointerException("This line cannot be empty.");
         }
         if (fruit.getQuantity() < 0) {
-            throw new RuntimeException("The amount cannot be negative");
+            throw new RuntimeException("The amount cannot be negative.");
         }
         fruit.setQuantity(fruitShopDao.getValue(fruit) + fruit.getQuantity());
         fruitShopDao.save(fruit);
