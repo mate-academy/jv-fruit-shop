f7a928126ec702fe0adc852fb208e236fdd96b45
Iryna Hrynda <hryndairyna1@gmail.com>
2022-04-19 17:16:33 +0300

    - fixed negative values in database

--- src/main/java/core/basesyntax/dao/StorageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/StorageDaoImpl.java
@@ -8,11 +8,7 @@ import java.util.Set;
 public class StorageDaoImpl implements StorageDao {
     @Override
     public void decreaseFruitQuantity(Fruit fruit, Integer quantity) {
-        if (Storage.storage.containsKey(fruit)) {
-            Storage.storage.put(fruit, Storage.storage.get(fruit) - quantity);
-        } else {
-            Storage.storage.put(fruit, -quantity);
-        }
+        Storage.storage.put(fruit, Storage.storage.get(fruit) - quantity);
     }
 
     @Override
