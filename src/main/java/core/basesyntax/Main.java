1547afed2d9e279811eb27f843f6d9e1d0971290
yuriikozar <yuriikozar@gmail.com>
2022-04-21 20:11:17 +0300

    Commit 1 Solution + checkstyle v2

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,18 +6,16 @@ import core.basesyntax.service.write.WriteServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        new WriteServiceImpl().write("abob.csv","type,fruit,quantity\n"
+        new WriteServiceImpl().write("input.csv","type,fruit,quantity\n"
                 + "b,banana,20\n"
                 + "b,apple,100\n"
-                + "b,aboba,1488\n"
-                + "s,banana,1488\n"
+                + "s,banana,14\n"
                 + "p,banana,15\n"
                 + "r,apple,10\n"
-                + "p,aboba,54\n"
                 + "p,apple,20\n"
                 + "p,banana,5\n"
                 + "s,banana,544");
-        doTask("abob.csv", "luftwaffe.csv");
+        doTask("input.csv", "report.csv");
     }
 
     private static void doTask(String fromFile, String toFile) {
