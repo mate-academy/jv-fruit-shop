a373f86400e4ae134ceb08438ad20effa8dbc270
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-04-28 23:50:34 +0300

    deleted constants from FruitTransactionServiceImpl

--- src/main/java/service/impl/FruitTransactionServiceImpl.java
+++ src/main/java/service/impl/FruitTransactionServiceImpl.java
@@ -6,10 +6,6 @@ import service.FruitTransactionService;
 import strategy.OperationStrategy;
 
 public class FruitTransactionServiceImpl implements FruitTransactionService {
-    private static final String COMA = ",";
-    private static final int INDEX_OF_FRUIT = 1;
-    private static final int INDEX_OF_OPERATION = 0;
-    private static final int INDEX_OF_QUANTITY = 2;
     private OperationStrategy operationStrategy;
 
     public FruitTransactionServiceImpl(OperationStrategy operationStrategy) {
