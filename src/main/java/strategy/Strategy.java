e30746d0c71965eda8f4ce08aba3315028f86a75
Dmytro Koliada <dmitrikstar@gmail.com>
2022-04-29 14:49:37 +0300

    added map constructor

--- src/main/java/strategy/Strategy.java
+++ src/main/java/strategy/Strategy.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Strategy {
-    public OperationHandler get(String operation) {
+    public Map<String, OperationHandler> buildMap() {
         Map<String, OperationHandler> operationHandlerMap = new HashMap<>();
         operationHandlerMap.put("r", new ReturnOperationHandler());
         operationHandlerMap.put("p", new PurchaseOperationHandlerImpl());
         operationHandlerMap.put("b", new BalanceOperationHandler());
         operationHandlerMap.put("s", new SupplyOperationHandler());
-        return operationHandlerMap.get(operation);
+        return operationHandlerMap;
+    }
+
+    public OperationHandler get(String operation) {
+        return buildMap().get(operation);
     }
 }
