8c6e9198b1ad2bfba8624b5763c98b5b483816c9
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-05-03 16:12:56 +0300

    solution was fixed

--- src/main/java/core/basesyntax/service/impl/FileReaderService.java
+++ src/main/java/core/basesyntax/service/impl/FileReaderService.java
@@ -15,7 +15,8 @@ public class FileReaderService implements core.basesyntax.service.FileReaderServ
         try (Stream<String> lines = Files.lines(Path.of(path))) {
             readFromFile = lines.collect(Collectors.toList());
         } catch (IOException e) {
-            throw new RuntimeException("Cant`t read data from file" + path, e);
+            throw new RuntimeException("Cant`t read data from file"
+                    + Path.of(path).getFileName().toString(), e);
         }
         return readFromFile;
     }
