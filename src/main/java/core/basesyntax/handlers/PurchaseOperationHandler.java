ea6ad3ac5d0a82c07dbb0ffff1ce6d8d95bfaa98
Andrii Fabersky <Andriifabersky@gmail.com>
2022-05-03 10:28:54 +0300

    I fix mistake

--- src/main/java/core/basesyntax/handlers/PurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/handlers/PurchaseOperationHandler.java
@@ -6,7 +6,7 @@ import core.basesyntax.storage.Storage;
 public class PurchaseOperationHandler implements OperationHandler {
     public void process(Fruit fruit, Integer quantity) {
         Integer initialQuantity = Storage.storage.get(fruit);
-        if (quantity >= initialQuantity) {
+        if (quantity > initialQuantity) {
             throw new RuntimeException("Not enough fruit");
         }
         Storage.storage.put(fruit, initialQuantity - quantity);
