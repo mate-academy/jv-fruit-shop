0550d28a77c9d77310ee589103338fbf478967d9
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-06-06 20:40:47 +0300

    checkstyle

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -17,14 +17,14 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        MyFileReader myFileReader = new MyFileReaderImpl();
-        FruitCounter fruitCounter = new FruitCounterImpl();
-        MyFileWriter myFileWriter = new MyFileWriterImpl();
         Map<String, FruitHandler> strategy = new HashMap<>();
         strategy.put("b", new FruitAdder());
         strategy.put("s", new FruitAdder());
         strategy.put("r", new FruitAdder());
         strategy.put("p", new FruitSubtractor());
+        MyFileReader myFileReader = new MyFileReaderImpl();
+        FruitCounter fruitCounter = new FruitCounterImpl();
+        MyFileWriter myFileWriter = new MyFileWriterImpl();
 
         List<String> info = myFileReader.getDryInfo("src/main/resources/input.csv");
         fruitCounter.countFruits(info, strategy);
--- src/main/java/core/basesyntax/strategy/FruitSubtractor.java
+++ src/main/java/core/basesyntax/strategy/FruitSubtractor.java
@@ -5,7 +5,7 @@ import core.basesyntax.db.Storage;
 public class FruitSubtractor implements FruitHandler {
     @Override
     public void handle(String[] line) {
-        Storage.storage.merge(line[INDEX_FRUIT], Integer.parseInt(line[INDEX_AMOUNT]), (a, b) -> {
+        Storage.getStorage().merge(line[INDEX_FRUIT], Integer.parseInt(line[INDEX_AMOUNT]), (a, b) -> {
             if (a - b < 0) {
                 throw new RuntimeException("Do not have enough fruits for purchasing");
             }
