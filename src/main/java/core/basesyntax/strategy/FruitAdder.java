f3d161fdcb78ced74c33e259d0c0f91e7f95deac
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-06-06 21:11:46 +0300

    починилось, сам не знаю почему, вроде всё красиво теперь

--- src/main/java/core/basesyntax/strategy/FruitAdder.java
+++ src/main/java/core/basesyntax/strategy/FruitAdder.java
@@ -5,7 +5,7 @@ import core.basesyntax.db.Storage;
 public class FruitAdder implements FruitHandler {
     @Override
     public void handle(String[] line) {
-        Storage.storage.merge(line[INDEX_FRUIT],
+        Storage.getStorage().merge(line[INDEX_FRUIT],
                 Integer.parseInt(line[INDEX_AMOUNT]), Integer::sum);
     }
 }
--- src/main/java/core/basesyntax/strategy/FruitSubtractor.java
+++ src/main/java/core/basesyntax/strategy/FruitSubtractor.java
@@ -5,11 +5,12 @@ import core.basesyntax.db.Storage;
 public class FruitSubtractor implements FruitHandler {
     @Override
     public void handle(String[] line) {
-        Storage.storage.merge(line[INDEX_FRUIT], Integer.parseInt(line[INDEX_AMOUNT]), (a, b) -> {
-            if (a - b < 0) {
-                throw new RuntimeException("Do not have enough fruits for purchasing");
-            }
-            return a - b;
-        });
+        Storage.getStorage().merge(line[INDEX_FRUIT],
+                Integer.parseInt(line[INDEX_AMOUNT]), (a, b) -> {
+                if (a - b < 0) {
+                    throw new RuntimeException("Do not have enough fruits for purchasing");
+                }
+                return a - b;
+            });
     }
 }
