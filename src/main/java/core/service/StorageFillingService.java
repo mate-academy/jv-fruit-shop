3ecd574d46890f5ea12d675bd4324230ce5928e6
yaroslav <morozov.yrv@gmail.com>
2022-06-07 17:35:55 +0300

    StorageFillingService interface added

--- src/main/java/core/service/StorageFillingService.java
+++ src/main/java/core/service/StorageFillingService.java
@@ -2,26 +2,6 @@ package core.service;
 
 import java.util.List;
 
-public class StorageFillingService {
-    private final UpdateFruitsStrategy updateFruitsStrategy;
-
-    public StorageFillingService(UpdateFruitsStrategy updateFruitsStrategy) {
-        this.updateFruitsStrategy = updateFruitsStrategy;
-    }
-
-    public void fillStorage(List<String> fruitsOperations) {
-        for (String fruitOperation : fruitsOperations) {
-            try {
-                fruitOperation = fruitOperation.trim();
-                String[] operationData = fruitOperation.split(",");
-                String operation = operationData[0];
-                String fruitName = operationData[1];
-                int fruitQuantity = Integer.parseInt(operationData[2]);
-                updateFruitsStrategy.get(operation).doOperation(fruitName,fruitQuantity);
-            } catch (NumberFormatException
-                     | NullPointerException e) {
-                continue;
-            }
-        }
-    }
+public interface StorageFillingService {
+    void fillStorage(List<String> fruitsOperations);
 }
