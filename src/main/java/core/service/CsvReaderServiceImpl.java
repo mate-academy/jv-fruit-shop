cfda87a0327da5e184252b59c20d829a74c0d12b
yaroslav <morozov.yrv@gmail.com>
2022-06-07 17:32:30 +0300

    getFruitOperations -> readFromFile

--- src/main/java/core/service/CsvReaderServiceImpl.java
+++ src/main/java/core/service/CsvReaderServiceImpl.java
@@ -5,12 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-public class ReadCsvService {
-    public List getFruitOperations(String fileName) {
+public class CsvReaderServiceImpl implements CsvReaderService {
+    public List<String> readFromFile(String fileName) {
         try {
             return Files.readAllLines(Path.of(fileName));
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file: " + e);
+            throw new RuntimeException("Can't read file: " + fileName, e);
         }
     }
 }
