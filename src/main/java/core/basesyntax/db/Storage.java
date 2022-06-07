9a654e8598c28b0fcf4ceeb919d06f95362ea75e
gdp <gdp@zigzag.ints.net>
2022-06-07 15:53:34 +0300

    code refactoring after mentor review (1)
    - add input file into Git;
    - move settings inside classes;
    - avoid of class Product;
    - now storage keep Map<String, Integer>
    - move call services into class Main.

--- src/main/java/core/basesyntax/db/Storage.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax.db;
-
-import core.basesyntax.model.Product;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Storage {
-    public static final List<Product> products = new ArrayList<>();
-}
