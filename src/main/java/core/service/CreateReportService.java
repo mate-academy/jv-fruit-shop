a37444d6b1dcd19fc461ebaed6f089aaedb8d39e
yaroslav <morozov.yrv@gmail.com>
2022-06-07 17:31:23 +0300

    createReport returns report as String

--- src/main/java/core/service/CreateReportService.java
+++ src/main/java/core/service/CreateReportService.java
@@ -1,24 +1,5 @@
 package core.service;
 
-import core.dao.FruitDao;
-import core.dao.FruitDaoImpl;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Map;
-
-public class CreateReportService {
-    public void createReport(String fileName) {
-        StringBuilder stringBuilder = new StringBuilder("fruit,quantity\n");
-        FruitDao fruitDao = new FruitDaoImpl();
-        Map<String,Integer> fruitsMap = fruitDao.getAll();
-        for (Map.Entry<String,Integer> entry : fruitsMap.entrySet()) {
-            stringBuilder.append(entry.getKey() + "," + entry.getValue() + "\n");
-        }
-        try {
-            Files.write(Path.of(fileName),stringBuilder.toString().getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to file " + e);
-        }
-    }
+public interface CreateReportService {
+    String createReport();
 }
