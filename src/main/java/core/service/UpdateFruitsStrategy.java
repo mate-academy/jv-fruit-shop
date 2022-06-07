82e06476017f757633b948ca8804d8c436f0f635
yaroslav <morozov.yrv@gmail.com>
2022-06-07 17:33:57 +0300

    removed

--- src/main/java/core/service/UpdateFruitsStrategy.java
+++
@@ -1,7 +0,0 @@
-package core.service;
-
-import core.service.handlers.FruitOperationHandler;
-
-public interface UpdateFruitsStrategy {
-    FruitOperationHandler get(String fruitOperation);
-}
--- src/main/java/core/service/handlers/FruitOperationHandler.java
+++
@@ -1,5 +0,0 @@
-package core.service.handlers;
-
-public interface FruitOperationHandler {
-    void doOperation(String fruitName, int fruitsQuantity);
-}
