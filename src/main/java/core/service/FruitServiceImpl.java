dd535d99d4a8867d2e8ee8f45d651db8d6892f12
yaroslav <morozov.yrv@gmail.com>
2022-06-08 16:28:47 +0300

    added FruitService interface + impl

---
+++ src/main/java/core/service/FruitServiceImpl.java
@@ -0,0 +1,28 @@
+package core.service;
+
+import core.dao.FruitDao;
+import java.util.Map;
+
+public class FruitServiceImpl implements FruitService {
+
+    private final FruitDao fruitDao;
+
+    public FruitServiceImpl(FruitDao fruitDao) {
+        this.fruitDao = fruitDao;
+    }
+
+    @Override
+    public void update(String fruitName, int quantity) {
+        fruitDao.update(fruitName, quantity);
+    }
+
+    @Override
+    public int get(String fruitName) {
+        return fruitDao.get(fruitName);
+    }
+
+    @Override
+    public Map<String, Integer> getAll() {
+        return fruitDao.getAll();
+    }
+}
