e5644a331035bb61bf22e2f9640c7f05a9cf04df
yaroslav <morozov.yrv@gmail.com>
2022-06-08 22:04:26 +0300

    pos renamed to index

--- src/main/java/core/service/impl/FruitTransactionProcessorServiceImpl.java
+++ src/main/java/core/service/impl/FruitTransactionProcessorServiceImpl.java
@@ -6,9 +6,9 @@ import core.service.handlers.OperationHandler;
 import java.util.List;
 
 public class FruitTransactionProcessorServiceImpl implements FruitTransactionProcessorService {
-    private static final int OPERATION_POS = 0;
-    private static final int FRUIT_NAME_POS = 1;
-    private static final int FRUITS_QUANTITY_POS = 2;
+    private static final int OPERATION_INDEX = 0;
+    private static final int FRUIT_NAME_INDEX = 1;
+    private static final int FRUITS_QUANTITY_INDEX = 2;
     private final OperationHandlerStrategy operationHandlerStrategy;
 
     public FruitTransactionProcessorServiceImpl(OperationHandlerStrategy operationHandlerStrategy) {
@@ -18,11 +18,11 @@ public class FruitTransactionProcessorServiceImpl implements FruitTransactionPro
     public void fillStorage(List<String> lines) {
         for (String line : lines) {
             String[] operationData = line.split(",");
-            String operation = operationData[OPERATION_POS];
+            String operation = operationData[OPERATION_INDEX];
             OperationHandler operationHandler = operationHandlerStrategy.get(operation);
             if (operationHandler != null) {
-                String fruitName = operationData[FRUIT_NAME_POS];
-                int fruitsQuantity = Integer.parseInt(operationData[FRUITS_QUANTITY_POS]);
+                String fruitName = operationData[FRUIT_NAME_INDEX];
+                int fruitsQuantity = Integer.parseInt(operationData[FRUITS_QUANTITY_INDEX]);
                 operationHandler.handle(fruitName,fruitsQuantity);
             }
         }
