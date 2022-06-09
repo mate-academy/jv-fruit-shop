539f8b1ed988437bf6f1ebb57d5fc27c21e458e4
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-06-09 16:19:40 +0300

    Implement NegativeQuantityHandlerImpl and PositiveQuantityHandlerImpl classes

---
+++ src/main/java/core/basesyntax/strategy/impl/PositiveQuantityHandlerImpl.java
@@ -0,0 +1,20 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.strategy.QuantityHandler;
+
+public class PositiveQuantityHandlerImpl implements QuantityHandler {
+    @Override
+    public Integer getQuantity(FruitTransaction fruitTransaction) {
+        if (fruitTransaction == null) {
+            throw new RuntimeException("FruitTransaction cannot be null");
+        }
+        if (fruitTransaction.getFruit() == null) {
+            throw new RuntimeException("Fruit cannot be null");
+        }
+        if (fruitTransaction.getFruit().getName() == null) {
+            throw new RuntimeException("Fruit name cannot be null");
+        }
+        return fruitTransaction.getFruit().getQuantity();
+    }
+}
