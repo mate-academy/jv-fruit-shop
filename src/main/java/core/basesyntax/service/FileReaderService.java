2dccd2a164e6a0b6e8fcc5fd04598c21c2d1bc5f
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-06-10 17:05:22 +0300

    Change return type from List<FruitTransaction> to List<String> and implementation in readFromFile() method

--- src/main/java/core/basesyntax/service/FileReaderService.java
+++ src/main/java/core/basesyntax/service/FileReaderService.java
@@ -1,8 +1,7 @@
 package core.basesyntax.service;
 
-import core.basesyntax.model.FruitTransaction;
 import java.util.List;
 
 public interface FileReaderService {
-    List<FruitTransaction> readFromFile(String filePath);
+    List<String> readFromFile(String filePath);
 }
