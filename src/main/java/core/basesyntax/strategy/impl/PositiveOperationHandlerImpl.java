7de1989daef5f22ea69a9aaefeb7b08b9baec043
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-06-10 16:38:09 +0300

    Remove checks for null FruitTransaction in NegativeOperationHandlerImpl and PositiveOperationHandlerImpl classes

--- src/main/java/core/basesyntax/strategy/impl/PositiveOperationHandlerImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/PositiveOperationHandlerImpl.java
@@ -6,15 +6,6 @@ import core.basesyntax.strategy.OperationHandler;
 public class PositiveOperationHandlerImpl implements OperationHandler {
     @Override
     public Integer getQuantity(FruitTransaction fruitTransaction) {
-        if (fruitTransaction == null) {
-            throw new RuntimeException("FruitTransaction cannot be null");
-        }
-        if (fruitTransaction.getFruit() == null) {
-            throw new RuntimeException("Fruit cannot be null");
-        }
-        if (fruitTransaction.getFruit().getName() == null) {
-            throw new RuntimeException("Fruit name cannot be null");
-        }
         return fruitTransaction.getFruit().getQuantity();
     }
 }
