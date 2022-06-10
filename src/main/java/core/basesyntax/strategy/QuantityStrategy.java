e56d12a7e717cd847bf90831180c91a94cdb9831
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-06-10 16:25:07 +0300

    Update src/main/java/core/basesyntax/strategy/QuantityStrategy.java
    Co-authored-by: Mykyta Arkhanhelskyi <nickarchangel97@gmail.com>

--- src/main/java/core/basesyntax/strategy/QuantityStrategy.java
+++ src/main/java/core/basesyntax/strategy/QuantityStrategy.java
@@ -2,6 +2,6 @@ package core.basesyntax.strategy;
 
 import core.basesyntax.model.FruitTransaction;
 
-public interface QuantityStrategy {
+public interface OperationStrategy {
     QuantityHandler getQuantityHandler(FruitTransaction.Operation operation);
 }
