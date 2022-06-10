9ee8fc8836854b6e699be24b97d6ae19ce0a74ce
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-06-10 19:12:01 +0300

    Change logic of writeToFile() method in CsvFileWriteServiceImpl class

--- src/main/java/core/basesyntax/service/impl/CsvFileWriteServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriteServiceImpl.java
@@ -1,7 +1,5 @@
 package core.basesyntax.service.impl;
 
-import core.basesyntax.db.Storage;
-import core.basesyntax.model.Fruit;
 import core.basesyntax.service.FileWriterService;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,12 +7,9 @@ import java.nio.file.Paths;
 
 public class CsvFileWriteServiceImpl implements FileWriterService {
     @Override
-    public void writeToFile(String filePath) {
+    public void writeToFile(String report, String filePath) {
         try (var writer = Files.newBufferedWriter(Paths.get(filePath))) {
-            writer.write("fruit,quantity");
-            for (Fruit fruit : Storage.fruits) {
-                writer.write(System.lineSeparator() + fruit.getName() + "," + fruit.getQuantity());
-            }
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file: " + filePath, e);
         }
