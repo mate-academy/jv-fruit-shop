b670af3a99b7539af43a91cf2d121853fb4357a3
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-06-10 17:13:09 +0300

    Remove FruitTransactionDao interface and FruitTransactionDaoImpl class

--- src/main/java/core/basesyntax/dao/FruitTransactionDao.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.dao;
-
-import core.basesyntax.model.FruitTransaction;
-import java.util.List;
-
-public interface FruitTransactionDao {
-    void addAll(List<FruitTransaction> fruitTransactions);
-}
