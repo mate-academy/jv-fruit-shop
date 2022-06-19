9ed6e92cc56a9874674002a07b33ebb0093d1fe0
Maksym <Maksym_Personal@Dibrovs-MacBook-Pro.local>
2022-06-19 12:41:11 +0300

    TransactionList removed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import core.basesyntax.db.TransactionList;
-import core.basesyntax.service.CsvFormatter;
 import core.basesyntax.service.CsvReader;
 import core.basesyntax.service.CsvWriter;
 
@@ -12,10 +10,8 @@ public class Main {
         CsvReader csvReader = new CsvReader();
         String fileContent = csvReader.readFile("src/main/java/core/basesyntax"
                 + "/resources/Input.csv");
-        CsvFormatter csvFormatter = new CsvFormatter();
-        csvFormatter.csvToStringArrayList(fileContent);
-        LineProcessor lineProcessor = new LineProcessor();
-        lineProcessor.processLine(TransactionList.getTransactionList());
+        RawLineProcessor rawLineProcessor = new RawLineProcessor();
+        rawLineProcessor.fileToMap(fileContent);
         ReportFormatter reportFormatter = new ReportFormatter();
         String report = reportFormatter.formatReportAsCsvString();
         CsvWriter csvWriter = new CsvWriter();
