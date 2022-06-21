5cfd68f2877001c31d44eafedd14763a0fddeac7
Maksym Karamavrov <makcumkaka@gmail.com>
2022-06-21 15:28:15 +0300

    Third commit. After review

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import model.FruitTransaction;
 import service.FruitHandler;
 import service.impl.BalanceOfFruits;
+import service.impl.PurchaseFruits;
+import service.impl.ReturnFruits;
+import service.impl.SupplyFruits;
 import servicecsv.CreateReport;
 import servicecsv.DataHandler;
 import servicecsv.DataOperating;
@@ -23,6 +26,7 @@ import strategy.impl.TransactionStrategyImpl;
 
 public class Main {
     private static final String OPERATION_PATH = "src/main/resources/operation.csv";
+    private static final String REPORT_PATH = "src/main/resources/report.csv";
 
     public static void main(String[] args) {
         Map<String, FruitTransaction.Operation> transactionHandlerMap = new HashMap<>();
@@ -34,9 +38,9 @@ public class Main {
         FruitsDao fruitsDao = new FruitsDaoImpl();
         Map<FruitTransaction.Operation, FruitHandler> fruitHandlerMap = new HashMap<>();
         fruitHandlerMap.put(FruitTransaction.Operation.BALANCE, new BalanceOfFruits(fruitsDao));
-        fruitHandlerMap.put(FruitTransaction.Operation.SUPPLY, new BalanceOfFruits(fruitsDao));
-        fruitHandlerMap.put(FruitTransaction.Operation.RETURN, new BalanceOfFruits(fruitsDao));
-        fruitHandlerMap.put(FruitTransaction.Operation.PURCHASE, new BalanceOfFruits(fruitsDao));
+        fruitHandlerMap.put(FruitTransaction.Operation.SUPPLY, new SupplyFruits(fruitsDao));
+        fruitHandlerMap.put(FruitTransaction.Operation.RETURN, new ReturnFruits(fruitsDao));
+        fruitHandlerMap.put(FruitTransaction.Operation.PURCHASE, new PurchaseFruits(fruitsDao));
 
         TransactionStrategy transactionStrategy
                 = new TransactionStrategyImpl(transactionHandlerMap);
@@ -47,12 +51,12 @@ public class Main {
         CreateReport reportCreator = new CreateReportImpl();
         dataHandler.handleData(
                 dataOperating.passFruitData(fileReaderService
-                        .readTheFruitsStorage("src/main/resources/operation.csv")));
+                        .readTheFruitsStorage(OPERATION_PATH)));
         List<String> listOfFruits = reportCreator.createReport(fruitsDao.getCurrentFruitAmount());
         FileWriterService fileWriterService = new FileWriterImpl();
         System.out.println(listOfFruits);
         System.out.println(fruitsDao.getCurrentFruitAmount());
         fileWriterService.writeToFile(
-                "src/main/resources/report.csv", String.valueOf(listOfFruits));
+                REPORT_PATH, String.valueOf(listOfFruits));
     }
 }
