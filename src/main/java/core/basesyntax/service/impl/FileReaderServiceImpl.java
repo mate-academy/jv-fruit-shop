51ce42b609043db43b8abfafb223078fc018971d
Karina Tanasiichuk <k.tanasiichuk18@gmail.com>
2022-06-22 16:03:12 +0300

    file reader service is improved

--- src/main/java/core/basesyntax/service/impl/FileReaderServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/FileReaderServiceImpl.java
@@ -1,22 +1,17 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.service.FileReaderService;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class FileReaderServiceImpl implements FileReaderService {
     @Override
     public List<String> readFromFile(String filePath) {
-        List<String> activities = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String value = reader.readLine();
-            while (value != null) {
-                activities.add(value);
-                value = reader.readLine();
-            }
+        List<String> activities;
+        try {
+            activities = Files.readAllLines(Path.of(filePath));
         } catch (IOException e) {
             throw new RuntimeException("Can't find file by path: " + filePath, e);
         }
