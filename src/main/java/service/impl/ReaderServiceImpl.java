611980017ff989a9cefddcd902fc69c69e5df6e0
Tymur Tsvietkov <tymur.tsvietkov@gmail.com>
2022-06-22 17:44:43 +0300

    ReaderServiceImpl changed

--- src/main/java/service/impl/ReaderServiceImpl.java
+++ src/main/java/service/impl/ReaderServiceImpl.java
@@ -1,22 +1,17 @@
 package service.impl;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import service.ReaderService;
 
 public class ReaderServiceImpl implements ReaderService {
     @Override
     public List<String> readFile(String fromFileName) {
-        List<String> dataFromFile = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String lineFromFile = bufferedReader.readLine();
-            while (lineFromFile != null) {
-                dataFromFile.add(lineFromFile);
-                lineFromFile = bufferedReader.readLine();
-            }
+        List<String> dataFromFile;
+        try {
+            dataFromFile = Files.readAllLines(Path.of(fromFileName));
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file " + fromFileName, e);
         }
