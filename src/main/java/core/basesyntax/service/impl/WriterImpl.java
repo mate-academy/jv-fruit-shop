a9e2e08289005eb96bdd812581b95ceba5651278
kqlynero <kqlynero@gmail.com>
2022-06-29 14:32:24 +0300

    -fixed checkstyle

--- src/main/java/core/basesyntax/service/impl/WriterImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriterImpl.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 public class WriterImpl implements Writer {
     @Override
     public void writeDataToFile(String fileName, String destinationFileName) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(destinationFileName))) {
+        try (BufferedWriter bufferedWriter =
+                     new BufferedWriter(new FileWriter(destinationFileName))) {
             bufferedWriter.write(fileName);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file " + destinationFileName, e);
