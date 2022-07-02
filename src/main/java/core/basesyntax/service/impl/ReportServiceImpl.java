57b211e60490762888a1a9184aa62f56e9feee54
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-07-02 15:47:36 +0300

    created report

---
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -0,0 +1,26 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.service.ReportService;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReportServiceImpl implements ReportService {
+    private final FruitDao fruitDao;
+
+    public ReportServiceImpl(FruitDao fruitDao) {
+        this.fruitDao = fruitDao;
+    }
+
+    @Override
+    public List<String> report(String line) {
+        List<String> list = fruitDao.getAll()
+                .entrySet()
+                .stream()
+                .map(e -> e.getKey() + "," + e.getValue().toString())
+                .collect(Collectors.toList());
+
+        list.add(0, line);
+        return list;
+    }
+}
