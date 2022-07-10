47304bd7e5795c5dc44137995dcebe53bbea9f74
l_osyash <los.nazar24@gmail.com>
2022-07-10 19:10:02 +0300

    Fruit and FruitTransaction models are implemented.

---
+++ src/main/java/core/basesyntax/model/Fruit.java
@@ -0,0 +1,26 @@
+package core.basesyntax.model;
+
+public class Fruit {
+    private String fruitName;
+    private int quantity;
+
+    public Fruit(String fruitName) {
+        this.fruitName = fruitName;
+    }
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+    public void setFruitName(String fruitName) {
+        this.fruitName = fruitName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
