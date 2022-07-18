ed8d930e7b6e2f68611674309937cc3fa04e590a
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-18 15:14:57 +0300

    Removed getBalanceList from PivotDao

--- src/main/java/core/basesyntax/dao/PivotDao.java
+++ src/main/java/core/basesyntax/dao/PivotDao.java
@@ -1,11 +1,8 @@
 package core.basesyntax.dao;
 
-import core.basesyntax.model.Product;
 import java.util.List;
-import java.util.Map;
 
 public interface PivotDao {
-    void writePivotFile(String fileName, List<String> stringList);
+    void writePivotFile(List<String> stringList);
 
-    List<String> getBalanceList(Map<Product, Integer> balanceByProduct);
 }
