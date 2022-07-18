66ec48b3672830e99b873d07d032876e440fdcdd
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-18 15:16:40 +0300

    Rebuild Main with new implementations

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,33 +1,29 @@
 package core.basesyntax;
 
-import core.basesyntax.dao.BalanceDaoImpl;
-import core.basesyntax.db.Storage;
-import core.basesyntax.model.Product;
+import core.basesyntax.dao.PivotDaoImpl;
+import core.basesyntax.dao.TransactionDaoImpl;
 import core.basesyntax.service.BalanceService;
 import core.basesyntax.service.BalanceServiceImpl;
 import core.basesyntax.strategy.OperationStrategyImpl;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
-    private static final String BALANCE_FILE_NAME = "src/main/resources/database.csv";
-    private static final String PIVOT_FILE_NAME = "src/main/resources/pivot.csv";
 
     public static void main(String[] args) {
 
         BalanceService balanceService
-                = new BalanceServiceImpl(new OperationStrategyImpl());
+                = new BalanceServiceImpl(new TransactionDaoImpl(), new PivotDaoImpl(), new OperationStrategyImpl());
 
         //1 Read data from CSV file
-        Storage storage = new Storage(new BalanceDaoImpl().getBalanceFromFile(BALANCE_FILE_NAME));
+        balanceService.getTransactionsFromFile();
 
         //2 Process this data
-        Map<Product, Integer> balance = balanceService.getBalance(storage.getTransactions());
+        balanceService.calculateBalance();
 
         //3 Generate a report on processed data
-        List<String> balanceReport = balanceService.makeBalanceReport(balance);
+        List<String> reportList = balanceService.makeBalanceReport();
 
         //4 Write report to new file
-        balanceService.exportPivotToFile(PIVOT_FILE_NAME, balanceReport);
+        balanceService.exportPivotToFile(reportList);
     }
 }
