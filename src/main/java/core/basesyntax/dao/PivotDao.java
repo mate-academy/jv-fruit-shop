917ba2db3d5f73da01f300dcf0750aea36723c71
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-18 15:38:45 +0300

    Refactored some code

--- src/main/java/core/basesyntax/dao/PivotDao.java
+++ src/main/java/core/basesyntax/dao/PivotDao.java
@@ -4,5 +4,4 @@ import java.util.List;
 
 public interface PivotDao {
     void writePivotFile(List<String> stringList);
-
 }
--- src/main/java/core/basesyntax/service/BalanceService.java
+++ src/main/java/core/basesyntax/service/BalanceService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public interface BalanceService {
     void getTransactionsFromFile();
-    void calculateBalance();
-    List<String> makeBalanceReport();
-    void exportPivotToFile(List<String> report);
 
+    void calculateBalance();
+
+    List<String> makeBalanceReport();
+
+    void exportPivotToFile(List<String> report);
 }
