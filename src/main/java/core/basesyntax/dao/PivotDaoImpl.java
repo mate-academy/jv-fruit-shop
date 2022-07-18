8d4d5d190cabff469259caa8e96fc99c942ef03f
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-18 15:19:07 +0300

    Removed writePivotFile from PivotDaoImpl

--- src/main/java/core/basesyntax/dao/PivotDaoImpl.java
+++ src/main/java/core/basesyntax/dao/PivotDaoImpl.java
@@ -1,35 +1,9 @@
 package core.basesyntax.dao;
 
-import core.basesyntax.model.Product;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class PivotDaoImpl implements PivotDao {
     @Override
-    public void writePivotFile(String fileName, List<String> stringList) {
-        Path filePath = Path.of(fileName);
-        String header = "product,quantity";
-        try (FileWriter fileWriter = new FileWriter(filePath.toFile());
-                PrintWriter printWriter = new PrintWriter(fileWriter)) {
-            printWriter.printf(header + System.lineSeparator());
-            for (String str : stringList) {
-                printWriter.printf(str + System.lineSeparator());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to file " + fileName, e);
-        }
+    public void writePivotFile(List<String> stringList) {
     }
-
-    @Override
-    public List<String> getBalanceList(Map<Product, Integer> balanceByProduct) {
-        return balanceByProduct.entrySet().stream()
-                .map(p -> (p.getKey().getType() + "," + p.getValue()))
-                .collect(Collectors.toList());
-    }
-
 }
