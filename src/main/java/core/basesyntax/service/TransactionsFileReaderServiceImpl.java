3a0435fd227e4c96c99b5d76c3b290b0ee5e0676
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-20 15:36:52 +0300

    Renamed BalanceFileReaderServiceImpl to TransactionsFileReaderServiceImpl. Now getTransactionsFromFile return List<Transaction>

--- src/main/java/core/basesyntax/service/TransactionsFileReaderServiceImpl.java
+++ src/main/java/core/basesyntax/service/TransactionsFileReaderServiceImpl.java
@@ -2,14 +2,15 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.TransactionDao;
 import core.basesyntax.dao.TransactionDaoCsvImpl;
-import core.basesyntax.db.Storage;
+import core.basesyntax.model.Transaction;
+import java.util.List;
 
-public class BalanceFileReaderServiceImpl implements BalanceFileReaderService {
+public class TransactionsFileReaderServiceImpl implements TransactionsFileReaderService {
     private static final String BALANCE_FILE_NAME = "src/main/resources/database.csv";
     private final TransactionDao transactionDao = new TransactionDaoCsvImpl();
 
     @Override
-    public void getTransactionsFromFile() {
-        Storage.transactions.addAll(transactionDao.readFromFile(BALANCE_FILE_NAME));
+    public List<Transaction> getTransactionsFromFile() {
+        return transactionDao.readFromFile(BALANCE_FILE_NAME);
     }
 }
