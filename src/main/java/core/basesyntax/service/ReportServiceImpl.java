1429d4954125c6ab6c517ee04a432368968f1cbf
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-20 14:06:40 +0300

    Changed header for pivot table

--- src/main/java/core/basesyntax/service/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/ReportServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class ReportServiceImpl implements ReportService {
     @Override
     public String makeReport(Map<Fruit, Integer> balance) {
-        String header = "product,quantity";
+        String header = "fruit,quantity";
         String balanceString = balance.entrySet().stream()
                 .map(p -> (p.getKey().getType() + "," + p.getValue()))
                 .collect(Collectors.joining(System.lineSeparator()));
