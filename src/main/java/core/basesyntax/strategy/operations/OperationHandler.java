2375a1a4c5ec4cb8669a3cd8b169c88f620290ba
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-21 12:42:40 +0300

    Removed comment from OperationHandler

--- src/main/java/core/basesyntax/strategy/operations/OperationHandler.java
+++ src/main/java/core/basesyntax/strategy/operations/OperationHandler.java
@@ -3,6 +3,5 @@ package core.basesyntax.strategy.operations;
 import core.basesyntax.model.Transaction;
 
 public interface OperationHandler {
-    //int getOperationalQuantity(int quantity);
     void handle(Transaction transaction);
 }
