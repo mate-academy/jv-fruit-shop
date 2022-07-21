2136f612a2228b626db61a85a6891934d9d67f32
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-21 14:28:45 +0300

    Remade main to process with new classes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,28 +1,29 @@
 package core.basesyntax;
 
 import core.basesyntax.dao.FileWriterImpl;
+import core.basesyntax.dao.FileReaderCsvImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.Transaction;
-import core.basesyntax.service.BalanceService;
-import core.basesyntax.service.BalanceServiceImpl;
+import core.basesyntax.service.TransactionProcessor;
+import core.basesyntax.service.TransactionProcessorImpl;
 import core.basesyntax.service.ReportServiceImpl;
-import core.basesyntax.service.TransactionsFileReaderServiceImpl;
+import core.basesyntax.service.TransitionConvertorImpl;
 import core.basesyntax.strategy.OperationStrategyImpl;
 import java.util.List;
 
 public class Main {
     private static final String PIVOT_FILE_NAME = "src/main/resources/pivot.csv";
+    private static final String BALANCE_FILE_NAME = "src/main/resources/database.csv";
 
     public static void main(String[] args) {
 
-        BalanceService balanceService = new BalanceServiceImpl(new OperationStrategyImpl());
-
+        TransactionProcessor transactionProcessor = new TransactionProcessorImpl(new OperationStrategyImpl());
         //1 Read data from CSV file
-        List<Transaction> transactionsFromFile =
-                new TransactionsFileReaderServiceImpl().getTransactionsFromFile();
+        List<String> stringsFromFile = new FileReaderCsvImpl().readFromFile(BALANCE_FILE_NAME);
+        List<Transaction> transactionsFromFile = new TransitionConvertorImpl().convert(stringsFromFile);
 
         //2 Process this data
-        Storage.fruits.addAll(balanceService.calculateBalance(transactionsFromFile));
+        Storage.fruits.addAll(transactionProcessor.process(transactionsFromFile));
 
         //3 Generate a report on processed data
         String report = new ReportServiceImpl().makeReport(Storage.fruits);
