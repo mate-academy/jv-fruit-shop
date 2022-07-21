ea5a0045554afabd2a14107dfb9572dd944e3a7d
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-21 12:26:18 +0300

    Removed redundant class TransactionsFileReaderService

--- src/main/java/core/basesyntax/service/TransactionsFileReaderService.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service;
-
-import core.basesyntax.model.Transaction;
-import java.util.List;
-
-public interface TransactionsFileReaderService {
-    List<Transaction> getTransactionsFromFile();
-}
