ec8e4091c10ecc9640d4c0ac6593d2bf78943542
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-21 12:41:32 +0300

    Renamed TransitionService to TransitionConvertor. Some little name changes. Removed filter from convert method

--- src/main/java/core/basesyntax/service/TransitionService.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service;
-
-import core.basesyntax.model.Transaction;
-import java.util.List;
-
-public interface TransitionService {
-    List<Transaction> getTransactionsList(List<String> fileLines);
-}
