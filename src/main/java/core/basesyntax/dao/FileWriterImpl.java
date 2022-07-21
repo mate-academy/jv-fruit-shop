a61d21034e01a983555882cad0d17fa377875c5e
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-21 14:31:49 +0300

    Changed to use inputted pathToFile

--- src/main/java/core/basesyntax/dao/FileWriterImpl.java
+++ src/main/java/core/basesyntax/dao/FileWriterImpl.java
@@ -6,16 +6,14 @@ import java.io.PrintWriter;
 import java.nio.file.Path;
 
 public class FileWriterImpl implements core.basesyntax.dao.FileWriter {
-    private static final String PIVOT_FILE_NAME = "src/main/resources/pivot.csv";
-
     @Override
     public void writeToFile(String pathToFile, String report) {
-        Path filePath = Path.of(PIVOT_FILE_NAME);
+        Path filePath = Path.of(pathToFile);
         try (FileWriter fileWriter = new FileWriter(filePath.toFile());
                 PrintWriter printWriter = new PrintWriter(fileWriter)) {
             printWriter.printf(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file " + PIVOT_FILE_NAME, e);
+            throw new RuntimeException("Can't write to file " + pathToFile, e);
         }
     }
 }
