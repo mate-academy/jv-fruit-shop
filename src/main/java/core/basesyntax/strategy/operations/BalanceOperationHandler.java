1a6bcd125b03f41432d5c5fb51abbb35cc7ae035
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-22 14:57:32 +0300

    Changed BalanceOperationHandler: balance transaction give us starting quantity of the day

--- src/main/java/core/basesyntax/strategy/operations/BalanceOperationHandler.java
+++ src/main/java/core/basesyntax/strategy/operations/BalanceOperationHandler.java
@@ -1,12 +1,13 @@
 package core.basesyntax.strategy.operations;
 
 import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
 import core.basesyntax.model.Transaction;
 
 public class BalanceOperationHandler implements OperationHandler {
     @Override
     public void handle(Transaction transaction) {
-        Storage.fruits.replace(transaction.getProduct(),
-                    Storage.fruits.get(transaction.getProduct()) + transaction.getQuantity());
+        Fruit fruit = transaction.getProduct();
+        Storage.fruits.put(fruit, transaction.getQuantity());
     }
 }
