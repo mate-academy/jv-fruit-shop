2a189f2484e65ad4ecc3f6fa39438797275770a7
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-22 14:58:39 +0300

    Removed input parameter from makeReport method of ReportService

--- src/main/java/core/basesyntax/service/ReportService.java
+++ src/main/java/core/basesyntax/service/ReportService.java
@@ -1,8 +1,5 @@
 package core.basesyntax.service;
 
-import core.basesyntax.model.Fruit;
-import java.util.List;
-
 public interface ReportService {
-    String makeReport(List<Fruit> fruits);
+    String makeReport();
 }
--- src/main/java/core/basesyntax/service/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/ReportServiceImpl.java
@@ -1,15 +1,13 @@
 package core.basesyntax.service;
 
 import core.basesyntax.db.Storage;
-import core.basesyntax.model.Fruit;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReportServiceImpl implements ReportService {
     private static final String HEADER = "fruit,quantity";
 
     @Override
-    public String makeReport(List<Fruit> fruits) {
+    public String makeReport() {
         String fruitsInfo = Storage.fruits.entrySet().stream()
                 .map(f -> f.getKey().getName() + "," + f.getValue())
                 .collect(Collectors.joining(System.lineSeparator()));
