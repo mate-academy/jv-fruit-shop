2d192634e8d3cc94f85688d629473e62e4fb5b96
Roman Voitalik <voitalik1@gmail.com>
2022-08-10 14:44:22 +0300

    add another interface FruitService for doing transactions

---
+++ src/main/java/core/basesyntax/service/impl/FruitServiceImpl.java
@@ -0,0 +1,15 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.FruitService;
+import core.basesyntax.service.OperationStrategy;
+
+import java.util.List;
+
+public class FruitServiceImpl implements FruitService {
+    @Override
+    public void transactions(FruitDao fruitDao, List<FruitTransaction> fruits, OperationStrategy operationStrategy) {
+
+    }
+}
