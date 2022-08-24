a6ec785d2163746e872d58de2f944481b378bdb0
Serhii Buria <sergeiburya@gmail.com>
2022-08-24 20:58:04 +0300

    added implementation of the class for reading data from a file

--- src/main/java/core/basesyntax/MainFruitShop.java
+++ src/main/java/core/basesyntax/MainFruitShop.java
@@ -1,12 +1,21 @@
 package core.basesyntax;
 
+import core.basesyntax.service.FileReaderService;
+import core.basesyntax.service.impl.FileReaderServiceImpl;
+
+import java.util.List;
+
 public class MainFruitShop {
     public static void main(String[] args) {
+        FileReaderService fileReaderService = new FileReaderServiceImpl();
+        List<String> data = fileReaderService.readFromFile(INPUT_DATA_FILE);
+        data.stream().forEach(System.out::println);
+
 
 
 
     }
 
-    private static final String FROM_INPUT_DATA_FILE = "src/main/resources/input_data.csv";
-    private static final String TO_OUTPUT_DATA_FILE = "src/main/resources/output_data.csv";
+    private static final String INPUT_DATA_FILE = "src/main/resources/input_data.csv";
+    private static final String OUTPUT_DATA_FILE = "src/main/resources/output_data.csv";
 }
--- src/main/java/core/basesyntax/service/impl/FileReaderServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/FileReaderServiceImpl.java
@@ -2,11 +2,20 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.service.FileReaderService;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class FileReaderServiceImpl implements FileReaderService {
     @Override
     public List<String> readFromFile(String pathName) {
-        return null;
+        List<String> readFromFile;
+        try {
+            readFromFile = Files.readAllLines(Path.of(pathName));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't find file named: " + pathName, e);
+        }
+        return readFromFile;
     }
 }
