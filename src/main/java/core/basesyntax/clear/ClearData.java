4ac5a122c183e970e8267167aff7ebecac9f05a9
Nazar <litvik.12@gmail.com>
2022-08-25 14:09:07 +0300

    added clearing data if fileReport is not empty

---
+++ src/main/java/core/basesyntax/clear/ClearData.java
@@ -0,0 +1,5 @@
+package core.basesyntax.clear;
+
+public interface ClearData {
+    void clearDataIfExist(String fileReport);
+}
--- src/main/java/core/basesyntax/writedata/DataWritingImpl.java
+++ src/main/java/core/basesyntax/writedata/DataWritingImpl.java
@@ -1,5 +1,7 @@
 package core.basesyntax.writedata;
 
+import core.basesyntax.clear.ClearData;
+import core.basesyntax.clear.ClearDataImpl;
 import core.basesyntax.report.ReportData;
 import core.basesyntax.report.ReportDataImpl;
 import java.io.BufferedWriter;
@@ -8,6 +10,11 @@ import java.io.IOException;
 
 public class DataWritingImpl implements DataWriting {
     private ReportData reportData;
+    private ClearData clearData;
+
+    public void setClearData(ClearData clearData) {
+        this.clearData = clearData;
+    }
 
     public void setReportData(ReportData reportData) {
         this.reportData = reportData;
@@ -15,10 +22,11 @@ public class DataWritingImpl implements DataWriting {
 
     @Override
     public void writeData(String fileReport) {
+        setClearData(new ClearDataImpl());
         setReportData(new ReportDataImpl());
+        clearData.clearDataIfExist(fileReport);
         String[] strings = reportData.createDataReport();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileReport, true))) {
-
             for (String data : strings) {
                 writer.write(data + System.lineSeparator());
             }
