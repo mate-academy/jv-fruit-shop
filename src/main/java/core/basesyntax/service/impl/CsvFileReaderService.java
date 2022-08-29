5f906b6a761300de33f09790e8e95dc69b5b81bc
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-29 17:41:28 +0300

    move implementation class to another folder

--- src/main/java/core/basesyntax/service/impl/CsvFileReaderService.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileReaderService.java
@@ -1,4 +1,4 @@
-package core.basesyntax.service;
+package core.basesyntax.service.impl;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+import core.basesyntax.service.FileReaderService;
 
 public class CsvFileReaderService implements FileReaderService {
     @Override
