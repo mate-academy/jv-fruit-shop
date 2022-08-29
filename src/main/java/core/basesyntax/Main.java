02dd6eec3c417650079f1bcf512d34c1d64cfa1a
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-29 17:06:38 +0300

    create main

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File inputFile = new File("input.csv");
+        File outputFile = new File("output.csv");
+    }
+}
