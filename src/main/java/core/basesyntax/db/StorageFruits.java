c18927a8d3de4298f9bf34b26441f5d4a18b8ed5
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-29 21:18:05 +0300

    add storage

---
+++ src/main/java/core/basesyntax/db/StorageFruits.java
@@ -0,0 +1,8 @@
+package core.basesyntax.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StorageFruits {
+    public static final Map<String, Integer> fruits = new HashMap<>();
+}
