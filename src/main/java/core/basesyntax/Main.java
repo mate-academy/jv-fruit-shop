480a4a5d75f5968ec70132ae13732a2e05d0df6e
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-29 17:08:20 +0300

    work with file in main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,20 @@
 package core.basesyntax;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        File inputFile = new File("input.csv");
-        File outputFile = new File("output.csv");
+        File folder = new File("src\\main\\java\\core\\basesyntax\\resources");
+        File inputFile = new File(folder, "input.csv");
+
+        File outputFile = new File(folder, "output.csv");
+        if (!outputFile.exists()) {
+            try {
+                outputFile.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException("Output file not created", e);
+            }
+        }
     }
 }
