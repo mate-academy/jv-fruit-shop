44af206a4c6abe0cc1125cf343d1c5e86bb974b2
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-30 14:56:28 +0300

    override equals and hashcode methods

--- src/main/java/core/basesyntax/model/Fruit.java
+++ src/main/java/core/basesyntax/model/Fruit.java
@@ -1,5 +1,7 @@
 package core.basesyntax.model;
 
+import java.util.Objects;
+
 public class Fruit {
     private String name;
 
@@ -13,5 +15,18 @@ public class Fruit {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fruit fruit = (Fruit) o;
+        return Objects.equals(name, fruit.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
