11c6ac622bc5400200e24d2f7291bfd5500c49bf
Aptypio13 <aptypio668@gmail.com>
2022-08-30 21:45:35 +0300

    Create Storage.java static becouse we dont wont create a new object of storage and call ONE storege awerywere. Also create a Transaction.java class an object of transaction request from file data

---
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -0,0 +1,8 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.Fruit;
+import java.util.HashMap;
+
+public class Storage {
+    public static final HashMap<Fruit, Integer> storageDate = new HashMap<>();
+}
