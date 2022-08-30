fe084173b4f70813e2df9948ed0ba6adc09604fa
Aptypio13 <aptypio668@gmail.com>
2022-08-30 21:43:58 +0300

    create ReturnOperation.java where we do command return

---
+++ src/main/java/core/basesyntax/strategy/operations/ReturnOperation.java
@@ -0,0 +1,15 @@
+package core.basesyntax.strategy.operations;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.Transaction;
+import core.basesyntax.strategy.OperationStrategy;
+
+public class ReturnOperation implements OperationStrategy {
+    @Override
+    public void apply(Transaction transaction) {
+        Fruit fruit = transaction.getFruit();
+        Integer count = Storage.storageDate.get(fruit);
+        Storage.storageDate.put(fruit, count + transaction.getQuantity());
+    }
+}
