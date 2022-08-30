ec53a7e4208567177c23b53655940505b6d62483
fliarkovskyi <fliarkovskyi@gmail.com>
2022-08-30 17:50:57 +0300

    fix checkstyle ReportServiceImpl

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -2,19 +2,18 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.service.ReportService;
-
 import java.util.stream.Collectors;
 
 public class ReportServiceImpl implements ReportService {
-	private static final String HEADER = "fruit,quantity";
-	@Override
-	public String getReport() {
-		return HEADER + System.lineSeparator() +
-				Storage.getStorage()
-				.entrySet()
-				.stream()
-				.map(f -> f.getKey().getName() + "," + f.getValue())
-				.map((f) -> f + System.lineSeparator())
-				.collect(Collectors.joining());
-	}
+    private static final String HEADER = "fruit,quantity";
+
+    @Override
+    public String getReport() {
+        return HEADER
+                + System.lineSeparator()
+                + Storage.getStorage().entrySet().stream()
+                        .map(f -> f.getKey().getName() + "," + f.getValue())
+                        .map((f) -> f + System.lineSeparator())
+                        .collect(Collectors.joining());
+    }
 }
