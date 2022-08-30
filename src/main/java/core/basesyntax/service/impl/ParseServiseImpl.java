2893868476070c2a485f82569e05a727a6a6a49d
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-08-30 20:00:16 +0300

    fix mistakes using Lyubomyr tips

--- src/main/java/core/basesyntax/service/impl/ParseServiseImpl.java
+++ src/main/java/core/basesyntax/service/impl/ParseServiseImpl.java
@@ -8,15 +8,18 @@ import java.util.List;
 
 public class ParseServiseImpl implements ParseService {
     private static final String SEPARATOR = ",";
+    private static final int INDEX_OPERATION = 0;
+    private static final int INDEX_FRUIT = 1;
+    private static final int INDEX_QUANTUTY = 2;
 
     @Override
     public List<FruitTransaction> parseLine(List<String> lines) {
         List<FruitTransaction> result = new ArrayList<>();
         for (String line : lines) {
             String[] split = line.split(SEPARATOR);
-            int quantity = Integer.parseInt(split[2]);
-            Fruit fruit = new Fruit(split[1]);
-            String operation = split[0];
+            int quantity = Integer.parseInt(split[INDEX_QUANTUTY]);
+            Fruit fruit = new Fruit(split[INDEX_FRUIT]);
+            String operation = split[INDEX_OPERATION];
             result.add(new FruitTransaction(operation, fruit, quantity));
         }
         return result;
