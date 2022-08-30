8bf4f0ec0ee106b9e839bd3e4298c80b8bdafef4
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-30 16:24:26 +0300

    make getAll() method return Set<Fruit>

--- src/main/java/core/basesyntax/dao/StorageDao.java
+++ src/main/java/core/basesyntax/dao/StorageDao.java
@@ -4,9 +4,10 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
 
 import java.util.List;
+import java.util.Set;
 
 public interface StorageDao {
     Integer getAmount(Fruit fruit);
     void update(Fruit fruit, Integer amount);
-    List<Fruit> getAll();
+    Set<Fruit> getAll();
 }
