43b6308d55bc953f5c3cdc49138f8104d1fd8627
fliarkovskyi <fliarkovskyi@gmail.com>
2022-08-30 17:39:50 +0300

    fix checkstyle main

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Storage {
     private static Map<Fruit, Integer> storage = new HashMap<>();
 
-	public static Map<Fruit, Integer> getStorage() {
-		return storage;
-	}
+    public static Map<Fruit, Integer> getStorage() {
+        return storage;
+    }
 }
