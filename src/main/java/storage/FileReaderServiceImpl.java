67e6f006d64fb1ab2b3366db40ee67ca7525a031
Julia Kononenko <munguryan13@gmail.com>
2022-08-31 08:31:34 +0300

    added magic number

--- src/main/java/storage/FileReaderServiceImpl.java
+++ src/main/java/storage/FileReaderServiceImpl.java
@@ -9,11 +9,12 @@ import java.util.List;
 public class FileReaderServiceImpl implements FileReaderService {
     private static final File FILE_INPUT = new File("src/main/resources/input.csv");
     private static final Path PATH_OF_FILE = FILE_INPUT.toPath();
+    private static final int INDEX_OF_HEAD_ELEMENT = 0;
 
     public List<String> getInfoFromFile() {
         try {
             List<String> parsedInfo = Files.readAllLines(PATH_OF_FILE);
-            parsedInfo.remove(0);
+            parsedInfo.remove(INDEX_OF_HEAD_ELEMENT);
             return parsedInfo;
         } catch (IOException e) {
             throw new RuntimeException(String.format("Can't read from file %s", FILE_INPUT), e);
