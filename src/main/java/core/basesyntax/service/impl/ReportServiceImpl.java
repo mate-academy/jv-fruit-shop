b779d1cfb526c2156ed5a7456b07240b0e48349a
Aptypio13 <aptypio668@gmail.com>
2022-08-31 10:55:38 +0300

    Change method getReport to stream-version

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -3,21 +3,20 @@ package core.basesyntax.service.impl;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.service.ReportService;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ReportServiceImpl implements ReportService {
-    private static final String HEADER_FRUIT = "fruit";
-    private static final String HEADER_QUANTITY = "quantity";
+    private static final String HEADER_FRUIT = "fruit ";
+    private static final String HEADER_QUANTITY = "quantity ";
     private static final String APPENDER = ",";
 
     @Override
     public String getReport(Map<Fruit, Integer> storage) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(HEADER_FRUIT).append(APPENDER).append(HEADER_QUANTITY)
-                .append(System.lineSeparator());
-        for (Map.Entry<Fruit, Integer> entry : storage.entrySet()) {
-            stringBuilder.append(entry.getKey().getName()).append(APPENDER)
-                    .append(entry.getValue()).append(System.lineSeparator());
-        }
-        return stringBuilder.toString().trim();
+        return HEADER_FRUIT
+                + HEADER_QUANTITY
+                + System.lineSeparator()
+                + storage.entrySet().stream()
+                .map(s -> s.getKey().getName() + "," + s.getValue() + System.lineSeparator())
+                .collect(Collectors.joining());
     }
 }
