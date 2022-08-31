d1e21f1f5a5a3ffb5062816ed792bb5cf5e36549
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-08-31 10:58:37 +0300

    constance are created

--- src/main/java/core/basesyntax/service/impl/ParsedFileImpl.java
+++ src/main/java/core/basesyntax/service/impl/ParsedFileImpl.java
@@ -8,10 +8,12 @@ import java.util.stream.Collectors;
 
 public class ParsedFileImpl implements ParsedFile {
     private static final String SEPARATOR = ",";
+    private static final int INDEX_FIRST = 0;
+    private static final int INDEX_SECOND = 1;
 
     @Override
     public List<Transaction> parsedList(List<String> listFromParsed) {
-        listFromParsed.remove(0);
+        listFromParsed.remove(INDEX_FIRST);
         return listFromParsed.stream()
                 .map(l -> l.split(SEPARATOR))
                 .map(this::build)
@@ -22,11 +24,11 @@ public class ParsedFileImpl implements ParsedFile {
         Transaction transaction = new Transaction();
         for (Transaction.Operation operation : Transaction.Operation.values()) {
             if (operation.getOperation()
-                    .equals(list[0])) {
+                    .equals(list[INDEX_FIRST])) {
                 transaction.setOperation(operation);
             }
         }
-        Fruit fruit = new Fruit(list[1]);
+        Fruit fruit = new Fruit(list[INDEX_SECOND]);
         return new Transaction(transaction.getOperation(), fruit, Integer.parseInt(list[2]));
     }
 }
