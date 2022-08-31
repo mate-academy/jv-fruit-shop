a9c2ac8c3c45f532d7b9553fec1a32ab8f6404a1
IraHurko <iragurko19@gmail.com>
2022-08-31 11:51:01 +0300

    Add check in PurchaseOperationHandler

--- src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
@@ -4,14 +4,15 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.model.Transaction;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 public class PurchaseOperationHandler implements OperationHandler {
     @Override
     public void apply(Transaction transaction) {
         Fruit fruit = transaction.getFruit();
         Integer currentQuantity = Storage.get(fruit);
-        Storage.put(fruit, Optional.of(currentQuantity - transaction.getQuantity())
-                .orElseThrow(NoSuchElementException::new));
+        if (currentQuantity == null || currentQuantity < transaction.getQuantity()) {
+            throw new NoSuchElementException("Quantity of fruits is not enough for purchase");
+        }
+        Storage.put(fruit, currentQuantity - transaction.getQuantity());
     }
 }
