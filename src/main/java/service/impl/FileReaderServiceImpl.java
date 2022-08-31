57a26b2204c0f508142400cc9880b32f958b09a0
Julia Kononenko <munguryan13@gmail.com>
2022-08-31 16:59:22 +0300

    deleted constant

--- src/main/java/service/impl/FileReaderServiceImpl.java
+++ src/main/java/service/impl/FileReaderServiceImpl.java
@@ -7,12 +7,10 @@ import java.util.List;
 import service.FileReaderService;
 
 public class FileReaderServiceImpl implements FileReaderService {
-    private static final int INDEX_OF_HEAD_ELEMENT = 0;
 
     public List<String> getInfoFromFile(File file) {
         try {
             List<String> parsedInfo = Files.readAllLines(file.toPath());
-            parsedInfo.remove(INDEX_OF_HEAD_ELEMENT);
             return parsedInfo;
         } catch (IOException e) {
             throw new RuntimeException(String.format("Can't read from file %s", file), e);
