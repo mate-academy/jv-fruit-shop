f92e0a53046390307efedf3edee0f88ab0904f20
buuufy <s1mpleworkpost@gmail.com>
2022-08-31 17:32:10 +0300

    fix checkstyle

--- src/main/java/core/basesyntax/service/impl/ProcessDataServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ProcessDataServiceImpl.java
@@ -3,15 +3,14 @@ package core.basesyntax.service.impl;
 import core.basesyntax.model.Transaction;
 import core.basesyntax.service.ProcessDataService;
 import core.basesyntax.strategy.OperationStrategy;
-import core.basesyntax.strategy.OperationStrategyImpl;
 import core.basesyntax.strategy.handler.OperationHandler;
 import java.util.List;
 
 public class ProcessDataServiceImpl implements ProcessDataService {
-    OperationStrategy strategy;
+    private OperationStrategy strategy;
 
     public ProcessDataServiceImpl(OperationStrategy inputStrategy) {
-         strategy = inputStrategy;
+        strategy = inputStrategy;
     }
 
     @Override
