edd04d5cff2dc54b57eeff765f12ef20a68db3ea
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-08-31 18:55:00 +0300

    refacoring as commma and header constants

--- src/main/java/core/basesyntax/service/impl/ParserServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ParserServiceImpl.java
@@ -10,18 +10,19 @@ public class ParserServiceImpl implements ParseService {
     private static final int INDEX_OF_OPERATION = 0;
     private static final int INDEX_OF_FRUIT = 1;
     private static final int INDEX_OF_QUANTITY = 2;
-    private static final String SEPARATOR = ",";
+    private static final String COMMA = ",";
+    private static final int HEADER = 1;
 
     @Override
     public List<Transaction> parse(List<String> lines) {
         return lines.stream()
-                .skip(1)
+                .skip(HEADER)
                 .map(this::getTransaction)
                 .collect(Collectors.toList());
     }
 
     private Transaction getTransaction(String line) {
-        String[] fields = line.split(SEPARATOR);
+        String[] fields = line.split(COMMA);
         return new Transaction(fields[INDEX_OF_OPERATION],
                 new Fruit(fields[INDEX_OF_FRUIT]),
                 Integer.parseInt(fields[INDEX_OF_QUANTITY]));
