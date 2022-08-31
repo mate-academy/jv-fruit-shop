fa1eb9e8fca97f0f7d1828b55b087d61bec1d5ad
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-08-31 19:05:41 +0300

    refactoring constant to comma

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 
 public class ReportServiceImpl implements ReportService {
     private static final String HEADERS = "fruit,quantity";
-    private static final String SEPARATOR = ",";
+    private static final String COMMA = ",";
 
     @Override
     public String getReport() {
         String report = Storage.getData().entrySet().stream()
-                .map(totalFruits -> totalFruits.getKey() + SEPARATOR + totalFruits.getValue()
+                .map(totalFruits -> totalFruits.getKey() + COMMA + totalFruits.getValue()
                         + System.lineSeparator())
                 .collect(Collectors.joining());
         return HEADERS + System.lineSeparator() + report;
