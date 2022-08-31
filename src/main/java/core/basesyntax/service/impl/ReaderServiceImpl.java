16fc9e590adaaae994e9d711c61ac221d9742287
fliarkovskyi <fliarkovskyi@gmail.com>
2022-08-31 15:57:29 +0300

    fix ReaderServiceImpl

--- src/main/java/core/basesyntax/service/impl/ReaderServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReaderServiceImpl.java
@@ -10,13 +10,10 @@ import java.util.List;
 public class ReaderServiceImpl implements ReaderService {
     @Override
     public List<String> readFromFile(String fileName) {
-        List<String> lines;
-        Path path = Paths.get(fileName);
         try {
-            lines = Files.readAllLines(path);
+            return Files.readAllLines(Path.of(fileName));
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        return lines;
     }
 }
