818c16a37dd4bbf81898c9405fd1405e0eb6605f
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-31 12:31:12 +0300

    checkstyle

--- src/main/java/core/basesyntax/service/ParserService.java
+++ src/main/java/core/basesyntax/service/ParserService.java
@@ -1,9 +1,7 @@
 package core.basesyntax.service;
 
 import core.basesyntax.model.Transaction;
-import core.basesyntax.strategy.Strategy;
 import java.util.List;
-import java.util.Map;
 
 public interface ParserService {
     List<Transaction> parse(List<String> list);
