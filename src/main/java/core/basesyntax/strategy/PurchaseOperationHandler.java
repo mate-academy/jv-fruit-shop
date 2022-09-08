072a83efbd552441107c2773e70d9cbf5f45a4ff
Alina Nikiforova <0500302717a@gmail.com>
2022-09-08 12:31:59 +0200

    added PurchaseOperationHandler

---
+++ src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
@@ -0,0 +1,13 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+
+public class PurchaseOperationHandler implements OperationHandler {
+    @Override
+    public void getOperationHandler(FruitTransaction transaction) {
+        Integer currentQuantity = Storage.storageFruits.get(transaction.getFruit());
+        Storage.storageFruits.put(transaction.getFruit(),
+                currentQuantity - transaction.getQuantity());
+    }
+}
