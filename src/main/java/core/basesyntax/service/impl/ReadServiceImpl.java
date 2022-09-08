319266c0f4a3572c627707a102330fc4901abcff
Alina Nikiforova <0500302717a@gmail.com>
2022-09-08 12:32:25 +0200

    added ReadServiceImpl

---
+++ src/main/java/core/basesyntax/service/impl/ReadServiceImpl.java
@@ -0,0 +1,20 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.ReadService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ReadServiceImpl implements ReadService {
+    @Override
+    public List<String> readFromFile(String filePath) {
+        List<String> fruits;
+        try {
+            fruits = Files.readAllLines(Path.of(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read file: " + filePath);
+        }
+        return fruits;
+    }
+}
