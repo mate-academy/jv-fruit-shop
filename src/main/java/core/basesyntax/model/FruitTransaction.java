9312fa2f07cb1021a062d1a6f9e91621d3e9f13c
Alina Nikiforova <0500302717a@gmail.com>
2022-09-08 12:29:46 +0200

    added FruitTransaction

---
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -0,0 +1,61 @@
+package core.basesyntax.model;
+
+import java.util.Arrays;
+
+public class FruitTransaction {
+    private Operation operation;
+    private String fruit;
+    private int quantity;
+
+    public FruitTransaction(FruitTransaction.Operation operation, String fruit, int quantity) {
+        this.operation = operation;
+        this.fruit = fruit;
+        this.quantity = quantity;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "FruitTransaction{"
+                + "operation=" + operation
+                + ", fruit='" + fruit + '\''
+                + ", quantity=" + quantity
+                + '}';
+    }
+
+    public static Operation getFindOperation(String firstOperation) {
+
+        return Arrays.stream(Operation.values())
+               .filter(o -> o.getOperation().equals(firstOperation))
+               .findFirst()
+               .get();
+    }
+
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private String operation;
+
+        Operation(String operation) {
+            this.operation = operation;
+        }
+
+        public String getOperation() {
+            return operation;
+        }
+    }
+}
