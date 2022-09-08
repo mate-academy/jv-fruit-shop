0ad10dc62d213204ed22885a1215e597597a71bb
Alina Nikiforova <0500302717a@gmail.com>
2022-09-08 12:31:30 +0200

    added ParseFruits

---
+++ src/main/java/core/basesyntax/service/ParseFruits.java
@@ -0,0 +1,11 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.FruitTransaction;
+import java.util.List;
+
+public interface ParseFruits {
+    List<FruitTransaction> parse(List<String> lines);
+
+    FruitTransaction transaction(String line);
+
+}
