33c972a5f0d6c7b446fdda942a01561e617be252
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-09-09 13:01:28 +0300

    create dao

---
+++ src/main/java/core/basesyntax/dao/impl/StorageDaoImpl.java
@@ -0,0 +1,23 @@
+package core.basesyntax.dao.impl;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.storage.Storage;
+
+import java.util.Map;
+
+public class StorageDaoImpl implements StorageDao {
+    @Override
+    public void updateDataStorage(String descriptionOfGoods, Integer quantity) {
+        Storage.storage.put(descriptionOfGoods, quantity);
+    }
+
+    @Override
+    public Integer getRemainingGoods(String descriptionOfGoods) {
+        return Storage.storage.get(descriptionOfGoods);
+    }
+
+    @Override
+    public Map<String, Integer> getAllData() {
+        return Storage.storage;
+    }
+}
