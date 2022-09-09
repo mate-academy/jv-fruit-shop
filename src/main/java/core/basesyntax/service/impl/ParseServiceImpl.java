afd5a01616c07bc6cd02d521cbbbef782905ef72
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-09-09 10:58:22 +0300

    correct parser implementation

--- src/main/java/core/basesyntax/service/impl/ParseServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ParseServiceImpl.java
@@ -10,10 +10,12 @@ public class ParseServiceImpl implements ParseService {
     private static final int GOODS_NAME_INDEX = 1;
     private static final int QUANTITY_INDEX = 2;
     private static final String SEPARATOR = ",";
+    private static final int HEADER_INDEX = 0;
 
     @Override
     public List<Transaction> parser(List<String> data) {
         List<Transaction> resultData = new ArrayList<>();
+        data.remove(HEADER_INDEX);
         data.stream().map(s -> s.split(SEPARATOR))
                 .forEach(s -> resultData.add(new Transaction(Transaction.findOperationByName(s[TYPE_OPERATION_INDEX]),
                         s[GOODS_NAME_INDEX], Integer.parseInt(s[QUANTITY_INDEX]))));
