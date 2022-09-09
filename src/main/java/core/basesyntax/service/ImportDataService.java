fdf6fe60b2a6497d36e01a255aad32b85708dad2
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-09-09 08:41:05 +0300

    import data service implementation

---
+++ src/main/java/core/basesyntax/service/ImportDataService.java
@@ -0,0 +1,8 @@
+package core.basesyntax.service;
+
+import java.util.List;
+
+public interface ImportDataService {
+    public List<String> ReaderFromFile(String path);
+
+}
---
+++ src/main/java/core/basesyntax/service/impl/ImportDataServiceImpl.java
@@ -0,0 +1,18 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.ImportDataService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class  ImportDataServiceImpl implements ImportDataService {
+    @Override
+    public List<String> ReaderFromFile(String path) {
+        try {
+            return Files.readAllLines(Path.of(path));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file " + path);
+        }
+    }
+}
