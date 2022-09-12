4add493a1fdd43c567872923167f689c37c02ec3
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-09-12 12:13:45 +0300

    implementation Import data, parse data, processing, report, export data in main method

--- src/main/java/core/basesyntax/ShopMain.java
+++ src/main/java/core/basesyntax/ShopMain.java
@@ -3,15 +3,17 @@ package core.basesyntax;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.impl.StorageDaoImpl;
 import core.basesyntax.model.Transaction;
-import core.basesyntax.service.OperationProcessingStrategy;
-import core.basesyntax.service.impl.OperationProcessingStrategyImpl;
+import core.basesyntax.service.*;
+import core.basesyntax.service.impl.*;
 import core.basesyntax.strategy.TransactionsHandling;
 import core.basesyntax.strategy.impl.BalanceHandlingImpl;
 import core.basesyntax.strategy.impl.PurchaseHandlingImpl;
 import core.basesyntax.strategy.impl.ReturnHandlingImpl;
 import core.basesyntax.strategy.impl.SupplyHandlingImpl;
 
+import java.io.Reader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ShopMain {
@@ -19,6 +21,7 @@ public class ShopMain {
     private static final String TO_FILE = "src/main/resources/daily_report.csv";
 
     public static void main(String[] args) {
+        //Creation of instances of objects dao, map strategy, operation processing services
         StorageDao storageDao = new StorageDaoImpl();
         Map<Transaction.Operation, TransactionsHandling> strategy =
                 new HashMap<>();
@@ -28,5 +31,21 @@ public class ShopMain {
         strategy.put(Transaction.Operation.RETURN, new ReturnHandlingImpl(storageDao));
         OperationProcessingStrategy operationProcessingStrategy =
                 new OperationProcessingStrategyImpl(strategy);
+        //Import data
+        ImportDataService reader = new ImportDataServiceImpl();
+        List<String> dailyTransactionList = reader.ReaderFromCsvFile(FROM_FILE);
+        //Parse data
+        ParseService parserService = new ParseServiceImpl();
+        List<Transaction> transactionList = parserService.parser(dailyTransactionList);
+        //Operation processing
+        OperationProcessing operationProcessing =
+                new OperationProcessingImpl(operationProcessingStrategy);
+        operationProcessing.processingData(transactionList);
+        //Create report
+        ReportService reportService = new ReportServiceImpl(storageDao);
+        String report = reportService.report();
+        //Export data
+        ExportDataService writer = new ExportDataServiceImpl();
+        writer.writeToCsvFile(report, TO_FILE);
     }
 }
