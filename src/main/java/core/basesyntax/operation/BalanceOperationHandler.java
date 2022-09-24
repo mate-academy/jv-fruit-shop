4e377532828b933977a1861b4eee68f2818bfb3f
Oleksandr <dovy_al@ukr.net>
2022-09-24 16:45:47 +0300

    created interface OperationHandler

---
+++ src/main/java/core/basesyntax/operation/BalanceOperationHandler.java
@@ -0,0 +1,17 @@
+package core.basesyntax.operation;
+
+import core.basesyntax.FruitTransaction;
+import core.basesyntax.dao.FruitDao;
+
+public class BalanceOperationHandler implements OperationHandler{
+    private final FruitDao fruitDao;
+
+    public BalanceOperationHandler (FruitDao fruitDao){
+        this.fruitDao = fruitDao;
+    }
+
+    @Override
+    public void handler(FruitTransaction transaction) {
+        fruitDao.addFruit(transaction.getFruit(), transaction.getQuantity());
+    }
+}
