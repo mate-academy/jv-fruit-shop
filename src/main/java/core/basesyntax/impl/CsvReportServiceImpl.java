0ae10895802b475766013c7bd3e4067e58af5b0c
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-09-29 22:31:10 +0300

    code reworked

--- src/main/java/core/basesyntax/impl/CsvReportServiceImpl.java
+++ src/main/java/core/basesyntax/impl/CsvReportServiceImpl.java
@@ -4,13 +4,11 @@ import core.basesyntax.service.ReportService;
 import java.util.Map;
 
 public class CsvReportServiceImpl implements ReportService {
-    private static final String REPORT_TITLE = "fruit,quantity";
     private static final String DELIMITER = ",";
 
     @Override
     public String createReport(Map<String, Integer> fruitStorage) {
         StringBuilder reportBuilder = new StringBuilder();
-        reportBuilder.append(REPORT_TITLE).append(System.lineSeparator());
         for (Map.Entry<String, Integer> entry: fruitStorage.entrySet()) {
             reportBuilder.append(entry.getKey())
                     .append(DELIMITER)
