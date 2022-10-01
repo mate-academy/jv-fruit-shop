4e2dac01f26d9ef34ef7c8d26353b284cb713824
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-01 19:28:33 +0300

    Added toString() method to fruit class

--- src/main/java/core/basesyntax/model/Fruit.java
+++ src/main/java/core/basesyntax/model/Fruit.java
@@ -26,4 +26,9 @@ public final class Fruit {
     public int hashCode() {
         return name != null ? name.hashCode() : 0;
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
