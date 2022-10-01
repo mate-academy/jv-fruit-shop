6fd3c14de6f173187509e40359aa750eb686aa6e
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-01 15:48:17 +0300

    Implement Fruit class

--- src/main/java/core/basesyntax/model/Fruit.java
+++ src/main/java/core/basesyntax/model/Fruit.java
@@ -1,4 +1,29 @@
 package core.basesyntax.model;
 
-public class Fruit {
+import java.util.Objects;
+
+public final class Fruit {
+    private final String name;
+    public Fruit(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Fruit fruit = (Fruit) o;
+
+        return Objects.equals(name, fruit.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
