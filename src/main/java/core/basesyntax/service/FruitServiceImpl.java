7274ad340164ce1347d43eb6f2ebfc2282c1b6ef
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-01 19:29:45 +0300

    Implement generate report method

--- src/main/java/core/basesyntax/service/FruitServiceImpl.java
+++ src/main/java/core/basesyntax/service/FruitServiceImpl.java
@@ -6,16 +6,32 @@ import core.basesyntax.strategy.OperationStrategy;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class FruitServiceImpl implements FruitService {
     private final Map<Operation, OperationStrategy> operations;
 
-    public FruitServiceImpl(final Map<Operation, OperationStrategy> operations) {
+    public FruitServiceImpl(final Map<Operation,
+            OperationStrategy> operations) {
         this.operations = operations;
     }
 
     @Override
     public String generateReport(final List<FruitTransaction> transactions) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder("fruit,quantity");
+        stringBuilder.append(System.lineSeparator());
+        String result = transactions
+                .stream()
+                .collect(Collectors.groupingBy(
+                                FruitTransaction::getFruit,
+                                Collectors.summingInt(e -> operations.get(e.getOperation()).calculate(e.getQuantity()))
+                        ))
+                .entrySet()
+                .stream()
+                .map(e-> e.getKey() + "," + e.getValue())
+                .collect(Collectors.joining(System.lineSeparator()));
+        stringBuilder.append(result);
+        return stringBuilder.toString();
     }
 }
