9d17e05048fa0dfc8242b1447f198153cf25da92
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-01 19:27:46 +0300

    Added Operation from string get

--- src/main/java/core/basesyntax/model/Operation.java
+++ src/main/java/core/basesyntax/model/Operation.java
@@ -1,12 +1,17 @@
 package core.basesyntax.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Operation {
     BALANCE("b"),
     SUPPLY("s"),
     PURCHASE("p"),
     RETURN("r");
-
+    
     private final String operation;
+
+    private static final Map<String,Operation> ENUM_MAP;
 
     Operation(String operation) {
         this.operation = operation;
@@ -14,5 +19,21 @@ public enum Operation {
 
     public String getOperation() {
         return operation;
+    }
+
+    static {
+        Map<String,Operation> map = new HashMap<>();
+        for (final Operation instance : Operation.values()) {
+            map.put(instance.getOperation(),instance);
+        }
+        ENUM_MAP = map;
+    }
+
+    public static Operation get(String operation){
+        Operation o = ENUM_MAP.get(operation);
+        if(o == null){
+            throw new RuntimeException("Couldn't find enum value for " + operation);
+        }
+        return o;
     }
 }
