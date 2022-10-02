b947039430df43439c9b089b11239dbe6c74df57
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-02 22:58:46 +0300

    Add Storage

---
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -0,0 +1,15 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.Fruit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storage {
+    public static final Map<Fruit,Integer> storage;
+
+    static {
+        storage = new HashMap<>();
+    }
+
+}
