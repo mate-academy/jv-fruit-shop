b9f1974acb94f166b072acb4c5c481f0f71d5325
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-10-03 20:13:03 +0300

    code passed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,13 +2,20 @@ package core.basesyntax;
 
 import core.basesyntax.dao.FileReaderImpl;
 import core.basesyntax.dao.FileWriterImpl;
+import core.basesyntax.model.Operation;
 import core.basesyntax.model.Transaction;
 import core.basesyntax.service.CsvReportServiceImpl;
 import core.basesyntax.service.TransactionProcessor;
 import core.basesyntax.service.TransactionProcessorImpl;
 import core.basesyntax.service.TransitionConvertorImpl;
 import core.basesyntax.strategy.OperationStrategyImpl;
+import core.basesyntax.strategy.operations.BalanceOperationHandler;
+import core.basesyntax.strategy.operations.PurchaseOperationHandler;
+import core.basesyntax.strategy.operations.ReturnOperationHandler;
+import core.basesyntax.strategy.operations.SupplyOperationHandler;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     private static final String PIVOT_FILE_NAME = "src/main/resources/pivot.csv";
@@ -23,5 +30,14 @@ public class Main {
         transactionProcessor.process(transactionsFromFile);
         String report = new CsvReportServiceImpl().makeReport();
         new FileWriterImpl().writeToFile(PIVOT_FILE_NAME, report);
+    }
+
+    private static Map<Operation, BalanceOperationHandler> createOperationHandlerMap() {
+        HashMap<Operation, BalanceOperationHandler> operationHandlerMap = new HashMap<>();
+        operationHandlerMap.put(Operation.BALANCE, new BalanceOperationHandler());
+        operationHandlerMap.put(Operation.PURCHASE, new PurchaseOperationHandler());
+        operationHandlerMap.put(Operation.RETURN, new ReturnOperationHandler());
+        operationHandlerMap.put(Operation.SUPPLY, new SupplyOperationHandler());
+        return operationHandlerMap;
     }
 }
