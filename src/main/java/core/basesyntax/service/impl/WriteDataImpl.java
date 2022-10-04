a5287c24caf77b08b9548e0b53c1e4f131052a01
Vasyl Moshun <vasylmoshun@gmail.com>
2022-10-04 17:28:35 +0300

    Update src/main/java/core/basesyntax/service/impl/WriteDataImpl.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/service/impl/WriteDataImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriteDataImpl.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WriteDataImpl implements WriteData {
-
     @Override
     public void writeToFile(String report, String filePath) {
         File file = new File(filePath);
