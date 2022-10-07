2b2aa625746d6ac9aba1f70059420b87961a57e6
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-10-07 16:36:38 +0300

    Update ReportCsvParser.java

--- src/main/java/core/basesyntax/service/ReportCsvParser.java
+++ src/main/java/core/basesyntax/service/ReportCsvParser.java
@@ -3,7 +3,6 @@ package core.basesyntax.service;
 import core.basesyntax.model.FruitTransaction;
 import java.util.List;
 
-public interface ReportCsvParser {
-    
+public interface ReportCsvParser {  
     List<FruitTransaction> parse(List<String> dataFromFile);
 }
