d4e967cac2a1ebe195ea0f07fa4cea5cd39878e8
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-10-08 23:55:57 +0300

    Update OperationProcessing.java

--- src/main/java/core/basesyntax/service/OperationProcessing.java
+++ src/main/java/core/basesyntax/service/OperationProcessing.java
@@ -3,5 +3,5 @@ package core.basesyntax.service;
 import core.basesyntax.model.FruitTransaction;
 
 public interface OperationProcessing {
-    void count(FruitTransaction fruitTransaction);
+    void process(FruitTransaction fruitTransaction);
 }
