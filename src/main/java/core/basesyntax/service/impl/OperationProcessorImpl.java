7a2eb3407c8749dbba8f032e073a8a3ec6d39118
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-10-12 16:35:50 +0300

    Update OperationProcessorImpl.java

--- src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
+++ src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
@@ -1,14 +1,27 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.dao.FruitDao;
+import core.basesyntax.dao.impl.FruitDaoImpl;
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.OperationProcessor;
+import core.basesyntax.service.OperationProcessing;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
+import core.basesyntax.strategy.impl.BalanceOperationHandler;
+import core.basesyntax.strategy.impl.PurchaseOperationHandler;
+import core.basesyntax.strategy.impl.ReturnOperationHandler;
+import core.basesyntax.strategy.impl.SupplyOperationHandler;
 
 public class OperationProcessorImpl implements OperationProcessor {
-    private FruitDao fruitDao;
+    private static FruitDao fruitDao = new FruitDaoImpl();
     private OperationStrategy operationStrategy;
+    public static Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
+
+    static {
+        operationHandlerMap = Map.of(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler(fruitDao),
+                FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler(fruitDao),
+                FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler(fruitDao),
+                FruitTransaction.Operation.RETURN, new ReturnOperationHandler(fruitDao));
+    }
 
     public OperationProcessorImpl(FruitDao fruitDao, OperationStrategy operationStrategy) {
         this.fruitDao = fruitDao;
