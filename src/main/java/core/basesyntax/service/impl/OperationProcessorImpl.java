11f12cf78580d7ea4d11a131efc8873dc7105688
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-10-12 16:40:16 +0300

    Update OperationProcessorImpl.java

--- src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
+++ src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
@@ -3,7 +3,7 @@ package core.basesyntax.service.impl;
 import core.basesyntax.dao.FruitDao;
 import core.basesyntax.dao.impl.FruitDaoImpl;
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.OperationProcessing;
+import core.basesyntax.service.OperationProcessor;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
 import core.basesyntax.strategy.impl.BalanceOperationHandler;
