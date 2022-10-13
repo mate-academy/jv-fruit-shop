74468d7d25b216f845ce60626d03ec3926755a4d
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-10-13 17:37:36 +0300

    Update OperationProcessorImpl.java

--- src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
+++ src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
@@ -1,14 +1,13 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.dao.FruitDao;
-import core.basesyntax.dao.impl.FruitDaoImpl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.OperationProcessor;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
 
 public class OperationProcessorImpl implements OperationProcessor {
-    private FruitDao fruitDao = new FruitDaoImpl();
+    private FruitDao fruitDao;
     private OperationStrategy operationStrategy;
 
     public OperationProcessorImpl(FruitDao fruitDao, OperationStrategy operationStrategy) {
