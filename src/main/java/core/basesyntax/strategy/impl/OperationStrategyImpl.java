afaaf966f1b85871c1f560e3a3de75c49fc556b3
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-10-13 17:10:19 +0300

    Update OperationStrategyImpl.java

--- src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
@@ -1,12 +1,27 @@
 package core.basesyntax.strategy.impl;
 
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.dao.impl.FruitDaoImpl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
 import java.util.Map;
 
 public class OperationStrategyImpl implements OperationStrategy {
+    private FruitDao fruitDao = new FruitDaoImpl();
     private Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
+
+    public OperationStrategyImpl() {
+        operationHandlerMap
+                = Map.of(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler(fruitDao),
+                FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler(fruitDao),
+                FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler(fruitDao),
+                FruitTransaction.Operation.RETURN, new ReturnOperationHandler(fruitDao));
+    }
+
+    public Map<FruitTransaction.Operation, OperationHandler> getOperationHandlerMap() {
+        return operationHandlerMap;
+    }
 
     public OperationStrategyImpl(Map<FruitTransaction.Operation,
             OperationHandler> operationHandlerMap) {
