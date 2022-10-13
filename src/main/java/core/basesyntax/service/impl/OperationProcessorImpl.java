34afed21d29f70e3ef258e5df9b8d8e525e52165
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-10-13 17:10:13 +0300

    Update OperationProcessorImpl.java

--- src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
+++ src/main/java/core/basesyntax/service/impl/OperationProcessorImpl.java
@@ -6,35 +6,14 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.OperationProcessor;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
-import core.basesyntax.strategy.impl.BalanceOperationHandler;
-import core.basesyntax.strategy.impl.PurchaseOperationHandler;
-import core.basesyntax.strategy.impl.ReturnOperationHandler;
-import core.basesyntax.strategy.impl.SupplyOperationHandler;
-import java.util.Map;
 
 public class OperationProcessorImpl implements OperationProcessor {
-    private static final Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
-    private static FruitDao fruitDao = new FruitDaoImpl();
-    private static OperationStrategy operationStrategy;
-
-    static {
-        operationHandlerMap = 
-            Map.of(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler(fruitDao),
-                FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler(fruitDao),
-                FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler(fruitDao),
-                FruitTransaction.Operation.RETURN, new ReturnOperationHandler(fruitDao));
-    }
-
-    public OperationProcessorImpl() {
-    }
+    private FruitDao fruitDao = new FruitDaoImpl();
+    private OperationStrategy operationStrategy;
 
     public OperationProcessorImpl(FruitDao fruitDao, OperationStrategy operationStrategy) {
         this.fruitDao = fruitDao;
         this.operationStrategy = operationStrategy;
-    }
-
-    public Map<FruitTransaction.Operation, OperationHandler> getOperationHandlerMap() {
-        return operationHandlerMap;
     }
 
     @Override
