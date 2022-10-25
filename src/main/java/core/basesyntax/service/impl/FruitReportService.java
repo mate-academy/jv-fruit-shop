07e2fe35f57a3c95e06ac895eb682236e58523c0
shmax <mshveda28@gmail.com>
2022-10-25 14:17:19 +0300

    Add constant to store separator

--- src/main/java/core/basesyntax/service/impl/FruitReportService.java
+++ src/main/java/core/basesyntax/service/impl/FruitReportService.java
@@ -5,13 +5,14 @@ import java.util.Map;
 
 public class FruitReportService implements ReportService {
     private static final String HEADER = "fruit,quantity";
+    private static final String CSV_SEPARATOR = ",";
 
     @Override
     public String calculateReport(Map<String, Integer> data) {
         StringBuilder report = new StringBuilder(HEADER);
         data.forEach((key, value)
                 -> report.append(System.lineSeparator())
-                .append(key).append(",").append(value)
+                .append(key).append(CSV_SEPARATOR).append(value)
         );
         return report.toString();
     }
