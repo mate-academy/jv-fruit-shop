dd55d1e6d8b9ecb99d599705cdd1cc9fa52be16b
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-10-25 14:12:21 +0300

    Divided logic parse and supply data. v5

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,34 +5,17 @@ import core.basesyntax.service.impl.FileReaderServiceImpl;
 import core.basesyntax.service.impl.FileWriterServiceImpl;
 import core.basesyntax.service.impl.ReportServiceImpl;
 import core.basesyntax.service.impl.TransactionParserImpl;
-import core.basesyntax.strategy.Strategy;
-import core.basesyntax.strategy.StrategyImpl;
-import core.basesyntax.strategy.operation.Operation;
-import core.basesyntax.strategy.operation.impl.Balance;
-import core.basesyntax.strategy.operation.impl.Purchase;
-import core.basesyntax.strategy.operation.impl.Return;
-import core.basesyntax.strategy.operation.impl.Supply;
-import java.util.HashMap;
+import core.basesyntax.strategy.operation.impl.FruitService;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     private static final String INPUT_FILE_NAME = "src/main/resources/TransactionsPerDay.csv";
     private static final String OUTPUT_FILE_NAME = "src/main/resources/DailyReport.csv";
 
     public static void main(String[] args) {
-        Map<Transaction.Operation, Operation> map = new HashMap<>();
-        map.put(Transaction.Operation.BALANCE, new Balance());
-        map.put(Transaction.Operation.SUPPLY, new Supply());
-        map.put(Transaction.Operation.RETURN, new Return());
-        map.put(Transaction.Operation.PURCHASE, new Purchase());
-        Strategy operationStrategy = new StrategyImpl(map);
         List<String> data = new FileReaderServiceImpl().readFromFile(INPUT_FILE_NAME);
         List<Transaction> transactions = new TransactionParserImpl().parse(data);
-        for (Transaction transaction : transactions) {
-            Operation handler = operationStrategy.getByOperation(transaction.getOperation());
-            handler.apply(transaction);
-        }
+        new FruitService().processTransaction(transactions);
         String report = new ReportServiceImpl().getReport();
         new FileWriterServiceImpl().writeToFile(report, OUTPUT_FILE_NAME);
     }
--- src/main/java/core/basesyntax/strategy/operation/impl/FruitService.java
+++ src/main/java/core/basesyntax/strategy/operation/impl/FruitService.java
@@ -3,8 +3,27 @@ package core.basesyntax.strategy.operation.impl;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.model.Transaction;
 import core.basesyntax.storage.Storage;
+import core.basesyntax.strategy.Strategy;
+import core.basesyntax.strategy.StrategyImpl;
+import core.basesyntax.strategy.operation.Operation;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FruitService {
+    public void processTransaction(List<Transaction> transactions) {
+        Map<Transaction.Operation, Operation> map = new HashMap<>();
+        map.put(Transaction.Operation.BALANCE, new Balance());
+        map.put(Transaction.Operation.SUPPLY, new Supply());
+        map.put(Transaction.Operation.RETURN, new Return());
+        map.put(Transaction.Operation.PURCHASE, new Purchase());
+        Strategy operationStrategy = new StrategyImpl(map);
+        for (Transaction transaction : transactions) {
+            Operation handler = operationStrategy.getByOperation(transaction.getOperation());
+            handler.apply(transaction);
+        }
+    }
+
     public void processIncomingTransaction(Transaction transaction) {
         Fruit fruit = transaction.getFruit();
         if (!Storage.storage.containsKey(fruit)) {
