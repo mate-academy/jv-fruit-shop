7156c6096d034837ebc73539a896c60f6971715e
Pavlo Gook <pavlogook@gmail.com>
2022-10-27 15:59:55 +0300

    Storage fruits has become a static field

--- src/main/java/core/basesyntax/dao/StorageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/StorageDaoImpl.java
@@ -5,20 +5,18 @@ import java.util.Map;
 import java.util.Set;
 
 public class StorageDaoImpl implements StorageDao {
-    private static final Map<String, Integer> FRUIT_MAP = new Storage().getFruits();
-
     @Override
     public Integer getValue(String key) {
-        return FRUIT_MAP.get(key);
+        return Storage.fruits.get(key);
     }
 
     @Override
     public Integer putValue(String key, Integer value) {
-        return FRUIT_MAP.put(key, value);
+        return Storage.fruits.put(key, value);
     }
 
     @Override
     public Set<Map.Entry<String, Integer>> getEntrySet() {
-        return FRUIT_MAP.entrySet();
+        return Storage.fruits.entrySet();
     }
 }
--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -4,9 +4,5 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    private final Map<String, Integer> fruits = new HashMap<>();
-
-    public Map<String, Integer> getFruits() {
-        return fruits;
-    }
+    public static final Map<String, Integer> fruits = new HashMap<>();
 }
