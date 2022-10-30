dcdca5f7262881422f3c024dc9aa4a5e618cf417
Pavlo.Polovyi <pasha32662@gmail.com>
2022-10-30 14:29:02 +0200

    Remove switch case from DataExtractor and shift this logic to enum.

--- src/main/java/core/fruitshop/OperationType.java
+++ src/main/java/core/fruitshop/OperationType.java
@@ -1,5 +1,28 @@
 package core.fruitshop;
 
+import core.fruitshop.exceptions.IncorrectOperationTypeException;
+
 public enum OperationType {
-    BALANCE, PURCHASE, RETURN, SUPPLY
+    BALANCE("b"), PURCHASE("p"), RETURN("r"), SUPPLY("s");
+    private static final String INCORRECT_TYPE_EXCEPTION = "Incorrect operation type. "
+            + "Expected: " + BALANCE.getType() + ","
+            + SUPPLY.getType() + "," + RETURN.getType() + "or"
+            + PURCHASE.getType() + ", but was: ";
+    private final String type;
+    OperationType(String type) {
+        this.type = type;
+    }
+
+    public static OperationType fromString(String string) {
+        for (OperationType type : OperationType.values()) {
+            if (type.getType().equals(string)) {
+                return type;
+            }
+        }
+        throw new IncorrectOperationTypeException(INCORRECT_TYPE_EXCEPTION + string);
+    }
+
+    public String getType() {
+        return type;
+    }
 }
--- src/main/java/core/fruitshop/services/impl/CsvDataExtractorImpl.java
+++ src/main/java/core/fruitshop/services/impl/CsvDataExtractorImpl.java
@@ -1,7 +1,6 @@
 package core.fruitshop.services.impl;
 
 import core.fruitshop.OperationType;
-import core.fruitshop.exceptions.IncorrectOperationTypeException;
 import core.fruitshop.model.FruitTransaction;
 import core.fruitshop.services.DataExtractor;
 
@@ -9,13 +8,6 @@ public class CsvDataExtractorImpl implements DataExtractor {
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int PRODUCT_NAME_INDEX = 1;
     private static final int PRODUCT_AMOUNT_INDEX = 2;
-    private static final String BALANCE = "b";
-    private static final String SUPPLY = "s";
-    private static final String RETURN = "r";
-    private static final String PURCHASE = "p";
-    private static final String INCORRECT_TYPE_EXCEPTION = "Incorrect operation type. "
-            + "Expected: " + BALANCE + ","
-            + SUPPLY + "," + RETURN + "or" + PURCHASE + ", but was: ";
     private static final String COLUMN_SEPARATOR = ",";
     private String[] stringToParse;
 
@@ -27,18 +19,7 @@ public class CsvDataExtractorImpl implements DataExtractor {
 
     private OperationType getOperationType() {
         String type = stringToParse[OPERATION_TYPE_INDEX];
-        switch (type) {
-            case BALANCE:
-                return OperationType.BALANCE;
-            case SUPPLY:
-                return OperationType.SUPPLY;
-            case RETURN:
-                return OperationType.RETURN;
-            case PURCHASE:
-                return OperationType.PURCHASE;
-            default:
-                throw new IncorrectOperationTypeException(INCORRECT_TYPE_EXCEPTION + type);
-        }
+        return OperationType.fromString(type);
     }
 
     private String getProductName() {
