3244e818741133088cc44554b302c6753880ba54
Bohdan Senyk <senyk2001@gmail.com>
2022-10-31 10:13:08 +0200

    created fruit-shop solution

---
+++ src/main/java/core/basesyntax/strategy/impl/PurchaseOperationHandlerImpl.java
@@ -0,0 +1,25 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.strategy.OperationHandler;
+
+public class PurchaseOperationHandlerImpl implements OperationHandler {
+    private final StorageDao storageDao;
+
+    public PurchaseOperationHandlerImpl(StorageDao storageDao) {
+        this.storageDao = storageDao;
+    }
+
+    @Override
+    public void changeQuantity(FruitTransaction fruitTransaction) {
+        if (fruitTransaction.getQuantity()
+                > storageDao.getRemainFruit(fruitTransaction.getFruit())) {
+            throw new RuntimeException(
+                    fruitTransaction.getFruit() + " cannot be purchase. They aren`t enough.");
+        }
+        storageDao.updateData(fruitTransaction.getFruit(),
+                storageDao.getRemainFruit(fruitTransaction.getFruit())
+                        - fruitTransaction.getQuantity());
+    }
+}
