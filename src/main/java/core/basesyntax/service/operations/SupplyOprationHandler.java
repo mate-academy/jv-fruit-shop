6fd2ef2c36bb2fb18db3b57de352891769c08026
MuzykaVP <muzykavp.work@gmail.com>
2022-11-02 11:56:30 +0200

    implemented OperationHandler`s

---
+++ src/main/java/core/basesyntax/service/operations/SupplyOprationHandler.java
@@ -0,0 +1,13 @@
+package core.basesyntax.service.operations;
+
+import core.basesyntax.dao.FruitShopDao;
+import core.basesyntax.dao.impl.FruitShopDaoImpl;
+import core.basesyntax.model.FruitTransaction;
+
+public class SupplyOprationHandler implements OperationHandler {
+    @Override
+    public void operate(FruitTransaction fruitTransaction) {
+        FruitShopDao fruitShopStorage = new FruitShopDaoImpl();
+        fruitShopStorage.supply(fruitTransaction.getFruit(), fruitTransaction.getQuantity());
+    }
+}
