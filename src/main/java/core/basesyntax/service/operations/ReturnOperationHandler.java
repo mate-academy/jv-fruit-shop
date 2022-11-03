4cecc63bccefd24a8725897e015b6db5bade7ffd
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-11-03 17:14:27 +0200

    simplified operations classes

--- src/main/java/core/basesyntax/service/operations/ReturnOperationHandler.java
+++ src/main/java/core/basesyntax/service/operations/ReturnOperationHandler.java
@@ -7,12 +7,12 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.OperationHandler;
 
 public class ReturnOperationHandler implements OperationHandler {
+    private final FruitDao fruitDao = new FruitDaoImpl();
+
     @Override
     public void getOperation(FruitTransaction fruitTransaction) {
-        FruitDao fruitDao = new FruitDaoImpl();
         Fruit fruitFromDb = fruitDao.get(fruitTransaction);
-        int oldAmount = fruitFromDb.getAmountInShop();
-        int newAmount = oldAmount + fruitTransaction.getQuantity();
-        fruitDao.update(fruitTransaction, newAmount);
+        fruitDao.update(fruitTransaction,
+                fruitFromDb.getAmountInShop() + fruitTransaction.getQuantity());
     }
 }
