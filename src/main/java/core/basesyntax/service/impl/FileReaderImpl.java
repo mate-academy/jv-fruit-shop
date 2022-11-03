d79c4e67dfe295fcc2259276113dd9effb5cf7e0
MuzykaVP <muzykavp.work@gmail.com>
2022-11-03 21:23:02 +0200

    naming fixes

--- src/main/java/core/basesyntax/service/impl/FileReaderImpl.java
+++ src/main/java/core/basesyntax/service/impl/FileReaderImpl.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 public class FileReaderImpl implements FileReader {
     @Override
