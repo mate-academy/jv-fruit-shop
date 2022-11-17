0dea5b70610b04f8302c58b6fb6fcadb87eae3f4
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-17 19:17:06 +0200

    change logic in Return and Supply

--- src/main/java/core/basesyntax/service/operations/Supply.java
+++ src/main/java/core/basesyntax/service/operations/Supply.java
@@ -5,6 +5,6 @@ import static core.basesyntax.db.Storage.storage;
 public class Supply implements IOperation {
     @Override
     public void makeOperation(String key, Integer value) {
-        storage.put(key, storage.get(key) + value);
+        storage.put(key, (storage.get(key) != null ? storage.get(key) : 0) + value);
     }
 }
