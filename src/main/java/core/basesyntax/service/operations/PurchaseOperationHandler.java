35471a495347932794efe53d244134b4cfe95dd1
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-19 15:01:27 +0200

    check availability

--- src/main/java/core/basesyntax/service/operations/PurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/service/operations/PurchaseOperationHandler.java
@@ -5,6 +5,10 @@ import static core.basesyntax.db.Storage.storage;
 public class PurchaseOperationHandler implements OperationHandler {
     @Override
     public void handle(String key, Integer value) {
-        storage.put(key, storage.get(key) - value);
+        if (storage.containsKey(key) && storage.get(key) >= value) {
+            storage.put(key, storage.get(key) - value);
+        } else {
+            System.out.println("Mission impossible");
+        }
     }
 }
