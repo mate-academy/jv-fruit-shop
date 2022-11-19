3918bbb5cf894de65b63ab38725e26593149372b
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-19 11:57:36 +0200

    added buffered input

--- src/main/java/core/basesyntax/dao/PutInFile.java
+++ src/main/java/core/basesyntax/dao/PutInFile.java
@@ -1,14 +1,15 @@
 package core.basesyntax.dao;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class PutInFile implements IPutInfoDao {
     @Override
     public void putData(String path, String report) {
-        try (FileOutputStream fos = new FileOutputStream(path)) {
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path))) {
             byte[] buffer = report.getBytes();
-            fos.write(buffer);
+            bos.write(buffer);
         } catch (IOException e) {
             throw new RuntimeException("File not found " + e);
         }
