4a99194b0ce309d0eb80a70d42c15da6fb7da09d
Roman Yuskevych <yuskevych@gmail.com>
2022-12-19 22:08:59 +0200

    added fixes after review

--- src/main/java/service/ParseServiceImpl.java
+++ src/main/java/service/ParseServiceImpl.java
@@ -6,6 +6,7 @@ import model.FruitTransaction;
 
 public class ParseServiceImpl implements ParseService {
     private static final String SEPARATOR = ",";
+    private static final String ONLY_NUMERIC = "^[0-9]+$";
     private static final int OPERATION_INDEX = 0;
     private static final int FRUIT_INDEX = 1;
     private static final int QUANTITY_INDEX = 2;
@@ -21,16 +22,15 @@ public class ParseServiceImpl implements ParseService {
 
     private FruitTransaction getFruitTransaction(String[] strings) {
         FruitTransaction fruitTransaction = new FruitTransaction();
-        try {
-            fruitTransaction.setOperation(FruitTransaction.Operation
-                    .getByCode(strings[OPERATION_INDEX]));
-            fruitTransaction.setFruit(strings[FRUIT_INDEX]);
-            fruitTransaction.setQuantity(Integer.parseInt(strings[QUANTITY_INDEX]));
-        } catch (RuntimeException ignored) {
-            fruitTransaction.setOperation(null);
-            fruitTransaction.setFruit(null);
-            fruitTransaction.setQuantity(0);
-        }
+        fruitTransaction.setOperation(FruitTransaction.Operation
+                .getByCode(strings[OPERATION_INDEX]));
+        fruitTransaction.setFruit(strings[FRUIT_INDEX]);
+        fruitTransaction.setQuantity(isNumber(strings[QUANTITY_INDEX])
+                ? Integer.parseInt(strings[QUANTITY_INDEX]) : 0);
         return fruitTransaction;
+    }
+
+    private boolean isNumber (String str) {
+        return str.matches(ONLY_NUMERIC);
     }
 }
