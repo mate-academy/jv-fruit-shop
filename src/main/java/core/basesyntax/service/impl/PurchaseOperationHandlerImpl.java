6970602adb4ed0071770e3dc47fb71a4301bc99d
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-12-20 13:40:40 +0000

    Strategy & handlers  added.

---
+++ src/main/java/core/basesyntax/service/impl/PurchaseOperationHandlerImpl.java
@@ -0,0 +1,11 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.OperationHandler;
+
+public class PurchaseOperationHandlerImpl implements OperationHandler {
+    @Override
+    public void operate(FruitTransaction fruitTransaction) {
+
+    }
+}
---
+++ src/main/java/core/basesyntax/service/impl/ReturnOperationHandlerImpl.java
@@ -0,0 +1,11 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.OperationHandler;
+
+public class ReturnOperationHandlerImpl implements OperationHandler {
+    @Override
+    public void operate(FruitTransaction fruitTransaction) {
+
+    }
+}
