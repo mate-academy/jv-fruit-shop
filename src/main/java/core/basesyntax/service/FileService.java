f64b6a130c7791c6abd794c744e4f6dbc8fdb3f3
Vadym Valchak <vadym.valchak@gmail.com>
2022-12-21 18:07:58 +0200

    Update src/main/java/core/basesyntax/service/FileService.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/core/basesyntax/service/FileService.java
+++ src/main/java/core/basesyntax/service/FileService.java
@@ -5,5 +5,5 @@ import java.util.List;
 public interface FileService {
     List<String> readFromFile(String fileName);
 
-    void writeToFile(String fileName);
+    void writeToFile(String fileName, String text);
 }
