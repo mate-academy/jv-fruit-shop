fbed080721598f2046e6c072838555bf450f4e55
Ivan Vovnyanko <mooooonkit@gmail.com>
2022-12-27 18:03:37 +0200

    maven

--- src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.strategy.impl;
 
 import core.basesyntax.operations.Operation;
 import core.basesyntax.strategy.OperationStrategy;
-
 import java.util.Map;
 
 public class OperationStrategyImpl implements OperationStrategy {
