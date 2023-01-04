376bd002a58683c6f9c5d0ee80a3568631775ecc
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2023-01-04 12:44:39 +0200

    checkstyle added

--- src/main/java/service/operations/PurchaseOperationHandler.java
+++ src/main/java/service/operations/PurchaseOperationHandler.java
@@ -10,9 +10,9 @@ public class PurchaseOperationHandler implements OperationHandler {
         Integer qtyInStorage = Storage.fruits.get(transaction.getFruit());
         Integer quantityInTransaction = transaction.getQuantity();
         if (quantityInTransaction > qtyInStorage) {
-            throw new RuntimeException("Not enough "+ transaction.getFruit() + " in shop storage. "
-                     + "Should be at least " + transaction.getQuantity() + " items. But storage has only "
-                    + qtyInStorage);
+            throw new RuntimeException("Not enough " + transaction.getFruit() + " in shop storage. "
+                     + "Should be at least " + transaction.getQuantity()
+                    + " items. But storage has only " + qtyInStorage);
         }
         Integer newQty = qtyInStorage - quantityInTransaction;
         Storage.fruits.put(transaction.getFruit(), newQty);
