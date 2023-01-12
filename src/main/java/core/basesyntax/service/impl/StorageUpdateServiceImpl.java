235c61fa4fecd61cf267fdeeb78d5e485c7b82da
Artem Voitiuk <artemon2504@gmail.com>
2023-01-12 16:16:03 +0200

    Fruit Shop implementation 7 commit

--- src/main/java/core/basesyntax/service/impl/StorageUpdateServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/StorageUpdateServiceImpl.java
@@ -1,6 +1,5 @@
 package core.basesyntax.service.impl;
 
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.model.Operation;
 import core.basesyntax.service.StorageUpdateService;
