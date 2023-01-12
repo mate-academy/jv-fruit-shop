d7ab6bd84ed9e552fe4ad278ff1ce37adf907fd1
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2023-01-13 01:06:13 +0200

    writer/reader became more easy

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -5,9 +5,11 @@ import core.basesyntax.service.ReportGenerator;
 import java.util.stream.Collectors;
 
 public class ReportGeneratorImpl implements ReportGenerator {
+    private static final String FILE_HEADER = "fruit,quantity" + System.lineSeparator();
+
     @Override
     public String generateReport() {
-        return FruitStorage.fruits.entrySet().stream()
+        return FILE_HEADER + FruitStorage.fruits.entrySet().stream()
                 .map(element -> element.getKey() + "," + element.getValue())
                 .collect(Collectors.joining(System.lineSeparator()));
     }
--- src/main/java/core/basesyntax/service/impl/WriteFileServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriteFileServiceImpl.java
@@ -1,19 +1,16 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.service.WriteFileService;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WriteFileServiceImpl implements WriteFileService {
-    private static final String FILE_HEADER = "fruit,quantity" + System.lineSeparator();
 
     @Override
     public void writeToFile(String path, String data) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(
-                new FileWriter(path, false))) {
-            bufferedWriter.write(FILE_HEADER);
-            bufferedWriter.write(data);
+        try {
+            Files.writeString(Path.of(path), data);
         } catch (IOException e) {
             throw new RuntimeException(
                     String.format("Can`t write data: %s ,to file %s", data, path));
