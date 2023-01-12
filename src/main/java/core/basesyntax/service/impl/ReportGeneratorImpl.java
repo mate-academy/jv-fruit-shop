662d637ab5e5740aa6ec73635869cf0ddb4c2d10
Maksim Surzhik <kabaneri15@gmail.com>
2023-01-12 16:10:33 +0200

    changes1

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -5,13 +5,17 @@ import java.util.Map;
 
 public class ReportGeneratorImpl implements ReportGenerator {
     private static final String REGEX = ",";
-
+    private static final String FRUIT = "fruit";
+    private static final String AMOUNT = "amount";
     @Override
     public String makeReport(Map<String, Integer> dataFromStorage) {
         StringBuilder builder = new StringBuilder();
+        builder.append(FRUIT)
+                .append(REGEX)
+                .append(AMOUNT)
+                .append(System.lineSeparator());
         for (Map.Entry<String, Integer> map : dataFromStorage.entrySet()) {
-            builder
-                    .append(map.getKey())
+            builder.append(map.getKey())
                     .append(REGEX)
                     .append(map.getValue())
                     .append(System.lineSeparator());
