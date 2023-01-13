ea9ee183fa362d8d1c7deb00cf8902abcbff9b49
Дмитро <dima.smyk99.19@gmail.com>
2023-01-13 15:02:30 +0200

    remove constructors

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -5,15 +5,6 @@ public class FruitTransaction {
     private String fruit;
     private int quantity;
 
-    public FruitTransaction() {
-    }
-
-    public FruitTransaction(Operation operation, String fruit, int quantity) {
-        this.operation = operation;
-        this.fruit = fruit;
-        this.quantity = quantity;
-    }
-
     public Operation getOperation() {
         return operation;
     }
