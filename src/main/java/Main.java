b2616e19ea7f2dec9bc1e49ef72c8b0be5a7a675
Polina Lysytska <nikolavna100500@gmail.com>
2023-01-13 08:53:24 +0200

    static class initializer in main class was implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,25 +13,26 @@ import service.impl.ReportGeneratorImpl;
 import service.impl.TransactionProcessorImpl;
 
 public class Main {
-    private static FileReadService fileReadService = new FileReadServiceImpl();
-    private static FruitTransactionParser fruitTransactionParser = new FruitTransactionParserImpl();
-    private static TransactionProcessor transactionProcessor = new TransactionProcessorImpl();
-    private static ReportGenerator reportGenerator = new ReportGeneratorImpl();
-    private static FileWriteService fileWriteService = new FileWriteServiceImpl();
+    private static final FileReadService FILE_READ_SERVICE;
+    private static final FruitTransactionParser FRUIT_TRANSACTION_PARSER;
+    private static final TransactionProcessor TRANSACTION_PROCESSOR;
+    private static final ReportGenerator REPORT_GENERATOR;
+    private static final FileWriteService FILE_WRITE_SERVICE;
 
-    Main() {
-        fileReadService = new FileReadServiceImpl();
-        fruitTransactionParser = new FruitTransactionParserImpl();
-        transactionProcessor = new TransactionProcessorImpl();
-        reportGenerator = new ReportGeneratorImpl();
-        fileWriteService = new FileWriteServiceImpl();
+    static {
+        FILE_READ_SERVICE = new FileReadServiceImpl();
+        FRUIT_TRANSACTION_PARSER = new FruitTransactionParserImpl();
+        TRANSACTION_PROCESSOR = new TransactionProcessorImpl();
+        REPORT_GENERATOR = new ReportGeneratorImpl();
+        FILE_WRITE_SERVICE = new FileWriteServiceImpl();
     }
 
     public static void main(String[] args) {
-        String dataFromFile = fileReadService.readFromFile(Path.of("src/main/resources/input.csv"));
-        List<FruitTransaction> transactions = fruitTransactionParser.toTransactions(dataFromFile);
-        transactionProcessor.process(transactions);
-        String report = reportGenerator.generate();
-        fileWriteService.writeToFile(Path.of("src/main/resources/output.csv"), report);
+        String dataFromFile =
+                FILE_READ_SERVICE.readFromFile(Path.of("src/main/resources/input.csv"));
+        List<FruitTransaction> transactions = FRUIT_TRANSACTION_PARSER.toTransactions(dataFromFile);
+        TRANSACTION_PROCESSOR.process(transactions);
+        String report = REPORT_GENERATOR.generate();
+        FILE_WRITE_SERVICE.writeToFile(Path.of("src/main/resources/output.csv"), report);
     }
 }
