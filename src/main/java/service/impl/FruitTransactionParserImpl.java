c671e8841efa4aded681205bec1e2e7da16cf1be
Polina Lysytska <nikolavna100500@gmail.com>
2023-01-13 14:36:42 +0200

    stream was optimized and also regex constant was added

--- src/main/java/service/impl/FruitTransactionParserImpl.java
+++ src/main/java/service/impl/FruitTransactionParserImpl.java
@@ -10,19 +10,19 @@ public class FruitTransactionParserImpl implements FruitTransactionParser {
     private static final int OPERATION_FIELD_INDEX = 0;
     private static final int FRUIT_FIELD_INDEX = 1;
     private static final int QUANTITY_FIELD_INDEX = 2;
-    private static final int FIELDS_NAMES_INDEX = 0;
+    private static final String dataSeparator = ",";
 
     @Override
     public List<FruitTransaction> toTransactions(String data) {
         String[] lines = data.split(System.lineSeparator());
         return Stream.of(lines)
-                .filter(line -> !line.equals(lines[FIELDS_NAMES_INDEX]))
+                .skip(1)
                 .map(this::parseTransaction)
                 .collect(Collectors.toList());
     }
 
     private FruitTransaction parseTransaction(String line) {
-        String[] fields = line.split(",");
+        String[] fields = line.split(dataSeparator);
         FruitTransaction.Operation operation =
                 FruitTransaction.Operation.fromString(fields[OPERATION_FIELD_INDEX]);
         String fruit = fields[FRUIT_FIELD_INDEX];
