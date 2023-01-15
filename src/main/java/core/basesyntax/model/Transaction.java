ff11dce595ce66f3760377cd3b75f795e7e9bba1
Dmytro Koval <dimonkoval@gmail.com>
2023-01-15 16:26:44 +0200

    1

---
+++ src/main/java/core/basesyntax/model/Transaction.java
@@ -0,0 +1,36 @@
+package core.basesyntax.model;
+
+public class Transaction {
+    private Operation operation;
+    private String fruit;
+    private int quantity;
+
+    public Transaction(String fruit, int quantity) {
+        this.fruit = fruit;
+        this.quantity = quantity;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
