70ad3a5aa5ace67bdf965ed5a265c0b7b45e64c6
yuliia.kyianytsia <u.kiyanitsa@gmail.com>
2023-01-17 09:18:56 +0200

    checkstyle

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -7,14 +7,12 @@ import core.basesyntax.service.ReportService;
 import core.basesyntax.service.TransactionHandler;
 import core.basesyntax.service.impl.CsvFileReaderServiceImpl;
 import core.basesyntax.service.impl.CsvReportServiceImpl;
-
 import java.util.List;
 
 public class Main {
 
     private static final String INPUT_DATA_PATH = "src/main/resources/inputData.csv";
     private static final String OUTPUT_DATA_PATH = "src/main/resources/report.csv";
-
 
     public static void main(String[] args) {
         FileReaderService fileReaderService = new CsvFileReaderServiceImpl();
