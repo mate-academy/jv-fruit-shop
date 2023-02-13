dec3d6fd6bee021ef8c7dcb3ff04c403bf51665a
Yaroslav Liashchenko <liaroswin@gmail.com>
2023-02-13 18:58:37 +0200

    created FileReaderService and implemented it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,14 @@
-import java.io.BufferedWriter;
+import service.CsvFileReaderServiceImpl;
+import service.FileReaderService;
+
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        File inputData = new File("src/main/resources/inputData.csv");
-        try {
-            inputData.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can’t create file", e);
-        }
-
-        List<String> list = new ArrayList<>();
-        list.add("b,banana,20");
-        list.add("b,apple,100");
-        list.add("s,banana,100");
-        list.add("p,banana,13");
-        list.add("r,apple,10");
-        list.add("p,apple,20");
-        list.add("p,banana,5");
-        list.add("s,banana,50");
-
-        for (String line : list) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(inputData, true))) {
-                bufferedWriter.write(line + System.lineSeparator());
-            } catch (IOException e) {
-                throw new RuntimeException();
-            }
-        }
+        String pathToInputDataFile = "src/main/resources/inputData.csv";
+        File inputData = new File(pathToInputDataFile);
+        FileReaderService csvFileReaderService = new CsvFileReaderServiceImpl();
+        System.out.println(csvFileReaderService.getDataFromFile(inputData));
     }
 }
---
+++ src/main/java/service/FileReaderService.java
@@ -0,0 +1,8 @@
+package service;
+
+import java.io.File;
+import java.util.List;
+
+public interface FileReaderService {
+    List<String> getDataFromFile(File inputFile);
+}
