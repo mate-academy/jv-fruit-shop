ff14ced375175dd17af7f09f97116a68bfc4d522
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-02-15 20:26:41 +0200

    added model of Operation and Transaction

---
+++ src/main/java/core/basesyntax/model/Transaction.java
@@ -0,0 +1,37 @@
+package core.basesyntax.model;
+
+public class Transaction {
+    private Operation operation;
+    private String fruitName;
+    private int quantity;
+
+    public Transaction(Operation operation, String fruitName, int quantity) {
+        this.operation = operation;
+        this.fruitName = fruitName;
+        this.quantity = quantity;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+    public void setFruitName(String fruitName) {
+        this.fruitName = fruitName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
