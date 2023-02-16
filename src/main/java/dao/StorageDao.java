619ac0dc4d9a663034b381edd6e8919649980bd7
Yaroslav Liashchenko <liaroswin@gmail.com>
2023-02-16 13:52:27 +0200

    added StorageDao with add and update methods

---
+++ src/main/java/dao/StorageDao.java
@@ -0,0 +1,6 @@
+package dao;
+
+public interface StorageDao {
+    void add(String fruit, Integer amount);
+    void update(String fruit, Integer amount);
+}
---
+++ src/main/java/dao/StorageDaoImpl.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class StorageDaoImpl implements StorageDao {
+    @Override
+    public void add(String fruit, Integer amount) {
+
+    }
+
+    @Override
+    public void update(String fruit, Integer amount) {
+
+    }
+}
