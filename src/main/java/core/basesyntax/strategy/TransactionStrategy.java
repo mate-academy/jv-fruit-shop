90b3926bea580cdf2a9005817e5004a0ba42abeb
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-02-18 14:39:01 +0200

    updated strategy for processing data

--- src/main/java/core/basesyntax/strategy/TransactionStrategy.java
+++ src/main/java/core/basesyntax/strategy/TransactionStrategy.java
@@ -1,8 +1,13 @@
 package core.basesyntax.strategy;
 
 import core.basesyntax.model.Operation;
+import core.basesyntax.model.Transaction;
 import core.basesyntax.service.transaction.TransactionHandler;
 
+import java.util.List;
+
 public interface TransactionStrategy {
+    void processTransaction(List<Transaction> transactions);
+
     TransactionHandler get(Operation operation);
 }
--- src/main/java/core/basesyntax/strategy/TransactionStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/TransactionStrategyImpl.java
@@ -1,8 +1,10 @@
 package core.basesyntax.strategy;
 
 import core.basesyntax.model.Operation;
+import core.basesyntax.model.Transaction;
 import core.basesyntax.service.transaction.TransactionHandler;
 
+import java.util.List;
 import java.util.Map;
 
 public class TransactionStrategyImpl implements TransactionStrategy {
@@ -13,7 +15,15 @@ public class TransactionStrategyImpl implements TransactionStrategy {
     }
 
     @Override
+    public void processTransaction(List<Transaction> transactions) {
+        for (Transaction transaction : transactions) {
+            get(transaction.getOperation()).makeTransaction(transaction);
+        }
+    }
+
+    @Override
     public TransactionHandler get(Operation operation) {
         return transactionHandlerMap.get(operation);
     }
+
 }
