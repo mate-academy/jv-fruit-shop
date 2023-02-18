944baa13d31f33bac00c7c9091665339277df9bc
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-02-18 14:40:48 +0200

    created FileWriterService

---
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriterServiceImpl.java
@@ -0,0 +1,18 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.FileWriterService;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CsvFileWriterServiceImpl implements FileWriterService {
+    @Override
+    public void writeToFile(String result, String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file " + fileName, e);
+        }
+    }
+}
