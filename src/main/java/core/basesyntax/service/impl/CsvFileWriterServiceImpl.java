a053a7312ad0d210d3dcca98d267f8abec67eaab
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-02-19 17:42:27 +0200

    some fix naming

--- src/main/java/core/basesyntax/service/impl/CsvFileWriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriterServiceImpl.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 
 public class CsvFileWriterServiceImpl implements FileWriterService {
     @Override
-    public void writeToFile(String result, String fileName) {
+    public void writeToFile(String report, String fileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
-            bufferedWriter.write(result);
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + fileName, e);
         }
