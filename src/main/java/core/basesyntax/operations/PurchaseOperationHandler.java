05ece4c74295a773ea91b951dfe3e34e3f2340ec
Ilya Samilyak <samilyakilya@gmail.com>
2023-02-20 21:58:10 +0200

    hw-fruit-shop-4_after review

--- src/main/java/core/basesyntax/operations/PurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/operations/PurchaseOperationHandler.java
@@ -8,6 +8,9 @@ public class PurchaseOperationHandler implements OperationHandler {
     @Override
     public void handle(FruitTransaction fruitTransaction) {
         Integer oldSum = Storage.fruits.get(fruitTransaction.getFruit());
+        if (oldSum <= 0 || oldSum < fruitTransaction.getQuantity()) {
+            throw new RuntimeException("The oldSum doesn't correct " + oldSum);
+        }
         Storage.fruits.put(fruitTransaction.getFruit(),oldSum - fruitTransaction.getQuantity());
     }
 }
