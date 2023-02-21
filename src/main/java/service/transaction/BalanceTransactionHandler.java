1326156c544bba3286322c276dccf669827689ab
Yaroslav Liashchenko <liaroswin@gmail.com>
2023-02-21 19:49:30 +0200

    modified TransactionHandler and all its implementations

--- src/main/java/service/transaction/BalanceTransactionHandler.java
+++ src/main/java/service/transaction/BalanceTransactionHandler.java
@@ -3,7 +3,7 @@ package service.transaction;
 public class BalanceTransactionHandler implements TransactionHandler {
 
     @Override
-    public void apply(String fruit, Integer amount) {
-        storageDao.add(fruit, amount);
+    public Integer calculateRemnant(Integer remnant, Integer amount) {
+        return remnant + amount;
     }
 }
--- src/main/java/service/transaction/ReturnTransactionHandler.java
+++ src/main/java/service/transaction/ReturnTransactionHandler.java
@@ -2,8 +2,7 @@ package service.transaction;
 
 public class ReturnTransactionHandler implements TransactionHandler {
     @Override
-    public void apply(String fruit, Integer amount) {
-        Integer newAmount = storageDao.getAmount(fruit) + amount;
-        storageDao.update(fruit, newAmount);
+    public Integer calculateRemnant(Integer remnant, Integer amount) {
+        return remnant + amount;
     }
 }
