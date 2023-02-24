7728d51d88ca309a0d09d526cf704cd58506eab8
WallRun <dimonty2003@gmail.com>
2023-02-24 22:03:08 +0200

    Added a map to FruitTransactionStrategy to store handler instances

--- src/main/java/core/basesyntax/strategy/FruitTransactionStrategy.java
+++ src/main/java/core/basesyntax/strategy/FruitTransactionStrategy.java
@@ -6,19 +6,31 @@ import core.basesyntax.service.impl.FruitBalanceService;
 import core.basesyntax.service.impl.FruitPurchaseService;
 import core.basesyntax.service.impl.FruitReturnService;
 import core.basesyntax.service.impl.FruitSupplyService;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FruitTransactionStrategy {
+    private final Map<FruitTransaction.Operation, FruitTransactionHandler> handlerMap;
+
+    public FruitTransactionStrategy() {
+        handlerMap = new HashMap<>();
+        handlerMap.put(FruitTransaction.Operation.PURCHASE, new FruitPurchaseService());
+        handlerMap.put(FruitTransaction.Operation.BALANCE, new FruitBalanceService());
+        handlerMap.put(FruitTransaction.Operation.RETURN, new FruitReturnService());
+        handlerMap.put(FruitTransaction.Operation.SUPPLY, new FruitSupplyService());
+    }
+
     public FruitTransactionHandler getFruitTransactionService(
             FruitTransaction.Operation operation) {
         switch (operation) {
             case PURCHASE:
-                return new FruitPurchaseService();
+                return handlerMap.get(FruitTransaction.Operation.PURCHASE);
             case SUPPLY:
-                return new FruitSupplyService();
+                return handlerMap.get(FruitTransaction.Operation.SUPPLY);
             case RETURN:
-                return new FruitReturnService();
+                return handlerMap.get(FruitTransaction.Operation.RETURN);
             case BALANCE:
-                return new FruitBalanceService();
+                return handlerMap.get(FruitTransaction.Operation.BALANCE);
             default:
                 throw new RuntimeException("Illegal operation");
         }
