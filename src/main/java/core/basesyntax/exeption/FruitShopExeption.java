43debabcf7534c70e2908ca6bbf51e5589a0e4bc
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-27 15:13:49 +0200

    Add custom exeption

--- src/main/java/core/basesyntax/exeption/FruitShopExeption.java
+++ src/main/java/core/basesyntax/exeption/FruitShopExeption.java
@@ -1,6 +1,6 @@
 package core.basesyntax.exeption;
 
-public class FruitShopExeption extends RuntimeException{
+public class FruitShopExeption extends RuntimeException {
     public FruitShopExeption(String message) {
         super(message);
     }
