5536771d3ae9253d1db24fb1f7619073d514b27e
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-03-05 11:43:10 +0100

    3

--- src/main/java/db/Storage.java
+++ src/main/java/db/Storage.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-
     private static Map<String, Integer> storage = new HashMap<>();
 
     public static Map<String, Integer> getStorage() {
