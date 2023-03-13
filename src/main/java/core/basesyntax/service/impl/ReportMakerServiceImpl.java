c00e8c78b17a57bc236f661c61ebd4802c839409
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-03-13 12:35:09 +0200

    Use lineSeparator

--- src/main/java/core/basesyntax/service/impl/ReportMakerServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportMakerServiceImpl.java
@@ -6,14 +6,13 @@ import java.util.Map;
 
 public class ReportMakerServiceImpl implements ReportMakerService {
     private static final String HEADER = "fruit,quantity";
-    private static final String NEW_LINE = "\n";
     private static final char SEPARATOR = ',';
 
     @Override
     public String makeReport(Map<String, Integer> data) {
         StringBuilder report = new StringBuilder(HEADER);
         for (Map.Entry<String, Integer> pair : Storage.getStorage().entrySet()) {
-            report.append(NEW_LINE)
+            report.append(System.lineSeparator())
                     .append(pair.getKey())
                     .append(SEPARATOR)
                     .append(pair.getValue());
