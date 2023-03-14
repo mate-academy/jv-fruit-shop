5b1c672a143215a63c3b29455212ecf19b621ad5
Nikita <nsalogub96@gmail.com>
2023-03-14 15:51:14 +0200

    Fix problem with PurchaseHandler2

--- src/main/java/strategy/handler/PurchaseHandler.java
+++ src/main/java/strategy/handler/PurchaseHandler.java
@@ -8,12 +8,11 @@ public class PurchaseHandler implements TransactionHandler {
     @Override
     public void handle(FruitTransaction transaction) {
         int realQuantity = Storage.fruits.get(transaction.getFruit());
-        int quantityToPurchase =
-                Storage.fruits.get(transaction.getFruit()) - transaction.getQuantity();
-        if (quantityToPurchase < 0) {
+        int quantityAfterPurchase = realQuantity - transaction.getQuantity();
+        if (quantityAfterPurchase < 0) {
             throw new RuntimeException("Storage doesn't have enough products to sell");
-        } else {
-            Storage.fruits.put(transaction.getFruit(), quantityToPurchase);
+        }
+        Storage.fruits.put(transaction.getFruit(), quantityAfterPurchase);
         }
     }
 }
