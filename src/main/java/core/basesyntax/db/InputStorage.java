480fe14bbb18e03471ac264385cdb4991f27815f
Maksym <maxim.yashyn@gmail.com>
2023-03-14 18:09:22 +0200

    created InputStorage and OutputStorage

---
+++ src/main/java/core/basesyntax/db/InputStorage.java
@@ -0,0 +1,10 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.Fruit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputStorage {
+    public static final List<Fruit> fruits = new ArrayList<>();
+}
