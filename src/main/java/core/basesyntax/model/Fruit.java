a59e3a843793ad09e935c49fe38aa6d297f600eb
Maksym <maxim.yashyn@gmail.com>
2023-03-14 20:40:54 +0200

    changed logic of Fruit class

--- src/main/java/core/basesyntax/model/Fruit.java
+++ src/main/java/core/basesyntax/model/Fruit.java
@@ -2,16 +2,38 @@ package core.basesyntax.model;
 
 public class Fruit {
     private Integer value;
+    private ACTIVITY activity;
+    private TYPE type;
 
-    public Fruit(Integer value) {
-        this.value = value;
+    public Integer getValue() {
+        return value;
     }
 
     public void setValue(Integer value) {
         this.value = value;
     }
 
-    public Integer getValue() {
-        return value;
+    public ACTIVITY getActivity() {
+        return activity;
+    }
+
+    public void setActivity(ACTIVITY activity) {
+        this.activity = activity;
+    }
+
+    public TYPE getType() {
+        return type;
+    }
+
+    public void setType(TYPE type) {
+        this.type = type;
+    }
+
+    public static enum ACTIVITY {
+        EXPIRED, PURCHASE, RETURN, SUPPLY;
+    }
+
+    public static enum TYPE {
+        APPLE, BANANA, ORANGE, MANGO;
     }
 }
