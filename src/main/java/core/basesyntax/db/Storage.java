fbb4e9f39ca8d83ec9969a518c0906a61a7ab24b
Maksym <maxim.yashyn@gmail.com>
2023-03-14 22:50:30 +0200

    renamed from InputStorage

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -5,6 +5,6 @@ import core.basesyntax.model.Fruit;
 import java.util.ArrayList;
 import java.util.List;
 
-public class InputStorage {
+public class Storage {
     public static final List<Fruit> fruits = new ArrayList<>();
 }
