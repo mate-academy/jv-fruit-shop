946d9b21807477459ac4cd8ed5c5c34f42877284
Artem Grunin <artem.grunin.2006@gmail.com>
2023-03-15 00:17:23 +0200

    created some constants from magic values

--- src/main/java/core/basesyntax/service/impl/ParseServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ParseServiceImpl.java
@@ -7,14 +7,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ParseServiceImpl implements ParserService {
+    public static final String SEPARATOR = ",";
+    public static final int OPERATION_INDEX = 0;
+    public static final int FRUIT_INDEX = 1;
+    public static final int QUANTITY_INDEX = 2;
     @Override
     public List<FruitTransaction> parse(List<String> strings) {
         return strings.stream()
-                .map(string -> string.split(","))
+                .map(string -> string.split(SEPARATOR))
                 .map(parts -> new FruitTransaction(
-                        FruitTransaction.Operation.getOperationByCode(parts[0]),
-                        parts[1],
-                        Integer.parseInt(parts[2])
+                        FruitTransaction.Operation
+                                .getOperationByCode(parts[OPERATION_INDEX]),
+                        parts[FRUIT_INDEX],
+                        Integer.parseInt(parts[QUANTITY_INDEX])
                 ))
                 .collect(Collectors.toList());
     }
