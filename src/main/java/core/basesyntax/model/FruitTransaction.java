79538d31ffbaed08ec5383853af45ae17302f50e
Maksym <maxim.yashyn@gmail.com>
2023-03-15 16:19:54 +0200

    deleted setters

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -17,24 +17,12 @@ public class FruitTransaction {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     public Operation getOperation() {
         return operation;
     }
 
-    public void setOperation(Operation operation) {
-        this.operation = operation;
-    }
-
     public String getFruit() {
         return fruit;
-    }
-
-    public void setFruit(String fruit) {
-        this.fruit = fruit;
     }
 
     public enum Operation {
