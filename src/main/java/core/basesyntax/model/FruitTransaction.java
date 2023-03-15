b6df86fb92d4bcc8235d4c9b6b0de516db1d6bea
Maksym <maxim.yashyn@gmail.com>
2023-03-15 10:28:02 +0200

    changed structure of FruitTransaction

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -1,39 +1,25 @@
 package core.basesyntax.model;
 
 public class FruitTransaction {
-    private Integer value;
-    private ACTIVITY activity;
-    private TYPE type;
+    private int quantity;
+    private Operation operation;
+    private String fruit;
 
-    public Integer getValue() {
-        return value;
-    }
+    public enum Operation {
+        BALANCE("b"),
+        EXPIRED("e"),
+        PURCHASE("p"),
+        RETURN("r"),
+        SUPPLY("s");
 
-    public void setValue(Integer value) {
-        this.value = value;
-    }
+        private String code;
 
-    public ACTIVITY getActivity() {
-        return activity;
-    }
+        Operation(String code) {
+            this.code = code;
+        }
 
-    public void setActivity(ACTIVITY activity) {
-        this.activity = activity;
-    }
-
-    public TYPE getType() {
-        return type;
-    }
-
-    public void setType(TYPE type) {
-        this.type = type;
-    }
-
-    public enum ACTIVITY {
-        BALANCE, EXPIRED, PURCHASE, RETURN, SUPPLY;
-    }
-
-    public enum TYPE {
-        APPLE, BANANA, ORANGE, MANGO;
+        public String getCode() {
+            return code;
+        }
     }
 }
