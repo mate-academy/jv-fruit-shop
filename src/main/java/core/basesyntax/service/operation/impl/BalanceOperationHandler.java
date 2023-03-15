9a01f5b50fb12a43064f693c9cefb445a278b867
Maksym <maxim.yashyn@gmail.com>
2023-03-15 11:43:36 +0200

    added and implemented OperationHandlers

---
+++ src/main/java/core/basesyntax/service/operation/impl/BalanceOperationHandler.java
@@ -0,0 +1,14 @@
+package core.basesyntax.service.operation.impl;
+
+import core.basesyntax.dao.FruitShopDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.operation.OperationHandler;
+
+public class BalanceOperationHandler implements OperationHandler {
+    FruitShopDaoImpl fruitShopDao;
+    @Override
+    public void operation(FruitTransaction fruitTransaction) {
+        fruitShopDao.add(fruitTransaction.getFruit(), fruitTransaction.getQuantity());
+    }
+}
---
+++ src/main/java/core/basesyntax/service/operation/impl/ExpiredOperationHandler.java
@@ -0,0 +1,17 @@
+package core.basesyntax.service.operation.impl;
+
+import core.basesyntax.dao.FruitShopDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.operation.OperationHandler;
+
+public class ExpiredOperationHandler implements OperationHandler {
+    private FruitShopDaoImpl fruitShopDao;
+
+    @Override
+    public void operation(FruitTransaction fruitTransaction) {
+        int amount = Storage.fruits.get(fruitTransaction.getFruit());
+        int result = amount - fruitTransaction.getQuantity();
+        fruitShopDao.add(fruitTransaction.getFruit(), result);
+    }
+}
