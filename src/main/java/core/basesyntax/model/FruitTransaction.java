7282fb920f71049dc8f93ce8eb41be88a413de64
Maksym <maxim.yashyn@gmail.com>
2023-03-15 10:29:58 +0200

    added getters, setters and constructor

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -5,6 +5,36 @@ public class FruitTransaction {
     private Operation operation;
     private String fruit;
 
+    public FruitTransaction(int quantity, Operation operation, String fruit) {
+        this.quantity = quantity;
+        this.operation = operation;
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
     public enum Operation {
         BALANCE("b"),
         EXPIRED("e"),
