76d2db67374046ad4a8202242f3813aef010f3cd
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-03-15 15:03:59 +0200

    implemented report generation

--- src/main/java/service/impl/CsvFileWriterService.java
+++ src/main/java/service/impl/CsvFileWriterService.java
@@ -1,17 +1,16 @@
 package service.impl;
 
-import service.WriterData;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import service.WriterData;
 
 public class CsvFileWriterService implements WriterData {
     @Override
     public void write(String report, String path) {
         File file = new File(path);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file));){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file" + e);
