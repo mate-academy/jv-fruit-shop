e72bf1eb2f0ca14eaf73642f3e7b7da73ebb79cb
Maksym <maxim.yashyn@gmail.com>
2023-03-15 11:09:10 +0200

    added magick numbers

--- src/main/java/core/basesyntax/service/impl/ConverterDataServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ConverterDataServiceImpl.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConverterDataServiceImpl implements ConverterDataService {
+    private static final String LINE_SEPARATOR = ",";
     @Override
     public List<String[]> convert(List<String> data) {
         List<String[]> convertedData = new ArrayList<>();
         for (String s : data) {
-            String[] splittedString = s.split(","); //TODO: Magick Number
+            String[] splittedString = s.split(LINE_SEPARATOR);
             convertedData.add(splittedString);
         }
         return convertedData;
--- src/main/java/core/basesyntax/service/impl/FruitServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/FruitServiceImpl.java
@@ -7,14 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FruitServiceImpl implements FruitService {
+    private static final int INDEX_OF_QUANTITY = 2;
+    private static final int INDEX_OF_FRUIT = 1;
+    private static final int INDEX_OF_OPERATION = 0;
 
     @Override
     public List<FruitTransaction> addNewFruit(List<String[]> convertedData) {
         List<FruitTransaction> fruitTransactionsList = new ArrayList<>();
         for (String[] s : convertedData) {
-            int quantity = Integer.parseInt(s[2]);
-            String fruit = s[1];
-            String operation = s[0];
+            int quantity = Integer.parseInt(s[INDEX_OF_QUANTITY]);
+            String fruit = s[INDEX_OF_FRUIT];
+            String operation = s[INDEX_OF_OPERATION];
             fruitTransactionsList.add(new FruitTransaction(quantity,
                     FruitTransaction.Operation.getOperation(operation), fruit));
         }
