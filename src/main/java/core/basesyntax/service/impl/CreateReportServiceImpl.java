394c9885ee584defcfb9fc5d682559da239e5cb0
Maksym <maxim.yashyn@gmail.com>
2023-03-15 12:20:26 +0200

    created and implemented CreateReportService

---
+++ src/main/java/core/basesyntax/service/impl/CreateReportServiceImpl.java
@@ -0,0 +1,18 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.CreateReportService;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CreateReportServiceImpl implements CreateReportService {
+    private static final String LINE_SEPARATOR = ",";
+    private static final String FIRST_ROW = "fruits, quantity";
+
+    @Override
+    public String createReport(Map<String, Integer> data) {
+        return FIRST_ROW + System.lineSeparator() + data.entrySet().stream()
+                .map(d -> d.getKey() + LINE_SEPARATOR + d.getValue() + System.lineSeparator())
+                .collect(Collectors.joining());
+    }
+}
