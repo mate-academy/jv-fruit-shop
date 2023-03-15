fd5860878b72f6f229a9c6b9dbabc2cd322ce3a5
Artem Grunin <artem.grunin.2006@gmail.com>
2023-03-15 13:19:30 +0200

    refactored try-catch blocks in WriteService

--- src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
@@ -10,16 +10,12 @@ public class WriterServiceImpl implements WriterService {
     @Override
     public void writeToFile(String pathToFile, String report) {
         File file = new File(pathToFile);
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file",e);
-        }
-
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            file.createNewFile();
             writer.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
+            throw new RuntimeException("Can't write data to file on the path "
+                    + pathToFile, e);
         }
     }
 }
