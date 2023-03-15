04dc2712d6a61a733a6a6be702240f27ef0a623d
Maksym <maxim.yashyn@gmail.com>
2023-03-15 10:27:14 +0200

    deleted redundant classes

--- src/main/java/core/basesyntax/service/ActivityStrategy.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service;
-
-import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.activity.ActivityHandler;
-
-public interface ActivityStrategy {
-    ActivityHandler get(FruitTransaction.ACTIVITY activity);
-}
