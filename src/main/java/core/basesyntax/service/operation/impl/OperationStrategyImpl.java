081365bf095e338bc3fbd606755992c2ccbb7da6
Maksym <maxim.yashyn@gmail.com>
2023-03-15 13:45:07 +0200

    changed structure and logick of OperationStrategyImpl

--- src/main/java/core/basesyntax/service/operation/impl/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/service/operation/impl/OperationStrategyImpl.java
@@ -4,16 +4,17 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.operation.OperationHandler;
 import core.basesyntax.service.operation.OperationStrategy;
 
+import java.util.Map;
+
 public class OperationStrategyImpl implements OperationStrategy {
+    Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
+
+    public OperationStrategyImpl(Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap) {
+        this.operationHandlerMap = operationHandlerMap;
+    }
+
     @Override
     public OperationHandler get(FruitTransaction.Operation operation) {
-        switch (operation) {
-            case BALANCE: return new BalanceOperationHandler();
-            case SUPPLY: return new SupplyOperationHandler();
-            case RETURN: return new ReturnOperationHandler();
-            case EXPIRED: return new ExpiredOperationHandler();
-            case PURCHASE: return new PurchaseOperationHandler();
-            default: throw new RuntimeException("Cannot find operation: " + operation.toString());
-        }
+        return operationHandlerMap.get(operation);
     }
 }
