325589bc5432ec6547a3e1897f774c9c1a2b6ebf
Maksym <maxim.yashyn@gmail.com>
2023-03-15 12:22:18 +0200

    added getter for Storage

--- src/main/java/core/basesyntax/dao/FruitShopDao.java
+++ src/main/java/core/basesyntax/dao/FruitShopDao.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao;
 import core.basesyntax.model.FruitTransaction;
 
 import java.util.List;
+import java.util.Map;
 
 public interface FruitShopDao {
     void add(String fruit, Integer value);
+    Map<String, Integer> get();
 }
