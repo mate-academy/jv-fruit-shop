014082e2e108aea4fcc34ed5101ecb737dfcdb30
Maksym <maxim.yashyn@gmail.com>
2023-03-15 12:20:35 +0200

    created and implemented WriteDataService

---
+++ src/main/java/core/basesyntax/service/WriteDataService.java
@@ -0,0 +1,5 @@
+package core.basesyntax.service;
+
+public interface WriteDataService {
+    void writeDataToFile(String data, String toFile);
+}
---
+++ src/main/java/core/basesyntax/service/impl/WriteDataServiceImpl.java
@@ -0,0 +1,20 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.WriteDataService;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteDataServiceImpl implements WriteDataService {
+    @Override
+    public void writeDataToFile(String data, String toFile) {
+        File file = new File(toFile);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
