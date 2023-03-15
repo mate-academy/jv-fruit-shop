12657bff1976e2278a6acb32e36c6784796d7c7f
Maksym <maxim.yashyn@gmail.com>
2023-03-15 10:22:34 +0200

    deleted redundant classes

--- src/main/java/core/basesyntax/service/activity/ReturnActivityHandler.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service.activity;
-
-public class ReturnActivityHandler implements ActivityHandler {
-    @Override
-    public Integer actionWithFruit(Integer amountOfFruit, Integer value) {
-        return amountOfFruit + value;
-    }
-}
--- src/main/java/core/basesyntax/service/activity/SupplyActivityHandler.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service.activity;
-
-public class SupplyActivityHandler implements ActivityHandler {
-    @Override
-    public Integer actionWithFruit(Integer amountOfFruit, Integer value) {
-        return amountOfFruit + value;
-    }
-}
