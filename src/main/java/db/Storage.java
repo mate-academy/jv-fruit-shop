410434e995a576cd6da921bb5fa4142b1455c478
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-03-15 16:45:21 +0200

    fixed checkstyle mistake

--- src/main/java/db/Storage.java
+++ src/main/java/db/Storage.java
@@ -6,10 +6,6 @@ import java.util.Map;
 public class Storage {
     public static final Map<String, Integer> fruits = new HashMap<>();
 
-//    public static Map<String, Integer> getFruitsMap() {
-//        return fruits;
-//    }
-
     public static void putOrChange(String fruitName, int quantity) {
         if (fruits.containsKey(fruitName)) {
             quantity += fruits.get(fruitName);
