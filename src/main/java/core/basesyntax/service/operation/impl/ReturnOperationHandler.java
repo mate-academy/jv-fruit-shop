7553c0781300fcac3036bcbfd63cb1c7025fa510
Maksym <maxim.yashyn@gmail.com>
2023-03-15 14:41:13 +0200

    added constructor

--- src/main/java/core/basesyntax/service/operation/impl/ReturnOperationHandler.java
+++ src/main/java/core/basesyntax/service/operation/impl/ReturnOperationHandler.java
@@ -1,12 +1,17 @@
 package core.basesyntax.service.operation.impl;
 
+import core.basesyntax.dao.FruitShopDao;
 import core.basesyntax.dao.FruitShopDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.operation.OperationHandler;
 
 public class ReturnOperationHandler implements OperationHandler {
-    private FruitShopDaoImpl fruitShopDao;
+    private FruitShopDao fruitShopDao;
+
+    public ReturnOperationHandler(FruitShopDao fruitShopDao) {
+        this.fruitShopDao = fruitShopDao;
+    }
 
     @Override
     public void operation(FruitTransaction fruitTransaction) {
