50f324e098bf4542825ff87a766ad0d6cbe61256
copypasterr <114933128+copypasterr@users.noreply.github.com>
2023-03-16 18:05:10 +0200

    test

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -44,6 +44,5 @@ public class Main {
         WriterService fileWriterService = new WriterServiceImpl();
         fileWriterService.write(report, OUTPUT_FILE_PATH);
         System.out.println("Fruit shop report: " + System.lineSeparator() + report);
-        System.lineSeparator();
     }
 }
