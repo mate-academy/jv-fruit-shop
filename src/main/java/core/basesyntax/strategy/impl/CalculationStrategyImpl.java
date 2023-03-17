7e5d824a571600221138febf4993265ef142182b
Sanyk Volodymyr <sanikvladimir@gmail.com>
2023-03-17 16:09:51 +0200

    added CalculationStrategy & main method

--- src/main/java/core/basesyntax/strategy/impl/CalculationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/CalculationStrategyImpl.java
@@ -2,18 +2,23 @@ package core.basesyntax.strategy.impl;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.CalculationStrategy;
-import java.util.List;
+import core.basesyntax.strategy.OperationHandler;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CalculationStrategyImpl implements CalculationStrategy {
-    private final List<CalculationStrategy> strategies;
+    private final Map<FruitTransaction.Operation, OperationHandler> transactionsMap;
 
-    public CalculationStrategyImpl(List<CalculationStrategy> strategies) {
-
-        this.strategies = strategies;
+    public CalculationStrategyImpl() {
+        transactionsMap = new HashMap<>();
+        transactionsMap.put(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler());
+        transactionsMap.put(FruitTransaction.Operation.RETURN, new ReturnOperationHandler());
+        transactionsMap.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler());
+        transactionsMap.put(FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler());
     }
 
     @Override
     public void calculate(FruitTransaction transaction) {
-        //todo
+        transactionsMap.get(transaction.getOperation()).operate(transaction);
     }
 }
