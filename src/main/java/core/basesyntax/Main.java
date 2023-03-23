aeca2d2a19ac48ea88de19e733bd13176e4e3dd0
Artur Muliukov <arthur.muliukov@gmail.com>
2023-03-23 12:50:38 +0100

    Version 1.15

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.dao.TransactionDaoImpl;
 import core.basesyntax.service.FruitShopServiceImpl;
+import core.basesyntax.service.filereader.FileParserImpl;
+import core.basesyntax.service.filereader.FileReaderImpl;
+import core.basesyntax.service.filewriter.FileWriterImpl;
 import core.basesyntax.service.interfaces.FruitShopService;
+import core.basesyntax.service.transactions.TransactionStrategyImpl;
 
 public class Main {
     public static void main(String[] args) {
-        FruitShopService fruitShopService = new FruitShopServiceImpl();
+        FruitShopService fruitShopService = new FruitShopServiceImpl(new TransactionDaoImpl(),
+                new FileReaderImpl(), new FileParserImpl(),
+                new FileWriterImpl(), new TransactionStrategyImpl());
         fruitShopService.createReport("src/main/resources/input.csv");
     }
 }
--- src/main/java/core/basesyntax/service/FruitShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/FruitShopServiceImpl.java
@@ -1,21 +1,16 @@
 package core.basesyntax.service;
 
 import core.basesyntax.dao.TransactionDao;
-import core.basesyntax.dao.TransactionDaoImpl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.filereader.FileParser;
-import core.basesyntax.service.filereader.FileParserImpl;
 import core.basesyntax.service.filereader.FileReader;
-import core.basesyntax.service.filereader.FileReaderImpl;
 import core.basesyntax.service.filewriter.FileWriter;
-import core.basesyntax.service.filewriter.FileWriterImpl;
 import core.basesyntax.service.interfaces.FruitShopService;
 import core.basesyntax.service.interfaces.strategy.TransactionStrategy;
 import core.basesyntax.service.transactions.BalanceTransactionHandler;
 import core.basesyntax.service.transactions.PurchaseTransactionHandler;
 import core.basesyntax.service.transactions.ReturnTransactionHandler;
 import core.basesyntax.service.transactions.SupplyTransactionHandler;
-import core.basesyntax.service.transactions.TransactionStrategyImpl;
 import java.util.List;
 
 public class FruitShopServiceImpl implements FruitShopService {
@@ -25,12 +20,14 @@ public class FruitShopServiceImpl implements FruitShopService {
     private final FileWriter writeToFile;
     private final TransactionStrategy transaction;
 
-    public FruitShopServiceImpl() {
-        transactionDao = new TransactionDaoImpl();
-        readFromFile = new FileReaderImpl();
-        parseDataFromFile = new FileParserImpl();
-        writeToFile = new FileWriterImpl();
-        transaction = new TransactionStrategyImpl();
+    public FruitShopServiceImpl(TransactionDao transactionDao,
+                                FileReader readFromFile, FileParser parseDataFromFile,
+                                FileWriter writeToFile, TransactionStrategy transaction) {
+        this.transactionDao = transactionDao;
+        this.readFromFile = readFromFile;
+        this.parseDataFromFile = parseDataFromFile;
+        this.writeToFile = writeToFile;
+        this.transaction = transaction;
     }
 
     @Override
