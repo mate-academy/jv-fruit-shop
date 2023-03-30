159eecc19f92d76bb2f0b0f40e9d2b72aafb8ead
Melnychuk <melnychukmyhailo@gmail.com>
2023-03-30 16:45:09 +0300

    Field name refactor.

--- src/main/java/core/basesyntax/strategy/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategyImpl.java
@@ -5,15 +5,15 @@ import core.basesyntax.service.operation.OperationHandler;
 import java.util.Map;
 
 public class OperationStrategyImpl implements OperationStrategy {
-    private final Map<FruitTransaction.Operation, OperationHandler> operationStrategyHashMap;
+    private final Map<FruitTransaction.Operation, OperationHandler> operationStrategyMap;
 
     public OperationStrategyImpl(Map<FruitTransaction.Operation,
             OperationHandler> operationStrategyHashMap) {
-        this.operationStrategyHashMap = operationStrategyHashMap;
+        this.operationStrategyMap = operationStrategyHashMap;
     }
 
     @Override
     public OperationHandler get(FruitTransaction.Operation operation) {
-        return operationStrategyHashMap.get(operation);
+        return operationStrategyMap.get(operation);
     }
 }
