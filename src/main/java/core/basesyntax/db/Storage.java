8f18d76082d40b885c0cf184e72a9c5f062114d3
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2023-04-10 21:57:01 +0300

    Checkstyle fixed

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -4,5 +4,5 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    public static final HashMap<String,Integer> records = new HashMap<>();
+    public static final Map<String,Integer> records = new HashMap<>();
 }
