08fff268d8fcd0cf61273198e04731d9e8486fd4
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-04-18 13:37:39 +0300

    created storage and model with realization for fruit shop

---
+++ src/main/java/core/basesyntax/model/Fruit.java
@@ -0,0 +1,55 @@
+package core.basesyntax.model;
+
+public class Fruit {
+    private Operation operation;
+    private String fruit;
+    private int amount;
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private String code;
+
+        Operation(String code) {
+            this.code = code;
+        }
+
+        public static Operation getCode(String code) {
+            switch (code) {
+                case ("b") : return Operation.BALANCE;
+                case ("s") : return Operation.SUPPLY;
+                case ("p") : return Operation.PURCHASE;
+                case ("r") : return Operation.RETURN;
+                default:
+                    return null;
+            }
+        }
+    }
+}
