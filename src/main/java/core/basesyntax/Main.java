08c3750f72d7370737592801c493240a5dc79981
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-04-26 19:07:02 +0300

    my realization fruitshop task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,14 +1,20 @@
 package core.basesyntax;
 
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.*;
-import core.basesyntax.service.operation.*;
+import core.basesyntax.service.FruitTransferImpl;
+import core.basesyntax.service.ReadScvService;
+import core.basesyntax.service.ReadScvServiceImpl;
+import core.basesyntax.service.ReportService;
+import core.basesyntax.service.ReportServiceImpl;
+import core.basesyntax.service.operation.BalanceOperation;
+import core.basesyntax.service.operation.OperationHandler;
+import core.basesyntax.service.operation.PurchaseOperation;
+import core.basesyntax.service.operation.SupplyOperation;
+import core.basesyntax.service.operation.ReturnOperation;
 import core.basesyntax.service.strategy.OperationStrategy;
 import core.basesyntax.service.strategy.OperationStrategyImp;
-
 import java.util.HashMap;
 import java.util.Map;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -23,6 +29,5 @@ public class Main {
         fruitTransfer.transfer();
         ReportService report = new ReportServiceImpl();
         report.createReport();
-
     }
 }
