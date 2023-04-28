f0de8e1272a30da63495d5bcb0ba70b2923f7336
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-04-28 23:44:26 +0300

    fixed fruitshop task

--- src/main/java/core/basesyntax/service/impl/WriteScvServiceIml.java
+++ src/main/java/core/basesyntax/service/impl/WriteScvServiceIml.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
 public class WriteScvServiceIml implements WriteScvService {
     @Override
     public void writeDataScvFile(String data, String fileName) {
