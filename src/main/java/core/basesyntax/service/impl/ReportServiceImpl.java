e49d19d41f526d8911db523972ed8866e0075199
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-05-01 22:26:27 +0300

    fixed fruitshop task

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.ReportService;
-
 import java.util.List;
 
 public class ReportServiceImpl implements ReportService {
