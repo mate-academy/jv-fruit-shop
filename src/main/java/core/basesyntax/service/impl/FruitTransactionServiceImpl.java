6a8ea37177d2b917d3394d53e837522bdf4e404e
Александр Загородний <git.hub.zagar@gmail.com>
2023-05-02 23:36:43 +0200

    implemented the FruitShop task according to SOLID principles

---
+++ src/main/java/core/basesyntax/service/impl/FruitTransactionServiceImpl.java
@@ -0,0 +1,34 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.dao.FruitTranzactionDao;
+import core.basesyntax.dao.impl.FruitTransactionDaoImpl;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.model.Operation;
+import core.basesyntax.service.FruitTransactionService;
+import java.util.List;
+
+public class FruitTransactionServiceImpl implements FruitTransactionService {
+    private static final int INDEX_OPERATION = 0;
+    private static final int INDEX_FRUIT = 1;
+    private static final int INDEX_QUANTITY = 2;
+    private static final String COMMA = ",";
+    private final FruitTranzactionDao fruitTranzactionDao = new FruitTransactionDaoImpl();
+    
+    @Override
+    public void createNewFruitTransaction(List<String> fileData) {
+        for (String data : fileData) {
+            FruitTransaction fruitTransaction = new FruitTransaction();
+            String[] dataValue = data.split(COMMA);
+            
+            for (Operation operation : Operation.values()) {
+                if (operation.getCode().equals(dataValue[INDEX_OPERATION])) {
+                    fruitTransaction.setOperation(operation);
+                    break;
+                }
+            }
+            fruitTransaction.setFruit(dataValue[INDEX_FRUIT]);
+            fruitTransaction.setQuantity(Integer.parseInt(dataValue[INDEX_QUANTITY]));
+            fruitTranzactionDao.add(fruitTransaction);
+        }
+    }
+}
