517b67e5876305f7823134d464c0fa61fd3f18c0
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-05-13 19:20:28 +0300

    hw-jv-fruit-shop

--- src/main/java/core/basesyntax/strategy/handler/impl/ReturnHandler.java
+++ src/main/java/core/basesyntax/strategy/handler/impl/ReturnHandler.java
@@ -2,6 +2,7 @@ package core.basesyntax.strategy.handler.impl;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.handler.OperationHandler;
+
 public class ReturnHandler implements OperationHandler {
 
     @Override
--- src/main/java/core/basesyntax/strategy/handler/impl/SupplyHandler.java
+++ src/main/java/core/basesyntax/strategy/handler/impl/SupplyHandler.java
@@ -2,6 +2,7 @@ package core.basesyntax.strategy.handler.impl;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.handler.OperationHandler;
+
 public class SupplyHandler implements OperationHandler {
     @Override
     public void handle(FruitTransaction transaction) {
