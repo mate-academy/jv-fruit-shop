396803d88877a4f16312f639bf3911ec2c9c3fac
Nikita <katkovnikitka@gmail.com>
2023-05-22 11:15:16 +0200

    Update

---
+++ src/main/java/core/basesyntax/service/impl/FruitServiceImpl.java
@@ -0,0 +1,18 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.FruitService;
+import core.basesyntax.strategy.OperationHandler;
+import core.basesyntax.strategy.OperationStrategy;
+import java.util.List;
+
+public class FruitServiceImpl implements FruitService {
+    @Override
+    public void getOperationByFruit(List<FruitTransaction> fruitTransactionList,
+                                    OperationStrategy strategy) {
+        for (FruitTransaction fruitTransaction : fruitTransactionList) {
+            OperationHandler handler = strategy.get(fruitTransaction.getOperation());
+            handler.operateTransaction(fruitTransaction);
+        }
+    }
+}
---
+++ src/main/java/core/basesyntax/service/impl/ReaderServiceImpl.java
@@ -0,0 +1,20 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.ReaderService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ReaderServiceImpl implements ReaderService {
+    @Override
+    public List<String> readFromFile(String fileName) {
+        try {
+            List<String> list = Files.readAllLines(Path.of(fileName));
+            list.remove(0);
+            return list;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't get data from file " + fileName, e);
+        }
+    }
+}
