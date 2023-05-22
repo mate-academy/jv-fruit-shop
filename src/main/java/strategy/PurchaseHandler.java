000685b146c0f1e008d2479b22e2485aa17647a7
Ivan Yuriv <yuriv1024@gmail.com>
2023-05-22 21:27:04 +0300

    Exceptions in class PurchaseHandler were added.

--- src/main/java/strategy/PurchaseHandler.java
+++ src/main/java/strategy/PurchaseHandler.java
@@ -8,8 +8,14 @@ public class PurchaseHandler implements OperationHandler {
     @Override
     public void handle(FruitTransaction fruitTransaction) {
         String fruitName = fruitTransaction.getFruit();
+        if (fruitName == null) {
+            throw new RuntimeException("No such fruit found");
+        }
         int purchasedQuantity = fruitTransaction.getQuantity();
         int oldQuantity = Storage.fruits.get(fruitName);
+        if (purchasedQuantity - oldQuantity < 0) {
+            throw new RuntimeException("Such a number of fruits not found");
+        }
         Storage.fruits.put(fruitName, oldQuantity - purchasedQuantity);
     }
 }
