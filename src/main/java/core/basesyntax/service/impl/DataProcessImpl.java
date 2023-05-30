c20fd45a248aa150fad689fad772201cf24544c0
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-05-30 18:05:53 +0300

    add fixes to the DataProcess interface and DataProcessImpl class after students review

--- src/main/java/core/basesyntax/service/impl/DataProcessImpl.java
+++ src/main/java/core/basesyntax/service/impl/DataProcessImpl.java
@@ -3,15 +3,15 @@ package core.basesyntax.service.impl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.DataProcess;
 import core.basesyntax.strategy.OperationStrategy;
-
 import java.util.List;
 
 public class DataProcessImpl implements DataProcess {
-    OperationStrategy operationStrategy;
+    private OperationStrategy operationStrategy;
 
     public DataProcessImpl(OperationStrategy operationStrategy) {
         this.operationStrategy = operationStrategy;
     }
+
     @Override
     public void addDataToDB(List<FruitTransaction> fruitTransactions) {
         for (FruitTransaction fruitTransaction : fruitTransactions) {
