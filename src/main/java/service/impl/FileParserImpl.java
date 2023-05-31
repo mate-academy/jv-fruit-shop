65b8b28bc03068fa214522f918dcf40c5462d3bc
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-05-31 17:51:51 +0200

    1. Removed redundant structure InFileStructure. 2. In FileParserImpl removed all logic related to InFileStructure.

--- src/main/java/service/impl/FileParserImpl.java
+++ src/main/java/service/impl/FileParserImpl.java
@@ -3,7 +3,6 @@ package service.impl;
 import java.util.ArrayList;
 import java.util.List;
 import model.FruitTransaction;
-import model.InFileStructure;
 import model.OperationType;
 import service.FileParser;
 
@@ -12,24 +11,20 @@ public class FileParserImpl implements FileParser {
     private static final byte TYPE = 0;
     private static final byte FRUIT = 1;
     private static final byte QUANTITY = 2;
+    private static final byte COLUMNS_NAMES = 0;
 
     @Override
     public List<FruitTransaction> getFruitTransaction(List<String> data) {
-        InFileStructure inFileStructure;
         List<FruitTransaction> fruitTransactionList = new ArrayList<>();
-        byte lineNumber = 0;
+
+        data.remove(COLUMNS_NAMES);
 
         for (var line : data) {
             String[] splitString = line.split(ONLY_LETTERS_SEPARATOR);
-            if (lineNumber < 1) {
-                inFileStructure = new InFileStructure(splitString[TYPE],
-                        splitString[FRUIT], splitString[QUANTITY]);
-                lineNumber++;
-            } else {
-                fruitTransactionList
-                        .add(new FruitTransaction(OperationType.getByCode(splitString[TYPE]),
-                        splitString[FRUIT], Integer.parseInt(splitString[QUANTITY])));
-            }
+            fruitTransactionList
+                    .add(new FruitTransaction(OperationType.getByCode(splitString[TYPE]),
+                            splitString[FRUIT], Integer.parseInt(splitString[QUANTITY])));
+
         }
         return fruitTransactionList;
     }
