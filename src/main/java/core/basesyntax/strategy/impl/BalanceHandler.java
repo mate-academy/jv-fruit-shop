ae863cf643d4367a0b40bdb8630c93933ef7b64f
Petro Kobevko <kobevkopi@gmail.com>
2023-05-31 16:46:09 +0200

    Refactored magic number in BalanceHandler class

--- src/main/java/core/basesyntax/strategy/impl/BalanceHandler.java
+++ src/main/java/core/basesyntax/strategy/impl/BalanceHandler.java
@@ -5,10 +5,14 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.FruitTransactionHandler;
 
 public class BalanceHandler implements FruitTransactionHandler {
+    private static final int MINIMUM_BALANCE_AMOUNT = 1;
+
     @Override
     public void handle(FruitTransaction fruitTransaction) {
-        if (fruitTransaction.getQuantity() < 0) {
-            throw new RuntimeException("Balance-transaction quantity cannot be less than zero");
+        if (fruitTransaction.getQuantity() < MINIMUM_BALANCE_AMOUNT) {
+            throw new RuntimeException(
+                    "Balance-transaction quantity cannot be less than "
+                            + MINIMUM_BALANCE_AMOUNT);
         }
         Storage.storageMap.put(fruitTransaction.getFruit(), fruitTransaction.getQuantity());
     }
