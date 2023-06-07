9aa26b1a1baf8253e2665b0f27eb081705bcf791
Ros <rospsix@gmail.com>
2023-06-07 11:22:49 +0100

    handles a fruit purchase transaction by subtracting the purchased quantity from the existing quantity in the Storage.fruits map. If the purchase quantity is valid, it updates the quantity in the map.

---
+++ src/main/java/core/basesyntax/strategy/handler/PurchaseHandler.java
@@ -0,0 +1,18 @@
+package core.basesyntax.strategy.handler;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+
+public class PurchaseHandler implements OperationHandler {
+    @Override
+    public void operate(FruitTransaction transaction) {
+        Integer storageQuantity = Storage.fruits.get(transaction.getFruit());
+        int purchaseQuantity = transaction.getQuantity();
+        if (purchaseQuantity <= storageQuantity && purchaseQuantity > 0) {
+            int updateQuantity = storageQuantity - purchaseQuantity;
+            Storage.put(transaction.getFruit(), updateQuantity);
+        } else {
+            throw new IllegalArgumentException("Invalid purchase Quantity" + purchaseQuantity);
+        }
+    }
+}
