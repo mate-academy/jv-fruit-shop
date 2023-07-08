861c8dd9f1c49ac5034882c05d131b27cb773805
Bohdan Shevtsov <shevtsov.b@gmail.com>
2023-07-08 16:38:41 +0300

    commit all classes

--- src/main/java/core/basesyntax/service/impl/FileReaderImpl.java
+++ src/main/java/core/basesyntax/service/impl/FileReaderImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.service.FileReaderService;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -11,16 +10,14 @@ public class FileReaderImpl implements FileReaderService {
     @Override
     public FileReaderResult readFile(String inputFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
-            String line = reader.readLine(); //skip header
+            String line;
             StringBuilder contentBuilder = new StringBuilder();
             while ((line = reader.readLine()) != null) {
                 contentBuilder.append(line).append("\n");
             }
             return new FileReaderResult(contentBuilder.toString());
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found ",e);
         } catch (IOException e) {
-            throw new RuntimeException("Input output error ", e);
+            throw new RuntimeException("Can't read the file ", e);
         }
     }
 }
