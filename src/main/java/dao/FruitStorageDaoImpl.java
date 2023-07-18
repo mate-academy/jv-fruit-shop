bfff5dc23d72321f39de67e18c91fe8079e4caf7
Artem <artemenko.as@gmail.com>
2023-07-18 10:30:27 +0300

    FruitStorage class renamed to Storage, inner logic removed to FruitStorageDaoImpl class. Added interface FruitStorageDao

--- src/main/java/dao/FruitStorageDaoImpl.java
+++ src/main/java/dao/FruitStorageDaoImpl.java
@@ -1,64 +1,66 @@
-package db;
+package dao;
 
-import java.util.HashMap;
+import db.Storage;
 import java.util.Map;
 import model.Fruit;
 
-public class FruitStorage {
-    private final Map<String,Integer> fruitStock;
+public class FruitStorageDaoImpl implements FruitStorageDao {
+    private static final int MIN_QUANTITY = 0;
+    private final Map<String,Integer> fruitStock = Storage.FRUITS;
 
-    public FruitStorage() {
-        fruitStock = new HashMap<>();
-    }
-
+    @Override
     public Fruit getFruit(String name) {
         if (fruitStock.get(name) == null) {
             throw new RuntimeException("There is no such fruit type " + name + " in storage");
         }
         Integer quantity = fruitStock.get(name);
-        if (quantity < 0) {
+        if (quantity < MIN_QUANTITY) {
             throw new RuntimeException("Stored quantity can't be less than zero");
         }
-        return new Fruit(name, quantity == null ? 0 : quantity);
+        return new Fruit(name, quantity == null ? MIN_QUANTITY : quantity);
     }
 
-    public void setFruit(String name, Integer quantity) {
+    @Override
+    public void set(String name, Integer quantity) {
         fruitStock.put(name, quantity);
     }
 
-    public void addFruitsToStock(String name, Integer quantity) {
-        if (quantity < 0) {
+    @Override
+    public void add(String name, Integer quantity) {
+        if (quantity < MIN_QUANTITY) {
             throw new RuntimeException("Fruit quantity can't be less than zero");
         }
         Integer storedQuantity = fruitStock.get(name);
         if (storedQuantity == null) {
-            storedQuantity = 0;
+            storedQuantity = MIN_QUANTITY;
         }
-        if (storedQuantity < 0) {
+        if (storedQuantity < MIN_QUANTITY) {
             throw new RuntimeException("Stored quantity can't be less than zero");
         }
         fruitStock.put(name, storedQuantity + quantity);
     }
 
-    public void removeFruitsFromStock(String name, Integer quantity) {
+    @Override
+    public void remove(String name, Integer quantity) {
         if (fruitStock.get(name) == null) {
             throw new RuntimeException("There is no such fruit type " + name + " in storage");
         }
         Integer storedQuantity = fruitStock.get(name);
         if (storedQuantity == null) {
-            storedQuantity = 0;
+            storedQuantity = MIN_QUANTITY;
         }
         Integer resultQuantity = storedQuantity - quantity;
-        if (resultQuantity < 0) {
+        if (resultQuantity < MIN_QUANTITY) {
             throw new RuntimeException("Stored quantity can't be less than zero");
-        } else if (resultQuantity == 0) {
+        } else if (resultQuantity == MIN_QUANTITY) {
             fruitStock.remove(name);
         } else {
             fruitStock.put(name, resultQuantity);
         }
     }
 
-    public Map<String, Integer> getFruitStock() {
+    @Override
+    public Map<String, Integer> getFruits() {
         return fruitStock;
     }
 }
