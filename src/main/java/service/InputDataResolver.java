f5f4015e0f9488426e78e7603da409a328566d8d
Artem <artemenko.as@gmail.com>
2023-07-18 10:32:11 +0300

    All requested changes were made.

--- src/main/java/service/InputDataResolver.java
+++ src/main/java/service/InputDataResolver.java
@@ -4,23 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import model.InputDataType;
 
-public class InputDataResolver {
-    public ArrayList<InputDataType> resolveData(List<String> lines) {
-        ArrayList<InputDataType> result = new ArrayList<>();
-        for (String entry : lines) {
-            String[] data = entry.split(",");
-            if (data.length < 3) {
-                throw new RuntimeException("Invalid input data " + entry);
-            }
-            String operation = data[0];
-            String fruit = data[1];
-            int quantity = Integer.parseInt(data[2]);
-            if (quantity < 0) {
-                throw new RuntimeException("Fruit quantity is " + quantity
-                        + "\n Fruit quantity must be greater or equal to zero");
-            }
-            result.add(new InputDataType(operation, fruit, quantity));
-        }
-        return result;
-    }
+public interface InputDataResolver {
+    ArrayList<InputDataType> resolveData(List<String> lines);
 }
