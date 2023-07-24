5d532de06ab93d3af646d1efa0f962f046b087bc
heilxwotan1 <noobdima228@gmail.com>
2023-07-24 14:43:36 +0300

    fixed mistakes from review

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -13,11 +13,15 @@ import core.basesyntax.service.OperationProcessImpl;
 import java.util.List;
 
 public class Main {
+    private static final String LINE = System.lineSeparator();
+    private static final String INPUT_FILE_PATH = "src\\main\\resources\\test.csv";
+    private static final String OUTPUT_FILE_PATH = "src\\main\\resources\\dailyReport";
+
     public static void main(String[] args) {
         FileReaderServiceImpl fileReaderService = new FileReaderServiceImpl();
         DataInputParser dataParser = new DataInputParserImpl();
         List<String> dataFromFile = fileReaderService
-                .readFromFile("src\\main\\java\\core\\basesyntax\\test.csv");
+                .readFromFile(INPUT_FILE_PATH);
         List<FruitTransaction> fruitTransactionList = dataParser.parseData(dataFromFile);
         System.out.println(fruitTransactionList);
         OperationProcess operationProcess = new OperationProcessImpl();
@@ -25,6 +29,6 @@ public class Main {
         DataOutputParser dataOutputParser = new DataOutputParserImpl();
         String report = dataOutputParser.parseData();
         FileWriterService fileWriterService = new FileWriterServiceImpl();
-        fileWriterService.write(report, "dailyReport");
+        fileWriterService.write(report, OUTPUT_FILE_PATH);
     }
 }
