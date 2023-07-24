435839d2f40673eeb5004d5e35ee2531cd9d9c4a
Zhuk Sergei <svzhuk@ukr.net>
2023-07-24 19:16:16 +0300

    Solution

---
+++ src/main/java/core/basesyntax/strategy/ReturnOperationHandler.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+
+public class ReturnOperationHandler implements OperationHandler {
+    @Override
+    public void handle(FruitTransaction transaction) {
+        int oldQuantity = Storage.fruitStorage.get(transaction.getFruit());
+        Storage.fruitStorage.put(transaction.getFruit(), transaction.getQuantity() + oldQuantity);
+    }
+}
