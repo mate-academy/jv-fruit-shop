80fc81aaf38f723dfbb443477fef30032bb2f824
ZVIIR <denisarius2000s@gmail.com>
2023-07-31 20:43:24 +0300

    fixed data parser

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -15,5 +15,6 @@ public class Main {
     public static void main(String[] args) {
         List<String> fileData = READER_SERVICE.read(INPUT_FILE_PATH);
         List<FruitTransaction> parsedFile = PARSER_SERVICE.parse(fileData);
+
     }
 }
--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -1,12 +1,16 @@
 package core.basesyntax.model;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class FruitTransaction {
     private final OperationType operation;
     private final String name;
     private final int amount;
 
     public FruitTransaction(String operation, String fruit, int amount) {
-        this.operation = OperationType.valueOf(operation);
+        this.operation = OperationType.get(operation);
         this.name = fruit;
         this.amount = amount;
     }
@@ -30,6 +34,7 @@ public class FruitTransaction {
         RETURN("r");
 
         private final String code;
+        private static final Map<String, OperationType> ENUM_MAP;
 
         OperationType(String code) {
             this.code = code;
@@ -37,6 +42,18 @@ public class FruitTransaction {
 
         public String getCode() {
             return code;
+        }
+
+        static {
+            Map<String, OperationType> map = new ConcurrentHashMap<>();
+            for (OperationType instance : OperationType.values()) {
+                map.put(instance.getCode().toLowerCase(), instance);
+            }
+            ENUM_MAP = Collections.unmodifiableMap(map);
+        }
+
+        public static OperationType get(String name) {
+            return ENUM_MAP.get(name.toLowerCase());
         }
     }
 }
