5d307a5776f5f7c5265fac91739d0510572347d8
ZVIIR <denisarius2000s@gmail.com>
2023-07-31 16:09:48 +0300

    added skip for titles in CsvReaderService

--- src/main/java/core/basesyntax/service/impl/CsvReaderService.java
+++ src/main/java/core/basesyntax/service/impl/CsvReaderService.java
@@ -15,7 +15,7 @@ public class CsvReaderService implements ReaderService {
     public List<String> read(String filePath) {
         Path path = Paths.get(filePath);
         try (Stream<String> lines = Files.lines(path)) {
-            return lines.collect(Collectors.toList());
+            return lines.skip(1).collect(Collectors.toList());
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file " + filePath, e);
         }
