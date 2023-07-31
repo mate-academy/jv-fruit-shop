dd1d978cd6e78234171ffd832d972f3ff0b9071b
ZVIIR <denisarius2000s@gmail.com>
2023-07-31 16:21:32 +0300

    implement data parser

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,16 +1,19 @@
 package core.basesyntax;
 
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.Parser;
 import core.basesyntax.service.impl.CsvReaderService;
 import core.basesyntax.service.ReaderService;
-
+import core.basesyntax.service.impl.ParserImpl;
 import java.util.List;
 
 public class Main {
+    private static final ReaderService READER_SERVICE = new CsvReaderService();
+    private static final Parser PARSER_SERVICE = new ParserImpl();
     private static final String INPUT_FILE_PATH = "src/main/resources/CsvInputData.csv";
+
     public static void main(String[] args) {
-        ReaderService readerService = new CsvReaderService();
-        List<String> fileData = readerService.read(INPUT_FILE_PATH);
-
-
+        List<String> fileData = READER_SERVICE.read(INPUT_FILE_PATH);
+        List<FruitTransaction> parsedFile = PARSER_SERVICE.parse(fileData);
     }
 }
--- src/main/java/core/basesyntax/service/impl/ParserImpl.java
+++ src/main/java/core/basesyntax/service/impl/ParserImpl.java
@@ -4,10 +4,26 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.Parser;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ParserImpl implements Parser {
+    private static final String COMMA_REGEX = ",";
+    private static final int OPERATION_INDEX = 0;
+    private static final int FRUIT_INDEX = 1;
+    private static final int AMOUNT_INDEX = 2;
+
     @Override
     public List<FruitTransaction> parse(List<String> data) {
-        return null;
+        return data.stream()
+                .map(line -> line.split(COMMA_REGEX))
+                .map(this::createFruitTransaction)
+                .collect(Collectors.toList());
+    }
+
+    private FruitTransaction createFruitTransaction(String[] dataLine) {
+        return new FruitTransaction(
+                dataLine[OPERATION_INDEX],
+                dataLine[FRUIT_INDEX],
+                Integer.parseInt(dataLine[AMOUNT_INDEX]));
     }
 }
