9e622c8677f94ad96497882c9ad6e9ed8fe5f0fe
Влад <dakadofordisluv@gmail.com>
2023-08-01 15:55:27 +0300

    added 'Main' to run application

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+import dao.DbDao;
+import dao.StorageDao;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.Transaction;
+import service.file.CsvFileReader;
+import service.file.CsvFileWriter;
+import service.file.FileReader;
+import service.file.FileWriter;
+import service.report.DbReporter;
+import service.report.Reporter;
+import service.transaction.CsvTransactionParser;
+import service.transaction.ProductTransactionExecutor;
+import service.transaction.TransactionExecutor;
+import service.transaction.TransactionParser;
+import service.transaction.strategy.ProductTransactionStrategy;
+import service.transaction.strategy.TransactionStrategy;
+import service.transaction.strategy.type.BalanceTransaction;
+import service.transaction.strategy.type.PurchaseTransaction;
+import service.transaction.strategy.type.ReturnTransaction;
+import service.transaction.strategy.type.SupplyTransaction;
+import service.transaction.strategy.type.TransactionHandler;
+
+public class Main {
+    private static String RECORDS_FILE_NAME = "Records.csv";
+
+    public static void main(String[] args) {
+        DbDao dbDao = new StorageDao();
+        FileReader fileReader = new CsvFileReader();
+        TransactionParser transactionParser = new CsvTransactionParser();
+        TransactionExecutor transactionExecutor = buildTransactionExecutor(dbDao);
+        Reporter reporter = new DbReporter(dbDao);
+        FileWriter fileWriter = new CsvFileWriter();
+
+        List<String> data = fileReader.read(RECORDS_FILE_NAME);
+        List<Transaction> transactions = transactionParser.parse(data);
+        transactionExecutor.execute(transactions);
+        List<String> report = reporter.generate();
+        fileWriter.write(report);
+    }
+
+    private static TransactionExecutor buildTransactionExecutor(DbDao dbDao) {
+        Map<Transaction.Type, TransactionHandler> handlers = new HashMap<>();
+        handlers.put(Transaction.Type.BALANCE, new BalanceTransaction(dbDao));
+        handlers.put(Transaction.Type.PURCHASE, new PurchaseTransaction(dbDao));
+        handlers.put(Transaction.Type.SUPPLY, new SupplyTransaction(dbDao));
+        handlers.put(Transaction.Type.RETURN, new ReturnTransaction(dbDao));
+        TransactionStrategy transactionStrategy = new ProductTransactionStrategy(handlers);
+        return new ProductTransactionExecutor(transactionStrategy);
+    }
+}
