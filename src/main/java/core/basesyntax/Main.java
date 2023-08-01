9bdb400b886d1205ad2b24eb15b84bc82038d465
Ihor Vasylenko <vasylenkoihor23@gmail.com>
2023-08-01 13:11:07 +0300

    improved solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -24,30 +24,28 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-
+        // read data from file
         CsvReaderService csvReaderServiceImpl = new CsvReaderServiceImpl();
         String path = "src/main/resources/input.csv";
         List<String> input = csvReaderServiceImpl.readFromFile(path);
-
+        //convert data from file to Java object
         FruitDao newDao = new FruitDaoImpl();
         DaoService daoService = new DaoServiceImpl(newDao);
         daoService.convertData(input);
-
+        //process Java object
         Map<Fruit.Operation, ActivitiesService> activitiesServiceMap = new HashMap<>();
         activitiesServiceMap.put(Fruit.Operation.BALANCE, new Balance());
         activitiesServiceMap.put(Fruit.Operation.PURCHASE, new Purchase());
         activitiesServiceMap.put(Fruit.Operation.SUPPLY, new Supply());
         activitiesServiceMap.put(Fruit.Operation.RETURN, new Return());
-
         QuantityModificationServiceImpl quantModServ = new QuantityModificationServiceImpl(
                 new ActivityStrategyImpl(activitiesServiceMap), newDao);
         List<Fruit> fruits = quantModServ.modifyQuantity();
-
+        //create report
         ReportCreatorService report = new ReportCreatorServiceImpl();
         Map<String, Integer> report1 = report.createReport(fruits);
-
+        //write report to file
         CsvWriterService csvWriterService = new CsvWriterServiceImpl();
         csvWriterService.writeToReport("src/main/resources/report.csv", report1);
-
     }
 }
