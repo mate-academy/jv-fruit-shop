c197cee6ed8d65dda5ac1d61a030c73de1500299
Vitalii <vitaliyplys1@gmail.com>
2023-08-01 19:29:00 +0300

    added fixes

---
+++ src/main/java/core/basesyntax/service/impl/ReaderServiceImpl.java
@@ -0,0 +1,18 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.ReaderService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReaderServiceImpl implements ReaderService {
+    @Override
+    public List<String> readFromFile(String filePath) {
+        try {
+            return Files.readAllLines(Paths.get(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't find file by path: " + filePath, e);
+        }
+    }
+}
