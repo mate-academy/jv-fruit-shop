c7489a437cdaf3dd37e7f6c21374d07382534bd8
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-08-01 19:50:24 +0300

    Add validation for purchase operation

--- src/main/java/core/basesyntax/strategy/PurchaseOperation.java
+++ src/main/java/core/basesyntax/strategy/PurchaseOperation.java
@@ -6,6 +6,20 @@ import core.basesyntax.storage.Storage;
 public class PurchaseOperation implements OperationStrategy {
     @Override
     public void apply(Fruit fruit, Integer quantity) {
-        Storage.fruits.merge(fruit, quantity, (oldValue, subtrahend) -> oldValue - subtrahend);
+        Integer newValue = Storage.fruits
+                .computeIfPresent(fruit, (k, v) -> purchase(v, quantity));
+        if (newValue == null) {
+            throw new IllegalArgumentException(
+                    "Such fruit is not exist in the Storage: " + fruit.getName());
+        }
+    }
+
+    private Integer purchase(Integer oldValue, Integer subtrahend) {
+        if (oldValue < subtrahend) {
+            throw new IllegalArgumentException(
+                    "There are not enough fruits. Amount: " + oldValue
+                            + ", but need " + subtrahend);
+        }
+        return  oldValue - subtrahend;
     }
 }
