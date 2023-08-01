0d0c492261754e0178daf07e475136856deda869
ZVIIR <denisarius2000s@gmail.com>
2023-08-01 14:44:01 +0300

    moved magic number to constants

--- src/main/java/core/basesyntax/service/impl/CsvReaderService.java
+++ src/main/java/core/basesyntax/service/impl/CsvReaderService.java
@@ -10,11 +10,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CsvReaderService implements ReaderService {
+    private static final int TITLE_ROW = 1;
+
     @Override
     public List<String> read(String filePath) {
         Path path = Paths.get(filePath);
         try (Stream<String> lines = Files.lines(path)) {
-            return lines.skip(1).collect(Collectors.toList());
+            return lines.skip(TITLE_ROW).collect(Collectors.toList());
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file " + filePath, e);
         }
