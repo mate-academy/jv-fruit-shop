4d0848bb0be7a4c056492c9c339f40f871e8c363
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-08-02 22:40:13 +0300

    Removed redundant null check and moved File to try

--- src/main/java/core/basesyntax/impl/ReadFromCsv.java
+++ src/main/java/core/basesyntax/impl/ReadFromCsv.java
@@ -1,22 +1,19 @@
 package core.basesyntax.impl;
 
-import core.basesyntax.service.ReadService;
+import core.basesyntax.service.ReadFileService;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
-public class ReadFromCsv implements ReadService {
+public class ReadFileFromCsv implements ReadFileService {
     @Override
     public List<String> readFromFile(String path) {
-        if (path == null) {
-            throw new RuntimeException("Can't read from file with path null");
-        }
-        File fileToRead = new File(path);
         try {
+            File fileToRead = new File(path);
             return Files.readAllLines(fileToRead.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read from file with path: " + fileToRead.getPath());
+            throw new RuntimeException("Can't read from file with path: " + path, e);
         }
     }
 }
