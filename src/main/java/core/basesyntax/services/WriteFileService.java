a58aac442d044347a8d762dc9dca65a65d4640d4
Mykyta Zuber <nikitazuber@gmail.com>
2023-08-02 10:33:10 +0300

    Refactor code

--- src/main/java/core/basesyntax/services/WriteFileService.java
+++ src/main/java/core/basesyntax/services/WriteFileService.java
@@ -2,8 +2,6 @@ package core.basesyntax.services;
 
 import core.basesyntax.dao.StoreDao;
 
-import java.util.Map;
-
 public interface WriteFileService {
     void writeToFile(StoreDao dao, String fileName);
 }
