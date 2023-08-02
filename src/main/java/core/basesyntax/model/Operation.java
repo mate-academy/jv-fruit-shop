df88b3b2470600a92b4651763bc1d343daefba66
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-08-02 22:41:41 +0300

    Use streams

--- src/main/java/core/basesyntax/model/Operation.java
+++ src/main/java/core/basesyntax/model/Operation.java
@@ -1,27 +1,24 @@
 package core.basesyntax.model;
 
+import java.util.Arrays;
+
 public enum Operation {
     BALANCE("b"),
     SUPPLY("s"),
     PURCHASE("p"),
     RETURN("r");
 
-    private String code;
+    private final String code;
 
     Operation(String code) {
         this.code = code;
     }
 
     public static Operation convertToOperation(String code) {
-        for (Operation operation : Operation.values()) {
-            if (operation.code.equals(code)) {
-                return operation;
-            }
-        }
-        throw new RuntimeException("There is no such operation with code: " + code);
-    }
-
-    public String getCode() {
-        return code;
+        return Arrays.stream(Operation.values())
+                .filter(operation -> operation.code.equals(code))
+                .findFirst()
+                .orElseThrow(() ->
+                        new RuntimeException("There is no such operation with code: " + code));
     }
 }
