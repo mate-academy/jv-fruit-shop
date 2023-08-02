1e8e5aaee266b6ce25944a0801b0f437a1d327d4
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-08-02 22:38:00 +0300

    Maintained SRP

--- src/main/java/core/basesyntax/impl/OperationProcess.java
+++ src/main/java/core/basesyntax/impl/OperationProcess.java
@@ -1,32 +1,27 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.model.Operation;
 import core.basesyntax.service.DataProcesser;
-import core.basesyntax.strategy.BalanceOperation;
+import core.basesyntax.strategy.BalanceOperationHandler;
 import core.basesyntax.strategy.OperationHandler;
-import core.basesyntax.strategy.PurchaseOperation;
-import core.basesyntax.strategy.ReturnOperation;
-import core.basesyntax.strategy.SupplyOperation;
+import core.basesyntax.strategy.PurchaseOperationHandler;
+import core.basesyntax.strategy.ReturnOperationHandler;
+import core.basesyntax.strategy.SupplyOperationHandler;
 import java.util.List;
+import java.util.Map;
 
 public class OperationProcess implements DataProcesser {
+    private static final Map<Operation, OperationHandler> operationDefiner = Map.of(
+            Operation.BALANCE, new BalanceOperationHandler(),
+            Operation.PURCHASE, new PurchaseOperationHandler(),
+            Operation.RETURN, new ReturnOperationHandler(),
+            Operation.SUPPLY, new SupplyOperationHandler());
+
     public void processData(List<FruitTransaction> transactions) {
         for (FruitTransaction transaction : transactions) {
-            OperationHandler handler;
-            switch (transaction.getOperation()) {
-                case BALANCE: handler = new BalanceOperation();
-                    break;
-                case PURCHASE: handler = new PurchaseOperation();
-                    break;
-                case RETURN: handler = new ReturnOperation();
-                    break;
-                case SUPPLY: handler = new SupplyOperation();
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: "
-                            + transaction.getOperation());
-            }
-            handler.update(transaction.getFruit(), transaction.getQuantity());
+            OperationHandler handler = operationDefiner.get(transaction.getOperation());
+            handler.handle(transaction.getFruit(), transaction.getQuantity());
         }
     }
 }
