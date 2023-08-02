216b58e1a80a7084306d78f0e9b3c46a4c132522
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-08-02 22:42:55 +0300

    Update WriteService.java

--- src/main/java/core/basesyntax/service/WriteService.java
+++ src/main/java/core/basesyntax/service/WriteService.java
@@ -1,7 +1,5 @@
 package core.basesyntax.service;
 
-import java.util.List;
-
-public interface WriteService {
-    void writeToFile(List<String> data, String path);
+public interface WriteDataToFileService {
+    void writeToFile(String data, String path);
 }
