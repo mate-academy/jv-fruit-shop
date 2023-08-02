4331fe4fb96a27a713e8ed81281c81fcd8762fae
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-08-02 18:51:46 +0300

    improved convert() method

--- src/main/java/core/basesyntax/service/implementation/DataConversionServiceImpl.java
+++ src/main/java/core/basesyntax/service/implementation/DataConversionServiceImpl.java
@@ -2,35 +2,38 @@ package core.basesyntax.service.implementation;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.DataConversionService;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class DataConversionServiceImpl implements DataConversionService {
     private static final String COMMA = ",";
     private static final int OPERATION_INDEX = 0;
     private static final int PRODUCT_INDEX = 1;
     private static final int AMOUNT_INDEX = 2;
-    private static final String HEADER = "type,fruit,quantity";
+    private static final int HEADER_SIZE = 1;
+    private static final String SEPARATOR = System.lineSeparator();
 
     @Override
     public List<FruitTransaction> convert(String rawData) {
-        List<FruitTransaction> list = new ArrayList<>();
-        for (String line : rawData.split(System.lineSeparator())) {
-            if (Objects.equals(line, HEADER)) {
-                continue;
+        return Arrays.stream(rawData.split(SEPARATOR))
+                .skip(HEADER_SIZE)
+                .map(this::convertLineToTransaction)
+                .collect(Collectors.toList());
+    }
+
+    private FruitTransaction convertLineToTransaction(String line) {
+        String[] dataUnits = line.split(COMMA);
+        FruitTransaction fruitTransaction = new FruitTransaction();
+        for (FruitTransaction.Operation operation : FruitTransaction.Operation.values()) {
+            if (operation.getCode().equals(dataUnits[OPERATION_INDEX])) {
+                fruitTransaction.setOperation(operation);
             }
-            String[] dataUnits = line.split(COMMA);
-            FruitTransaction fruitTransaction = new FruitTransaction();
-            for (FruitTransaction.Operation operation : FruitTransaction.Operation.values()) {
-                if (operation.getCode().equals(dataUnits[OPERATION_INDEX])) {
-                    fruitTransaction.setOperation(operation);
-                }
-            }
-            fruitTransaction.setFruit(dataUnits[PRODUCT_INDEX]);
-            fruitTransaction.setQuantity(Integer.parseInt(dataUnits[AMOUNT_INDEX]));
-            list.add(fruitTransaction);
         }
-        return list;
+        fruitTransaction.setFruit(dataUnits[PRODUCT_INDEX]);
+        fruitTransaction.setQuantity(Integer.parseInt(dataUnits[AMOUNT_INDEX]));
+        return fruitTransaction;
     }
 }
+
+
