8775aafb6693ab89625e1ba26763bc161bdaf600
Mykyta Zuber <nikitazuber@gmail.com>
2023-08-03 10:28:29 +0300

    Fixed code

--- src/main/java/core/basesyntax/services/actions/ActionHandler.java
+++ src/main/java/core/basesyntax/services/actions/ActionHandler.java
@@ -7,8 +7,11 @@ public interface ActionHandler {
     boolean actionStore(String nameOfGoods, Integer valueOfTask);
 
     default void validData(Storage fruitDB, String nameOfGoods) {
+        if (fruitDB == null) {
+            throw new ValidationDataException("ActionHandle error db is null");
+        }
         if (fruitDB.getStorageFruits().isEmpty()) {
-            throw new ValidationDataException("Task error db is empty");
+            throw new ValidationDataException("ActionHandle error db is empty");
         }
         if (fruitDB.getStorageFruits().get(nameOfGoods) == null) {
             throw new ValidationDataException("Client cant buy not exist product!");
