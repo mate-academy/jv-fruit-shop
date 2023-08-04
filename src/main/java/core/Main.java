cd93750294ceef469789eb33ea88b5c10ab6280c
DaniilNepelev <kakashilvng@gmail.com>
2023-08-04 13:04:13 +0300

    replace strategy to the main method

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -6,17 +6,16 @@ import core.service.OperationType;
 import core.service.ReadServiceImpl;
 import core.service.SplitDataImpl;
 import core.service.WriteReportServiceImpl;
-import core.strategy.DataOperationStrategyImpl;
-import core.transactions.OperationHandler;
+import core.transactions.*;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     private static final ReadServiceImpl readService = new ReadServiceImpl();
     private static final SplitDataImpl splitDataImpl = new SplitDataImpl();
-    private static final Map<OperationType, OperationHandler> strategy =
-            new DataOperationStrategyImpl().strategy();
-    private static final FruitStore fruitStore = new FruitStore(strategy);
+    private static final FruitStore fruitStore = new FruitStore(strategy());
     private static final WriteReportServiceImpl writeReport = new WriteReportServiceImpl();
     private static final String PathFrom = "src/main/dataBase.csv";
 
@@ -26,5 +25,14 @@ public class Main {
         List<OperationData> createReport = fruitStore.processOperations(splitData);
         String reportString = fruitStore.convertListToString(createReport);
         writeReport.createReport("firstReport",reportString);
+    }
+
+    private static Map<OperationType, OperationHandler> strategy() {
+        Map<OperationType, OperationHandler> operationHandlers = new HashMap<>();
+        operationHandlers.put(OperationType.B, new BalanceOperationHandler());
+        operationHandlers.put(OperationType.S, new SupplyOperationHandler());
+        operationHandlers.put(OperationType.R, new ReturnOperationHandler());
+        operationHandlers.put(OperationType.P, new PurchaseOperationHandler());
+        return operationHandlers;
     }
 }
--- src/main/java/core/strategy/DataOperationStrategy.java
+++
@@ -1,9 +0,0 @@
-package core.strategy;
-
-import core.service.OperationType;
-import core.transactions.OperationHandler;
-import java.util.Map;
-
-public interface DataOperationStrategy {
-    Map<OperationType, OperationHandler> strategy();
-}
