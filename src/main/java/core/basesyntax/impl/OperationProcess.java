d2e26ea6e2c7501ed3bb56a180d65ad8e8399fc5
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-08-05 22:54:40 +0300

    Removed map

--- src/main/java/core/basesyntax/impl/OperationProcess.java
+++ src/main/java/core/basesyntax/impl/OperationProcess.java
@@ -12,13 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 public class OperationProcess implements DataProcesser {
-    private static final Map<Operation, OperationHandler> operationDefiner = Map.of(
-            Operation.BALANCE, new BalanceOperationHandler(),
-            Operation.PURCHASE, new PurchaseOperationHandler(),
-            Operation.RETURN, new ReturnOperationHandler(),
-            Operation.SUPPLY, new SupplyOperationHandler());
-
-    public void processData(List<FruitTransaction> transactions) {
+    public void processData(List<FruitTransaction> transactions, Map<Operation, OperationHandler> operationDefiner) {
         for (FruitTransaction transaction : transactions) {
             OperationHandler handler = operationDefiner.get(transaction.getOperation());
             handler.handle(transaction.getFruit(), transaction.getQuantity());
