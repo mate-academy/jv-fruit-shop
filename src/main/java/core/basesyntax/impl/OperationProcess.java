ef95e4c821a45480ce2bfc66a0685b037833a4ea
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-08-05 23:00:25 +0300

    Fixed checkstyle

--- src/main/java/core/basesyntax/impl/OperationProcess.java
+++ src/main/java/core/basesyntax/impl/OperationProcess.java
@@ -3,16 +3,13 @@ package core.basesyntax.impl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.model.Operation;
 import core.basesyntax.service.DataProcesser;
-import core.basesyntax.strategy.BalanceOperationHandler;
 import core.basesyntax.strategy.OperationHandler;
-import core.basesyntax.strategy.PurchaseOperationHandler;
-import core.basesyntax.strategy.ReturnOperationHandler;
-import core.basesyntax.strategy.SupplyOperationHandler;
 import java.util.List;
 import java.util.Map;
 
 public class OperationProcess implements DataProcesser {
-    public void processData(List<FruitTransaction> transactions, Map<Operation, OperationHandler> operationDefiner) {
+    public void processData(List<FruitTransaction> transactions,
+                            Map<Operation, OperationHandler> operationDefiner) {
         for (FruitTransaction transaction : transactions) {
             OperationHandler handler = operationDefiner.get(transaction.getOperation());
             handler.handle(transaction.getFruit(), transaction.getQuantity());
