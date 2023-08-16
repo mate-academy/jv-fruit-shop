64d04ca4ec84693b5d071604ef92e6975b81d133
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-16 18:32:19 +0300

    fix problem

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.*;
-import core.basesyntax.service.impl.*;
+import core.basesyntax.service.FruitTransactionService;
+import core.basesyntax.service.ParseService;
+import core.basesyntax.service.ReaderService;
+import core.basesyntax.service.ReportService;
+import core.basesyntax.service.WriterService;
+import core.basesyntax.service.impl.FruitTransactionServiceImpl;
+import core.basesyntax.service.impl.ParseServiceImpl;
+import core.basesyntax.service.impl.ReaderServiceImpl;
+import core.basesyntax.service.impl.ReportServiceImpl;
+import core.basesyntax.service.impl.WriterServiceImpl;
 import core.basesyntax.strategy.OperationStrategy;
 import core.basesyntax.strategy.impl.OperationStrategyImpl;
 import java.util.List;
--- src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
@@ -7,13 +7,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OperationStrategyImpl implements OperationStrategy {
-    private final Map<FruitTransaction.Operation, OperationActivity> operationActivities = new HashMap<>();
+    private final Map<FruitTransaction.Operation, OperationActivity> operationActivities =
+            new HashMap<>();
 
     public OperationStrategyImpl() {
-        operationActivities.put(FruitTransaction.Operation.BALANCE, new BalanceOperationActivity());
-        operationActivities.put(FruitTransaction.Operation.SUPPLY, new SuppliyOperationActivity());
-        operationActivities.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationActivity());
-        operationActivities.put(FruitTransaction.Operation.RETURN, new ReturnOperationActivity());
+        operationActivities
+                .put(FruitTransaction.Operation.BALANCE, new BalanceOperationActivity());
+        operationActivities
+                .put(FruitTransaction.Operation.SUPPLY, new SuppliyOperationActivity());
+        operationActivities
+                .put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationActivity());
+        operationActivities
+                .put(FruitTransaction.Operation.RETURN, new ReturnOperationActivity());
     }
 
     @Override
