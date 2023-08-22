11f9bc9c40f4237a2a57d304701f3c51dcde6a30
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-08-22 18:08:14 +0300

    i don't now how call this commit.. maybe "improvement condition order"

--- src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 public class WriterServiceImpl implements WriterService {
     @Override
     public void writeToFile(String text, String filePath) {
+        if (text.isEmpty()) {
+            throw new RuntimeException("Text can't be empty");
+        }
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
-            if (text.isEmpty()) {
-                throw new RuntimeException("Text can't be empty");
-            }
             bufferedWriter.write(text);
         } catch (IOException e) {
             throw new RuntimeException("Can't write text to file: " + filePath, e);
