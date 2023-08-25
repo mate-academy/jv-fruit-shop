0bc8f0faecedfc18e30731fabeb48c70657a1c6c
Nikita <kaptetos@gmail.com>
2023-08-25 11:15:05 +0300

    This class represents a single fruit transaction. This class represents a single fruit transaction

---
+++ src/main/java/core/basesyntax/FruitTransaction.java
@@ -0,0 +1,48 @@
+package core.basesyntax;
+
+public class FruitTransaction {
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private final String code;
+
+        Operation(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+
+    private Operation operation;
+    private String fruit;
+    private int quantity;
+
+    public FruitTransaction.Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(FruitTransaction.Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
