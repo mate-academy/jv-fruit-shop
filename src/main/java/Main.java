c25cd6454b486c8afcbf398cffc750a9fc4423a6
Stanislav Golubkov <golubkovstas83@gmail.com>
2023-09-02 21:07:44 +0300

    version7-stas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,44 @@
 import model.FruitTransaction;
 import service.FileWriterService;
 import service.ShopService;
-import service.impl.*;
+import service.impl.ShopServiceImpl;
+import service.impl.TransactionParseImpl;
+import service.impl.FileReaderServiceImpl;
+import service.impl.ReportServiceImpl;
+import service.impl.BalanceOperationTransactionImpl;
+import service.impl.PurchaseOperationTransactionImpl;
+import service.impl.ReturnOperationTransactionImpl;
+import service.impl.SupplyOperationTransactionImpl;
+import service.impl.FileWriterServiceImpl;
 import service.operation.OperationTransaction;
 import strategy.OperationStrategy;
-import strategy.OperationStrategyImpl;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import strategy.OperationStrategyImpl;
 
 public class Main {
     public static void main(String[] args) {
         String fileRead = "src/main/resources/data.csv";
-        String fileWrite = "src/main/resources/report.csv";
         List<String> dataContent = new FileReaderServiceImpl().readFromFile(fileRead);
-        List<FruitTransaction> transactionList = new TransactionParseImpl().transactionParse(dataContent);
         Map<FruitTransaction.Operation, OperationTransaction> operationOperationTransactionMap
                 = new HashMap<>();
-        operationOperationTransactionMap.put(FruitTransaction.Operation.BALANCE, new BalanceOperationTransactionImpl());
-        operationOperationTransactionMap.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationTransactionImpl());
-        operationOperationTransactionMap.put(FruitTransaction.Operation.SUPPLY, new SupplyOperationTransactionImpl());
-        operationOperationTransactionMap.put(FruitTransaction.Operation.RETURN, new ReturnOperationTransactionImpl());
-        OperationStrategy operationStrategy = new OperationStrategyImpl(operationOperationTransactionMap);
+        operationOperationTransactionMap
+                .put(FruitTransaction.Operation.BALANCE, new BalanceOperationTransactionImpl());
+        operationOperationTransactionMap
+                .put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationTransactionImpl());
+        operationOperationTransactionMap
+                .put(FruitTransaction.Operation.SUPPLY, new SupplyOperationTransactionImpl());
+        operationOperationTransactionMap
+                .put(FruitTransaction.Operation.RETURN, new ReturnOperationTransactionImpl());
+        OperationStrategy operationStrategy
+                = new OperationStrategyImpl(operationOperationTransactionMap);
+        List<FruitTransaction> transactionList
+                = new TransactionParseImpl().transactionParse(dataContent);
         ShopService shopService = new ShopServiceImpl(operationStrategy);
         shopService.shopTransactions(transactionList);
         String report = new ReportServiceImpl().createReport();
+        String fileWrite = "src/main/resources/report.csv";
         FileWriterService fileWriterService = new FileWriterServiceImpl();
         fileWriterService.fileWriter(report, fileWrite);
     }
