525cb96b3818ef6a4d035f148c13aba8f2ae4a0c
Nikita <kaptetos@gmail.com>
2023-09-09 16:35:45 +0300

    PurchaseHandler is a specific handler for the "purchase" operation. Implements the OperationHandlerInterface interface and processes purchase transactions based on the number of fruits purchased.

---
+++ src/main/java/core/basesyntax/PurchaseHandler.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Map;
+
+public class PurchaseHandler implements OperationHandler {
+    @Override
+    public void handleTransaction(FruitTransaction transaction, Map<String, Integer> storage) {
+        String fruit = transaction.getFruit();
+        int quantity = transaction.getQuantity();
+
+        int currentQuantity = storage.getOrDefault(fruit, 0);
+        storage.put(fruit, currentQuantity - quantity);
+    }
+}
