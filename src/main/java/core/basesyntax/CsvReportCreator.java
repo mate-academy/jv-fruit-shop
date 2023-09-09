99d60d5bbe6b888ec32da2114484294d1f4adeee
Nikita <kaptetos@gmail.com>
2023-09-09 16:31:11 +0300

    Implements the ReportCreator interface. Responsible for creating CSV reports.

---
+++ src/main/java/core/basesyntax/CsvReportCreator.java
@@ -0,0 +1,34 @@
+package core.basesyntax;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class CsvReportCreator implements ReportCreator {
+    private String filePath;
+
+    public CsvReportCreator(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public void createReport(Map<String, Integer> fruitInventory,
+                             String filePath) throws IOException {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write("fruit,quantity\n");
+
+            for (Map.Entry<String, Integer> entry : fruitInventory.entrySet()) {
+                String fruit = entry.getKey();
+                int quantity = entry.getValue();
+
+                String line = String.format("%s,%d\n", fruit, quantity);
+                writer.write(line);
+
+            }
+
+            System.out.println("Report created successfully" + filePath);
+        } catch (IOException e) {
+            System.err.println("Error creating report" + e.getMessage());
+        }
+    }
+}
