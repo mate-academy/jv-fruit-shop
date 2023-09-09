433ca552df3361b40b80157683f2b02163e1fab0
Nikita <kaptetos@gmail.com>
2023-09-09 16:26:02 +0300

    Responsible for parsing transaction data from CSV format. Implements the TransactionParser interface. The parseTransactions method takes a list of strings and returns a list of FruitTransaction objects.

---
+++ src/main/java/core/basesyntax/CsvTransactionParser.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvTransactionParser implements TransactionParser {
+    @Override
+    public List<FruitTransaction> parseTransactions(List<String> data) {
+        List<FruitTransaction> transactions = new ArrayList<>();
+        for (String line : data) {
+            String[] parts = line.split(",");
+            if (parts.length == 3) {
+                String operationCode = parts[0];
+                String fruit = parts[1];
+                int quantity = Integer.parseInt(parts[2]);
+                transactions.add(new FruitTransaction(
+                        FruitTransaction.Operation.valueOf(operationCode),
+                        fruit,
+                        quantity
+                ));
+            }
+        }
+        return transactions;
+    }
+}
