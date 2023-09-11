f7b96ffe01d927971f128938b484c76567f5f6ea
Nikita <kaptetos@gmail.com>
2023-09-11 17:02:51 +0300

    modified as recommended

--- src/main/java/core/basesyntax/DefaultOperationStrategy.java
+++ src/main/java/core/basesyntax/DefaultOperationStrategy.java
@@ -1,16 +1,12 @@
 package core.basesyntax;
 
-import java.util.HashMap;
+
 import java.util.Map;
 
 public class DefaultOperationStrategy implements OperationStrategy {
-    private Map<FruitTransaction.Operation, OperationHandler> handlers = new HashMap<>();
-
-    public DefaultOperationStrategy() {
-        handlers.put(FruitTransaction.Operation.BALANCE, new BalanceHandler());
-        handlers.put(FruitTransaction.Operation.SUPPLY, new SupplyHandler());
-        handlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseHandler());
-        handlers.put(FruitTransaction.Operation.RETURN, new ReturnHandler());
+    private Map<FruitTransaction.Operation, OperationHandler> handlers;
+    public DefaultOperationStrategy(Map<FruitTransaction.Operation, OperationHandler> handlers) {
+        this.handlers = handlers;
     }
 
     @Override
