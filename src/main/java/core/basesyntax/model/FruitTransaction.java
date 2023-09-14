8fbaadb4239695226abc70a0f018d59ca1d4a581
Salnikov Markiian <salnikov.markiyan@gmail.com>
2023-09-14 19:02:33 +0300

    added all strategy, model for transaction, basic services

---
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -0,0 +1,53 @@
+package core.basesyntax.model;
+
+import java.util.Arrays;
+
+public class FruitTransaction {
+    private final Operation operation;
+    private final String fruit;
+    private final int quantity;
+
+    public FruitTransaction(Operation operation, String fruit, int quantity) {
+        this.operation = operation;
+        this.fruit = fruit;
+        this.quantity = quantity;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private final String code;
+
+        private Operation(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public static Operation getOperationByCode(String code) {
+            return Arrays.stream(Operation.values())
+                    .filter(c -> c.getCode().equals(code))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Unknown code operation by "
+                            + code));
+        }
+
+    }
+}
