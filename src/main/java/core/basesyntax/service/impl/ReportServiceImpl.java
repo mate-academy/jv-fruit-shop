410fe680e34e23daf156af9d0cb7bcdbdbfe84d0
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-09-17 15:01:46 +0300

    minor changes of code added

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -2,22 +2,23 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.service.ReportService;
+
 import java.util.Map;
 
 public class ReportServiceImpl implements ReportService {
-    private static final Map<String, Integer> STORE = Storage.STORAGE;
-    private static final String FIRST_ROW = "fruit,quantity\n";
-    private static final String DELIMITER = ",";
+  private static final Map<String, Integer> STORE = Storage.STORAGE;
+  private static final String FIRST_ROW = "fruit,quantity\n";
+  private static final String DELIMITER = ",";
 
-    @Override
-    public String createReport() {
-        StringBuilder builder = new StringBuilder(FIRST_ROW);
-        for (Map.Entry<String, Integer> entry : STORE.entrySet()) {
-            builder.append(entry.getKey());
-            builder.append(DELIMITER);
-            builder.append(entry.getValue());
-            builder.append(System.lineSeparator());
-        }
-        return builder.toString();
+  @Override
+  public String createReport() {
+    StringBuilder builder = new StringBuilder(FIRST_ROW);
+    for (Map.Entry<String, Integer> entry : STORE.entrySet()) {
+      builder.append(entry.getKey())
+              .append(DELIMITER)
+              .append(entry.getValue())
+              .append(System.lineSeparator());
     }
+    return builder.toString();
+  }
 }
