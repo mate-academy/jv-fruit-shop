d90a05f7985965a1ca00e8b277f963b1cc56f5f5
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-09-17 15:11:04 +0300

    passed mvn clean package

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -2,23 +2,22 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.service.ReportService;
-
 import java.util.Map;
 
 public class ReportServiceImpl implements ReportService {
-  private static final Map<String, Integer> STORE = Storage.STORAGE;
-  private static final String FIRST_ROW = "fruit,quantity\n";
-  private static final String DELIMITER = ",";
+    private static final Map<String, Integer> STORE = Storage.STORAGE;
+    private static final String FIRST_ROW = "fruit,quantity\n";
+    private static final String DELIMITER = ",";
 
-  @Override
-  public String createReport() {
-    StringBuilder builder = new StringBuilder(FIRST_ROW);
-    for (Map.Entry<String, Integer> entry : STORE.entrySet()) {
-      builder.append(entry.getKey())
-              .append(DELIMITER)
-              .append(entry.getValue())
-              .append(System.lineSeparator());
+    @Override
+    public String createReport() {
+        StringBuilder builder = new StringBuilder(FIRST_ROW);
+        for (Map.Entry<String, Integer> entry : STORE.entrySet()) {
+            builder.append(entry.getKey())
+                    .append(DELIMITER)
+                    .append(entry.getValue())
+                    .append(System.lineSeparator());
+        }
+        return builder.toString();
     }
-    return builder.toString();
-  }
 }
