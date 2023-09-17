55f07e6f572e1d2041cd581def298cf724e4e3f9
hp <volodya.gata@gmail.com>
2023-09-17 15:42:25 +0300

    fixed output

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    private static Map<String, Integer> storage = new HashMap<>();
+    private static final Map<String, Integer> storage = new HashMap<>();
 
     public static Map<String, Integer> getStorage() {
         return storage;
