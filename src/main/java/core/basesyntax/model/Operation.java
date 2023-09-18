25b103f528ab9a6efd88c7055cc9c16bd7384f7b
evgenkolner <evgen.kolner@gmail.com>
2023-09-18 16:23:10 +0300

    added solution2

--- src/main/java/core/basesyntax/model/Operation.java
+++ src/main/java/core/basesyntax/model/Operation.java
@@ -1,7 +1,5 @@
 package core.basesyntax.model;
 
-import java.util.Arrays;
-
 public enum Operation {
     BALANCE("b"),
     SUPPLY("s"),
@@ -16,13 +14,5 @@ public enum Operation {
 
     public String getCode() {
         return code;
-    }
-
-    public static Operation getOperationTypeByCode(String code) {
-        return Arrays.stream(Operation.values())
-                .filter(e -> e.getCode().equals(code))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("Can't find this operation type "
-                        + code));
     }
 }
