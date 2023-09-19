ec1e6a0d435410b0ec55d092b68399953d32a820
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-09-19 12:14:28 +0300

    move strategy to the Main method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,21 +1,33 @@
 package core.basesyntax;
 
 import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.model.OperationType;
 import core.basesyntax.service.impl.ParseServiceImpl;
 import core.basesyntax.service.impl.ProcessServiceImpl;
 import core.basesyntax.service.impl.ReaderServiceImpl;
 import core.basesyntax.service.impl.ReportCreatorServiceImpl;
 import core.basesyntax.service.impl.WriteServiceImpl;
+import core.basesyntax.strategy.Operation;
+import core.basesyntax.strategy.impl.BalanceOperation;
+import core.basesyntax.strategy.impl.PurchaseOperation;
+import core.basesyntax.strategy.impl.ReturnOperation;
+import core.basesyntax.strategy.impl.SupplyOperation;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
     private static final String FILE_PATH_TO_READ = "src\\resources\\input.csv";
     private static final String FILE_PATH_TO_WRITE = "src\\resources\\report.csv";
+    private static final HashMap<OperationType, Operation> STRATEGY_MAP = new HashMap<>();
 
     public static void main(String[] args) {
+        STRATEGY_MAP.put(OperationType.BALANCE, new BalanceOperation());
+        STRATEGY_MAP.put(OperationType.PURCHASE, new PurchaseOperation());
+        STRATEGY_MAP.put(OperationType.SUPPLY, new SupplyOperation());
+        STRATEGY_MAP.put(OperationType.RETURN, new ReturnOperation());
         List<String> lines = new ReaderServiceImpl().readFromFile(FILE_PATH_TO_READ);
         List<FruitTransaction> fruitTransactionList = new ParseServiceImpl().parse(lines);
-        new ProcessServiceImpl().processTransactions(fruitTransactionList);
+        new ProcessServiceImpl().processTransactions(fruitTransactionList, STRATEGY_MAP);
         String report = new ReportCreatorServiceImpl().createReport();
         new WriteServiceImpl().writeToFile(FILE_PATH_TO_WRITE, report);
     }
--- src/main/java/core/basesyntax/service/impl/ProcessServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ProcessServiceImpl.java
@@ -4,28 +4,14 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.model.OperationType;
 import core.basesyntax.service.ProcessService;
 import core.basesyntax.strategy.Operation;
-import core.basesyntax.strategy.impl.BalanceOperation;
-import core.basesyntax.strategy.impl.PurchaseOperation;
-import core.basesyntax.strategy.impl.ReturnOperation;
-import core.basesyntax.strategy.impl.SupplyOperation;
 import java.util.List;
 import java.util.Map;
 
 public class ProcessServiceImpl implements ProcessService {
     @Override
-    public void processTransactions(List<FruitTransaction> fruitTransactionList) {
-        Map<OperationType, Operation> strategyMap = getStrategyMap();
+    public void processTransactions(List<FruitTransaction> fruitTransactionList,
+                                    Map<OperationType, Operation> strategyMap) {
         fruitTransactionList.forEach(f -> strategyMap
                 .get(f.getOperationType()).performOperation(f));
-    }
-
-    private static Map<OperationType, Operation> getStrategyMap() {
-        Map<OperationType, Operation> strategyMap = Map.of(
-                OperationType.BALANCE, new BalanceOperation(),
-                OperationType.PURCHASE, new PurchaseOperation(),
-                OperationType.SUPPLY, new SupplyOperation(),
-                OperationType.RETURN, new ReturnOperation()
-        );
-        return strategyMap;
     }
 }
