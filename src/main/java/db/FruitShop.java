fdf1a98b5302ab9b3f53c20c0287bb5a75baa4b4
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-06 17:34:47 +0300

    add a database FruitShop

---
+++ src/main/java/db/FruitShop.java
@@ -0,0 +1,9 @@
+package db;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.FruitTransaction;
+
+public class FruitShop {
+    public static final List<FruitTransaction> fruitShop = new ArrayList<>();
+}
