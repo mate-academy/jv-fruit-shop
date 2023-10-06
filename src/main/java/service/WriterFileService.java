186ec2d93feb1eefd3db65c1547455a3b6890ca6
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-06 17:47:03 +0300

    add WriterFileService for write to file

---
+++ src/main/java/service/WriterFileService.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface WriterFileService {
+    void writeToFile(String report);
+}
---
+++ src/main/java/service/impl/WriterFileServiceImpl.java
@@ -0,0 +1,18 @@
+package service.impl;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import service.WriterFileService;
+
+public class WriterFileServiceImpl implements WriterFileService {
+    private static final String FILE_NAME_OUTPUT = "output.csv";
+
+    @Override
+    public void writeToFile(String report) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME_OUTPUT))) {
+            writer.write(report);
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t write to file" + FILE_NAME_OUTPUT, e);
+        }
+    }
+}
