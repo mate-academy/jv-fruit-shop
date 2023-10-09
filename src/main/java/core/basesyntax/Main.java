2b1166034b5354992ec3599fac5212b114bbee5a
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-09 19:28:37 +0300

    changed to match my changes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,46 +1,50 @@
 package core.basesyntax;
 
-import dao.FruitShopDao;
-import dao.impl.FruitShopDaoCsvImpl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import model.FruitTransaction;
 import service.ActivitiesStrategy;
+import service.FruitShopService;
 import service.ReaderFileService;
 import service.ReportService;
-import service.activities.ActivitiesHandler;
-import service.activities.BalanceActivitiesHandler;
-import service.activities.PurchaseActivitiesHandler;
-import service.activities.ReturnActivitiesHandler;
-import service.activities.SupplyActivitiesHandler;
+import service.WriterFileService;
+import service.activities.BalanceTransactionHandler;
+import service.activities.PurchaseTransactionHandler;
+import service.activities.ReturnTransactionHandler;
+import service.activities.SupplyTransactionHandler;
+import service.activities.TransactionHandler;
 import service.impl.ActivitiesStrategyImpl;
+import service.impl.FruitShopServiceImpl;
 import service.impl.ReaderFileServiceImpl;
 import service.impl.ReportServiceImpl;
+import service.impl.WriterFileServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        Map<FruitTransaction.Operation, ActivitiesHandler> activitiesHandlerMap = new HashMap<>();
+        Map<FruitTransaction.Operation, TransactionHandler> activitiesHandlerMap = new HashMap<>();
         activitiesHandlerMap.put(FruitTransaction.Operation.BALANCE,
-                new BalanceActivitiesHandler());
+                new BalanceTransactionHandler());
         activitiesHandlerMap.put(FruitTransaction.Operation.PURCHASE,
-                new PurchaseActivitiesHandler());
+                new PurchaseTransactionHandler());
         activitiesHandlerMap.put(FruitTransaction.Operation.RETURN,
-                new ReturnActivitiesHandler());
+                new ReturnTransactionHandler());
         activitiesHandlerMap.put(FruitTransaction.Operation.SUPPLY,
-                new SupplyActivitiesHandler());
+                new SupplyTransactionHandler());
 
         ReaderFileService readerFileService = new ReaderFileServiceImpl();
         List<String> listFromFile = readerFileService.readFromFile();
 
         ActivitiesStrategy activitiesStrategy = new ActivitiesStrategyImpl(activitiesHandlerMap);
 
-        FruitShopDao fruitShopDao = new FruitShopDaoCsvImpl(activitiesStrategy, readerFileService);
-        fruitShopDao.processTransactions(listFromFile);
+        FruitShopService fruitShopService = new FruitShopServiceImpl(activitiesStrategy);
+        fruitShopService.convert(listFromFile);
 
-        ReportService reportService = new ReportServiceImpl(fruitShopDao);
+        ReportService reportService = new ReportServiceImpl(fruitShopService);
 
-        reportService.createReport();
+        List<String> report = reportService.createReport();
+        WriterFileService writerFileService = new WriterFileServiceImpl();
+        writerFileService.writeToFile(report);
     }
 
 }
