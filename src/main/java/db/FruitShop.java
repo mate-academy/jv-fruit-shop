2966af69977f04284a4f201f100a06213f9687f9
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-09 19:24:03 +0300

    rewrote through the HashMap

--- src/main/java/db/FruitShop.java
+++
@@ -1,9 +0,0 @@
-package db;
-
-import java.util.ArrayList;
-import java.util.List;
-import model.FruitTransaction;
-
-public class FruitShop {
-    public static final List<FruitTransaction> fruitShop = new ArrayList<>();
-}
---
+++ src/main/java/db/FruitShopStorage.java
@@ -0,0 +1,9 @@
+package db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FruitShopStorage {
+    //public static final List<FruitTransaction> fruitShop = new ArrayList<>();
+    public static final Map<String, Integer> fruitShop = new HashMap<>();
+}
