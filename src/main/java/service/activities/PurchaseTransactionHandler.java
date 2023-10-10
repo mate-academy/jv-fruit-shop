6600aac8f3dce635142887aa238bba82ec66e64f
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-10 17:57:11 +0300

    add error message

--- src/main/java/service/activities/PurchaseTransactionHandler.java
+++ src/main/java/service/activities/PurchaseTransactionHandler.java
@@ -8,6 +8,9 @@ public class PurchaseTransactionHandler implements TransactionHandler {
     public void executeTransaction(FruitTransaction fruitTransaction) {
         String fruit = fruitTransaction.getFruit();
         int quantity = fruitTransaction.getQuantity();
+        if (quantity < 0) {
+            throw new RuntimeException("Initial quantity cannot be negative for fruit: " + fruit);
+        }
         int quantityPurchase = FruitShopStorage.fruitShop.get(fruit);
         FruitShopStorage.fruitShop.put(fruit, quantityPurchase - quantity);
     }
--- src/main/java/service/activities/SupplyTransactionHandler.java
+++ src/main/java/service/activities/SupplyTransactionHandler.java
@@ -8,6 +8,9 @@ public class SupplyTransactionHandler implements TransactionHandler {
     public void executeTransaction(FruitTransaction fruitTransaction) {
         String fruit = fruitTransaction.getFruit();
         int quantity = fruitTransaction.getQuantity();
+        if (quantity < 0) {
+            throw new RuntimeException("Initial quantity cannot be negative for fruit: " + fruit);
+        }
         int quantitySupply = FruitShopStorage.fruitShop.get(fruit);
         FruitShopStorage.fruitShop.put(fruit,quantitySupply + quantity);
     }
