336009b6b79d53966baca65d4620f55365926e7b
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-10 19:00:22 +0300

    deleted unnecessary lines

--- src/main/java/service/impl/ReportServiceImpl.java
+++ src/main/java/service/impl/ReportServiceImpl.java
@@ -4,19 +4,11 @@ import db.FruitShopStorage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import service.FruitShopService;
 import service.ReportService;
 
 public class ReportServiceImpl implements ReportService {
-    private FruitShopService fruitShopService;
-
-    public ReportServiceImpl(FruitShopService fruitShopService) {
-        this.fruitShopService = fruitShopService;
-    }
-
     @Override
     public List<String> createReport() {
-        //стоворюємо репорт
         Map<String, Integer> collectFruit = FruitShopStorage.fruitShop;
         List<String> reportLines = new ArrayList<>();
         reportLines.add("fruit,quantity");
