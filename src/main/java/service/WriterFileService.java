32593c1ef4851964a53afde41b83052952e7f1d4
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-10 17:55:49 +0300

    changed the implementation of the method so that the filePath is entered from the main

--- src/main/java/service/WriterFileService.java
+++ src/main/java/service/WriterFileService.java
@@ -3,5 +3,5 @@ package service;
 import java.util.List;
 
 public interface WriterFileService {
-    void writeToFile(List<String> report);
+    void writeToFile(List<String> lines, String fileName);
 }
--- src/main/java/service/impl/WriterFileServiceImpl.java
+++ src/main/java/service/impl/WriterFileServiceImpl.java
@@ -6,17 +6,16 @@ import java.util.List;
 import service.WriterFileService;
 
 public class WriterFileServiceImpl implements WriterFileService {
-    private static final String FILE_NAME_OUTPUT = "src/main/resources/output.csv";
 
     @Override
-    public void writeToFile(List<String> reportList) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME_OUTPUT))) {
-            for (String report : reportList) {
-                writer.write(report);
+    public void writeToFile(List<String> lines, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String line : lines) {
+                writer.write(line);
                 writer.newLine();
             }
         } catch (Exception e) {
-            throw new RuntimeException("Can`t write to file" + FILE_NAME_OUTPUT, e);
+            throw new RuntimeException("Can`t write to file" + fileName, e);
         }
     }
 }
