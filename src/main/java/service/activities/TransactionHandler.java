61c9087d39ed00606eac26d7a177cdbc25598879
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-10 17:59:54 +0300

    delete the unnecessary message

--- src/main/java/service/activities/TransactionHandler.java
+++ src/main/java/service/activities/TransactionHandler.java
@@ -3,6 +3,5 @@ package service.activities;
 import model.FruitTransaction;
 
 public interface TransactionHandler {
-    //викон операцію і записати в fruitShop
     void executeTransaction(FruitTransaction fruitTransaction);
 }
