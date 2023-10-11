c97e652793c5b612657aa48aaf71c4d950e1f4a5
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-11 18:43:53 +0300

    delete message

--- src/main/java/db/FruitShopStorage.java
+++ src/main/java/db/FruitShopStorage.java
@@ -4,6 +4,5 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FruitShopStorage {
-    //public static final List<FruitTransaction> fruitShop = new ArrayList<>();
     public static final Map<String, Integer> fruitShop = new HashMap<>();
 }
