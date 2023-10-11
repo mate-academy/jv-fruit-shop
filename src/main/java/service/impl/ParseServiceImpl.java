29b7b3b464773b841d5007f3ddb0744cd546e57e
Solomia Bobyak <mia.bobyak@gmail.com>
2023-10-11 18:44:20 +0300

    add constant value

--- src/main/java/service/impl/ParseServiceImpl.java
+++ src/main/java/service/impl/ParseServiceImpl.java
@@ -7,6 +7,10 @@ import service.ParseService;
 
 public class ParseServiceImpl implements ParseService {
 
+    private static final int OPERATION_CODE_INDEX = 0;
+    private static final int FRUIT_CODE_INDEX = 1;
+    private static final int QUANTITY_CODE_INDEX = 2;
+
     @Override
     public List<FruitTransaction> parseTransactions(List<String> lines) {
         List<FruitTransaction> fruitTransactionList = new ArrayList<>();
@@ -19,12 +23,12 @@ public class ParseServiceImpl implements ParseService {
     private FruitTransaction getFromCvsRow(String line) {
         String[] fields = line.split(",");
         FruitTransaction fruitTransaction = new FruitTransaction();
-        String operationCode = fields[0].trim();
+        String operationCode = fields[OPERATION_CODE_INDEX].trim();
         FruitTransaction.Operation operation = FruitTransaction.Operation.fromCode(operationCode);
 
         fruitTransaction.setOperation(operation);
-        fruitTransaction.setFruit(fields[1]);
-        fruitTransaction.setQuantity(Integer.parseInt(fields[2]));
+        fruitTransaction.setFruit(fields[FRUIT_CODE_INDEX]);
+        fruitTransaction.setQuantity(Integer.parseInt(fields[QUANTITY_CODE_INDEX]));
         return fruitTransaction;
     }
 }
