d37c219a2052d661363b18c007164d447b88820b
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:39:24 +0300

    added changes to the class BalanceOperationHandlerImpl

--- src/main/java/core/basesyntax/service/Operation/BalanceOperationHandlerImpl.java
+++ src/main/java/core/basesyntax/service/Operation/BalanceOperationHandlerImpl.java
@@ -1,2 +1,20 @@
-package core.basesyntax.service.Operation;public class BalanceOperationHandlerImpl {
+package core.basesyntax.service.Operation;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+
+public class BalanceOperationHandlerImpl implements OperationHandler {
+
+    @Override
+    public void handle(FruitTransaction transaction) {
+        if (transaction == null) {
+            throw new RuntimeException("Transaction is invalid");
+        }
+        String fruitName = transaction.getFruit();
+        int quantityFruit = transaction.getQuantity();
+        if (quantityFruit < 0) {
+            throw new RuntimeException("Quantity must be greater or equals 0!");
+        }
+        Storage.storage.put(fruitName, quantityFruit);
+    }
 }
