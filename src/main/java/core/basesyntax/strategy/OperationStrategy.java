2f53dce9cc7a4eefe19e41260bf9a80243c2723d
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 12:58:41 +0300

    added changes to the class OperationStrategy

--- src/main/java/core/basesyntax/strategy/OperationStrategy.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategy.java
@@ -1,7 +1,7 @@
 package core.basesyntax.strategy;
 
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.Operation.OperationHandler;
+import core.basesyntax.service.operation.OperationHandler;
 
 public interface OperationStrategy {
     OperationHandler get(FruitTransaction.Operation operation);
