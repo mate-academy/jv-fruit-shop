929428fcdcba111e40fd403f9ec47f0ab898f8c1
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:44:42 +0300

    added changes to the class Storage

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -1,2 +1,8 @@
-package core.basesyntax.db;public class Storage {
+package core.basesyntax.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storage {
+    public static final Map<String, Integer> storage = new HashMap<>();
 }
