494992bdad7706984da0c201d6b32243d34f4255
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 18:41:59 +0300

    added changes to the class ReportServiceImpl

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.service.ReportService;
-import java.util.stream.Collectors;
 
 public class ReportServiceImpl implements ReportService {
     private static final String TITLE = "fruit,quantity" + System.lineSeparator();
@@ -10,14 +9,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Override
     public String createReport() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(TITLE);
+        StringBuilder builder = new StringBuilder(TITLE);
         Storage.storage.entrySet().stream()
-                .map(fruit -> builder.append(fruit.getKey())
-                        .append(SEPARATOR)
-                        .append(fruit.getValue())
-                        .append(System.lineSeparator()))
-                .collect(Collectors.joining());
+                .map(fruit -> builder.append(fruit.getKey()));
         return builder.toString();
     }
 }
