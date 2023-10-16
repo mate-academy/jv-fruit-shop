ad69d8830bd8d71d733fef19bd1c07fc3d484d4a
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:38:13 +0300

    added changes to the class ReturnOperationHandlerImpl

--- src/main/java/core/basesyntax/service/Operation/ReturnOperationHandlerImpl.java
+++ src/main/java/core/basesyntax/service/Operation/ReturnOperationHandlerImpl.java
@@ -1,2 +1,19 @@
-package core.basesyntax.service.Operation;public class ReturnOperationHandlerImpl {
+package core.basesyntax.service.Operation;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+
+public class ReturnOperationHandlerImpl implements OperationHandler {
+    @Override
+    public void handle(FruitTransaction transaction) {
+        if (transaction.getQuantity() < 0) {
+            throw new RuntimeException("Quantity can be greater than zero");
+        }
+        String fruitName = transaction.getFruit();
+        int transactionQuantity = transaction.getQuantity();
+        if (Storage.storage.containsKey(fruitName)) {
+            int updatedQuantity = Storage.storage.get(fruitName) + transactionQuantity;
+            Storage.storage.replace(fruitName, updatedQuantity);
+        }
+    }
 }
