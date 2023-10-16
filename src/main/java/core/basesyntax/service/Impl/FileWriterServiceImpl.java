e7a15f36b3552b7986bad7cce3815057caca4618
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:42:23 +0300

    added changes to the class FileWriterServiceImpl

--- src/main/java/core/basesyntax/service/Impl/FileWriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/Impl/FileWriterServiceImpl.java
@@ -1,2 +1,19 @@
-package core.basesyntax.service.Impl;public class FileWriterServiceImpl {
+package core.basesyntax.service.Impl;
+
+import core.basesyntax.service.FileWriterService;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterServiceImpl implements FileWriterService {
+    @Override
+    public void write(String report, String filePath) {
+        File file = new File(filePath);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write report to file: " + filePath, e);
+        }
+    }
 }
