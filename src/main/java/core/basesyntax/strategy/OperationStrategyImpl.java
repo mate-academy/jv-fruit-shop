e5a1424616261c5945f3eafb126e2c5bd1f05a8c
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:35:44 +0300

    added changes to the class OperationStrategyImpl

--- src/main/java/core/basesyntax/strategy/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategyImpl.java
@@ -1,2 +1,19 @@
-package core.basesyntax.strategy;public class OperationStrategyImpl {
+package core.basesyntax.strategy;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.Operation.OperationHandler;
+import java.util.Map;
+
+public class OperationStrategyImpl implements OperationStrategy {
+    private Map<FruitTransaction.Operation, OperationHandler> operationOperationHandlerMap;
+
+    public OperationStrategyImpl(Map<FruitTransaction.Operation,
+            OperationHandler> operationOperationHandlerMap) {
+        this.operationOperationHandlerMap = operationOperationHandlerMap;
+    }
+
+    @Override
+    public OperationHandler get(FruitTransaction.Operation operation) {
+        return operationOperationHandlerMap.get(operation);
+    }
 }
