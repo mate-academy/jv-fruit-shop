e78f5aefef5f43637ceb1dcfff5403769b076856
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:43:07 +0300

    added changes to the class FruitShopService

--- src/main/java/core/basesyntax/service/FruitShopService.java
+++ src/main/java/core/basesyntax/service/FruitShopService.java
@@ -1,2 +1,8 @@
-package core.basesyntax.service;public interface FruitShopService {
+package core.basesyntax.service;
+
+import core.basesyntax.model.FruitTransaction;
+import java.util.List;
+
+public interface FruitShopService {
+    void processTransactions(List<FruitTransaction> transactions);
 }
