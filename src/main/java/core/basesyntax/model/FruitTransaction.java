56f8c660864b567a598aeb28333b6435a2300ced
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:44:23 +0300

    added changes to the class FruitTransaction

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -1,6 +1,6 @@
 package core.basesyntax.model;
 
-import java.math.BigDecimal;
+import java.util.Objects;
 
 public class FruitTransaction {
     private Operation operation;
@@ -13,17 +13,65 @@ public class FruitTransaction {
         this.quantity = quantity;
     }
 
-    // Getters and setters
     public Operation getOperation() {
         return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
     }
 
     public String getFruit() {
         return fruit;
     }
 
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
     public int getQuantity() {
         return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        FruitTransaction that = (FruitTransaction) o;
+
+        if (quantity != that.quantity) {
+            return false;
+        }
+        if (operation != that.operation) {
+            return false;
+        }
+        return Objects.equals(fruit, that.fruit);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = operation != null ? operation.hashCode() : 0;
+        result = 31 * result + (fruit != null ? fruit.hashCode() : 0);
+        result = 31 * result + quantity;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "FruitTransaction{"
+                + "operation=" + operation
+                + ", fruit='" + fruit + '\''
+                + ", quantity=" + quantity
+                + '}';
     }
 
     public enum Operation {
@@ -40,6 +88,15 @@ public class FruitTransaction {
 
         public String getCode() {
             return code;
+        }
+
+        public static Operation getOperationByCode(String code) {
+            for (Operation operation : Operation.values()) {
+                if (operation.getCode().equals(code)) {
+                    return operation;
+                }
+            }
+            throw new IllegalArgumentException("Invalid operation code: " + code);
         }
     }
 }
