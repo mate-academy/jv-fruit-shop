2b9ec0c43d9f3c86bbaedd5e10cba8c1d2a88788
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 18:43:29 +0300

    added changes to the class FruitTransaction

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -1,7 +1,5 @@
 package core.basesyntax.model;
 
-import java.util.Objects;
-
 public class FruitTransaction {
     private Operation operation;
     private String fruit;
@@ -35,43 +33,6 @@ public class FruitTransaction {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        FruitTransaction that = (FruitTransaction) o;
-
-        if (quantity != that.quantity) {
-            return false;
-        }
-        if (operation != that.operation) {
-            return false;
-        }
-        return Objects.equals(fruit, that.fruit);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = operation != null ? operation.hashCode() : 0;
-        result = 31 * result + (fruit != null ? fruit.hashCode() : 0);
-        result = 31 * result + quantity;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "FruitTransaction{"
-                + "operation=" + operation
-                + ", fruit='" + fruit + '\''
-                + ", quantity=" + quantity
-                + '}';
     }
 
     public enum Operation {
