c8a7432b332b61d8d5fd5c963038ef9c0fb8fe6f
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:42:05 +0300

    added changes to the class FruitShopServiceImpl

--- src/main/java/core/basesyntax/service/Impl/FruitShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/Impl/FruitShopServiceImpl.java
@@ -1,4 +1,23 @@
 package core.basesyntax.service.Impl;
 
-public class FruitSopServiceImpl {
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.FruitShopService;
+import core.basesyntax.service.Operation.OperationHandler;
+import core.basesyntax.strategy.OperationStrategy;
+import java.util.List;
+
+public class FruitShopServiceImpl implements FruitShopService {
+    private OperationStrategy operationStrategy;
+
+    public FruitShopServiceImpl(OperationStrategy operationStrategy) {
+        this.operationStrategy = operationStrategy;
+    }
+
+    @Override
+    public void processTransactions(List<FruitTransaction> transactions) {
+        for (FruitTransaction transaction : transactions) {
+            OperationHandler handler = operationStrategy.get(transaction.getOperation());
+            handler.handle(transaction);
+        }
+    }
 }
--- src/main/java/core/basesyntax/service/Impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/Impl/ReportServiceImpl.java
@@ -1,2 +1,23 @@
-package core.basesyntax.service.Impl;public class ReportServiceImpl {
+package core.basesyntax.service.Impl;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.service.ReportService;
+import java.util.stream.Collectors;
+
+public class ReportServiceImpl implements ReportService {
+    private static final String TITLE = "fruit,quantity" + System.lineSeparator();
+    private static final String SEPARATOR = ",";
+
+    @Override
+    public String createReport() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(TITLE);
+        Storage.storage.entrySet().stream()
+                .map(fruit -> builder.append(fruit.getKey())
+                        .append(SEPARATOR)
+                        .append(fruit.getValue())
+                        .append(System.lineSeparator()))
+                .collect(Collectors.joining());
+        return builder.toString();
+    }
 }
