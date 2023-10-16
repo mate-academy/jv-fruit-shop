aabf7b702f4e3218f2981762b66fc83934c3dd0d
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 18:42:50 +0300

    added changes to the class FileReaderServiceImpl

--- src/main/java/core/basesyntax/service/impl/FileReaderServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/FileReaderServiceImpl.java
@@ -5,15 +5,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FileReaderServiceImpl implements FileReaderService {
-    private static final int OFFSET = 1;
-
     @Override
     public List<String> read(String filePath) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            return reader.lines().skip(OFFSET).collect(Collectors.toList());
+            return reader.lines().collect(java.util.stream.Collectors.toList());
         } catch (IOException exception) {
             throw new RuntimeException("Can't read the file " + filePath);
         }
