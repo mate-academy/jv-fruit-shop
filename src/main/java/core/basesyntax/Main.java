415c1f2ac1156708b58e25b35154410da75359de
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:45:02 +0300

    added changes to the class Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import core.basesyntax.service.FileWriterService;
+import core.basesyntax.service.Impl.FileWriterServiceImpl;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
-
+        FileWriterService writerService = new FileWriterServiceImpl();
+        writerService.write("salty", null);
     }
 }
