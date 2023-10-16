ce8771b9cf2bdb3366b2fe8674e8dece021382a1
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 11:37:48 +0300

    added changes to the class SupplyOperationHandlerImpl

--- src/main/java/core/basesyntax/service/Operation/SupplyOperationHandlerImpl.java
+++ src/main/java/core/basesyntax/service/Operation/SupplyOperationHandlerImpl.java
@@ -1,2 +1,20 @@
-package core.basesyntax.service.Operation;public class SupplyOperationHandlerImpl {
+package core.basesyntax.service.Operation;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+
+public class SupplyOperationHandlerImpl implements OperationHandler {
+
+    @Override
+    public void handle(FruitTransaction transaction) {
+        if (transaction.getQuantity() < 0) {
+            throw new RuntimeException("Quantity can be greater than zero");
+        }
+        String fruitName = transaction.getFruit();
+        int transactionQuantity = transaction.getQuantity();
+        if (Storage.storage.containsKey(fruitName)) {
+            int updatedQuantity = Storage.storage.get(fruitName) + transactionQuantity;
+            Storage.storage.replace(fruitName, updatedQuantity);
+        }
+    }
 }
