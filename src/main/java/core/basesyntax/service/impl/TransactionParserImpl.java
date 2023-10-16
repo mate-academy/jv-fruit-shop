991e2a1555039530bd4ef57a5c3b0900ae5b144b
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-16 21:04:37 +0300

    added changes to the class TransactionParserImpl

--- src/main/java/core/basesyntax/service/impl/TransactionParserImpl.java
+++ src/main/java/core/basesyntax/service/impl/TransactionParserImpl.java
@@ -15,14 +15,14 @@ public class TransactionParserImpl implements TransactionParser {
     public List<FruitTransaction> parseData(List<String> data) {
         try {
             return data.stream()
-                    .map(this::parseLine)
+                    .map(this::TransactionFromData)
                     .collect(Collectors.toList());
         } catch (NumberFormatException e) {
             throw new RuntimeException("Invalid value for fruit quantity");
         }
     }
 
-    private FruitTransaction parseLine(String line) {
+    private FruitTransaction TransactionFromData(String line) {
         String[] elements = line.split(SEPARATOR);
         String code = elements[INDEX_CODE_OPERATION];
         String name = elements[INDEX_FRUIT_NAME];
