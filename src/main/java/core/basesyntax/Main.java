f97fcf900f5ad1a563e39074059d26136400a324
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-17 18:47:50 +0300

    added changes to the class Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,13 +1,28 @@
 package core.basesyntax;
 
-import core.basesyntax.service.FileWriterService;
-import core.basesyntax.service.impl.FileWriterServiceImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
 
 public class Main {
     public static void main(String[] args) {
-        FileWriterService writerService = new FileWriterServiceImpl();
-        String content = "banana";
-        String filePath = "src/test/resources/FileWriter.csv";
-        writerService.write(content, filePath);
+        // Add some fruit transactions to the storage
+        Storage.storage.put("apple", 10);
+        Storage.storage.put("banana", 15);
+        FruitTransaction applePurchase = new FruitTransaction(
+                FruitTransaction.Operation.PURCHASE, "apple", 10);
+        FruitTransaction bananaPurchase = new FruitTransaction(
+                FruitTransaction.Operation.PURCHASE, "banana", 15);
+        processTransaction(applePurchase);
+        processTransaction(bananaPurchase);
+        System.out.println("Updated Storage:");
+        Storage.storage.forEach((fruit, quantity) -> {
+            System.out.println(fruit + ": " + quantity);
+        });
+    }
+
+    private static void processTransaction(FruitTransaction transaction) {
+        if (transaction.getOperation() == FruitTransaction.Operation.PURCHASE) {
+            Storage.storage.merge(transaction.getFruit(), transaction.getQuantity(), Integer::sum);
+        }
     }
 }
