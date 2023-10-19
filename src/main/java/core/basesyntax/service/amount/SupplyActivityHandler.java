aad0934dd7efe377dc394c3165de49501a9aca30
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-10-19 13:56:16 +0300

    fixed a mistake with mvn clean package

--- src/main/java/core/basesyntax/service/amount/SupplyActivityHandler.java
+++ src/main/java/core/basesyntax/service/amount/SupplyActivityHandler.java
@@ -1,7 +1,6 @@
 package core.basesyntax.service.amount;
 
 import core.basesyntax.dao.FruitTransactionDao;
-import core.basesyntax.dao.FruitTransactionDaoImpl;
 import core.basesyntax.model.FruitTransaction;
 
 public class SupplyActivityHandler implements ActivityHandler {
