ae5036e9702ffbc3c101f90455c50f0ecad04c3c
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-10-23 10:44:13 +0300

    fixed mvn clean package mistakes

--- src/main/java/core/basesyntax/model/Operation.java
+++ src/main/java/core/basesyntax/model/Operation.java
@@ -10,12 +10,13 @@ public enum Operation {
     Operation(String code) {
         this.code = code;
     }
+
     public static Operation getOperationFromString(String string) {
-            return switch (string) {
-                case "r" -> Operation.RETURN;
-                case "s" -> Operation.SUPPLY;
-                case "b" -> Operation.BALANCE;
-                default -> Operation.PURCHASE;
-            };
+        return switch (string) {
+            case "r" -> Operation.RETURN;
+            case "s" -> Operation.SUPPLY;
+            case "b" -> Operation.BALANCE;
+            default -> Operation.PURCHASE;
+        };
     }
 }
