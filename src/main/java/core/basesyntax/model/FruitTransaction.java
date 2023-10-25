1d20a42ed4016cd04a1c5b1aa5aec02c92f8b55e
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-10-25 19:40:04 +0300

    Removed useless methods

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -21,24 +21,12 @@ public class FruitTransaction {
         return operation;
     }
 
-    public void setOperation(Operation operation) {
-        this.operation = operation;
-    }
-
     public String getFruitName() {
         return fruitName;
     }
 
-    public void setFruitName(String fruitName) {
-        this.fruitName = fruitName;
-    }
-
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
     @Override
