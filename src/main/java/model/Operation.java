68f325515b7106ac061b7379254f21d795575d4e
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-30 21:41:42 +0200

    Added method to get Operation type from String.

--- src/main/java/model/Operation.java
+++ src/main/java/model/Operation.java
@@ -1,18 +1,31 @@
 package model;
 
+import exceptions.NoSuchOperationException;
+
+import java.util.NoSuchElementException;
+
 public enum Operation {
     BALANCE("b"),
     SUPPLY("s"),
     PURCHASE("p"),
     RETURN("r");
 
+    private static final String NO_SUCH_OPERATION_MESSAGE = "This operation is not in the supported list: ";
     private final String code;
 
     Operation(String code) {
       this.code = code;
     }
-
     public String getCode() {
-      return code;
+        return code;
     }
-  }
+
+    public static Operation fromString(String operationInString) {
+        for (Operation operation : Operation.values()) {
+            if (operation.code.equalsIgnoreCase(operationInString)) {
+                return operation;
+            }
+        }
+        throw new NoSuchOperationException(NO_SUCH_OPERATION_MESSAGE + operationInString);
+    }
+}
