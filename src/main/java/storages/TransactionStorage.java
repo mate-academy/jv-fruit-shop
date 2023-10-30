9f2c972524913f933e778fc1d053cfb2bc4b066f
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-30 22:27:52 +0200

    Create storages for the FruitTransaction entity.

---
+++ src/main/java/storages/TransactionStorage.java
@@ -0,0 +1,10 @@
+package storages;
+
+import model.FruitTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionStorage {
+    private final List<FruitTransaction> transactionList = new ArrayList<>();
+}
