139c1d30a89dc2771337cb641cc89e1d0002fd12
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-30 22:46:51 +0200

    Moved parsing process to separate method.

--- src/main/java/files/CsvReader.java
+++ src/main/java/files/CsvReader.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
-public class CsvReader implements Reader{
+public class CsvReader implements Reader {
     private static final String CSV_DATA_SEPARATOR = ",";
     public static final int OPERATION_ARRAY_INDEX = 0;
     public static final int FRUIT_ARRAY_INDEX = 1;
@@ -25,16 +25,20 @@ public class CsvReader implements Reader{
         try(BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
             String dataLine = reader.readLine();
             while (dataLine != null) {
-                String[] data = dataLine.split(CSV_DATA_SEPARATOR);
-                String operationType = data[OPERATION_ARRAY_INDEX];
-                String fruit = data[FRUIT_ARRAY_INDEX];
-                int quantity = Integer.parseInt(data[QUANTITY_ARRAY_INDEX]);
-                Operation operation = Operation.fromString(operationType);
-                transactionList.add(new FruitTransaction(operation, fruit, quantity));
+                transactionList.add(parseDataToFruitTransaction(dataLine));
                 dataLine = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(CANT_READ_DATA_FROM_FILE_MESSAGE + path.getFileName() );
         }
+    }
+
+    private FruitTransaction parseDataToFruitTransaction(String dataLine) {
+        String[] data = dataLine.split(CSV_DATA_SEPARATOR);
+        String operationType = data[OPERATION_ARRAY_INDEX];
+        String fruit = data[FRUIT_ARRAY_INDEX];
+        int quantity = Integer.parseInt(data[QUANTITY_ARRAY_INDEX]);
+        Operation operation = Operation.fromString(operationType);
+        return new FruitTransaction(operation, fruit, quantity);
     }
 }
