aa63dbe1aa4d2711de497b77a9d4b258066331a4
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-30 23:22:37 +0200

    Created psvm. Tested program.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import app.FruitShopApp;
+
+public class Main {
+    private static final FruitShopApp fruitShopApp = new FruitShopApp();
+    public static void main(String[] args) {
+        fruitShopApp.createDailyReport();
+    }
+}
