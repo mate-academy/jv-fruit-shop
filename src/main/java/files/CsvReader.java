97a687e52e2c7cbc9c993e079d32f04bc3a61dab
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-30 21:40:39 +0200

    Added implementation for the CsvReader, CsvWriter.

--- src/main/java/files/CsvReader.java
+++ src/main/java/files/CsvReader.java
@@ -1,4 +1,40 @@
 package files;
 
-public class CsvReader {
+import model.FruitTransaction;
+import model.Operation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public class CsvReader implements Reader{
+    private static final String CSV_DATA_SEPARATOR = ",";
+    public static final int OPERATION_ARRAY_INDEX = 0;
+    public static final int FRUIT_ARRAY_INDEX = 1;
+    public static final int QUANTITY_ARRAY_INDEX = 2;
+    public static final String CANT_READ_DATA_FROM_FILE_MESSAGE = "Can't read data from file: ";
+    private final List<FruitTransaction> transactionList;
+
+    public CsvReader(List<FruitTransaction> transactionList) {
+        this.transactionList = transactionList;
+    }
+    @Override
+    public void readFromFile(Path path) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+            String dataLine = reader.readLine();
+            while (dataLine != null) {
+                String[] data = dataLine.split(CSV_DATA_SEPARATOR);
+                String operationType = data[OPERATION_ARRAY_INDEX];
+                String fruit = data[FRUIT_ARRAY_INDEX];
+                int quantity = Integer.parseInt(data[QUANTITY_ARRAY_INDEX]);
+                Operation operation = Operation.fromString(operationType);
+                transactionList.add(new FruitTransaction(operation, fruit, quantity));
+                dataLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(CANT_READ_DATA_FROM_FILE_MESSAGE + path.getFileName() );
+        }
+    }
 }
--- src/main/java/files/CsvWriter.java
+++ src/main/java/files/CsvWriter.java
@@ -6,12 +6,14 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class CsvWriter implements Writer{
+    public static final String CANT_WRITE_DATA_INTO_THE_FILE_MESSAGE = "Can't write data into the file: ";
+
     @Override
     public void writeIntoFile(Path path, String data) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))) {
             writer.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data into the file" + path.getFileName());
+            throw new RuntimeException(CANT_WRITE_DATA_INTO_THE_FILE_MESSAGE + path.getFileName());
         }
     }
 }
