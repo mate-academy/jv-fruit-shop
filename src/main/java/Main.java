ea9081e7647801711915a2d9c6694c6c552ddc39
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-31 10:35:07 +0200

    For Reader, Writer, Reporter inside FruitShopApp are set in the constructor.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import app.FruitShopApp;
+import files.CsvReader;
+import files.CsvWriter;
+import reporter.FruitReporterImpl;
 
 public class Main {
-    private static final FruitShopApp fruitShopApp = new FruitShopApp();
+    private static final FruitShopApp fruitShopApp = new FruitShopApp(
+            new CsvWriter(), new CsvReader(), new FruitReporterImpl());
 
     public static void main(String[] args) {
         fruitShopApp.createDailyReport();
--- src/main/java/app/FruitShopApp.java
+++ src/main/java/app/FruitShopApp.java
@@ -2,8 +2,6 @@ package app;
 
 import static storages.TransactionStorage.transactionList;
 
-import files.CsvReader;
-import files.CsvWriter;
 import files.Reader;
 import files.Writer;
 import java.util.HashMap;
@@ -16,18 +14,23 @@ import operation.OperationStrategyImpl;
 import operation.PurchaseOperationHandlerImpl;
 import operation.ReturnOperationHandlerImpl;
 import operation.SupplyOperationHandlerImpl;
-import reporter.FruitReporterImpl;
 import reporter.Reporter;
 
 public class FruitShopApp {
     private static final String TEST_DATA_FILE_NAME = "data.csv";
     private static final String TEST_RESULT_FILE_NAME = "result.csv";
-    private final Writer writer = new CsvWriter();
-    private final Reader reader = new CsvReader();
-    private final Reporter reporter = new FruitReporterImpl();
+    private final Writer writer;
+    private final Reader reader;
+    private final Reporter reporter;
     private final Map<Operation, OperationHandler> operationOperationHandlerMap = new HashMap<>();
     private final OperationStrategyImpl operationStrategy =
             new OperationStrategyImpl(operationOperationHandlerMap);
+
+    public FruitShopApp(Writer writer, Reader reader, Reporter reporter) {
+        this.writer = writer;
+        this.reader = reader;
+        this.reporter = reporter;
+    }
 
     public void createDailyReport() {
         fillOperationMap();
