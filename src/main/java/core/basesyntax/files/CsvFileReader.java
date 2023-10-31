aa256da0a0588e44f0d6020bce8e75b067463093
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-31 12:03:04 +0200

    For now use StreamAPI to read and filter data from file.

--- src/main/java/core/basesyntax/files/CsvFileReader.java
+++ src/main/java/core/basesyntax/files/CsvFileReader.java
@@ -7,21 +7,14 @@ import java.util.List;
 
 public class CsvFileReader implements FileReader {
     private static final String CANT_READ_DATA_FROM_FILE_MESSAGE = "Can't read data from file: ";
+    public static final int AMOUNT_OF_LINES_TO_SKIP = 1;
 
     @Override
     public List<String> readFromFile(String path) {
         List<String> dataLines = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(path))) {
-            String dataLine = reader.readLine();
-            boolean isFirstLineRead = false;
-            while (dataLine != null) {
-                if(!isFirstLineRead) {
-                    isFirstLineRead = true;
-                    continue;
-                }
-                dataLines.add(dataLine);
-                dataLine = reader.readLine();
-            }
+            reader.lines().skip(AMOUNT_OF_LINES_TO_SKIP)
+                    .forEach(dataLines::add);
         } catch (IOException e) {
             throw new RuntimeException(CANT_READ_DATA_FROM_FILE_MESSAGE + path);
         }
