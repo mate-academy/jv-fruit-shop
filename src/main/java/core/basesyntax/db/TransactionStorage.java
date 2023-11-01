4b506c86686733a90d7a69e17904d5dfc0d5dbd1
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-11-01 18:25:17 +0200

    Delete redundant storage and dao it's dao.

--- src/main/java/core/basesyntax/db/TransactionStorage.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax.db;
-
-import core.basesyntax.model.FruitTransaction;
-import java.util.ArrayList;
-import java.util.List;
-
-public class TransactionStorage {
-    public static final List<FruitTransaction> transactionList = new ArrayList<>();
-}
