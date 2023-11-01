485e1d56fa53d928f2c780a206f12d93e8478eb5
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-11-01 22:34:43 +0200

    added constant exception message to CsvDataWriter class

--- src/main/java/core/basesyntax/service/impl/CsvDataWriter.java
+++ src/main/java/core/basesyntax/service/impl/CsvDataWriter.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class CsvDataWriter implements DataWriter {
+    private static final String EXCEPTION_MESSAGE = "Can't write to file";
     public void writeToFile(String destinationFileName, String reportData) {
         File destinationfile = new File(destinationFileName);
         try {
             Files.write(destinationfile.toPath(), reportData.getBytes());
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file" + destinationFileName, e);
+            throw new RuntimeException(EXCEPTION_MESSAGE + destinationFileName, e);
         }
     }
 }
