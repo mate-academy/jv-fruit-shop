59b6ca5126e939868238f88ab674afa72ea0940c
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-11-01 22:41:21 +0200

    added constants for split line indexes in CsvReadDataParseImpl

--- src/main/java/core/basesyntax/service/impl/CsvReadDataParserImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvReadDataParserImpl.java
@@ -8,17 +8,21 @@ import java.util.List;
 
 public class CsvReadDataParserImpl implements ReadDataParser {
     private static final String SPLIT_REGEX = ",";
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int FRUIT_NAME_INDEX = 1;
+    private static final int AMOUNT_INDEX = 2;
+
 
     @Override
     public ArrayList<FruitTransaction> convertToFruitTransactionList(List<String> fileLines) {
         ArrayList<FruitTransaction> operationsList = new ArrayList<>();
         for (int i = 1; i < fileLines.size(); i++) {
             String[] splitedLine = fileLines.get(i).split(SPLIT_REGEX);
-            String operationTypeCode = splitedLine[0];
-            String fruitType = splitedLine[1];
-            int quantity = Integer.parseInt(splitedLine[2]);
+            String operationTypeCode = splitedLine[OPERATION_TYPE_INDEX];
+            String fruitType = splitedLine[FRUIT_NAME_INDEX];
+            int amount = Integer.parseInt(splitedLine[AMOUNT_INDEX]);
             OperationType operationType = OperationType.fromCode(operationTypeCode);
-            operationsList.add(new FruitTransaction(operationType, fruitType, quantity));
+            operationsList.add(new FruitTransaction(operationType, fruitType, amount));
         }
         return operationsList;
     }
