5ddfe482bccdb2b9201978c5cbc33d90fa701521
Vadym Hurnik <vad.com.z@gmail.com>
2023-11-02 10:28:59 +0200

    added constants

--- src/main/java/core/basesyntax/service/FruitParser.java
+++ src/main/java/core/basesyntax/service/FruitParser.java
@@ -6,14 +6,17 @@ import java.util.List;
 import java.util.function.Function;
 
 public class FruitParser {
+    private static final int OPERATION_INDEX = 0;
+    private static final int FRUIT_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
     private static final String REGEX_COMMA = ",";
 
     public List<FruitTransaction> parseLinesInFile(List<String> linesInFile) {
         Function<String[], FruitTransaction> stringsToFruitTransaction = params -> {
             FruitTransaction fruitTransaction = new FruitTransaction();
-            fruitTransaction.setOperation(Operation.getOperationByCode(params[0]));
-            fruitTransaction.setFruit(params[1]);
-            fruitTransaction.setQuantity(Integer.parseInt(params[2]));
+            fruitTransaction.setOperation(Operation.getOperationByCode(params[OPERATION_INDEX]));
+            fruitTransaction.setFruit(params[FRUIT_INDEX]);
+            fruitTransaction.setQuantity(Integer.parseInt(params[QUANTITY_INDEX]));
             return fruitTransaction;
         };
         return linesInFile.stream()
