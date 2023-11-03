9a412f3b3bffd3f001a85401f049426d2b1c2c7d
Anton Babych <antonbabi13@gmail.com>
2023-11-03 13:18:43 +0200

    UPDATED! Improve code, move title delete logic from FileService to main, add ENUM to type marking, rename proccessOperaton to processTransactions, change format for report, now it not List<String> but String Delete all magic numbers.

--- src/main/java/core/basesyntax/service/FileServiceImpl.java
+++ src/main/java/core/basesyntax/service/FileServiceImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.service;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
