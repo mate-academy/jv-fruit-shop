1ab9ca13b25daaf5fd4cbe4905101f6930b344b3
IhorTrokhymchuk <hameleon009@gmail.com>
2023-11-08 17:15:34 +0200

    new arch

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -9,17 +9,14 @@ import core.basesyntax.service.FileReaderImpl;
 import core.basesyntax.service.FileWriter;
 import core.basesyntax.service.FileWriterImpl;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class Main {
     public static void main(String[] args) {
         FileWriter fileWriter = new FileWriterImpl();
         FileReader fileReader = new FileReaderImpl();
         Transaction transaction = new TransactionImpl();
         ReportCreator reportCreator = new ReportCreatorImpl();
-        
+
         transaction.getTransactionList(fileReader.readFile("file.csv"));
-        fileWriter.writeReport(reportCreator.updateStorage(),"report_file.csv");
+        fileWriter.writeReport(reportCreator.updateStorage(), "report_file.csv");
     }
 }
