27c04e63b9bee9be3188baadbb0bf182bb399bc9
IhorTrokhymchuk <hameleon009@gmail.com>
2023-11-08 17:25:40 +0200

    marge

--- src/main/java/core/basesyntax/dao/OperationHandler.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.dao;
-
-import core.basesyntax.db.FruitDB;
-
-public interface OperationHandler {
-    void performOperation(FruitDB fruitDB, String name, Integer quantity);
-}
--- src/main/java/core/basesyntax/dao/OperationManager.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.dao;
-
-import core.basesyntax.db.FruitDB;
-import java.util.List;
-
-public interface OperationManager {
-    void addInfoToDB(List<String> stringList, FruitDB fruitDB);
-}
