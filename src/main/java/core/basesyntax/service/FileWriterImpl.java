29edc3d2f1631519a956ecb63116d1db76e1f29b
IhorTrokhymchuk <hameleon009@gmail.com>
2023-11-08 17:29:43 +0200

    new final solution

---
+++ src/main/java/core/basesyntax/service/FileWriterImpl.java
@@ -0,0 +1,23 @@
+package core.basesyntax.service;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class FileWriterImpl implements FileWriter {
+    private static final String HEAD_OF_REPORT = "fruit,quantity";
+
+    @Override
+    public void writeReport(Map<String, Integer> reportMap, String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new java.io.FileWriter(fileName))) {
+            bufferedWriter.write(HEAD_OF_REPORT);
+            for (Map.Entry<String, Integer> entry : reportMap.entrySet()) {
+                bufferedWriter.newLine();
+                bufferedWriter.write(entry.getKey() + "," + entry.getValue());
+            }
+            System.out.println("Write to file successful");
+        } catch (IOException e) {
+            throw new RuntimeException("Cant write file");
+        }
+    }
+}
