df3e30e45a9885a5f6eb0a9b857791906aa7562f
IhorTrokhymchuk <hameleon009@gmail.com>
2023-11-08 15:16:59 +0200

    4.1 commit

--- src/main/java/core/basesyntax/dao/StorageUpdatorImpl.java
+++ src/main/java/core/basesyntax/dao/StorageUpdatorImpl.java
@@ -2,15 +2,25 @@ package core.basesyntax.dao;
 
 import core.basesyntax.dao.operation.Operation;
 import core.basesyntax.dao.operation.OperationHandler;
+import core.basesyntax.dao.operation.ReturnOperation;
 import core.basesyntax.dao.transaction.FruitTransaction;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StorageUpdatorImpl implements StorageUpdator {
+    private final Map<Operation, OperationHandler> operationHandlerMap;
+
+    public StorageUpdatorImpl() {
+        operationHandlerMap = new HashMap<>();
+        operationHandlerMap.put(Operation.RETURN, ReturnOperation)
+    }
+
     @Override
     public void updateStorage(List<FruitTransaction> fruitTransactionList) {
         for (FruitTransaction fruitTransaction : fruitTransactionList) {
-            OperationHandler operationHandler = fruitTransaction;
+            OperationHandler operationHandler = operationHandlerMap.get(fruitTransaction.getOperation());
         }
     }
 }
