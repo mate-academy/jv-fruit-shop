bac0029d11f7e9eb6e477c548e1afeae037a03bb
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-11-14 18:51:40 +0200

    implement enum 'Operation'

---
+++ src/main/java/core/basesyntax/Operation.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public enum Operation {
+    BALANCE("b"),
+    SUPPLY("s"),
+    PURCHASE("p"),
+    RETURN("r");
+
+    private final String code;
+
+    Operation(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
