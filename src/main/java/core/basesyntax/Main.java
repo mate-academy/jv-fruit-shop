662462c36b5e126040f0e3731c2b42709bc21a77
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-11-17 15:28:57 +0200

    rename

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,30 +3,33 @@ package core.basesyntax;
 import core.basesyntax.model.ItemTransaction;
 import core.basesyntax.service.Converter;
 import core.basesyntax.service.DataWriter;
-import core.basesyntax.service.Handler;
+import core.basesyntax.service.TransactionHandler;
 import core.basesyntax.service.Reader;
 import core.basesyntax.service.ReportCreator;
 import core.basesyntax.service.impl.ConverterImpl;
 import core.basesyntax.service.impl.DataWriterImpl;
-import core.basesyntax.service.impl.HandlerImpl;
+import core.basesyntax.service.impl.TransactionHandlerImpl;
 import core.basesyntax.service.impl.ReaderImpl;
 import core.basesyntax.service.impl.ReportCreatorImpl;
 import java.util.List;
 
 public class Main {
+    private final static String INPUT_FILE_PATH = "src/main/resources/input.csv";
+    private final static String OUTPUT_FILE_PATH = "src/main/resources/input.csv";
+
     public static void main(String[] args) {
         Reader csvReader = new ReaderImpl();
-        List<String> strings = csvReader.read("src/main/resources/input.csv");
+        List<String> strings = csvReader.read(INPUT_FILE_PATH);
         Converter converter = new ConverterImpl();
         List<ItemTransaction> itemTransactions = converter.convert(strings);
 
-        Handler handler = new HandlerImpl();
-        handler.handle(itemTransactions);
+        TransactionHandler transactionHandler = new TransactionHandlerImpl();
+        transactionHandler.handle(itemTransactions);
 
         ReportCreator reportCreator = new ReportCreatorImpl();
         List<String> report = reportCreator.report();
 
         DataWriter dataWriter = new DataWriterImpl();
-        dataWriter.write(report, "report.csv");
+        dataWriter.write(report, OUTPUT_FILE_PATH);
     }
 }
