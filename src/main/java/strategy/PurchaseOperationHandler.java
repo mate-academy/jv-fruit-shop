04e511b6d4693cca7c31df259d620b69ab50ee1f
Mateusz Rostek <17rostek17@gmail.com>
2023-11-17 14:36:17 +0100

    Implemented handlers

--- src/main/java/strategy/PurchaseOperationHandler.java
+++ src/main/java/strategy/PurchaseOperationHandler.java
@@ -1,10 +1,16 @@
 package strategy;
 
 import model.FruitTransaction;
+import storage.Storage;
 
 public class PurchaseOperationHandler implements OperationHandler {
     @Override
     public void handleOperation(FruitTransaction fruitTransaction) {
-
+        Integer quantity = Storage.fruitQuantities.get(fruitTransaction.getFruit());
+        Integer newQuantity = quantity - fruitTransaction.getQuantity();
+        if (newQuantity < 0) {
+            throw new RuntimeException("Balance can't be negative!");
+        }
+        Storage.fruitQuantities.put(fruitTransaction.getFruit(), newQuantity);
     }
 }
--- src/main/java/strategy/SupplyOperationHandler.java
+++ src/main/java/strategy/SupplyOperationHandler.java
@@ -1,10 +1,13 @@
 package strategy;
 
 import model.FruitTransaction;
+import storage.Storage;
 
 public class SupplyOperationHandler implements OperationHandler {
     @Override
     public void handleOperation(FruitTransaction fruitTransaction) {
-
+        Integer quantity = Storage.fruitQuantities.get(fruitTransaction.getFruit());
+        Integer newQuantity = quantity + fruitTransaction.getQuantity();
+        Storage.fruitQuantities.put(fruitTransaction.getFruit(), newQuantity);
     }
 }
