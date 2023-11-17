e5c9a6567397997c2251bc5de5385743fe05d32a
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-11-17 15:36:05 +0200

    сhange 'ReadImpl'

--- src/main/java/core/basesyntax/service/impl/ReaderImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReaderImpl.java
@@ -1,27 +1,20 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.service.Reader;
-import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ReaderImpl implements Reader {
     @Override
     public List<String> read(String filePath) {
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader reader =
-                     Files.newBufferedReader(Path.of(filePath), StandardCharsets.UTF_8)) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                lines.add(line);
-            }
+        try {
+            return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file: " + filePath, e);
+            throw new RuntimeException("Can`t read file, Path: " + filePath, e);
         }
-        return lines;
     }
 }
