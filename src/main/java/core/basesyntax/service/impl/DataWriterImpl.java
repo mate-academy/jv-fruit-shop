e080de4f4cd61a029237cc62a8c2068fa8af9860
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-11-17 16:15:55 +0200

    use nio package

--- src/main/java/core/basesyntax/service/impl/DataWriterImpl.java
+++ src/main/java/core/basesyntax/service/impl/DataWriterImpl.java
@@ -1,25 +1,20 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.service.DataWriter;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class DataWriterImpl implements DataWriter {
     @Override
     public void write(List<String> strings, String filePath) {
         String content = String.join(System.lineSeparator(), strings);
-        Path path = Paths.get(filePath);
         try {
-            BufferedWriter writer = Files.newBufferedWriter(path);
-            writer.write(content);
-            writer.close();
+            Files.writeString(Path.of(filePath), content);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write " + filePath
-                   + " with data : " + strings, e);
+            throw new RuntimeException("Unable to write data to the specified path: " + filePath
+                    + ". Data: " + strings, e);
         }
     }
 }
