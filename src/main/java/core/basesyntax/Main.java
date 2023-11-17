b76ca585dca4338904145598812b0da99f9a4e07
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-11-17 11:52:36 +0200

    implement Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
+import core.basesyntax.converter.Converter;
+import core.basesyntax.converter.ConverterImpl;
+import core.basesyntax.handler.Handler;
+import core.basesyntax.handler.HandlerImpl;
+import core.basesyntax.reader.Reader;
+import core.basesyntax.reader.ReaderImpl;
+import core.basesyntax.report.ReportCreator;
+import core.basesyntax.report.ReportCreatorImpl;
+import core.basesyntax.writer.DataWriter;
+import core.basesyntax.writer.DataWriterImpl;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Reader csvReader = new ReaderImpl();
+        List<String> strings = csvReader.read("input.csv");
 
+        Converter converter = new ConverterImpl();
+        List<ItemTransaction> itemTransactions = converter.convert(strings);
+
+        Handler handler = new HandlerImpl();
+        handler.handle(itemTransactions);
+
+        ReportCreator reportCreator = new ReportCreatorImpl();
+        List<String> report = reportCreator.report();
+
+        DataWriter dataWriter = new DataWriterImpl();
+        dataWriter.write(report, "report.csv");
     }
 }
