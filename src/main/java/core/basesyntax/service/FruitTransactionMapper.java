860cfecdf2804795bddf73b8a64b0d572c540eb7
Oleh Shvets <olehpost69@gmail.com>
2023-12-04 18:38:37 +0300

    Fruit_shop_thirth_commit

--- src/main/java/core/basesyntax/service/FruitTransactionMapper.java
+++ src/main/java/core/basesyntax/service/FruitTransactionMapper.java
@@ -6,26 +6,28 @@ import java.util.List;
 
 public class FruitTransactionMapper {
     private static final String SEPARATOR = ",";
-    private static final int OPERATION = 0;
-    private static final int FRUIT = 1;
-    private static final int QUANTITY = 2;
+    private static final int OPERATION_INDEX = 0;
+    private static final int FRUIT_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
+    private static final int OFFSET = 1;
 
     public List<FruitTransaction> mapData(List<String> lines) {
         List<FruitTransaction> fruitTransactions = new ArrayList<>();
         if (lines == null || lines.isEmpty()) {
             throw new RuntimeException("Invalid data!");
         }
+        for (int i = OFFSET; i < lines.size(); i++) {
+        String[] elements = lines.get(i).split(SEPARATOR);
+        FruitTransaction.Operation operation =
+                FruitTransaction.Operation.findOperationByCode(elements[OPERATION_INDEX]);
+        String fruitName = elements[FRUIT_INDEX];
 
-        for (int i = 1; i < lines.size(); i++) {
-            String[] elements = lines.get(i).split(SEPARATOR);
-            FruitTransaction.Operation operation =
-                    FruitTransaction.Operation.findOperationByCode(elements[OPERATION]);
-            String fruitName = elements[FRUIT];
-            int quantity = Integer.parseInt(elements[QUANTITY]);
-            FruitTransaction fruitTransaction =
-                    new FruitTransaction(operation, fruitName, quantity);
-            fruitTransactions.add(fruitTransaction);
+        int quantity = Integer.parseInt(elements[QUANTITY_INDEX]);
+
+                FruitTransaction fruitTransaction =
+                        new FruitTransaction(operation, fruitName, quantity);
+                fruitTransactions.add(fruitTransaction);
+            }
+            return fruitTransactions;
         }
-        return fruitTransactions;
     }
-}
