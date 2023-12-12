f11640797b55968b47f4233c60d292ec4535c7d1
Mykola Horianin <anyavic2379@gmail.com>
2023-12-12 16:14:40 +0200

    Made changes

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -4,11 +4,12 @@ import core.basesyntax.service.ReportService;
 import java.util.Map;
 
 public class ReportServiceImpl implements ReportService {
+    private static final String TITLE = "fruit,quantity";
 
     @Override
     public String getReport(Map<String, Integer> storage) {
         StringBuilder report = new StringBuilder();
-        report.append("fruit,quantity");
+        report.append(TITLE);
         for (Map.Entry<String, Integer> entry : storage.entrySet()) {
             report.append("\n")
                     .append(entry.getKey())
