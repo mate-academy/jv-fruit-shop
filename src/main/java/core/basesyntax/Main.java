e70ec4de59356f42fdf51a6bb942b09894236f98
Serhii Kopervas <sergkopervas@gmail.com>
2023-12-24 21:24:29 +0100

    Fruit Store, version 1.0.2-beta. Code correction. Added new interfaces and classes. Changed the data storage class.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,12 +1,32 @@
 package core.basesyntax;
 
+import core.basesyntax.dao.ReadReportImpl;
+import core.basesyntax.dao.WriteBalanceImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.service.ConvertDataImpl;
+import core.basesyntax.service.CreateBalanceImpl;
+import core.basesyntax.service.FruitTransaction;
+import core.basesyntax.service.ProcessingImpl;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FruitDao fruitDao = new FruitDao();
-        System.out.println("Reading file *.csv: " + fruitDao.readCSV());
-        System.out.println("Writing balance: " + fruitDao.fruitBalance.writeBalance());
+        Storage.storage.put("apple", 0);
+        Storage.storage.put("banana", 0);
+
+        ReadReportImpl readData = new ReadReportImpl();
+
+        ConvertDataImpl convertData = new ConvertDataImpl();
+        ArrayList<FruitTransaction> fruitTransactionList = convertData.convert(readData.read());
+        ProcessingImpl processing = new ProcessingImpl();
+        processing.makeTransaction(fruitTransactionList);
+        CreateBalanceImpl createBalance = new CreateBalanceImpl();
+        String balance = createBalance.createReport();
+
+        WriteBalanceImpl writeBalance = new WriteBalanceImpl();
+        writeBalance.write(balance);
     }
 }
--- src/main/java/core/basesyntax/service/FruitTransaction.java
+++ src/main/java/core/basesyntax/service/FruitTransaction.java
@@ -1,4 +1,7 @@
-package core.basesyntax;
+package core.basesyntax.service;
+
+import core.basesyntax.Operation;
+
 /*
     Java objects
  */
