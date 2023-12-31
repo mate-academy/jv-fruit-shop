55d8fbc1bccfdb35458019447574917f879018cb
AdamMudrak <mudrets2000@gmail.com>
2023-12-31 17:28:13 +0200

    Add reader implementation

--- src/main/java/core/basesyntax/services/impl/DataReaderCSVImpl.java
+++ src/main/java/core/basesyntax/services/impl/DataReaderCSVImpl.java
@@ -1,11 +1,24 @@
 package core.basesyntax.services.impl;
 
 import core.basesyntax.services.DataReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class DataReaderCSVImpl implements DataReader {
 
   @Override
   public StringBuilder readFromFileAndHoldData(String fromFileName) {
-    return null;
+    StringBuilder rawData = new StringBuilder();
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+      String line = bufferedReader.readLine();
+      while (line != null) {
+        rawData.append(line).append(",");
+        line = bufferedReader.readLine();
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Can't read from file", e);
+    }
+    return rawData;
   }
 }
