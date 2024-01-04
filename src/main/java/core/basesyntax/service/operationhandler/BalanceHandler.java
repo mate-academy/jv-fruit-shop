65e177e3bde03e9291c9a3f24e8cb2609b2e2563
gulini32 <gulini32@gmail.com>
2024-01-04 16:34:09 +0200

    HW

--- src/main/java/core/basesyntax/service/operationhandler/BalanceHandler.java
+++ src/main/java/core/basesyntax/service/operationhandler/BalanceHandler.java
@@ -6,11 +6,6 @@ import core.basesyntax.model.FruitTransaction;
 public class BalanceHandler implements OperationHandler {
     @Override
     public void handleOperation(FruitTransaction transaction, Storage storage) {
-        Integer currentQuantity = Storage.fruits.get(transaction.getFruit());
-        if (currentQuantity != null) {
-            System.out.println(currentQuantity);
-        } else {
-            System.out.println("No such fruit in storage");
-        }
+        Storage.fruits.put(transaction.getFruit(), transaction.getQuantity());
     }
 }
