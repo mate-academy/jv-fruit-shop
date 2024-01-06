1b30e4aeb6a02d64a08a9443e5b11e2871c16ff6
Adam Mudrak <113807446+AdamMudrak@users.noreply.github.com>
2024-01-06 13:52:29 +0200

    Update src/main/java/core/basesyntax/services/fileprocessing/impl/FileDataReaderCsvImpl.java
    Co-authored-by: Oleh Kuzan <49767049+okuzan@users.noreply.github.com>

--- src/main/java/core/basesyntax/services/fileprocessing/impl/FileDataReaderCsvImpl.java
+++ src/main/java/core/basesyntax/services/fileprocessing/impl/FileDataReaderCsvImpl.java
@@ -12,10 +12,9 @@ public class FileDataReaderCsvImpl implements FileDataReader {
     public List<String> readFromFileAndHoldData(String fromFileName) {
         List<String> rawData = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line = bufferedReader.readLine();
-            while (line != null) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
                 rawData.add(line);
-                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
