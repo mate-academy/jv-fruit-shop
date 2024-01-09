311c0c32432b562652ad46323790523c58d047d6
AdamMudrak <mudrets2000@gmail.com>
2024-01-09 18:11:46 +0200

    Delete iteration from the class, handle negative result and lack of a certain fruit

--- src/main/java/core/basesyntax/services/handlers/impl/PurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/services/handlers/impl/PurchaseOperationHandler.java
@@ -1,6 +1,8 @@
 package core.basesyntax.services.handlers.impl;
 
 import core.basesyntax.db.Storage;
+import core.basesyntax.exceptions.NegativeResultException;
+import core.basesyntax.exceptions.NoSuchFruitFoundException;
 import core.basesyntax.models.FruitTransaction;
 import core.basesyntax.services.handlers.OperationHandler;
 import java.util.Map;
@@ -8,12 +10,16 @@ import java.util.Map;
 public class PurchaseOperationHandler implements OperationHandler {
     @Override
     public void handleOperation(FruitTransaction fruitTransaction) {
-        for (Map.Entry<String, Integer> fruitTypeAndAmount : Storage.getFruitsEntrySet()) {
-            if (fruitTypeAndAmount.getKey().equals(fruitTransaction.getFruit())) {
-                fruitTypeAndAmount.setValue(
-                        fruitTypeAndAmount.getValue() - fruitTransaction.getQuantity());
-                return;
+        Map.Entry<String, Integer> fruit =
+                Storage.iterateAndFindFruits(fruitTransaction.getFruit());
+        if (fruit != null) {
+            int fruitQuantityAfterOperation = fruit.getValue() - fruitTransaction.getQuantity();
+            if (fruitQuantityAfterOperation < 0) {
+                throw new NegativeResultException("Can't purchase more products than available");
             }
+            fruit.setValue(fruitQuantityAfterOperation);
+        } else {
+            throw new NoSuchFruitFoundException("Can't purchase...No such fruit found...");
         }
     }
 }
