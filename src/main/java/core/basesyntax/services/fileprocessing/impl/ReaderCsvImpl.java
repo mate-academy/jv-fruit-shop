947be6b321b2874c03de54080298eb0715745448
AdamMudrak <mudrets2000@gmail.com>
2024-01-09 18:17:59 +0200

    Add filename to the exception

--- src/main/java/core/basesyntax/services/fileprocessing/impl/ReaderCsvImpl.java
+++ src/main/java/core/basesyntax/services/fileprocessing/impl/ReaderCsvImpl.java
@@ -1,6 +1,6 @@
 package core.basesyntax.services.fileprocessing.impl;
 
-import core.basesyntax.services.fileprocessing.Reader;//If I apply your naming, there is a conflict in imports
+import core.basesyntax.services.fileprocessing.Reader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,15 +9,15 @@ import java.util.List;
 
 public class ReaderCsvImpl implements Reader {
     @Override
-    public List<String> readFile(String fromFileName) {
+    public List<String> readFile(String filename) {
         List<String> rawData = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 rawData.add(line);
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read from file", e);
+            throw new RuntimeException("Can't read from file " + filename, e);
         }
         return rawData;
     }
