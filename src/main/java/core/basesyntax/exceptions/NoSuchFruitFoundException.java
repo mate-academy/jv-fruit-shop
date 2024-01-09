b8cdbfc98adbbdbc70e94c97752d5f248ca0ff7d
AdamMudrak <mudrets2000@gmail.com>
2024-01-09 18:11:05 +0200

    Add exceptions for negative purchase operation result and lack of a certain fruit in DB

---
+++ src/main/java/core/basesyntax/exceptions/NoSuchFruitFoundException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exceptions;
+
+public class NoSuchFruitFoundException extends RuntimeException {
+    public NoSuchFruitFoundException(String message) {
+        super(message);
+    }
+}
