f0bb7e26106425e5fdd5481608b5145db8aaf4e1
AdamMudrak <mudrets2000@gmail.com>
2024-01-09 18:08:16 +0200

    Make indexes constants

--- src/main/java/core/basesyntax/services/fileprocessing/impl/TransactionGetterImpl.java
+++ src/main/java/core/basesyntax/services/fileprocessing/impl/TransactionGetterImpl.java
@@ -6,14 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionGetterImpl implements TransactionGetter {
+    private static final int OPERATION_INDEX = 0;
+    private static final int FRUIT_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
+
     @Override
     public List<FruitTransaction> getTransactionsData(List<String[]> dividedData) {
         List<FruitTransaction> fruitTransactions = new ArrayList<>();
         for (String[] strings : dividedData) {
             FruitTransaction fruitTransaction = new FruitTransaction();
-            fruitTransaction.setOperation(fruitTransaction.getOperationFromEnum(strings[0]));
-            fruitTransaction.setFruit(strings[1]);
-            fruitTransaction.setQuantity(Integer.parseInt(strings[2]));
+            fruitTransaction.setOperation(fruitTransaction
+                    .getOperationFromEnum(strings[OPERATION_INDEX]));
+            fruitTransaction.setFruit(strings[FRUIT_INDEX]);
+            fruitTransaction.setQuantity(Integer.parseInt(strings[QUANTITY_INDEX]));
             fruitTransactions.add(fruitTransaction);
         }
         return fruitTransactions;
