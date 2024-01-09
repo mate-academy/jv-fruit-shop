747a7475f12dbfb286551f22fd98487d8443ede2
AdamMudrak <mudrets2000@gmail.com>
2024-01-09 22:07:12 +0200

    Change way of skipping 1 line in the report

--- src/main/java/core/basesyntax/services/fileprocessing/impl/DataSplitterImpl.java
+++ src/main/java/core/basesyntax/services/fileprocessing/impl/DataSplitterImpl.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 public class DataSplitterImpl implements DataSplitter {
     private static final String STRING_SPLITTER = ",";
-    private static final String SKIP_LINE_IF_CONTAINS_TYPE = "type";
+    private static final int OFFSET = 1;
 
     @Override
     public List<String[]> divideData(List<String> rawData) {
         return rawData.stream().map(string -> string.split(STRING_SPLITTER))
-                .filter(strings -> !strings[0].equals(SKIP_LINE_IF_CONTAINS_TYPE)).toList();
+                .skip(OFFSET)
+                .toList();
     }
 }
