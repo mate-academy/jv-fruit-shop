2b9f43f35becc463e0dfba8b2d6d3a3d77610be0
Vadym Stanishevskyi <mainvadym@gmail.com>
2024-01-14 01:15:05 +0100

    added TransactionStrategy and TransactionHandler interfaces with their implementation to apply Strategy pattern

---
+++ src/main/java/core/basesyntax/strategy/handler/TransactionHandler.java
@@ -0,0 +1,9 @@
+package core.basesyntax.strategy.handler;
+
+import core.basesyntax.model.FruitTransaction;
+import java.util.Map;
+
+public interface TransactionHandler {
+    void proceed(Map<String, Integer> map, FruitTransaction transaction);
+
+}
