bcc37b182d0d906074fd2a19b4b76d0db1fa76c7
Slava_Moiseev <qualityengineerinkom@gmail.com>
2024-01-21 17:45:57 +0200

    Finished task, first redaction

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    private static Map<String, Integer> fruitsStorage = new HashMap<>();
+    private static final Map<String, Integer> fruitsStorage = new HashMap<>();
 
     public static Map<String, Integer> getFruitsStorage() {
         return fruitsStorage;
