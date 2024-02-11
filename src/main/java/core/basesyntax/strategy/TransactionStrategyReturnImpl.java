43bd219a0e3a3ae28a5cca93ed4bd98798c91182
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2024-02-11 20:30:20 +0200

    added RuntimeException in TransactionStrategyReturnImpl

--- src/main/java/core/basesyntax/strategy/TransactionStrategyReturnImpl.java
+++ src/main/java/core/basesyntax/strategy/TransactionStrategyReturnImpl.java
@@ -6,16 +6,20 @@ public class TransactionStrategyReturnImpl implements TransactionStrategy {
     @Override
     public Integer balanceUpdater(String fruitName, int quantity) {
         Storage storage = new Storage();
-        int result = storage.getFruitsTransactions().get(fruitName) + quantity;
-        if (result < 0) {
-            throw new RuntimeException("Balance couldn't be less '0' "
-                    + "after returned: balance " + fruitName + " = " + result);
-        } else if (quantity < 0) {
-            throw new RuntimeException("Returned quantity couldn't be less '0'.\n"
-                    + "Invalid data received from input file: return "
-                    + fruitName + " = " + quantity);
+        if (storage.getFruitsTransactions().get(fruitName) != null) {
+            int result = storage.getFruitsTransactions().get(fruitName) + quantity;
+            if (result < 0) {
+                throw new RuntimeException("Balance couldn't be less '0' "
+                        + "after returned: balance " + fruitName + " = " + result);
+            } else if (quantity < 0) {
+                throw new RuntimeException("Returned quantity couldn't be less '0'.\n"
+                        + "Invalid data received from input file: return "
+                        + fruitName + " = " + quantity);
+            }
+            storage.getFruitsTransactions().put(fruitName, result);
+            return result;
         }
-        storage.getFruitsTransactions().put(fruitName, result);
-        return result;
+        throw new RuntimeException("This fruit: " + fruitName
+                + " has not been sold and cannot be returned.");
     }
 }
