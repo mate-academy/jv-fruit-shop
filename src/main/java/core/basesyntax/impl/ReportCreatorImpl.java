84c4e3eb745c9a50fe6a4e60369f36ca76ac9447
petro feniak <petro.fenyak@gmail.com>
2024-03-12 23:24:08 +0200

    forms data after processing for writing to file

---
+++ src/main/java/core/basesyntax/impl/ReportCreatorImpl.java
@@ -0,0 +1,22 @@
+package core.basesyntax.impl;
+
+import core.basesyntax.servise.ReportCreator;
+import java.util.Map;
+
+public class ReportCreatorImpl implements ReportCreator {
+    private static final String TITLE_ROW = "\t" + "fruit,quantity" + "\n";
+    private static final String ROW_SEPARATOR = ",";
+
+    @Override
+    public String reportCreator(Map<String, Integer> inputData) {
+        StringBuilder builderReportRow = new StringBuilder(TITLE_ROW);
+        for (Map.Entry<String, Integer> inputDataEntry : inputData.entrySet()) {
+            builderReportRow.append("\t")
+                    .append(inputDataEntry.getKey())
+                    .append(ROW_SEPARATOR)
+                    .append(inputDataEntry.getValue())
+                    .append(System.lineSeparator());
+        }
+        return builderReportRow.toString();
+    }
+}
---
+++ src/main/java/core/basesyntax/servise/ReportCreator.java
@@ -0,0 +1,7 @@
+package core.basesyntax.servise;
+
+import java.util.Map;
+
+public interface ReportCreator {
+    String reportCreator(Map<String, Integer> inputData);
+}
