e073923d65cf37492c2e4b496efcb09237922185
petro feniak <petro.fenyak@gmail.com>
2024-03-13 21:55:46 +0200

    implements logic for the operation type return

---
+++ src/main/java/core/basesyntax/strategy/ReturnOperationHandler.java
@@ -0,0 +1,13 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.db.FruitStorage;
+import core.basesyntax.model.FruitTransaction;
+
+public class ReturnOperationHandler implements OperationHandler {
+    @Override
+    public void fruitTransactionHandler(FruitTransaction transaction) {
+        int quantityInTransaction = transaction.getQuantity();
+        FruitStorage.fruitTransactionStorage.put(transaction.getFruit(),
+                FruitStorage.getQuantity(transaction) + quantityInTransaction);
+    }
+}
