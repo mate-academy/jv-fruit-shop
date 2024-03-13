3bf0a748ce3ca5414085e512fcf6887534977658
petro feniak <petro.fenyak@gmail.com>
2024-03-13 21:53:28 +0200

    is a generalization for all handler

---
+++ src/main/java/core/basesyntax/strategy/OperationHandler.java
@@ -0,0 +1,8 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.model.FruitTransaction;
+
+public interface OperationHandler {
+    void fruitTransactionHandler(FruitTransaction transaction);
+
+}
