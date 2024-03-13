95959ff4bec344014953d87d0ce19ba63877cbb5
petro feniak <petro.fenyak@gmail.com>
2024-03-13 21:47:12 +0200

    declares a method to handle all transactions

---
+++ src/main/java/core/basesyntax/servise/TransactionProcessor.java
@@ -0,0 +1,11 @@
+package core.basesyntax.servise;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.strategy.OperationHandler;
+import java.util.List;
+import java.util.Map;
+
+public interface TransactionProcessor {
+    void transactionProcessor(List<FruitTransaction> fruitTransactionsList,
+                              Map<FruitTransaction.Operation, OperationHandler> strategy);
+}
