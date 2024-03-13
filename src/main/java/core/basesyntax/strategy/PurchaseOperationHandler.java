e9c2a757ac38d914be307c69765ff93872ea13dd
petro feniak <petro.fenyak@gmail.com>
2024-03-13 21:55:37 +0200

    implements logic for the operation type purchase

---
+++ src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
@@ -0,0 +1,20 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.db.FruitStorage;
+import core.basesyntax.model.FruitTransaction;
+
+public class PurchaseOperationHandler implements OperationHandler {
+    @Override
+    public void fruitTransactionHandler(FruitTransaction transaction) {
+        int quantityInTransaction = transaction.getQuantity();
+        if (quantityInTransaction <= FruitStorage.getQuantity(transaction)) {
+            FruitStorage.fruitTransactionStorage.put(transaction.getFruit(),
+                    FruitStorage.getQuantity(transaction) - quantityInTransaction);
+        } else {
+            throw new RuntimeException("Not enough quantity for sell: fruit - "
+                    + transaction.getFruit()
+                    + " | quantity - "
+                    + transaction.getQuantity());
+        }
+    }
+}
