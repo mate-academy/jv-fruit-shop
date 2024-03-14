3101d0e7cd9fb234bcfbbd7986ae2ab5be029dc9
petro feniak <petro.fenyak@gmail.com>
2024-03-14 19:30:16 +0200

    used Map.of() method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -7,27 +7,26 @@ import core.basesyntax.impl.ReaderServiceImplFromCsv;
 import core.basesyntax.impl.ReportCreatorImpl;
 import core.basesyntax.impl.WriterServiceImplToCsv;
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.servise.ReaderService;
-import core.basesyntax.servise.ReportCreator;
-import core.basesyntax.servise.TransactionParser;
-import core.basesyntax.servise.TransactionProcessor;
-import core.basesyntax.servise.WriterService;
+import core.basesyntax.service.ReaderService;
+import core.basesyntax.service.ReportCreator;
+import core.basesyntax.service.TransactionParser;
+import core.basesyntax.service.TransactionProcessor;
+import core.basesyntax.service.WriterService;
 import core.basesyntax.strategy.BalanceOperationHandler;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.PurchaseOperationHandler;
 import core.basesyntax.strategy.ReturnOperationHandler;
 import core.basesyntax.strategy.SupplyOperationHandler;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<FruitTransaction.Operation, OperationHandler> strategy = new HashMap<>();
-        strategy.put(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler());
-        strategy.put(FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler());
-        strategy.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler());
-        strategy.put(FruitTransaction.Operation.RETURN, new ReturnOperationHandler());
+        Map<FruitTransaction.Operation, OperationHandler> strategy =
+                Map.of(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler(),
+                FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler(),
+                FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler(),
+                FruitTransaction.Operation.RETURN, new ReturnOperationHandler());
 
         ReaderService readerFromFile = new ReaderServiceImplFromCsv();
 
