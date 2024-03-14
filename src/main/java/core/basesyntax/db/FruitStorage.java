767aa4c907f75cc9c9aa011a0df009417ca43c40
petro feniak <petro.fenyak@gmail.com>
2024-03-14 19:31:10 +0200

    method getQuantity input type String

--- src/main/java/core/basesyntax/db/FruitStorage.java
+++ src/main/java/core/basesyntax/db/FruitStorage.java
@@ -1,6 +1,5 @@
 package core.basesyntax.db;
 
-import core.basesyntax.model.FruitTransaction;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -8,14 +7,15 @@ import java.util.stream.IntStream;
 public class FruitStorage {
     public static final Map<String, Integer> fruitTransactionStorage = new HashMap<>();
 
-    public static int getQuantity(FruitTransaction transaction) {
+    public static int getQuantity(String fruit) {
         return fruitTransactionStorage.entrySet().stream()
-                .filter(k -> k.getKey().equals(transaction.getFruit()))
+                .filter(k -> k.getKey().equals(fruit))
                 .flatMapToInt(v ->
                         IntStream.of(v.getValue()))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Can not find quantity of fruit :"
-                        + transaction.getFruit()
+                        + fruit
                         + " in database"));
+
     }
 }
