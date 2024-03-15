6e7672f26d4bc0b0b80c17a4d4924041224b73b6
petro feniak <petro.fenyak@gmail.com>
2024-03-15 22:33:33 +0200

    fixed problem with if else block

--- src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/strategy/PurchaseOperationHandler.java
@@ -7,15 +7,14 @@ public class PurchaseOperationHandler implements OperationHandler {
     @Override
     public void handlerOperation(FruitTransaction transaction) {
         int quantityInTransaction = transaction.getQuantity();
-        if (quantityInTransaction <= FruitStorage.getQuantity(transaction.getFruit())) {
-            FruitStorage.fruitTransactionStorage.put(transaction.getFruit(),
-                    FruitStorage.getQuantity(transaction.getFruit())
-                            - quantityInTransaction);
+        if (quantityInTransaction > FruitStorage.getQuantity(transaction.getFruit())) {
+            throw new RuntimeException("Not enough quantity for sell: fruit - "
+                    + transaction.getFruit()
+                    + " | quantity - "
+                    + transaction.getQuantity());
         }
-        throw new RuntimeException("Not enough quantity for sell: fruit - "
-                + transaction.getFruit()
-                + " | quantity - "
-                + transaction.getQuantity());
-
+        FruitStorage.fruitTransactionStorage.put(transaction.getFruit(),
+                FruitStorage.getQuantity(transaction.getFruit())
+                        - quantityInTransaction);
     }
 }
