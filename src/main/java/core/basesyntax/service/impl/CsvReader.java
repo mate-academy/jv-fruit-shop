7552c03048a6c434c75c0aed7a1bfa1c4da6d22a
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-03-17 09:38:38 +0200

    implemented the DataReader interface, CsvReader implementation with the read()

---
+++ src/main/java/core/basesyntax/service/impl/CsvReader.java
@@ -0,0 +1,20 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.DataReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CsvReader implements DataReader {
+    private static final String READING_FAILURE_MESSAGE = "Failed to read from the file";
+
+    @Override
+    public List<String> read(String fileName) {
+        try {
+            return Files.readAllLines(Paths.get(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(READING_FAILURE_MESSAGE + fileName, e);
+        }
+    }
+}
