ca23f6e0d33d46a7ebb0ed596f443f0249ef881b
makaroshyna <katya23225@gmail.com>
2024-03-18 23:20:07 +0200

    Write report in main().

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,9 +5,13 @@ import core.basesyntax.dao.FruitDaoImpl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.model.Operation;
 import core.basesyntax.service.DataParser;
+import core.basesyntax.service.ReportCreator;
 import core.basesyntax.service.ReportReader;
+import core.basesyntax.service.ReportWriter;
 import core.basesyntax.service.impl.DataParserImpl;
+import core.basesyntax.service.impl.ReportCreatorImpl;
 import core.basesyntax.service.impl.ReportReaderImpl;
+import core.basesyntax.service.impl.ReportWriterImpl;
 import core.basesyntax.strategy.BalanceStrategy;
 import core.basesyntax.strategy.DecreaseStrategy;
 import core.basesyntax.strategy.FruitStrategy;
@@ -17,24 +21,27 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-
-    private static final String PATHNAME = "src/main/resources/data.csv";
+    private static final String INPUT_PATHNAME = "src/main/resources/data.csv";
+    private static final String REPORT_PATHNAME = "src/main/resources/report.csv";
     private static Map<Operation, OperationHandler> strategyMap;
 
     public static void main(String[] args) {
-        // report reading - done
         ReportReader reportReader = new ReportReaderImpl();
-        List<String> readFile = reportReader.readFile(PATHNAME);
-        // process data - done
+        List<String> readFile = reportReader.readFile(INPUT_PATHNAME);
+
         DataParser dataParser = new DataParserImpl();
         List<FruitTransaction> fruitTransactions = dataParser.processAll(readFile);
-        // apply strategy
+
         FruitDao fruitDao = new FruitDaoImpl();
         mapBuilder(fruitDao);
         FruitStrategy fruitStrategy = new FruitStrategy(strategyMap);
         fruitStrategy.processData(fruitTransactions);
-        // form a report
-        // write a report
+
+        ReportCreator reportCreator = new ReportCreatorImpl();
+        String report = reportCreator.createReport(fruitDao.getFruits());
+
+        ReportWriter reportWriter = new ReportWriterImpl();
+        reportWriter.writeReport(report, REPORT_PATHNAME);
     }
 
     private static void mapBuilder(FruitDao fruitDao) {
