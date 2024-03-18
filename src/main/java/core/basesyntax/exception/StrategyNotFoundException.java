a32de90e371f5556657f6d6fd91be21520628c5f
Valentyn Sharshon <woosay333@gmail.com>
2024-03-18 23:02:29 +0200

    created StrategyNotFoundException class

---
+++ src/main/java/core/basesyntax/exception/StrategyNotFoundException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class StrategyNotFoundException extends RuntimeException {
+    public StrategyNotFoundException(String message) {
+        super(message);
+    }
+}
