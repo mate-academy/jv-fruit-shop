02ba392c90db94a811df3a86aa826b50618b1a49
makaroshyna <katya23225@gmail.com>
2024-03-18 23:16:35 +0200

    Implement report creating class.

---
+++ src/test/java/core/basesyntax/service/impl/ReportCreatorImplTest.java
@@ -0,0 +1,33 @@
+package core.basesyntax.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import core.basesyntax.service.ReportCreator;
+import java.util.Map;
+import java.util.TreeMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReportCreatorImplTest {
+    private ReportCreator reportCreator;
+
+    @BeforeEach
+    void setUp() {
+        reportCreator = new ReportCreatorImpl();
+    }
+
+    @Test
+    void createReport_ok() {
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("banana", 20);
+        map.put("apple", 50);
+
+        String actual = reportCreator.createReport(map);
+        String expected = """
+                fruit,quantity
+                apple,50
+                banana,20
+                """;
+        assertEquals(expected, actual);
+    }
+}
