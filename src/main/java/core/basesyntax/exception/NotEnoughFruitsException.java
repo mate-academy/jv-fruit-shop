c61da132b3b68925fd064f9d0a13786d64a016fd
Valentyn Sharshon <woosay333@gmail.com>
2024-03-18 23:02:37 +0200

    created NotEnoughFruitsException class

---
+++ src/main/java/core/basesyntax/exception/NotEnoughFruitsException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class NotEnoughFruitsException extends RuntimeException {
+    public NotEnoughFruitsException(String message) {
+        super(message);
+    }
+}
