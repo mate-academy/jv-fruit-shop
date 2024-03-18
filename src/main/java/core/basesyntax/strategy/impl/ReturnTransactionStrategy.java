9d8cfcfd621e861de5ec91003c034a99e5cec5f0
Valentyn Sharshon <woosay333@gmail.com>
2024-03-18 23:05:06 +0200

    created ReturnTransactionStrategy class with processData method implementation

---
+++ src/main/java/core/basesyntax/strategy/impl/ReturnTransactionStrategy.java
@@ -0,0 +1,15 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.strategy.TransactionStrategy;
+
+public class ReturnTransactionStrategy implements TransactionStrategy {
+    @Override
+    public void processData(FruitTransaction transaction) {
+        int currentQuantity = fruitDao.getQuantity(transaction.getFruit());
+        fruitDao.setQuantity(
+                transaction.getFruit(),
+                currentQuantity + transaction.getQuantity()
+        );
+    }
+}
