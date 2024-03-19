f65b19915edf9871a9b8a476b8e9f08f6c6a85b6
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-03-19 14:18:25 +0200

    Total mess

--- src/main/java/core/basesyntax/service/strategy/impl/BalanceOperationHandler.java
+++ src/main/java/core/basesyntax/service/strategy/impl/BalanceOperationHandler.java
@@ -1,16 +1,17 @@
 package core.basesyntax.service.strategy.impl;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.dto.FruitTransactionDto;
 import core.basesyntax.service.strategy.OperationHandler;
 
 public class BalanceOperationHandler implements OperationHandler {
     @Override
     public void apply(FruitTransactionDto dto) {
-
+        Storage.fruitsQuantity.put(dto.fruitName(), dto.quantity());
     }
 
     @Override
     public boolean isApplicable(FruitTransactionDto dto) {
-        return false;
+        return "b".equals(dto.operationType());
     }
 }
