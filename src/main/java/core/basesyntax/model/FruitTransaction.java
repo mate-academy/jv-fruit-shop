d2230b939e4a5c4b0c9dcc2668e7ef6ba2dfe179
Valentyn Sharshon <woosay333@gmail.com>
2024-03-19 16:36:38 +0200

    made FruitTransaction a record

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -2,28 +2,7 @@ package core.basesyntax.model;
 
 import java.util.Arrays;
 
-public class FruitTransaction {
-    private final Operation operation;
-    private final String fruit;
-    private final int quantity;
-
-    public FruitTransaction(Operation operation, String fruit, int quantity) {
-        this.operation = operation;
-        this.fruit = fruit;
-        this.quantity = quantity;
-    }
-
-    public Operation getOperation() {
-        return operation;
-    }
-
-    public String getFruit() {
-        return fruit;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
+public record FruitTransaction(Operation operation, String fruit, int quantity) {
 
     public enum Operation {
         BALANCE("b"),
