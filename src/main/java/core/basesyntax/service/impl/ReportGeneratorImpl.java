1610c2838f9dd2a253f25c19d8c5127cbd39b23d
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-03-21 17:59:41 +0200

    added changes according to review part 3

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReportGeneratorImpl implements ReportGenerator {
+    private static final String SEPARATOR = ",";
+    private static final String TITLE = "fruit,quantity";
     private Storage storage;
 
     public ReportGeneratorImpl(Storage storage) {
@@ -15,8 +17,8 @@ public class ReportGeneratorImpl implements ReportGenerator {
     @Override
     public List<String> generate() {
         List<String> formattedList = new ArrayList<>();
-        formattedList.add("fruit,quantity");
-        storage.getFruits().forEach((fruit, quantity) -> formattedList.add(fruit + "," + quantity));
+        formattedList.add(TITLE);
+        storage.getFruits().forEach((fruit, quantity) -> formattedList.add(fruit + SEPARATOR + quantity));
         return formattedList;
     }
 }
