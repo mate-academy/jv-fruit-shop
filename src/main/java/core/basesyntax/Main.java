48bc9a9726f97675ea5fff46f518426dd2b59f16
Владислав Чабак <vlad.chabak@gmail.com>
2024-03-29 15:35:26 +0000

    have no idea how to separate this: String[] meaning = line.split(",");

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,6 +8,7 @@ import core.basesyntax.service.impl.FruitShopServiceImpl;
 public class Main {
     private static final String PATH = "src/main/java/core/basesyntax/db/data.csv";
     private static final String REPORT = "src/main/java/core/basesyntax/db/report.csv";
+
     public static void main(String[] args) {
         FileWriterService writer = new FileWriterServiceImpl();
         FruitShopService transactions = new FruitShopServiceImpl();
