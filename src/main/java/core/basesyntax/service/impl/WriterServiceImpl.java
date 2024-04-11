c6f1362c20a02efaaa99174d4239589cb96ce78d
MarynaBodina <bananafox2002@gmail.com>
2024-04-11 14:43:08 +0300

    "task completed"

--- src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
@@ -8,10 +8,9 @@ import java.nio.file.Path;
 public class WriterServiceImpl implements WriterService {
     @Override
     public void writeDataToFile(String data, String filePath) {
-        try {
-            FileWriter fileWriter = new FileWriter(Path.of(filePath).toFile());
+        try (FileWriter fileWriter = new FileWriter(Path.of(filePath).toFile())) {
+            FileWriter writer = new FileWriter(Path.of(filePath).toFile());
             fileWriter.write(data);
-            fileWriter.close();
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + filePath, e);
         }
