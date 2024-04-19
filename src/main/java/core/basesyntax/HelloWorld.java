9cb34f64e884108d5785e61457773afb051f485d
Vlad <mazafackery@gmail.com>
2024-04-19 19:04:22 +0300

    commit with HelloWorld

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,9 +1,4 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class HelloWorld {
-    // HINT: In the `public static void main(String[] args)` it is better to create instances of your classes, 
-    // and call their methods, but do not write any business logic in the `main` method!
 }
