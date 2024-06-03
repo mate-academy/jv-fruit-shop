cef9332c2b39a4b7258d933463d7ba65a5bcbeba
Yarolsav <kiritok712@gmail.com>
2024-06-03 15:21:10 +0200

    make_literal_to_constant

--- src/main/java/core/basesyntax/DataConverterImpl.java
+++ src/main/java/core/basesyntax/DataConverterImpl.java
@@ -5,15 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataConverterImpl implements DataConverter {
+    private static final String COMMA = ",";
+    private static final int START_INDEX = 1;
+    private static final int OPERATION_INDEX = 0;
+    private static final int FRUIT_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
+
     @Override
     public List<FruitTransaction> convertToTransaction(List<String> data) {
         List<FruitTransaction> transactions = new ArrayList<>();
-        for (String line : data.subList(1, data.size())) {
-            String[] parts = line.split(",");
+        for (String line : data.subList(START_INDEX, data.size())) {
+            String[] parts = line.split(COMMA);
             FruitTransaction.Operation operation
-                    = FruitTransaction.Operation.valueOf(parts[0].toUpperCase());
-            String fruit = parts[1];
-            int quantity = Integer.parseInt(parts[2]);
+                    = FruitTransaction.Operation.valueOf(parts[OPERATION_INDEX].toUpperCase());
+            String fruit = parts[FRUIT_INDEX];
+            int quantity = Integer.parseInt(parts[QUANTITY_INDEX]);
             transactions.add(new FruitTransaction(operation, fruit, quantity));
         }
         return transactions;
