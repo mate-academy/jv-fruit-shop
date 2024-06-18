84ad0b93f141cf15891527983b55b55e867c8096
mgsorigo <marek.gucma@sorigo.pl>
2024-06-19 00:32:40 +0200

    fix

--- src/main/java/core/basesyntax/strategy/SupplyOperation.java
+++ src/main/java/core/basesyntax/strategy/SupplyOperation.java
@@ -6,6 +6,10 @@ import java.util.Map;
 public class SupplyOperation implements OperationHandler {
     @Override
     public void handle(FruitTransaction transaction, Map<String, Integer> inventory) {
+        if (transaction.getQuantity() < 0) {
+            throw new RuntimeException(
+                    "Invalid supply quantity for fruit: " + transaction.getFruit());
+        }
         inventory.put(transaction.getFruit(),
                 inventory.getOrDefault(transaction.getFruit(), 0) + transaction.getQuantity());
     }
