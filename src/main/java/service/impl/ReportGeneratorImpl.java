56d0e0b8c243429acd987b0e324b22eaa21b9c04
Anton <panyutin15@gmail.com>
2024-06-20 14:56:59 +0300

    fixed code after Elena-Bruyako recomendations

--- src/main/java/service/impl/ReportGeneratorImpl.java
+++ src/main/java/service/impl/ReportGeneratorImpl.java
@@ -5,14 +5,17 @@ import java.util.Map;
 import service.ReportGenerator;
 
 public class ReportGeneratorImpl implements ReportGenerator {
+    private static final String FIRST_LINE_OF_REPORT = "fruit,quantity";
+    private static final String COMMA_SIGN = ",";
+
     @Override
     public String getReport() {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("fruit,quantity").append(System.lineSeparator());
+        stringBuilder.append(FIRST_LINE_OF_REPORT).append(System.lineSeparator());
 
         for (Map.Entry<String, Integer> pair : Storage.reports.entrySet()) {
             stringBuilder.append(pair.getKey())
-                    .append(",")
+                    .append(COMMA_SIGN)
                     .append(pair.getValue())
                     .append(System.lineSeparator());
         }
--- src/main/java/strategy/impl/BalanceOperation.java
+++ src/main/java/strategy/impl/BalanceOperation.java
@@ -5,6 +5,9 @@ import strategy.OperationHandler;
 public class BalanceOperation implements OperationHandler {
     @Override
     public int apply(int quantity) {
-        return quantity;
+        if (quantity >= 0) {
+            return quantity;
+        }
+        throw new RuntimeException("alance can't be negative, in file balance = " + quantity);
     }
 }
