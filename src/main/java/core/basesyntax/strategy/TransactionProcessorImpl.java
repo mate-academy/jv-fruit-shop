088688323a7d1bd1833582181b86397d88ba1014
rus1980707 <rus1980707@gmail.com>
2024-06-23 12:01:23 +0300

    jv-fruit-shop fixed

--- src/main/java/core/basesyntax/strategy/TransactionProcessorImpl.java
+++ src/main/java/core/basesyntax/strategy/TransactionProcessorImpl.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TransactionProcessorImpl implements TrabnsactionProcessor {
+public class TransactionProcessorImpl implements TransactionProcessor {
 
     @Override
     public Map<String, Integer> process(List<FruitTransaction> transactions) {
