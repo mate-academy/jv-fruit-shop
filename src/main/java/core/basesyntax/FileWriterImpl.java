f26dc58b67f6bd324ea7907c4abb5df69b1d88bd
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 08:23:23 +0300

    FileWriterImpl.java

--- src/main/java/core/basesyntax/FileWriterImpl.java
+++ src/main/java/core/basesyntax/FileWriterImpl.java
@@ -1,11 +1,10 @@
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileWriterImpl implements FileWriter {
     @Override
     public void write(String data, String filePath) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+        try (BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(filePath))) {
             writer.write(data);
         } catch (IOException e) {
             e.printStackTrace();
