7b1f7e1ab73d2af1df673d6b1ac193c70b515582
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 08:33:49 +0300

    PurchaseOperation.java

--- src/main/java/core/basesyntax/PurchaseOperation.java
+++ src/main/java/core/basesyntax/PurchaseOperation.java
@@ -1,15 +1,15 @@
 import java.util.Map;
 
-public class ReturnOperation implements OperationHandler {
+public class PurchaseOperation implements OperationHandler {
     private final Map<String, Integer> fruitStorage;
 
-    public ReturnOperation(Map<String, Integer> fruitStorage) {
+    public PurchaseOperation(Map<String, Integer> fruitStorage) {
         this.fruitStorage = fruitStorage;
     }
 
     @Override
     public void apply(FruitTransaction transaction) {
         fruitStorage.put(transaction.getFruit(), 
-                fruitStorage.getOrDefault(transaction.getFruit(), 0) + transaction.getQuantity());
+                fruitStorage.getOrDefault(transaction.getFruit(), 0) - transaction.getQuantity());
     }
 }
