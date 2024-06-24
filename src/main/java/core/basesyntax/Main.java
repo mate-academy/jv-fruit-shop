24009ee994c8274efaf456c97d1ac7099fea1b68
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 17:13:00 +0300

    Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,23 +3,23 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
+    private static final String INPUT_FILE_PATH = "reportToRead.csv";
+    private static final String OUTPUT_FILE_PATH = "finalReport.csv";
+
     public static void main(String[] args) {
         FileReader fileReader = new FileReaderImpl();
-        List<String> inputReport = fileReader.read("reportToRead.csv");
+        List<String> inputReport = fileReader.read(INPUT_FILE_PATH);
 
         DataConverter dataConverter = new DataConverterImpl();
         final List<FruitTransaction> transactions = dataConverter.convertToTransaction(inputReport);
 
-        Map<FruitTransaction.Operation, OperationHandler> operationHandlers = new HashMap<>();
         Map<String, Integer> fruitStorage = new HashMap<>();
-        operationHandlers.put(FruitTransaction.Operation.BALANCE, 
-                new BalanceOperation(fruitStorage));
-        operationHandlers.put(FruitTransaction.Operation.PURCHASE, 
-                new PurchaseOperation(fruitStorage));
-        operationHandlers.put(FruitTransaction.Operation.RETURN, 
-                new ReturnOperation(fruitStorage));
-        operationHandlers.put(FruitTransaction.Operation.SUPPLY, 
-                new SupplyOperation(fruitStorage));
+        Map<FruitTransaction.Operation, OperationHandler> operationHandlers = Map.of(
+                FruitTransaction.Operation.BALANCE, new BalanceOperation(fruitStorage),
+                FruitTransaction.Operation.PURCHASE, new PurchaseOperation(fruitStorage),
+                FruitTransaction.Operation.RETURN, new ReturnOperation(fruitStorage),
+                FruitTransaction.Operation.SUPPLY, new SupplyOperation(fruitStorage)
+        );
         OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlers);
 
         ShopService shopService = new ShopServiceImpl(operationStrategy);
@@ -29,6 +29,6 @@ public class Main {
         String resultingReport = reportGenerator.getReport();
 
         FileWriter fileWriter = new FileWriterImpl();
-        fileWriter.write(resultingReport, "finalReport.csv");
+        fileWriter.write(resultingReport, OUTPUT_FILE_PATH);
     }
 }
