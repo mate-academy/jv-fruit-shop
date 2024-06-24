0c2fffad325c223a41d43d7edf0841f443fb06fe
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 17:11:44 +0300

    FileWriterImpl.java

--- src/main/java/core/basesyntax/FileWriterImpl.java
+++ src/main/java/core/basesyntax/FileWriterImpl.java
@@ -1,13 +1,17 @@
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileWriterImpl implements FileWriter {
+    private static final Logger LOGGER = Logger.getLogger(FileWriterImpl.class.getName());
+
     @Override
     public void write(String data, String filePath) {
-        try (BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(filePath))) {
-            writer.write(data);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new java.io.FileWriter(filePath))) {
+            bufferedWriter.write(data);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error writing to file: " + filePath, e);
         }
     }
 }
