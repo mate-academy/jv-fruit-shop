f003fada49e0b36d4a1605c0685e19509331cac2
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 07:51:46 +0300

    FruitTransaction.java

---
+++ src/main/java/core/basesyntax/FruitTransaction.java
@@ -0,0 +1,22 @@
+public class FruitTransaction {
+    private Operation operation;
+    private String fruit;
+    private int quantity;
+
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private String code;
+
+        Operation(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+}
