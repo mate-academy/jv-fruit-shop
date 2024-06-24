37ab10b691476ce12992d0b141242bd693cdf53b
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 08:06:37 +0300

    ShopServiceImpl.java

---
+++ src/main/java/core/basesyntax/ShopServiceImpl.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class ShopServiceImpl implements ShopService {
+    private final OperationStrategy operationStrategy;
+
+    public ShopServiceImpl(OperationStrategy operationStrategy) {
+        this.operationStrategy = operationStrategy;
+    }
+
+    @Override
+    public void process(List<FruitTransaction> transactions) {
+        for (FruitTransaction transaction : transactions) {
+            OperationHandler handler = operationStrategy.getHandler(transaction.getOperation());
+            handler.apply(transaction);
+        }
+    }
+}
