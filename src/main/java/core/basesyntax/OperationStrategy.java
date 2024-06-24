90b133b5fa99aeea0f3bd7934e05d1a70349eba7
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 19:15:57 +0300

    OperationStrategy.java

--- src/main/java/core/basesyntax/OperationStrategy.java
+++ src/main/java/core/basesyntax/OperationStrategy.java
@@ -1,3 +1,8 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.handler.OperationHandler;
+import core.basesyntax.model.FruitTransaction;
+
 public interface OperationStrategy {
     OperationHandler getHandler(FruitTransaction.Operation operation);
 }
