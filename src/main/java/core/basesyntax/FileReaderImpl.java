c1fc1999da0ad503f1bbaa4706a9aa221db28772
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 17:11:20 +0300

    FileReaderImpl.java

--- src/main/java/core/basesyntax/FileReaderImpl.java
+++ src/main/java/core/basesyntax/FileReaderImpl.java
@@ -2,18 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileReaderImpl implements FileReader {
+    private static final Logger LOGGER = Logger.getLogger(FileReaderImpl.class.getName());
+
     @Override
     public List<String> read(String filePath) {
         List<String> lines = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new java.io.FileReader(filePath))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(filePath))) {
             String line;
-            while ((line = br.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 lines.add(line);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error reading file: " + filePath, e);
         }
         return lines;
     }
