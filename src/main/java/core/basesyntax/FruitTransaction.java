e0dac3a02041fd3a9f39f05b51181fbdc613e467
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 07:56:46 +0300

    FruitTransaction.java

--- src/main/java/core/basesyntax/FruitTransaction.java
+++ src/main/java/core/basesyntax/FruitTransaction.java
@@ -3,6 +3,30 @@ public class FruitTransaction {
     private String fruit;
     private int quantity;
 
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public enum Operation {
         BALANCE("b"),
         SUPPLY("s"),
