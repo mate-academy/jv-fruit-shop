00fdc554c22a734de03155fbf7f703638c437fc8
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 08:04:44 +0300

    OperationStrategyImpl.java

---
+++ src/main/java/core/basesyntax/OperationStrategyImpl.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+
+public class OperationStrategyImpl implements OperationStrategy {
+    private final Map<FruitTransaction.Operation, OperationHandler> operationHandlers;
+
+    public OperationStrategyImpl(Map<FruitTransaction.Operation, OperationHandler> operationHandlers) {
+        this.operationHandlers = operationHandlers;
+    }
+
+    @Override
+    public OperationHandler getHandler(FruitTransaction.Operation operation) {
+        return operationHandlers.get(operation);
+    }
+}
