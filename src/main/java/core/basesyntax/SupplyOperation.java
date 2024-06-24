a05ef840e9a18e610f2b02c93b5e496a26ab7113
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-24 08:01:07 +0300

    SupplyOperation.java

---
+++ src/main/java/core/basesyntax/SupplyOperation.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class SupplyOperation implements OperationHandler {
+    private final Map<String, Integer> fruitStorage;
+
+    public SupplyOperation(Map<String, Integer> fruitStorage) {
+        this.fruitStorage = fruitStorage;
+    }
+
+    @Override
+    public void apply(FruitTransaction transaction) {
+        fruitStorage.put(transaction.getFruit(), 
+            fruitStorage.getOrDefault(transaction.getFruit(), 0) + transaction.getQuantity());
+    }
+}
