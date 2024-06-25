fb90518707b10ccf019920911a487377b6664bd9
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-25 12:40:14 +0300

    Delete src/main/java/core/basesyntax/core.basesyntax.service directory

--- src/main/java/core/basesyntax/core.basesyntax.service/ShopService.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service;
-
-import core.basesyntax.model.FruitTransaction;
-import java.util.List;
-
-public interface ShopService {
-    void process(List<FruitTransaction> transactions);
-}
