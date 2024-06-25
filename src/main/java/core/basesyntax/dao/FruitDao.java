55a7175a095b2bace1feb942f564b44d15e19a69
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-25 12:42:49 +0300

    FruitDao.java

---
+++ src/main/java/core/basesyntax/dao/FruitDao.java
@@ -0,0 +1,9 @@
+package core.basesyntax.dao;
+
+import core.basesyntax.model.FruitTransaction;
+import java.util.List;
+
+public interface FruitDao {
+    void add(FruitTransaction transaction);
+    List<FruitTransaction> getAll();
+}
