2a742240b4f7951703fecef09f748de6753821ee
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-25 11:51:35 +0300

    Delete src/main/java/core/basesyntax/DataConverter.java

--- src/main/java/core/basesyntax/DataConverter.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.service;
-
-import core.basesyntax.model.FruitTransaction;
-import java.util.List;
-
-public interface DataConverter {
-    List<FruitTransaction> convertToTransaction(List<String> data);
-}
