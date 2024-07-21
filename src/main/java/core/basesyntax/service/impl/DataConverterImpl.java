089ebae843109691a1f76abbfdc68155c134908c
Kate Drevilo <rabmargo@gmail.com>
2024-07-21 13:59:54 +0300

    New fixed mistakes fruit-shop. Fixed public class Main, refactoring methods convertToTransaction and parse

--- src/main/java/core/basesyntax/service/impl/DataConverterImpl.java
+++ src/main/java/core/basesyntax/service/impl/DataConverterImpl.java
@@ -6,36 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataConverterImpl implements DataConverter {
-    private static final int INDEX_OPERATION = 0;
-    private static final int INDEX_FRUIT = 1;
-    private static final int INDEX_QUANTITY = 2;
     private final Parser parser = new Parser();
 
     @Override
     public List<FruitTransaction> convertToTransaction(List<String> inputReport) {
-        List<FruitTransaction> transactions = new ArrayList<>();
-        List<String[]> parsedDate = parser.parse(inputReport);
-
-        for (String[] parts : parsedDate) {
-            FruitTransaction.Operation operation = FruitTransaction.Operation
-                    .fromCode(parts[INDEX_OPERATION]);
-            if (operation == null) {
-                throw new RuntimeException("Invalid operation: " + parts[INDEX_OPERATION]);
-            }
-            String fruit = parts[INDEX_FRUIT];
-            int quantity;
-            try {
-                quantity = Integer.parseInt(parts[INDEX_QUANTITY]);
-            } catch (NumberFormatException e) {
-                throw new RuntimeException("Invalid quantity: " + parts[INDEX_QUANTITY], e);
-            }
-
-            if (quantity < 0) {
-                throw new RuntimeException("Quantity cannot be negative: " + quantity);
-            }
-
-            transactions.add(new FruitTransaction(operation, fruit, quantity));
-        }
-        return transactions;
+        return parser.parse(inputReport);
     }
 }
--- src/main/java/core/basesyntax/service/impl/Parser.java
+++ src/main/java/core/basesyntax/service/impl/Parser.java
@@ -1,21 +1,55 @@
 package core.basesyntax.service.impl;
 
+import core.basesyntax.model.FruitTransaction;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
     private static final String HEADER = "type,fruit,quantity";
     private static final String DELIMITER = ",";
+    private static final int INDEX_OPERATION = 0;
+    private static final int INDEX_FRUIT = 1;
+    private static final int INDEX_QUANTITY = 2;
 
-    public List<String[]> parse(List<String> inputReport) {
-        List<String[]> parsedData = new ArrayList<>();
+    public List<FruitTransaction> parse(List<String> inputReport) {
+        List<FruitTransaction> transactions = new ArrayList<>();
         for (String line : inputReport) {
             if (line.equals(HEADER)) {
                 continue;
             }
             String[] parts = line.split(DELIMITER);
-            parsedData.add(parts);
+            FruitTransaction.Operation operation = getOperation(parts);
+            String fruit = getFruit(parts);
+            int quantity = getQuantity(parts);
+
+            transactions.add(new FruitTransaction(operation, fruit, quantity));
         }
-        return parsedData;
+        return transactions;
+    }
+
+    private FruitTransaction.Operation getOperation(String[] parts) {
+        FruitTransaction.Operation operation = FruitTransaction.Operation.fromCode(parts[INDEX_OPERATION]);
+        if (operation == null) {
+            throw new RuntimeException("Invalid operation: " + parts[INDEX_OPERATION]);
+        }
+        return operation;
+    }
+
+    private String getFruit(String[] parts) {
+        return parts[INDEX_FRUIT];
+    }
+
+    private int getQuantity(String[] parts) {
+        int quantity;
+        try {
+            quantity = Integer.parseInt(parts[INDEX_QUANTITY]);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Invalid quantity: " + parts[INDEX_QUANTITY], e);
+        }
+
+        if (quantity < 0) {
+            throw new RuntimeException("Quantity cannot be negative: " + quantity);
+        }
+        return quantity;
     }
 }
