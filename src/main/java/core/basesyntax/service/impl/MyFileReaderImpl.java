892fb83e882c3e7f8a781853414a22a873df6fe0
Bohdan <kbs20043131@gmail.com>
2024-08-06 23:53:07 +0300

    very hard task. Some problems with resourses

--- src/main/java/core/basesyntax/service/impl/MyFileReaderImpl.java
+++ src/main/java/core/basesyntax/service/impl/MyFileReaderImpl.java
@@ -3,6 +3,7 @@ package core.basesyntax.service.impl;
 import core.basesyntax.service.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +12,11 @@ public class MyFileReaderImpl implements FileReader {
     @Override
     public List<String> read(String filePath) {
         List<String> lines = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(getClass().getClassLoader().getResourceAsStream(filePath)))) {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath);
+        if (inputStream == null) {
+            throw new RuntimeException("File not found: " + filePath);
+        }
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = br.readLine()) != null) {
                 lines.add(line);
