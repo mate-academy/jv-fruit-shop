564e75358ebae56eb3b01dddc1f764bfdcec7c73
smagles <lesialist@icloud.com>
2024-08-08 23:04:00 +0200

    implement method

--- src/main/java/core/basesyntax/service/ShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/ShopServiceImpl.java
@@ -1,9 +1,16 @@
 package core.basesyntax.service;
 
+import core.basesyntax.FruitTransaction;
 import core.basesyntax.OperationStrategy;
-import core.basesyntax.service.ShopService;
+
+import java.util.List;
 
 public class ShopServiceImpl implements ShopService {
     public ShopServiceImpl(OperationStrategy operationStrategy) {
+    }
+
+    @Override
+    public void process(List<FruitTransaction> transactions) {
+
     }
 }
