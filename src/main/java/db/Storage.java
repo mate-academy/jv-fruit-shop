15b78c9b0efb946b2985167ecca6e68abeaa10b5
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-21 18:02:53 +0300

    Renamed map from FruitTransaction to fruitStock

--- src/main/java/db/Storage.java
+++ src/main/java/db/Storage.java
@@ -4,5 +4,5 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    public static final Map<String, Integer> fruitBalance = new HashMap<>();
+    public static final Map<String, Integer> fruitStock = new HashMap<>();
 }
