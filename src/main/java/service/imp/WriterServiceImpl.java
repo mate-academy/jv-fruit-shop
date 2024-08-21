c5b8a3877a49d75175f8db8a1457c2a6c46da7ed
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-21 20:50:18 +0300

    The WriteServiceIpml is a class that implements this interface and contains the specific logic for writing data to file.

---
+++ src/main/java/service/imp/WriterServiceImpl.java
@@ -0,0 +1,17 @@
+package service.imp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import service.WriterService;
+
+public class WriterServiceImpl implements WriterService {
+    @Override
+    public void write(String resultingReport, String fileName) {
+        try {
+            Files.write(Path.of(fileName), resultingReport.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + fileName);
+        }
+    }
+}
