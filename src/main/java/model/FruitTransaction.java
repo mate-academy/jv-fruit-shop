6a481149b00962f8182169e237740c18c35e6bd1
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-21 19:25:17 +0300

    The FruitTransaction model was created to simplify work with data

---
+++ src/main/java/model/FruitTransaction.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.util.Objects;
+
+public class FruitTransaction {
+    private Operation operation;
+    private String fruit;
+    private int quantity;
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FruitTransaction that = (FruitTransaction) o;
+        return getOperation() == that.getOperation() && Objects.equals(getFruit(), that.getFruit());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOperation(), getFruit());
+    }
+
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private String code;
+
+        Operation(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+}
