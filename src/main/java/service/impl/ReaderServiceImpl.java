154ed75e70d6433d1bcf0b565cc92ec2ded969f8
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-22 12:58:54 +0300

    Moved list dataFromFile to local variable. Added default error parameter in to the error message.

--- src/main/java/service/impl/ReaderServiceImpl.java
+++ src/main/java/service/impl/ReaderServiceImpl.java
@@ -1,21 +1,20 @@
-package service.imp;
+package service.impl;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import service.ReaderService;
 
 public class ReaderServiceImpl implements ReaderService {
-    private List<String> dataFromFile = new ArrayList<>();
 
     @Override
     public List<String> read(String fileName) {
+        List<String> dataFromFile;
         try {
             dataFromFile = Files.readAllLines(Path.of(fileName));
         } catch (IOException e) {
-            throw new RuntimeException("Can't find data with this path:  " + fileName);
+            throw new RuntimeException("Can't find data with this path:  " + fileName, e);
         }
         return dataFromFile;
     }
