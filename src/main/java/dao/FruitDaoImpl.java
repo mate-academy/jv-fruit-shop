6395d93c45bb7f4c8ffe299f7cabe6a174615845
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-22 12:56:19 +0300

    Changed names of methods according to checkstyle

--- src/main/java/dao/FruitDaoImpl.java
+++ src/main/java/dao/FruitDaoImpl.java
@@ -7,24 +7,24 @@ import java.util.Set;
 public class FruitDaoImpl implements FruitDao {
 
     @Override
-    public Integer getFruitBalance(String fruit) {
+    public Integer getBalance(String fruit) {
         return Storage.fruitStock.get(fruit);
     }
 
     @Override
-    public boolean addBalanceOfFruit(String fruit, int quantity) {
+    public boolean addBalance(String fruit, int quantity) {
         Storage.fruitStock.put(fruit,quantity);
         return Storage.fruitStock.containsKey(fruit)
                 && Storage.fruitStock.get(fruit) == quantity;
     }
 
     @Override
-    public void updateBalanceOfFruit(String fruit, int quantity) {
+    public void updateBalance(String fruit, int quantity) {
         Storage.fruitStock.put(fruit, quantity);
     }
 
     @Override
-    public Set<Map.Entry<String, Integer>> getAllFruit() {
+    public Set<Map.Entry<String, Integer>> getAllEntries() {
         return Storage.fruitStock.entrySet();
     }
 }
