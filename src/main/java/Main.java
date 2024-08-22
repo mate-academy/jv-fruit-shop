aeb68246de7af0916d5c2ce85a89c3e2dc10cafe
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-22 13:04:37 +0300

    Added some corrections to constructor signature.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import dao.FruitDaoImpl;
 import model.FruitTransaction;
 import service.DataConverter;
 import service.ReaderService;
 import service.ReportGenerator;
 import service.ShopService;
 import service.WriterService;
-import service.imp.DataConverterImpl;
-import service.imp.ReaderServiceImpl;
-import service.imp.ReportGeneratorImpl;
-import service.imp.ShopServiceImpl;
-import service.imp.WriterServiceImpl;
+import service.impl.DataConverterImpl;
+import service.impl.DataValidatorServiceImpl;
+import service.impl.ReaderServiceImpl;
+import service.impl.ReportGeneratorImpl;
+import service.impl.ShopServiceImpl;
+import service.impl.WriterServiceImpl;
 import service.operation.BalanceOperation;
 import service.operation.OperationHandler;
 import service.operation.PurchaseOperation;
@@ -23,26 +26,29 @@ import strategy.OperationStrategyImpl;
 public class Main {
     private static final String FROM_FILE = "src/main/resources/reportToRead.csv";
     private static final String TO_FILE = "src/main/resources/finalReport.csv";
-    private static List<FruitTransaction> transactions;
 
     public static void main(String[] arg) {
         ReaderService fileReader = new ReaderServiceImpl();
         List<String> inputReport = fileReader.read(FROM_FILE);
 
-        DataConverter dataConverter = new DataConverterImpl();
-        transactions = dataConverter.convertToTransaction(inputReport);
-
         Map<FruitTransaction.Operation, OperationHandler> operationHandlers = new HashMap<>();
-        operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
-        operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
-        operationHandlers.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
-        operationHandlers.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation());
+        operationHandlers.put(FruitTransaction.Operation.BALANCE,
+                new BalanceOperation(new FruitDaoImpl()));
+        operationHandlers.put(FruitTransaction.Operation.PURCHASE,
+                new PurchaseOperation(new FruitDaoImpl()));
+        operationHandlers.put(FruitTransaction.Operation.RETURN,
+                new ReturnOperation(new FruitDaoImpl()));
+        operationHandlers.put(FruitTransaction.Operation.SUPPLY,
+                new SupplyOperation(new FruitDaoImpl()));
         OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlers);
+
+        DataConverter dataConverter = new DataConverterImpl(new DataValidatorServiceImpl());
+        List<FruitTransaction> transactions = dataConverter.convertToTransaction(inputReport);
 
         ShopService shopService = new ShopServiceImpl(operationStrategy);
         shopService.process(transactions);
 
-        ReportGenerator reportGenerator = new ReportGeneratorImpl();
+        ReportGenerator reportGenerator = new ReportGeneratorImpl(new FruitDaoImpl());
         String resultingReport = reportGenerator.getReport();
 
         WriterService writerService = new WriterServiceImpl();
