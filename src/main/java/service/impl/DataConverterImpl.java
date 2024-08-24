3335a95eeea9e6df06363a2198acf57051e8e129
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-24 17:42:14 +0300

    Removed unnecessary checkReportLength method. Moved the logic of finding the correct operation in enum.

--- src/main/java/service/impl/DataConverterImpl.java
+++ src/main/java/service/impl/DataConverterImpl.java
@@ -1,9 +1,7 @@
 package service.impl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import model.FruitTransaction;
 import service.DataConverter;
 import service.ValidatorService;
@@ -24,26 +22,19 @@ public class DataConverterImpl implements DataConverter {
     @Override
     public List<FruitTransaction> convertToTransaction(List<String> inputReport) {
         List<FruitTransaction> fruitTransactions = new ArrayList<>();
-        checkReportLength(inputReport);
+        if (inputReport.size() <= EMPTY_REPORT_LENGTH) {
+            throw new RuntimeException("Input report is empty");
+        }
         for (int i = REPORT_START_INDEX; i < inputReport.size(); i++) {
             FruitTransaction fruitTransaction = new FruitTransaction();
             String[] line = inputReport.get(i).split(DATA_SEPARATOR);
             validator.validate(line);
-            fruitTransaction.setOperation(Arrays.stream(FruitTransaction.Operation.values())
-                    .filter(o -> o.getCode().equals(line[OPERATION_INDEX]))
-                    .findAny()
-                    .orElseThrow(() -> new NoSuchElementException("Invalid operation name: "
-                            + line[OPERATION_INDEX] + ". Must be one of these 'b,p,r,s'")));
+            fruitTransaction.setOperation(FruitTransaction
+                    .Operation.parseOperation(line[OPERATION_INDEX]));
             fruitTransaction.setFruit(line[FRUIT_INDEX]);
             fruitTransaction.setQuantity(Integer.parseInt(line[QUANTITY_INDEX]));
             fruitTransactions.add(fruitTransaction);
         }
         return fruitTransactions;
-    }
-
-    private void checkReportLength(List<String> data) {
-        if (data.size() <= EMPTY_REPORT_LENGTH) {
-            throw new RuntimeException("This reportFile is empty");
-        }
     }
 }
