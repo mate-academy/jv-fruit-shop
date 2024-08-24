d21734da4b7da48612fd1118354155173938e8f6
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-24 17:56:29 +0300

    I used the invert if condition to throw early - "guard clause pattern". Also changed the exception that will be thrown.

--- src/main/java/service/operation/PurchaseOperation.java
+++ src/main/java/service/operation/PurchaseOperation.java
@@ -1,6 +1,7 @@
 package service.operation;
 
 import dao.FruitDao;
+import exception.ValidationException;
 
 public class PurchaseOperation implements OperationHandler {
     private final FruitDao fruitDao;
@@ -12,12 +13,10 @@ public class PurchaseOperation implements OperationHandler {
     @Override
     public void handle(String fruit, int quantity) {
         int balanceAfterPurchase = fruitDao.getBalance(fruit) - quantity;
-        if (balanceAfterPurchase >= 0) {
-            fruitDao.updateBalance(fruit,balanceAfterPurchase);
-        } else {
-            throw new RuntimeException(String.format("You wanted to buy %d %ss "
-                    + "but available only %d %ss",quantity, fruit,fruitDao.getBalance(fruit),fruit
-            ));
+        if (balanceAfterPurchase < 0) {
+            throw new ValidationException(String.format("You wanted to buy %d %ss but available "
+                    + "only %d %ss", quantity, fruit, fruitDao.getBalance(fruit), fruit));
         }
+        fruitDao.updateBalance(fruit, balanceAfterPurchase);
     }
 }
