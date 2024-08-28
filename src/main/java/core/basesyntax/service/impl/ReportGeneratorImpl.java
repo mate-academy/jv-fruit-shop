fc7c12de95c4e188f4c09714163015322365ba08
Nikolai <colya.reznick@gmail.com>
2024-08-28 13:28:29 +0300

    Replaced end-of-line character with lineeparator

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -7,7 +7,7 @@ public class ReportGeneratorImpl<K, V> implements ReportGenerator {
     private static final String FIRST_COLUMN = "fruit";
     private static final String SECOND_COLUMN = "quantity";
     private static final String DELIMITER = ",";
-    private static final String NEW_LINE = "\n";
+    private static final String NEW_LINE = System.lineSeparator();
     private final Map<K, V> map;
 
     public ReportGeneratorImpl(Map<K, V> map) {
@@ -17,7 +17,7 @@ public class ReportGeneratorImpl<K, V> implements ReportGenerator {
     @Override
     public String getReport() {
         StringBuilder report = new StringBuilder();
-        report.append(FIRST_COLUMN + DELIMITER + SECOND_COLUMN + NEW_LINE);
+        report.append(FIRST_COLUMN + DELIMITER + SECOND_COLUMN).append(NEW_LINE);
 
         for (Map.Entry<K, V> entry : map.entrySet()) {
             report.append(entry.getKey())
