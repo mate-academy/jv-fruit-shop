fdbf34d93bbe4552ef04b04c95d321be0d6fdf3c
IvanIKS <kryanus2005@gmail.com>
2024-09-18 23:07:52 +0300

    Fixed style errors.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,22 +2,35 @@ package core.basesyntax;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.*;
-import core.basesyntax.service.impl.*;
-import core.basesyntax.strategy.*;
-import core.basesyntax.strategy.impl.*;
+import core.basesyntax.service.FileReader;
+import core.basesyntax.service.FileWriter;
+import core.basesyntax.service.ReportGenerator;
+import core.basesyntax.service.ShopService;
+import core.basesyntax.service.TransactionParser;
+import core.basesyntax.service.impl.FileReaderImpl;
+import core.basesyntax.service.impl.FileWriterImpl;
+import core.basesyntax.service.impl.ReportGeneratorImpl;
+import core.basesyntax.service.impl.ShopServiceImpl;
+import core.basesyntax.service.impl.TransactionParserImpl;
+import core.basesyntax.strategy.OperationHandler;
+import core.basesyntax.strategy.OperationStrategy;
+import core.basesyntax.strategy.impl.BalanceOperation;
+import core.basesyntax.strategy.impl.PurchaseOperation;
+import core.basesyntax.strategy.impl.OperationStrategyImpl;
+import core.basesyntax.strategy.impl.ReturnOperation;
+import core.basesyntax.strategy.impl.SupplyOperation;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        String readFilePath = "reportToRead.csv";
-        String writeFilePath = "writtenReport.csv";
+    private static final String READ_PATH = "reportToRead.csv";
+    private static final String WRITE_PATH = "writtenReport.csv";
 
+    public static void main(String[] args) {
         FileReader fileReader = new FileReaderImpl();
-        List<String> inputReport = fileReader.read(readFilePath);
+        List<String> inputReport = fileReader.read(READ_PATH);
 
         TransactionParser transactionParser = new TransactionParserImpl();
         List<FruitTransaction> transactions = transactionParser.parse(inputReport);
@@ -36,6 +49,6 @@ public class Main {
         String resultingReport = reportGenerator.getReport(storage);
 
         FileWriter fileWriter = new FileWriterImpl();
-        fileWriter.write(resultingReport, writeFilePath);
+        fileWriter.write(resultingReport, WRITE_PATH);
     }
 }
--- src/main/java/core/basesyntax/strategy/impl/PurchaseOperation.java
+++ src/main/java/core/basesyntax/strategy/impl/PurchaseOperation.java
@@ -7,14 +7,15 @@ import core.basesyntax.strategy.OperationHandler;
 public class PurchaseOperation implements OperationHandler {
     @Override
     public void handle(Storage storage, FruitTransaction transaction) throws RuntimeException {
-            String fruitName = transaction.getFruit();
-            int oldQuantity = storage.get(fruitName);
-            int newQuantity = oldQuantity - transaction.getQuantity();
-            if (newQuantity < 0) {
-                throw new RuntimeException(String.format(
-                        "Error purchasing fruits. Available quantity: %d for %s, " +
-                                "but trying to purchase %d.", oldQuantity, fruitName, transaction.getQuantity()));
-            }
-            storage.change(fruitName, newQuantity);
+        String fruitName = transaction.getFruit();
+        int oldQuantity = storage.get(fruitName);
+        int newQuantity = oldQuantity - transaction.getQuantity();
+        if (newQuantity < 0) {
+            throw new RuntimeException(String.format(
+                    "Error purchasing fruits. Available quantity: %d for %s, "
+                            + "but trying to purchase %d.",
+                    oldQuantity, fruitName, transaction.getQuantity()));
+        }
+        storage.change(fruitName, newQuantity);
     }
 }
