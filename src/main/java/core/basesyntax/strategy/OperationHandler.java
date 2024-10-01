45ba347ad06bae31fd4afbe45ad29172d3cbb240
Xoteki <xotekijke@gmail.com>
2024-10-01 22:20:57 +0300

    Revert "first solution added"
    This reverts commit 745fe10d370e93b2b19f73839e4703b6f571e1b8.

--- src/main/java/core/basesyntax/strategy/OperationHandler.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.strategy;
-
-import core.basesyntax.dao.FruitStorageDao;
-import core.basesyntax.model.FruitTransaction;
-
-public interface OperationHandler {
-    void updateFruitStorage(FruitTransaction fruitTransaction, FruitStorageDao fruitStorageDao);
-}
