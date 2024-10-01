4d2f2d6f9125387b9daee2b07742a21fb70ef6c3
Xoteki <xotekijke@gmail.com>
2024-10-01 21:57:21 +0300

    first solution added
    (cherry picked from commit 745fe10d370e93b2b19f73839e4703b6f571e1b8)

---
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -0,0 +1,72 @@
+package core.basesyntax.model;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FruitTransaction {
+    private Operation operation;
+    private String fruit;
+    private int quantity;
+
+    public FruitTransaction(Operation operation, String fruit, int quantity) {
+        this.operation = operation;
+        this.fruit = fruit;
+        this.quantity = quantity;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private static final Map<String, Operation> stringToEnum = Stream.of(values())
+                .collect(Collectors.toMap(Objects::toString, e -> e));
+
+        private String code;
+
+        Operation(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        @Override
+        public String toString() {
+            return code;
+        }
+
+        public static Optional<Operation> fromString(String code) {
+            return Optional.ofNullable(stringToEnum.get(code));
+        }
+    }
+}
