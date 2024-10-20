eba5175fe66e714da8a87b6acc9f24652814b3ae
skviropitek <skvyra.anton@gmail.com>
2024-10-20 17:22:26 +0200

    added solution

--- src/main/java/core/basesyntax/strategy/impl/OperationHandlerFactoryImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/OperationHandlerFactoryImpl.java
@@ -5,11 +5,12 @@ import core.basesyntax.strategy.OperationHandlerFactory;
 import core.basesyntax.strategy.OperationStrategy;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class OperationHandlerFactoryImpl implements OperationHandlerFactory {
 
     public OperationStrategy createOperationStrategy() {
-        Map<FruitTransaction.getOperation, OperationHandler> operationHandlers = new HashMap<>();
+        Map<FruitTransaction.getOperation, OperationHandler> operationHandlers = new Map<>();
         operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler());
         operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler());
         operationHandlers.put(FruitTransaction.Operation.RETURN, new ReturnOperationHandler());
