db26c76cc4a8b932575d01b107c1dbf3a31784d4
Borovski Cergei <borovskicerg0001@gmaol.com>
2024-11-01 15:41:30 +0200

    added try/catch block to method handle()

--- src/main/java/core/basesyntax/strategy/SupplyOperation.java
+++ src/main/java/core/basesyntax/strategy/SupplyOperation.java
@@ -2,16 +2,21 @@ package core.basesyntax.strategy;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.ShopTransaction;
+import exception.OperationException;
 
 public class SupplyOperation implements OperationHandler {
     @Override
     public void handle(ShopTransaction shopTransaction) {
-        String fruit = shopTransaction.getFruitName();
-        int weight = Storage.fruitShopStorage.get(fruit);
-        if (weight > 0) {
-            Storage.fruitShopStorage.put(fruit, weight + shopTransaction.getWeight());
-        } else {
-            Storage.fruitShopStorage.put(fruit, shopTransaction.getWeight());
+        try {
+            String fruit = shopTransaction.getFruitName();
+            int weight = Storage.fruitShopStorage.get(fruit);
+            if (weight > 0) {
+                Storage.fruitShopStorage.put(fruit, weight + shopTransaction.getWeight());
+            } else {
+                Storage.fruitShopStorage.put(fruit, shopTransaction.getWeight());
+            }
+        } catch (NullPointerException e) {
+            throw new OperationException("Transaction is not correct " + shopTransaction);
         }
     }
 }
