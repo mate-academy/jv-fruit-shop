f24bc62dd484aadc720a8691057241150ecbb75e
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-11-07 13:25:20 +0100

    first try

--- src/main/java/core/basesyntax/dao/fileReaderImpl.java
+++ src/main/java/core/basesyntax/dao/fileReaderImpl.java
@@ -1,9 +1,6 @@
 package core.basesyntax.dao;
 
-import core.basesyntax.model.Account;
-
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,7 +14,7 @@ public class fileReaderImpl implements fileReader {
        List<String> textInFile = null;
         try(BufferedReader reader = new BufferedReader(new FileReader(nameOfFile))) {
             textInFile = Files.readAllLines(Path.of(nameOfFile));
-            return textInFile.stream()
+             return textInFile.stream()
                     .filter(i -> i.startsWith("b") || i.startsWith("s")
                     || i.startsWith("p") || i.startsWith("r"))
                     .toArray(String[]::new);
