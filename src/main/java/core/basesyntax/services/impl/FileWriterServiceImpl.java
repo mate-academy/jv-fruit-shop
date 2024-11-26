fa31d1d4e4c557537bb96ac9c11f9a0e229c7367
Mihail <mishadyakivnich@gmail.com>
2024-11-26 18:44:36 +0200

    resolve2

--- src/main/java/core/basesyntax/services/impl/FileWriterServiceImpl.java
+++ src/main/java/core/basesyntax/services/impl/FileWriterServiceImpl.java
@@ -1,14 +1,13 @@
 package core.basesyntax.services.impl;
 
 import core.basesyntax.services.FileWriterService;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileWriterServiceImpl implements FileWriterService {
     @Override
-    public void writeToFile(String fileName, String report) {
+    public void writeToFile(String filePath, String report) {
         try {
             Files.writeString(Paths.get(filePath), report);
         } catch (IOException e) {
