a30bbcac94bfd86e9201f608fc20a305a1feeda4
skrypkasv <skrypkasv@gmail.com>
2024-12-17 01:13:14 +0200

    create FruitTransaction

---
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -0,0 +1,65 @@
+package core.basesyntax.model;
+
+public class FruitTransaction {
+    private Operation operation;
+    private String fruit;
+    private int quantity;
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "operation=" + operation
+               + ", fruit='" + fruit + '\''
+               + ", quantity=" + quantity;
+    }
+
+    public enum Operation {
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private String code;
+
+        Operation(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+
+    public FruitTransaction.Operation getOperationByCode(String code) {
+        return switch (code) {
+            case "b" -> Operation.BALANCE;
+            case "s" -> Operation.SUPPLY;
+            case "p" -> Operation.PURCHASE;
+            case "r" -> Operation.RETURN;
+            default -> throw new RuntimeException("Unexpected value: " + code);
+        };
+    }
+}
