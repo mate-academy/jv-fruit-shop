c8c2f6bdbe3abbde187f4a3e786390859dc817d3
skrypkasv <skrypkasv@gmail.com>
2024-12-17 01:17:41 +0200

    create Main

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,55 @@
+package core.basesyntax;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.DataConverter;
+import core.basesyntax.service.DataConverterImpl;
+import core.basesyntax.service.FileReader;
+import core.basesyntax.service.FileReaderImpl;
+import core.basesyntax.service.FileWriter;
+import core.basesyntax.service.FileWriterImpl;
+import core.basesyntax.service.ReportGenerator;
+import core.basesyntax.service.ReportGeneratorImpl;
+import core.basesyntax.service.ShopService;
+import core.basesyntax.service.ShopServiceImpl;
+import core.basesyntax.strategy.OperationHandlerStrategy;
+import core.basesyntax.strategy.OperationHandlerStrategyImpl;
+import core.basesyntax.strategy.handlers.BalanceOperation;
+import core.basesyntax.strategy.handlers.OperationHandler;
+import core.basesyntax.strategy.handlers.PurchaseOperation;
+import core.basesyntax.strategy.handlers.ReturnOperation;
+import core.basesyntax.strategy.handlers.SupplyOperation;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Path inputFile = Paths.get("src/main/resources/input.csv");
+        FileReader reader = new FileReaderImpl();
+        List<String> inputRecords = reader.read(inputFile);
+
+        Map<FruitTransaction.Operation, OperationHandler> operationHandlers = new HashMap<>();
+        operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
+        operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
+        operationHandlers.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
+        operationHandlers.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation());
+
+        DataConverter converter = new DataConverterImpl();
+        List<FruitTransaction> fruitTransactions = converter.convertToTransaction(inputRecords);
+
+        OperationHandlerStrategy operationHandlerStrategy =
+                new OperationHandlerStrategyImpl(operationHandlers);
+
+        ShopService shopService = new ShopServiceImpl(operationHandlerStrategy);
+        shopService.process(fruitTransactions);
+
+        ReportGenerator generator = new ReportGeneratorImpl();
+        String report = generator.getReport();
+
+        Path reportFile = Paths.get("src/main/resources/output.csv");
+        FileWriter fileWriter = new FileWriterImpl();
+        fileWriter.write(report, reportFile);
+    }
+}
