bd38bd5b83549652aa857145fcc6087aa19065fa
skrypkasv <skrypkasv@gmail.com>
2024-12-17 01:16:02 +0200

    create ReturnOperation

---
+++ src/main/java/core/basesyntax/strategy/handlers/ReturnOperation.java
@@ -0,0 +1,30 @@
+package core.basesyntax.strategy.handlers;
+
+import core.basesyntax.db.FruitStorage;
+import core.basesyntax.model.FruitTransaction;
+
+public class ReturnOperation implements OperationHandler {
+    @Override
+    public Boolean executeOperation(FruitTransaction transaction) {
+        String fruit = transaction.getFruit();
+        int quantity = transaction.getQuantity();
+
+        if (isValidTransactionData(fruit, quantity)) {
+            try {
+                if (FruitStorage.fruits.containsKey(fruit)) {
+                    quantity += FruitStorage.fruits.get(fruit);
+                    FruitStorage.fruits.put(fruit, quantity);
+                } else {
+                    FruitStorage.fruits.put(fruit, quantity);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Can't add data: " + fruit
+                                           + "=" + quantity + " to fruitStorage", e);
+            }
+        } else {
+            throw new RuntimeException("Not valid transactionData: " + transaction);
+        }
+
+        return FruitStorage.fruits.get(fruit) == quantity;
+    }
+}
