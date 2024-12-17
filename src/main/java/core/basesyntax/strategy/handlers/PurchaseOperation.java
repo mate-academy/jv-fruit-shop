c7dd8645f6be6e5363b1f59ddd3d4ffd4e68e32a
skrypkasv <skrypkasv@gmail.com>
2024-12-17 23:48:14 +0200

    fix PurchaseOperation

--- src/main/java/core/basesyntax/strategy/handlers/PurchaseOperation.java
+++ src/main/java/core/basesyntax/strategy/handlers/PurchaseOperation.java
@@ -5,32 +5,27 @@ import core.basesyntax.model.FruitTransaction;
 
 public class PurchaseOperation implements OperationHandler {
     @Override
-    public Boolean executeOperation(FruitTransaction transaction) {
+    public void executeOperation(FruitTransaction transaction) {
         String fruit = transaction.getFruit();
         int quantity = transaction.getQuantity();
+        checkIfTransactionDataIsValid(fruit, quantity);
 
-        if (isValidTransactionData(fruit, quantity)) {
-            try {
-                if (FruitStorage.fruits.containsKey(fruit)) {
-                    if (FruitStorage.fruits.get(fruit) >= quantity) {
-                        quantity = FruitStorage.fruits.get(fruit) - quantity;
-                        FruitStorage.fruits.put(fruit, quantity);
-                    } else {
-                        throw new RuntimeException(fruit + " is not enough"
-                                                   + " in fruitStorage for PURCHASE.");
-                    }
-                } else {
-                    throw new RuntimeException("Such fruit: " + fruit
-                                               + " is absent at fruitStorage!!!");
-                }
-            } catch (Exception e) {
-                throw new RuntimeException("Can't add data: " + fruit + "="
-                                           + quantity + " to fruitStorage", e);
-            }
-        } else {
-            throw new RuntimeException("Not valid transactionData: " + transaction);
+        if (!FruitStorage.fruits.containsKey(fruit)) {
+            throw new RuntimeException("Such fruit: " + fruit
+                    + " is absent at fruitStorage!!!");
+        }
+        if (FruitStorage.fruits.get(fruit) < quantity) {
+            throw new RuntimeException("Fruit: " + fruit + "=" + quantity
+                    + " it is more than at fruitStorage!!! ");
         }
 
-        return FruitStorage.fruits.get(fruit) == quantity;
+        int newQuantity = FruitStorage.fruits.get(fruit) - quantity;
+
+        try {
+            FruitStorage.fruits.put(fruit, newQuantity);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't add data: "
+                    + fruit + "=" + quantity + " to fruitStorage", e);
+        }
     }
 }
