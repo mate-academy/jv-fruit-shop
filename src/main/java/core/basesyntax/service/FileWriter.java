b06bafc54009751aa8ed51b152255bde2e055fa9
skrypkasv <skrypkasv@gmail.com>
2024-12-18 00:04:24 +0200

    fix Writer meth output

--- src/main/java/core/basesyntax/service/FileWriter.java
+++ src/main/java/core/basesyntax/service/FileWriter.java
@@ -3,5 +3,5 @@ package core.basesyntax.service;
 import java.nio.file.Path;
 
 public interface FileWriter {
-    Boolean write(String report, Path filePath);
+    void write(String report, Path filePath);
 }
--- src/main/java/core/basesyntax/service/FileWriterImpl.java
+++ src/main/java/core/basesyntax/service/FileWriterImpl.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 
 public class FileWriterImpl implements FileWriter {
     @Override
-    public Boolean write(String report, Path filePath) {
+    public void write(String report, Path filePath) {
         try {
             if (Files.deleteIfExists(filePath)) {
                 Files.createFile(filePath);
@@ -15,6 +15,5 @@ public class FileWriterImpl implements FileWriter {
         } catch (IOException e) {
             throw new RuntimeException(String.format("Can't write in file: '%s'.", filePath), e);
         }
-        return filePath.toFile().canRead();
     }
 }
