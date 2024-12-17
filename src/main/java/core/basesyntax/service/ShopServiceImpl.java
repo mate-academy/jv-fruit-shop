ec401715e4cb556291b6ef9150194a15f942a076
skrypkasv <skrypkasv@gmail.com>
2024-12-18 00:02:48 +0200

    fix ShopServiceImpl

--- src/main/java/core/basesyntax/service/ShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/ShopServiceImpl.java
@@ -5,22 +5,17 @@ import core.basesyntax.strategy.OperationHandlerStrategy;
 import java.util.List;
 
 public class ShopServiceImpl implements ShopService {
-    private OperationHandlerStrategy operationHandlerStrategy;
+    private final OperationHandlerStrategy operationHandlerStrategy;
 
     public ShopServiceImpl(OperationHandlerStrategy operationHandlerStrategy) {
         this.operationHandlerStrategy = operationHandlerStrategy;
     }
 
-    public OperationHandlerStrategy getOperationHandlerStrategy() {
-        return operationHandlerStrategy;
-    }
-
     @Override
-    public Boolean process(List<FruitTransaction> transactions) {
+    public void process(List<FruitTransaction> transactions) {
         for (FruitTransaction transaction : transactions) {
             operationHandlerStrategy.getOperationHandler(transaction)
                     .executeOperation(transaction);
         }
-        return true;
     }
 }
