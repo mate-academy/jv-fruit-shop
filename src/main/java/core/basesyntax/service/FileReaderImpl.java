54d8973bc7758c4b7b5a4da46da71e87f2c8b075
skrypkasv <skrypkasv@gmail.com>
2024-12-17 23:48:46 +0200

    fix FileReaderImpl

--- src/main/java/core/basesyntax/service/FileReaderImpl.java
+++ src/main/java/core/basesyntax/service/FileReaderImpl.java
@@ -3,22 +3,20 @@ package core.basesyntax.service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class FileReaderImpl implements FileReader {
     @Override
     public List<String> read(Path filePath) {
-        List<String> records = new ArrayList<>();
+        List<String> records;
         if (filePath.toFile().exists()) {
             try {
                 records = Files.readAllLines(filePath);
+                return records;
             } catch (IOException e) {
-                throw new RuntimeException("Can't read file: " + filePath, e);
+                throw new RuntimeException(String.format("Can't read file: %s", filePath), e);
             }
-        } else {
-            throw new RuntimeException("File: " + "'" + filePath + "'" + " doesn't exist.");
         }
-        return records;
+        throw new RuntimeException(String.format("File: '%s' doesn't exist.", filePath));
     }
 }
