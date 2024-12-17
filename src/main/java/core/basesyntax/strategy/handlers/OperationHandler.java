824bda0faec5bccd0ed072aa17153bb719e70a77
skrypkasv <skrypkasv@gmail.com>
2024-12-17 23:46:29 +0200

    fix OperationHandler interface

--- src/main/java/core/basesyntax/strategy/handlers/OperationHandler.java
+++ src/main/java/core/basesyntax/strategy/handlers/OperationHandler.java
@@ -3,9 +3,11 @@ package core.basesyntax.strategy.handlers;
 import core.basesyntax.model.FruitTransaction;
 
 public interface OperationHandler {
-    default Boolean isValidTransactionData(String fruit, int quantity) {
-        return fruit != null && quantity > 0;
+    default void checkIfTransactionDataIsValid(String fruit, int quantity) {
+        if (fruit == null || quantity < 0) {
+            throw new RuntimeException("Invalid data transaction.");
+        }
     }
 
-    Boolean executeOperation(FruitTransaction transaction);
+    void executeOperation(FruitTransaction transaction);
 }
