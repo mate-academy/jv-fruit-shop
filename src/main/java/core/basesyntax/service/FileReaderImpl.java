de55cd81200c279b3f0ffd7800474686f22ced68
skrypkasv <skrypkasv@gmail.com>
2024-12-18 18:38:54 +0200

    remove redundant check

--- src/main/java/core/basesyntax/service/FileReaderImpl.java
+++ src/main/java/core/basesyntax/service/FileReaderImpl.java
@@ -9,14 +9,11 @@ public class FileReaderImpl implements FileReader {
     @Override
     public List<String> read(Path filePath) {
         List<String> records;
-        if (filePath.toFile().exists()) {
-            try {
-                records = Files.readAllLines(filePath);
-                return records;
-            } catch (IOException e) {
-                throw new RuntimeException(String.format("Can't read file: %s", filePath), e);
-            }
+        try {
+            records = Files.readAllLines(filePath);
+            return records;
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("Can't read file: %s", filePath), e);
         }
-        throw new RuntimeException(String.format("File: '%s' doesn't exist.", filePath));
     }
 }
