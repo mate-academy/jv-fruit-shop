ea49800102b4d67be326b23e5df2e83268bd074c
Yaroslav Narozhnyi <gnredit9@gmail.com>
2024-12-27 19:22:15 +0200

    llllaass

--- src/main/java/core/basesyntax/Fruit.java
+++ src/main/java/core/basesyntax/Fruit.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Fruit {
-    Map<String,Integer> fruitMap;
+    private Map<String,Integer> fruitMap;
 
     public Fruit() {
         this.fruitMap = new HashMap<>();
@@ -14,27 +14,30 @@ public class Fruit {
         return fruitMap.getOrDefault(fruitType, 0);
     }
 
-   public int balance(String fruitType, int amount) {
+    public int balance(String fruitType, int amount) {
         fruitMap.put(fruitType,amount);
         return amount;
-   }
-   public int supply(String fruitType, int amount) {
+    }
+
+    public int supply(String fruitType, int amount) {
        int newAmount = fruitMap.get(fruitType) + amount;
        fruitMap.put(fruitType,newAmount);
         return newAmount;
-   }
-   public int returnFruit(String fruitType, int amount) {
+    }
+
+    public int returnFruit(String fruitType, int amount) {
         int returnedAmount = fruitMap.getOrDefault(fruitType, 0) + amount;
         fruitMap.put(fruitType, returnedAmount);
         return returnedAmount;
-   }
-   public int purchase(String fruitType, int amount) {
+    }
+
+    public int purchase(String fruitType, int amount) {
         int purchaseAmount = fruitMap.getOrDefault(fruitType, 0);
         if (purchaseAmount < amount) {
             throw new IllegalStateException("Not enough fruits");
         }
-        int newAmount = fruitMap.get(fruitType)- amount;
+        int newAmount = fruitMap.get(fruitType) - amount;
         fruitMap.put(fruitType,newAmount);
         return newAmount;
-   }
+    }
 }
--- src/main/java/core/basesyntax/WriteDataImpl.java
+++ src/main/java/core/basesyntax/WriteDataImpl.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
-import java.io.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class WriteDataImpl implements WriteData {
