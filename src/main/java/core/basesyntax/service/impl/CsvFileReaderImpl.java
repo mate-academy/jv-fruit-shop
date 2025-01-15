9861e7e7681d73ee147cc00926a68845560d1c24
Bohdan_SSX <mr.dimov77@gmail.com>
2025-01-15 20:21:34 +0200

    List lines introduced as a local variable at CsvFileReaderImpl class

--- src/main/java/core/basesyntax/service/impl/CsvFileReaderImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileReaderImpl.java
@@ -8,10 +8,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CsvFileReaderImpl implements FileReader {
-    private final List<String> lines = new ArrayList<>();
 
     @Override
     public List<String> read(String filename) {
+        List<String> lines = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(filename))) {
             while (scanner.hasNextLine()) {
                 lines.add(scanner.nextLine());
