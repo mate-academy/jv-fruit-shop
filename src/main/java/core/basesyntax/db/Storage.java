a8c5878ddff969cc8b9a2034af939aaf42eaa433
Bohdan_SSX <mr.dimov77@gmail.com>
2025-01-18 10:52:57 +0200

    storage access modifier changed

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    public static final Map<String, Integer> inventory = new HashMap<>();
+    private static final Map<String, Integer> inventory = new HashMap<>();
 
     public Map<String, Integer> getInventory() {
         return inventory;
