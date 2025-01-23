92aae01e5a90864947fad080c58cbdf9ceb08ecd
dantesworld <dantesworld2@gmail.com>
2025-01-23 13:58:41 +0200

    refactored because of Maven error

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -28,19 +28,18 @@ public class Main {
     private static final String FINAL_REPORT = "src/main/resources/finalReport.csv";
 
     public static void main(String[] args) {
-
-        CustomFileReader fileReader = new CustomFileReaderImpl();
-        List<String> inputReport = fileReader.read(REPORT_TO_READ);
-
-        DataConverter dataConverter = new DataConverterImpl();
-        List<FruitTransaction> transactions = dataConverter.convertToTransaction(inputReport);
-
         Map<FruitTransaction.Operation, OperationHandler> operationHandlers = new HashMap<>();
         operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
         operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
         operationHandlers.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
         operationHandlers.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation());
         OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlers);
+
+        CustomFileReader fileReader = new CustomFileReaderImpl();
+        List<String> inputReport = fileReader.read(REPORT_TO_READ);
+
+        DataConverter dataConverter = new DataConverterImpl();
+        List<FruitTransaction> transactions = dataConverter.convertToTransaction(inputReport);
 
         ShopService shopService = new ShopServiceImpl(operationStrategy);
         shopService.process(transactions);
