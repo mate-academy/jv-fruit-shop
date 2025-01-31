d240893fd78b1dc885982543a8b6e2bb049aeca6
eulu <anegamega@gmail.com>
2025-01-31 12:11:36 +0200

    Use constants for string literals

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -6,13 +6,15 @@ import java.util.stream.Collectors;
 import static core.basesyntax.Main.COMMA_SEPARATOR;
 
 public class ReportGeneratorImpl implements ReportGenerator {
+    public static final String SCV_FILE_HEADER = "fruit,quantity" + System.lineSeparator();
+
     @Override
     public String generateReport(Map<String, Integer> inventory) {
-        StringBuilder reportBuilder = new StringBuilder("fruit,quantity\n");
+        StringBuilder reportBuilder = new StringBuilder(SCV_FILE_HEADER);
         String reportBody = inventory.entrySet()
                 .stream()
                 .map(entry -> entry.getKey() + COMMA_SEPARATOR + entry.getValue())
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining(System.lineSeparator()));
         reportBuilder.append(reportBody);
         return reportBuilder.toString();
     }
