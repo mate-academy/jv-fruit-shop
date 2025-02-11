f386dadae48c9b9c42a326c6cba4d2b150440fa1
Mykola Turak <mturak294@gmail.com>
2025-02-11 22:27:46 +0200

    Update src/main/java/core/basesyntax/service/OperationStrategy.java
    Co-authored-by: Safarov Renat <98067127+JJJazl@users.noreply.github.com>

--- src/main/java/core/basesyntax/service/OperationStrategy.java
+++ src/main/java/core/basesyntax/service/OperationStrategy.java
@@ -3,5 +3,5 @@ package core.basesyntax.service;
 import core.basesyntax.service.operations.OperationHandler;
 
 public interface OperationStrategy {
-    OperationHandler execute(FruitTransaction fruitTransaction);
+    OperationHandler getOperationHandler(FruitTransaction fruitTransaction);
 }
