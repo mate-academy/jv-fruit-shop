895fdfbb085bf1f03178309fe1e48489826e706c
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-02-24 19:33:39 +0200

    Edited errors, changed String operation to ENUM Operation + added needed csv files

---
+++ src/main/java/core/basesyntax/Operation.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public enum Operation {
+    BALANCE("b"),
+    SUPPLY("s"),
+    PURCHASE("p"),
+    RETURN("r");
+
+    private final String code;
+
+    Operation(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public static Operation fromCode(String code) {
+        for (Operation op : values()) {
+            if (op.getCode().equalsIgnoreCase(code)) { // Сравнение без учета регистра
+                return op;
+            }
+        }
+        throw new IllegalArgumentException("Unknown operation code: " + code);
+    }
+}
---
+++ src/main/java/core/basesyntax/ReportGeneratorImpl.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ReportGeneratorImpl implements ReportCreator {
+    public static final String FIRST_COLUMN_UPDATED_UPDATED = "fruit";
+    public static final String SECOND_COLUMN_UPDATED_UPDATED = "quantity";
+
+    @Override
+    public List<String[]> createReport(Map<String, Integer> data) {
+        List<String[]> fruitsList = new ArrayList<>();
+        fruitsList.add(new String[]{FIRST_COLUMN_UPDATED_UPDATED, SECOND_COLUMN_UPDATED_UPDATED});
+        for (Map.Entry<String, Integer> entry : data.entrySet()) {
+            fruitsList.add(new String[]{entry.getKey(), String.valueOf(entry.getValue())});
+        }
+        return fruitsList;
+    }
+}
