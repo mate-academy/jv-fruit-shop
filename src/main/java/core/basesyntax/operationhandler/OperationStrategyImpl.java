ff3b2040ccf190bf546d5c6504106e79f3b9065e
Alexandros <olek4004@gmail.com>
2025-03-30 18:04:25 +0200

    checkstyle fix

--- src/main/java/core/basesyntax/operationhandler/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/operationhandler/OperationStrategyImpl.java
@@ -8,23 +8,24 @@ public class OperationStrategyImpl implements OperationStrategy {
     private Map<FruitTransaction.Operation,
             OperationHandler> operationHandlerMap;
 
-    public Map<FruitTransaction.Operation,
-            OperationHandler> getOperationHandlerMap() {
-        return operationHandlerMap;
-    }
-
-    public void setOperationHandlerMap(Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap) {
-        this.operationHandlerMap = operationHandlerMap;
-    }
-
     public OperationStrategyImpl(Map<FruitTransaction.Operation,
             OperationHandler> operationHandlers) {
         this.operationHandlerMap = operationHandlers;
     }
 
+    public Map<FruitTransaction.Operation,
+            OperationHandler> getOperationHandlerMap() {
+        return operationHandlerMap;
+    }
+
+    public void setOperationHandlerMap(Map<FruitTransaction.Operation,
+            OperationHandler> operationHandlerMap) {
+        this.operationHandlerMap = operationHandlerMap;
+    }
+
     @Override
-    public OperationHandler getOperation
-            (FruitTransaction.Operation operation) {
+    public OperationHandler getOperation(
+            FruitTransaction.Operation operation) {
         return operationHandlerMap.get(operation);
     }
 }
