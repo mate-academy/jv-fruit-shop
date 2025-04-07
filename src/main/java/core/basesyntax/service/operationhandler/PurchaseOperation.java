61af30527c6a55e781110a8a1c26a81db9876b4d
Ivanov <ivanshevchenko181@gmail.com>
2025-04-07 14:17:49 +0300

    fixed the issue

--- src/main/java/core/basesyntax/service/operationhandler/PurchaseOperation.java
+++ src/main/java/core/basesyntax/service/operationhandler/PurchaseOperation.java
@@ -7,6 +7,9 @@ public class PurchaseOperation implements OperationHandler {
     @Override
     public void process(FruitTransaction fruitTransaction) {
         int oldQuantity = ShopDataBase.getValue(fruitTransaction.getFruit());
+        if (oldQuantity - fruitTransaction.getQuantity() < oldQuantity) {
+            throw new RuntimeException("Not enough fruits in the storage");
+        }
         ShopDataBase.put(fruitTransaction.getFruit(), oldQuantity - fruitTransaction.getQuantity());
     }
 }
