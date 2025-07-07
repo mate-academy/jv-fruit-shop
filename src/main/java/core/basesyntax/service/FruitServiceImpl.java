06ae6dfecd9c8814d248087532a7d2b28824ac50
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-07-08 00:31:56 +0300

    added class FruitServiceImpl

---
+++ src/main/java/core/basesyntax/service/FruitServiceImpl.java
@@ -0,0 +1,18 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.model.Fruit;
+
+public class FruitServiceImpl implements FruitService {
+    private FruitDao fruitDao;
+
+    public FruitServiceImpl(FruitDao fruitDao) {
+        this.fruitDao = fruitDao;
+    }
+
+    @Override
+    public void createNewFruit(String fruitName, int count) {
+        Fruit fruit = new Fruit(fruitName,count);
+        fruitDao.add(fruit);
+    }
+}
