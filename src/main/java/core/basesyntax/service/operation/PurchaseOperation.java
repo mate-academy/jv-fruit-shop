77ad9ba57bccc13b480a371ab95cec675ff99e8b
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-07-08 00:28:31 +0300

    added class PurchaseOperation

---
+++ src/main/java/core/basesyntax/service/operation/PurchaseOperation.java
@@ -0,0 +1,8 @@
+package core.basesyntax.service.operation;
+
+public class PurchaseOperation implements OperationHandler {
+    @Override
+    public int getOperation() {
+        return -1;
+    }
+}
