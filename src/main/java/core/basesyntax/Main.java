a7a64f9249977292011d1601dade60c132fdd041
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-07-08 20:58:22 +0300

    deleted comment from class main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -33,31 +33,26 @@ public class Main {
 
     public static void main(String[] arg) {
 
-        // 3. Create and feel the map with all OperationHandler implementations
         Map<FruitTransaction.Operation, OperationHandler> operationHandlers = new HashMap<>();
         operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
         operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
         operationHandlers.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
         operationHandlers.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation());
 
-        // 1. Read the data from the input CSV file
         FileReader fileReader = new FileReaderImpl();
         List<String> inputReport = fileReader.read(INPUT_FILE_NAME);
 
-        // 2. Convert the incoming data into FruitTransactions list
         DataConverter dataConverter = new DataConverterImpl();
         List<FruitTransaction> transactions = dataConverter.convertToTransaction(inputReport);
         OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlers);
         FruitDao fruitDao = new FruitDaoImpl();
-        // 4. Process the incoming transactions with applicable OperationHandler implementations
+
         ShopService shopService = new ShopServiceImpl(fruitDao, operationStrategy);
         shopService.process(transactions);
 
-        // 5.Generate report based on the current Storage state
-        ReportGenerator reportGenerator = new ReportGeneratorImpl();
+         ReportGenerator reportGenerator = new ReportGeneratorImpl();
         String resultingReport = reportGenerator.getReport();
 
-        // 6. Write the received report into the destination file
         FileWriter fileWriter = new FileWriterImpl();
         fileWriter.write(resultingReport, OUTPUT_FILE_NAME);
     }
